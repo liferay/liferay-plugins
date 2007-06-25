29722fad68299beabb5ef265dce6bc369fbfe5f9
Brian Chan <brian.chan@liferay.com>
2007-06-25 09:13:37 +0000

    More portlets
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@7251 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/sample-jsf-myfaces-portlet/docroot/WEB-INF/src/com/sample/jsfmyfaces/portlet/LogURLEncoder.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.sample.jsfmyfaces.portlet;
+
+import com.liferay.portal.kernel.servlet.URLEncoder;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * <a href="LogURLEncoder.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class LogURLEncoder implements URLEncoder {
+
+	public String encodeURL(HttpServletResponse res, String path) {
+		if (_log.isInfoEnabled()) {
+			_log.info("Path " + path);
+		}
+
+		String encodedURL = res.encodeURL(path);
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Encoded URL " + encodedURL);
+		}
+
+		return encodedURL;
+	}
+
+	private static Log _log = LogFactory.getLog(LogURLEncoder.class);
+
+}
