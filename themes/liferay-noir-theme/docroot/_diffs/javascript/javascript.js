1a05fa0bbd14b9f9d1b8d1f7d30c5f3d426ad016
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2007-06-27 19:08:45 +0000

    New Diffs for Liferay Noir
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@7350 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ themes/liferay-noir-theme/docroot/_diffs/javascript/javascript.js
@@ -0,0 +1,71 @@
+var LiferayBrochure = function () {
+	var $ = jQuery;
+	return {
+		init: function() {
+			var instance = this;
+			
+			instance.handleLastChild();
+			instance.handleSearchForm();
+		},
+		
+		handleLastChild: function () {
+			var instance = this;
+			if (!$.browser.msie) {
+				return;
+			}
+			$('#footer ul li:last').addClass('last-child');
+		},
+		
+		handleSearchForm: function() {
+			var searchForm = $('#banner .search');	
+			
+			var searchInput = searchForm.find('input[@type=image]');
+			var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
+			
+			searchLink.click(
+				function() {
+					$(this).parents('form')[0].submit();
+				}
+			);
+			
+			searchInput.hide();
+			searchInput.before(searchLink);
+		}
+	};
+}();
+
+jQuery(document).ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
+	function() {
+		LiferayBrochure.init();
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	jQueryObj: the jQuery wrapped object of the current portlet
+	*/
+
+	function(portletId, jQueryObj) {
+	}
+);
+
+jQuery(document).last(
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
