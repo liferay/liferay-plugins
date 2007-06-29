a4acafe9ff14f22ac41e6269dc0673b0ebe854c2
Brian Chan <brian.chan@liferay.com>
2007-06-29 22:02:18 +0000

    Build fixes
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@7420 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/create.bat
+++ themes/create.bat
@@ -11,8 +11,8 @@ goto end
 	echo.
 	echo Usage: create.bat hello-world "Hello World"
 	echo.
-	echo The first hello-world will tell the script to put your theme in a newly created
-	echo hello-world-theme directory.
+	echo The first hello-world is your theme id. A new directory will be created based
+	echo on the theme id.
 	echo.
 	echo The second "Hello World" is the theme's display name. The quotation marks are
 	echo only needed because there is a space in the display name.
