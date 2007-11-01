ea7544ceea1fd7bc4880ae5b688aec5a21689576
Jonathan Neal <jonathan.neal@liferay.com>
2007-11-01 16:48:13 +0000

    updated theme.
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@10704 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ themes/higherground-theme/docroot/_diffs/javascript/javascript.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
+	function() {
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	jQueryObj: the jQuery wrapped object of the current portlet
+	*/
+
+	function(portletId, jQueryObj) {
+	}
+);
+
+jQuery(document).last(
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
