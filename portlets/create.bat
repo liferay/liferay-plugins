3e41851bac92599acdd00a5839a50add6b5552da
Richard Sezov <richard.sezov@liferay.com>
2007-11-21 20:50:32 +0000

    create.bat for portlet projects
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@11325 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/create.bat
@@ -0,0 +1,44 @@
+@echo off
+
+if "" == "%1" goto errorCreate
+if "" == "%2" goto errorCreate
+
+call ant -Dportlet.name=%1 -Dportlet.display.name=%2 create
+
+svn add %1-portlet
+
+cd %1-portlet\docroot
+
+svn ps svn:ignore -F .cvsignore .
+
+svn remove --force .cvsignore
+
+cd WEB-INF
+
+svn ps svn:ignore -F .cvsignore .
+
+svn remove --force .cvsignore
+
+cd ..
+
+cd ..
+
+call ant deploy
+
+cd ..
+
+goto end
+
+:errorCreate
+	echo.
+	echo Usage: create.bat hello-world "Hello World"
+	echo.
+	echo The first hello-world is your portlet id. A new directory will be created based
+	echo on the portlet id.
+	echo.
+	echo The second "Hello World" is the portlet's display name. The quotation marks are
+	echo only needed because there is a space in the display name.
+
+	goto end
+
+:end
