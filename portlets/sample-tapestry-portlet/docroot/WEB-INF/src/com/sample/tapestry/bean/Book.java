9385cf1bab196c5c7b4371d065717882ff21e328
Brian Chan <brian.chan@liferay.com>
2008-01-02 05:26:25 +0000

    Update copyright notice year
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@12467 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/sample-tapestry-portlet/docroot/WEB-INF/src/com/sample/tapestry/bean/Book.java
+++ portlets/sample-tapestry-portlet/docroot/WEB-INF/src/com/sample/tapestry/bean/Book.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
