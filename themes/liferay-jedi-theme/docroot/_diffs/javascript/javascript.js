4c4ff66396170d3a72a887f342edcbb2e8c8e57d
Ryan Park <ryan.park@liferay.com>
2008-01-02 18:24:46 +0000

    Added "Liferay Jedi" Theme
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@12478 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
@@ -0,0 +1,89 @@
+var LiferayJedi = function () {
+	var $ = jQuery;
+	return {
+		init: function() {
+			var instance = this;
+			
+			instance.handleSearchForm();
+			instance.dropDownMenu();
+			instance.handleLastChild();
+		},
+
+		handleSearchForm: function() {
+			var searchForm = $('#banner .search');	
+			
+			var searchInput = searchForm.find('input[@type=image]');
+			var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
+			
+			searchLink.click(
+				function() {
+					$(this).parents('form')[0].submit();
+				}
+			);
+			
+			searchInput.hide();
+			searchInput.before(searchLink);
+		},
+
+		handleLastChild: function () {
+			var instance = this;
+			if (!$.browser.msie) {
+				return;
+			}
+			$('#footer ul li:last').addClass('last-child');
+		},
+
+		dropDownMenu: function() {
+			$(".parent-nav-item").hover(
+				function () {
+					var instance = $(this);
+					var child = $('.child-menu', this);
+					instance.addClass("init");
+					child.slideDown(50);
+				},
+				function () {
+					var instance = $(this);
+					var child = $('.child-menu', this);
+					child.slideUp(1);
+					instance.removeClass("init");
+				}
+			);
+		}
+	};
+}();
+
+jQuery(document).ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
+	function() {
+		LiferayJedi.init();
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	jQueryObj: the jQuery wrapped object of the current portlet
+	*/
+
+	function(portletId, jQueryObj) {
+	}
+);
+
+jQuery(document).last(
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
