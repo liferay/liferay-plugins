3f93ebf7c67fdc9bad61ebd1e1264eef485ff124
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2008-04-05 01:20:14 +0000

    Cleaning up JS
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@14831 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
+++ themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
@@ -75,37 +75,7 @@ var LiferayJedi = function () {
 }();
 
 jQuery(document).ready(
-
-	/*
-	This function gets loaded when all the HTML, not including the portlets, is
-	loaded.
-	*/
-
 	function() {
 		LiferayJedi.init();
-	}
-);
-
-Liferay.Portlet.ready(
-
-	/*
-	This function gets loaded after each and every portlet on the page.
-
-	portletId: the current portlet's id
-	jQueryObj: the jQuery wrapped object of the current portlet
-	*/
-
-	function(portletId, jQueryObj) {
-	}
-);
-
-jQuery(document).last(
-
-	/*
-	This function gets loaded when everything, including the portlets, is on
-	the page.
-	*/
-
-	function() {
 	}
 );
