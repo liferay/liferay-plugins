0aa5fa4f36eb6188f07acd9a9a5a3a5b2bdf5d8b
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2008-04-05 01:15:20 +0000

    Tweaking layout of the language flags
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@14829 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
+++ themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
@@ -3,12 +3,50 @@ var LiferayJedi = function () {
 	return {
 		init: function() {
 			var instance = this;
-
+			
 			instance.handleSearchForm();
 			instance.handleLanguages();
 			instance.dropDownMenu();
 			instance.handleLastChild();
 		},
+
+		handleSearchForm: function() {
+			var searchForm = $('#banner .search');	
+			
+			var searchInput = searchForm.find('input[@type=image]');
+			var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
+			
+			searchLink.click(
+				function() {
+					$(this).parents('form')[0].submit();
+				}
+			);
+			
+			searchInput.hide();
+			searchInput.before(searchLink);
+		},
+
+		handleLastChild: function () {
+			var instance = this;
+
+			$('#footer ul li:last').addClass('last-child');
+		},
+		
+		handleLanguages: function() {
+			var instance = this;
+
+			var lang = jQuery('#banner .lang');
+			
+			if (!lang.length) {
+				var dock = jQuery('div.lfr-dock');				
+
+				// width of dock, plus 13px offset from the right, and 15 pixels for good spacing
+				var position = dock.width() + 28;
+
+				lang.css('right', position);
+			}
+		},
+		
 
 		dropDownMenu: function() {
 			$(".parent-nav-item").hoverIntent(
@@ -21,7 +59,7 @@ var LiferayJedi = function () {
 						instance.addClass("init");
 						child.slideDown(100);
 					},
-
+					
 					out: function () {
 						var instance = $(this);
 						var child = $('.child-menu', this);
@@ -30,46 +68,42 @@ var LiferayJedi = function () {
 					}
 				}
 			);
-		},
-
-		handleSearchForm: function() {
-			var searchForm = $('#banner .search');
-
-			var searchInput = searchForm.find('input[@type=image]');
-			var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
-
-			searchLink.click(
-				function() {
-					$(this).parents('form')[0].submit();
-				}
-			);
-
-			searchInput.hide();
-			searchInput.before(searchLink);
-		},
-
-		handleLastChild: function () {
-			var instance = this;
-
-			$('#footer ul li:last').addClass('last-child');
-		},
-
-		handleLanguages: function() {
-			var instance = this;
-
-			var lang = jQuery('#banner .lang');
-			var dock = jQuery('div.lfr-dock');
-
-			// width of dock, plus 13px offset from the right, and 15 pixels for good spacing
-			var position = dock.width() + 28;
-			
-			lang.css('right', position);
 		}
 	};
 }();
 
 jQuery(document).ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
 	function() {
 		LiferayJedi.init();
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	jQueryObj: the jQuery wrapped object of the current portlet
+	*/
+
+	function(portletId, jQueryObj) {
+	}
+);
+
+jQuery(document).last(
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
 	}
 );
