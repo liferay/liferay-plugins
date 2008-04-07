b64c7e7db0fe7da154cb4bf2c9b1db966202a20a
Brian Chan <brian.chan@liferay.com>
2008-04-07 07:44:18 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@14853 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
+++ themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
@@ -9,31 +9,6 @@ var LiferayJedi = function () {
 			instance.handleLastChild();
 		},
 
-		handleSearchForm: function() {
-			var searchForm = $('#banner .search');
-
-			if (searchForm.length) {
-				var searchInput = searchForm.find('input[@type=image]');
-				var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
-
-				searchLink.click(
-					function() {
-						$(this).parents('form')[0].submit();
-					}
-				);
-
-				searchInput.hide();
-				searchInput.before(searchLink);
-			}
-		},
-
-		handleLastChild: function () {
-			var instance = this;
-
-			$('#footer ul li:last').addClass('last-child');
-		},
-
-
 		dropDownMenu: function() {
 			$(".parent-nav-item").hoverIntent(
 				{
@@ -56,6 +31,30 @@ var LiferayJedi = function () {
 					}
 				}
 			);
+		},
+
+		handleSearchForm: function() {
+			var searchForm = $('#banner .search');
+
+			if (searchForm.length) {
+				var searchInput = searchForm.find('input[@type=image]');
+				var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
+
+				searchLink.click(
+					function() {
+						$(this).parents('form')[0].submit();
+					}
+				);
+
+				searchInput.hide();
+				searchInput.before(searchLink);
+			}
+		},
+
+		handleLastChild: function () {
+			var instance = this;
+
+			$('#footer ul li:last').addClass('last-child');
 		}
 	};
 }();
