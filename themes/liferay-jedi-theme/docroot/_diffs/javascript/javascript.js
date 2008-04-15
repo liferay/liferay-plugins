1835268c3001c4cf74930fc43c4ce5bcd48edc77
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2008-04-15 21:57:08 +0000

    Changing toggle controls since it's in the core as of 5.0.2
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@15115 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
+++ themes/liferay-jedi-theme/docroot/_diffs/javascript/javascript.js
@@ -7,24 +7,28 @@ var LiferayJedi = function () {
 			instance.handleSearchForm();
 			instance.dropDownMenu();
 			instance.handleLastChild();
-			instance.createToggleControls();
 		},
 
-		createToggleControls: function(args) {
-			var instance = this;
-
-			var dock = jQuery('.lfr-dock .lfr-dock-list');
-			var toggleLnk = jQuery('<a href="javascript: ;">Toggle Edit Controls</a>');
-			var li = jQuery('<li></li>');
-
-			toggleLnk.click(
-				function(event){
-					jQuery('.lfr-meta-actions, .portlet-borderless-bar, .portlet-icons').toggle();
+		handleSearchForm: function() {
+			var searchForm = $('#banner .search');	
+			
+			var searchInput = searchForm.find('input[@type=image]');
+			var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
+			
+			searchLink.click(
+				function() {
+					$(this).parents('form')[0].submit();
 				}
 			);
 
-			li.append(toggleLnk);
-			dock.find('> .my-places').before(li);
+			searchInput.hide();
+			searchInput.before(searchLink);
+		},
+
+		handleLastChild: function () {
+			var instance = this;
+
+			$('#footer ul li:last').addClass('last-child');
 		},
 
 		dropDownMenu: function() {
@@ -35,44 +39,18 @@ var LiferayJedi = function () {
 					over: function () {
 						var instance = $(this);
 						var child = $('.child-menu', this);
-
 						instance.addClass("init");
 						child.slideDown(100);
 					},
-
+					
 					out: function () {
 						var instance = $(this);
 						var child = $('.child-menu', this);
-
 						child.slideUp(50);
 						instance.removeClass("init");
 					}
 				}
 			);
-		},
-
-		handleSearchForm: function() {
-			var searchForm = $('#banner .search');
-
-			if (searchForm.length) {
-				var searchInput = searchForm.find('input[@type=image]');
-				var searchLink = $('<a class="search-input-link" href="javascript:;"></a>');
-
-				searchLink.click(
-					function() {
-						$(this).parents('form')[0].submit();
-					}
-				);
-
-				searchInput.hide();
-				searchInput.before(searchLink);
-			}
-		},
-
-		handleLastChild: function () {
-			var instance = this;
-
-			$('#footer ul li:last').addClass('last-child');
 		}
 	};
 }();
