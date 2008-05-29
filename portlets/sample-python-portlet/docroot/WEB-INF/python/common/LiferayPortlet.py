c1b90f92b95fd7f132f3f46adfed0f16e8a2dafb
Brian Chan <brian.chan@liferay.com>
2008-05-29 20:30:25 +0000

    Fix commit
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@16287 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/sample-python-portlet/docroot/WEB-INF/python/common/LiferayPortlet.py
@@ -0,0 +1,75 @@
+from com.liferay.portal.kernel.util import Validator;
+
+class LiferayPortlet:
+    
+    def __init__(self, req, res):
+        self.req = req;
+        self.res = res;
+    
+    def actionLink(self, text, params):
+        return self.link(self.actionUrl(params), text);
+    
+    def actionUrl(self, params):
+        portletUrl = self.res.createActionURL();
+    
+        return self.url(portletUrl, params);
+    
+    def link(self, url, text):
+        return "<a href='" + url + "'>" + text + "</a>";
+    
+    def renderLink(self, text, target, params):
+        return self.link(self.renderUrl(target, params), text);
+    
+    def renderUrl(self, target, params):
+        portletUrl = self.res.createRenderURL();
+    
+        if (Validator.isNotNull(target)):
+            portletUrl.setParameter("pythonFile", target);
+    
+        return self.url(portletUrl, params);
+    
+    def showPortletDetails(self, portletConfig):
+        return """
+            <table class='liferay-table'>
+            <tr>
+                <td>Portlet Name:</td>
+                <td> """ + portletConfig.getPortletName() + """</td>
+            </tr>
+            <tr>
+                <td>Preferences:</td>
+                <td>""" + str(self.req.getPreferences().getMap()) + """</td>
+            </tr>
+            <tr>
+                <td>Parameters:</td>
+                <td>""" + str(self.req.getParameterMap()) + """</td>
+            </tr>
+            </table>
+        """;
+    
+    def showUserDetails(self, userInfo):
+        return """
+            <table class='liferay-table'>
+            <tr>
+                <td>User ID:</td>
+                <td>""" + userInfo['liferay.user.id'] + """</td>
+            </tr>
+            <tr>
+                <td>First Name:</td>
+                <td>""" + userInfo['user.name.given'] + """</td>
+            </tr>
+            <tr>
+                <td>Last Name:</td>
+                <td>""" + userInfo['user.name.family'] + """</td>
+            </tr>
+            <tr>
+                <td>Email Address:</td>
+                <td>""" + userInfo['user.home-info.online.email'] + """</td>
+            </tr>
+            </table>
+        """;
+    
+    def url(self, portletUrl, params):
+        for key, value in params.iteritems():
+            portletUrl.setParameter(key, value);
+    
+        return portletUrl.toString();
