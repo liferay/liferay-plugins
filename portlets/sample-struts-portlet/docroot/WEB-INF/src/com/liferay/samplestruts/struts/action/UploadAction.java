ca545eeeabb95166f56707b01158b072a2aa52fc
Brian Chan <brian.chan@liferay.com>
2008-05-30 18:33:24 +0000

    Clean up plugins
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@16342 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/sample-struts-portlet/docroot/WEB-INF/src/com/liferay/samplestruts/struts/action/UploadAction.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.samplestruts.struts.action;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * <a href="UploadAction.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class UploadAction extends Action {
+
+	public ActionForward execute(
+			ActionMapping mapping, ActionForm form, HttpServletRequest req,
+			HttpServletResponse res)
+		throws Exception {
+
+		FileItemFactory factory = new DiskFileItemFactory();
+
+		ServletFileUpload upload = new ServletFileUpload(factory);
+
+		List items = upload.parseRequest(req);
+
+		Iterator iter = items.iterator();
+
+		String itemName = "";
+
+		while (iter.hasNext()) {
+			FileItem item = (FileItem) iter.next();
+
+			if (!item.isFormField()) {
+				if (_log.isInfoEnabled()) {
+					_log.info("Field name " + item.getFieldName());
+				}
+
+				itemName = item.getName();
+
+				if (_log.isInfoEnabled()) {
+					_log.info("Name " + itemName);
+					_log.info("Content type " + item.getContentType());
+					_log.info("In memory " + item.isInMemory());
+					_log.info("Size " + item.getSize());
+				}
+			}
+		}
+
+		req.setAttribute("file_name", itemName);
+
+		return mapping.findForward("/sample_struts_portlet/upload_success");
+	}
+
+	private static Log _log = LogFactory.getLog(UploadAction.class);
+
+}
