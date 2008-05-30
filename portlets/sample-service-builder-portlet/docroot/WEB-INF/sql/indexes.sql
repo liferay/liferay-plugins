ce315ff025b9f0cbc82437849b1e5802025d96d1
Brian Chan <brian.chan@liferay.com>
2008-05-30 17:52:09 +0000

    Clean up plugins
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@16339 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/sample-service-builder-portlet/docroot/WEB-INF/sql/indexes.sql
+++ portlets/sample-service-builder-portlet/docroot/WEB-INF/sql/indexes.sql
@@ -1,3 +1,1 @@
 create index IX_DEC05F3B on SSB_Foo (field2);
-
-create index IX_B618923 on Sample_Foo (field2);
--- portlets/sample-service-builder-portlet/docroot/WEB-INF/sql/tables.sql
+++ portlets/sample-service-builder-portlet/docroot/WEB-INF/sql/tables.sql
@@ -7,12 +7,3 @@ create table SSB_Foo (
 	field4 DATE null,
 	field5 VARCHAR(75) null
 );
-
-create table Sample_Foo (
-	fooId LONG not null primary key,
-	field1 VARCHAR(75) null,
-	field2 BOOLEAN,
-	field3 INTEGER,
-	field4 DATE null,
-	field5 VARCHAR(75) null
-);
