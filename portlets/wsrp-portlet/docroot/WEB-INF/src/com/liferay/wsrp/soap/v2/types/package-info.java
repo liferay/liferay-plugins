ad4bfe6eeddbbf66e3c3e239acfd394c1959c037
Brian Chan <brian.chan@liferay.com>
2008-07-08 01:04:57 +0000

    Make WSDL client a jar
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@17597 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/soap/v2/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:oasis:names:tc:wsrp:v2:types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.liferay.wsrp.soap.v2.types;
