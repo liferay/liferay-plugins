a602456c1c40afb2218843e50613546f7c289059
Brian Chan <brian.chan@liferay.com>
2008-07-08 01:16:04 +0000

    LEP-6578
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@17600 05bdf26c-840f-0410-9ced-eb539d925f36

--- layouttpl/create.bat
+++ layouttpl/create.bat
@@ -5,24 +5,6 @@ if "" == "%2" goto errorCreate
 
 call ant -Dlayouttpl.name=%1 -Dlayouttpl.display.name=%2 create
 
-svn add %1-layouttpl
-
-cd %1-layouttpl\docroot
-
-svn ps svn:ignore -F .cvsignore .
-
-svn remove --force .cvsignore
-
-cd WEB-INF
-
-svn ps svn:ignore -F .cvsignore .
-
-svn remove --force .cvsignore
-
-cd ..
-
-cd ..
-
 call ant deploy
 
 cd ..
@@ -33,11 +15,11 @@ goto end
 	echo.
 	echo Usage: create.bat hello-world "Hello World"
 	echo.
-	echo The first hello-world is your layouttpl id. A new directory will be created based
-	echo on the layouttpl id.
+	echo The first hello-world is your layouttpl id. A new directory will be created
+	echo based on the layouttpl id.
 	echo.
-	echo The second "Hello World" is the layouttpl's display name. The quotation marks are
-	echo only needed because there is a space in the display name.
+	echo The second "Hello World" is the layouttpl's display name. The quotation marks
+	echo are only needed because there is a space in the display name.
 
 	goto end
 
--- layouttpl/create.sh
+++ layouttpl/create.sh
@@ -6,34 +6,16 @@ if [ -z "$1" ]; then
 	echo
 	echo Usage: ./create.sh hello-world \"Hello World\"
 	echo
-	echo The first hello-world is your layouttpl id. A new directory will be created based
-	echo on the layouttpl id.
+	echo The first hello-world is your layouttpl id. A new directory will be created
+	echo based on the layouttpl id.
 	echo
-	echo The second \"Hello World\" is the layouttpl\'s display name. The quotation marks are
-	echo only needed because there is a space in the display name.
+	echo The second \"Hello World\" is the layouttpl\'s display name. The quotation marks
+	echo are only needed because there is a space in the display name.
 
 	exit 127
 fi
 
 ant -Dlayouttpl.name=$1 -Dlayouttpl.display.name=\"$2\" create
-
-svn add $1-layouttpl
-
-cd $1-layouttpl/docroot
-
-svn ps svn:ignore -F .cvsignore .
-
-svn remove --force .cvsignore
-
-cd WEB-INF
-
-svn ps svn:ignore -F .cvsignore .
-
-svn remove --force .cvsignore
-
-cd ..
-
-cd ..
 
 ant deploy
 
