0ec066ab695363b96afce113171e25de48da76d3
Ryan Park <ryan.park@liferay.com>
2008-09-30 00:07:13 +0000

    Removed Themes
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@20941 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/digg-theme/docroot/_diffs/javascript/javascript.js
+++
@@ -1,8 +0,0 @@
-Liferay.Util.actsAsAspect(Liferay.Dock);
-
-Liferay.Dock.before(
-	'init',
-	function() {
-		jQuery('.interactive-mode').removeClass('interactive-mode');
-	}
-);
--- themes/film-theme/docroot/_diffs/javascript/javascript.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(
-	function() {
-		jQuery('.lfr-dock').css('z-index', 10);
-
-		jQuery('#banner').css({
-			'z-index': 7
-		});
-	}
-);
