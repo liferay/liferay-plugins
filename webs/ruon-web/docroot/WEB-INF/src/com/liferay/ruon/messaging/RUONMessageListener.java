2448e24f46660ed99d3032ceebeb7ee5973892c0
Brian Chan <brian.chan@liferay.com>
2008-11-25 22:06:28 +0000

    LPS-560 Rollback, still bad formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@23126 05bdf26c-840f-0410-9ced-eb539d925f36

--- webs/ruon-web/docroot/WEB-INF/src/com/liferay/ruon/messaging/RUONMessageListener.java
+++ webs/ruon-web/docroot/WEB-INF/src/com/liferay/ruon/messaging/RUONMessageListener.java
@@ -1,43 +1,24 @@
 /**
-* Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
-
-/**
-* The contents of this file are subject to the terms of the Common Development
-* and Distribution License (the License). You may not use this file except in
-* compliance with the License.
-*
-* You can obtain a copy of the License at http://www.sun.com/cddl/cddl.html and
-* legal/CDDLv1.0.txt. See the License for the specific language governing
-* permission and limitations under the License.
-*
-* When distributing Covered Code, include this CDDL Header Notice in each file
-* and include the License file at legal/CDDLv1.0.txt.
-*
-* If applicable, add the following below the CDDL Header, with the fields
-* enclosed by brackets [] replaced by your own identifying information:
-* "Portions Copyrighted [year] [name of copyright owner]"
-*
-* Copyright 2008 Sun Microsystems Inc. All rights reserved.
-*/
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.liferay.ruon.messaging;
 
@@ -90,32 +71,7 @@ public class RUONMessageListener implements MessageListener {
 		}
 		else if (command.equals("updatePresence")) {
 			updatePresence(message);
-		}else if (command.equalsIgnoreCase("delegateToNetwork")){
-			delegateToNetwork(message);
 		}
-	}
-
-	protected void delegateToNetwork(Message message) throws Exception{
-		String name = message.getString("name");
-		Network network = NetworkLocalServiceUtil.getNetwork(name);
-		Converter converter = _converters.get(network.getNetworkId());
-
-		Map<String, Object> input = new HashMap<String, Object>();
-		input.put("command","delegateToNetwork");
-		input.put("message",message);
-
-		Object output = converter.convert(input);
-		JSONObject payLoadJSON = JSONFactoryUtil.createJSONObject();
-
-		if(output != null && ((String)output).equalsIgnoreCase("success")){
-			payLoadJSON.put("output","success");
-		}
-		else{
-			payLoadJSON.put("output","failure");
-		}
-
-		message.setPayload(payLoadJSON);
-		MessageBusUtil.sendMessage(message.getResponseDestination(), message);
 	}
 
 	protected void getPresences(Message message) throws Exception {
@@ -140,7 +96,7 @@ public class RUONMessageListener implements MessageListener {
 			}
 
 			Map<String, Object> input = new HashMap<String, Object>();
-			input.put("command","presenceOutput");
+
 			input.put("userId", userId);
 			input.put("online", online);
 			input.put("locale", locale);
