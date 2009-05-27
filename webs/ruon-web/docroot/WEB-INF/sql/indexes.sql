09fc2ba5392d45d6109903cf5706d6ab796fe10b
Brian Chan <brian.chan@liferay.com>
2009-05-27 22:57:52 +0000

    LPS-3503
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@32170 05bdf26c-840f-0410-9ced-eb539d925f36

--- webs/ruon-web/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,5 +0,0 @@
-create index IX_40390856 on Ruon_Network (name);
-
-create index IX_39240938 on Ruon_Presence (userId);
-create index IX_DE246521 on Ruon_Presence (userId, networkId);
-create index IX_C64EAE64 on Ruon_Presence (userId, online_);
