9585e0f1002f9efcfd7a91067912cf72097abf75
Brian Chan <brian.chan@liferay.com>
2009-06-23 01:28:16 +0000

    LPS-3747 Producer
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@33328 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/bind/ServiceDescriptionImpl.java
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/bind/ServiceDescriptionImpl.java
@@ -22,16 +22,27 @@
 
 package com.liferay.wsrp.bind;
 
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.service.PortletLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.util.axis.ServletUtil;
+import com.liferay.wsrp.model.WSRPProducer;
+import com.liferay.wsrp.service.WSRPProducerLocalServiceUtil;
+
 import java.rmi.RemoteException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import oasis.names.tc.wsrp.v2.intf.WSRP_v2_ServiceDescription_PortType;
 import oasis.names.tc.wsrp.v2.types.GetServiceDescription;
-import oasis.names.tc.wsrp.v2.types.InvalidRegistrationFault;
 import oasis.names.tc.wsrp.v2.types.LocalizedString;
-import oasis.names.tc.wsrp.v2.types.ModifyRegistrationRequiredFault;
-import oasis.names.tc.wsrp.v2.types.OperationFailedFault;
 import oasis.names.tc.wsrp.v2.types.PortletDescription;
-import oasis.names.tc.wsrp.v2.types.ResourceSuspendedFault;
 import oasis.names.tc.wsrp.v2.types.ServiceDescription;
 
 /**
@@ -45,23 +56,75 @@ public class ServiceDescriptionImpl
 
 	public ServiceDescription getServiceDescription(
 			GetServiceDescription getServiceDescription)
-		throws InvalidRegistrationFault, ModifyRegistrationRequiredFault,
-			   OperationFailedFault, RemoteException, ResourceSuspendedFault {
+		throws RemoteException {
+
+		try {
+			return doGetServiceDescription(getServiceDescription);
+		}
+		catch (RemoteException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	protected ServiceDescription doGetServiceDescription(
+			GetServiceDescription getServiceDescription)
+		throws Exception {
+
+		HttpServletRequest request = ServletUtil.getRequest();
+
+		long wsrpProducerId = ParamUtil.getLong(request, "wsrpProducerId");
+
+		WSRPProducer wsrpProducer =
+			WSRPProducerLocalServiceUtil.getWSRPProducer(wsrpProducerId);
 
 		ServiceDescription serviceDescription = new ServiceDescription();
 
-		PortletDescription portletDescription = new PortletDescription();
-
-		portletDescription.setPortletHandle("1");
-		portletDescription.setTitle(new LocalizedString("Hello World", null));
-
-		PortletDescription[] portletDescriptions = {
-			portletDescription
-		};
-
-		serviceDescription.setOfferedPortlets(portletDescriptions);
+		serviceDescription.setOfferedPortlets(
+			getPortletDescriptions(wsrpProducer));
 
 		return serviceDescription;
+	}
+
+	protected PortletDescription getPortletDescription(
+		WSRPProducer wsrpProducer, Portlet portlet) {
+
+		String title = PortalUtil.getPortletTitle(
+			portlet, wsrpProducer.getCompanyId(), LocaleUtil.getDefault());
+
+		PortletDescription portletDescription = new PortletDescription();
+
+		portletDescription.setPortletHandle(portlet.getPortletId());
+		portletDescription.setTitle(new LocalizedString(title, null));
+
+		return portletDescription;
+	}
+
+	protected PortletDescription[] getPortletDescriptions(
+		WSRPProducer wsrpProducer) {
+
+		List<PortletDescription> portletDescriptions =
+			new ArrayList<PortletDescription>();
+
+		String[] portletIds = StringUtil.split(wsrpProducer.getPortletIds());
+
+		for (String portletId : portletIds) {
+			Portlet portlet = PortletLocalServiceUtil.getPortletById(portletId);
+
+			if (portlet == null) {
+				continue;
+			}
+
+			PortletDescription portletDescription = getPortletDescription(
+				wsrpProducer, portlet);
+
+			portletDescriptions.add(portletDescription);
+		}
+
+		return portletDescriptions.toArray(
+			new PortletDescription[portletDescriptions.size()]);
 	}
 
 }
