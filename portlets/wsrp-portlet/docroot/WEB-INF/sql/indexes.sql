ccae31bece11ad011ec6a246e03055d3cdfb75cc
Brian Chan <brian.chan@liferay.com>
2009-11-20 18:39:18 +0000

    Rewrite using different pattern
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@41315 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/wsrp-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,5 +0,0 @@
-create index IX_8F3348D on WSRP_WSRPConsumer (companyId);
-
-create index IX_1CEEF2AA on WSRP_WSRPConsumerPortlet (wsrpConsumerId);
-
-create index IX_DD08A671 on WSRP_WSRPProducer (companyId);
