307bbc9dcc57583fc674139279e32790f25fc86b
Brian Chan <brian.chan@liferay.com>
2010-02-06 21:15:56 +0000

    Update year
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@45546 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/chat-portlet/docroot/WEB-INF/src/com/liferay/chat/service/base/EntryLocalServiceBaseImpl.java
+++ portlets/chat-portlet/docroot/WEB-INF/src/com/liferay/chat/service/base/EntryLocalServiceBaseImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
