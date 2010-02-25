5d6c2ab748631fbfa4a576ff825a52bfcab0b532
Ryan Park <ryan.park@liferay.com>
2010-02-25 22:56:01 +0000

    SOS-332 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@46900 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/so-theme/docroot/_diffs/javascript/javascript.js
+++ themes/so-theme/docroot/_diffs/javascript/javascript.js
@@ -10,27 +10,28 @@ AUI().use(
 
 			handleMySitesDropDown: function() {
 				var mySites = A.one('#navigation-top .my-sites');
-				
-				mySites.on(
-					'mouseover',
-					function(event) {
-						var menu = event.currentTarget;
-				
-						menu.addClass('open');
-						menu.one('.child-menu').show();
-					}
-				);
-				
-				mySites.on(
-					'mouseout',
-					function(event) {
-						var menu = event.currentTarget;
-				
-						menu.removeClass('open');
-						menu.one('.child-menu').hide();
-					}
-				);
-				
+
+				if (mySites) {
+					mySites.on(
+						'mouseover',
+						function(event) {
+							var menu = event.currentTarget;
+
+							menu.addClass('open');
+							menu.one('.child-menu').show();
+						}
+					);
+
+					mySites.on(
+						'mouseout',
+						function(event) {
+							var menu = event.currentTarget;
+
+							menu.removeClass('open');
+							menu.one('.child-menu').hide();
+						}
+					);
+				}
 			}
 		};
 	}
