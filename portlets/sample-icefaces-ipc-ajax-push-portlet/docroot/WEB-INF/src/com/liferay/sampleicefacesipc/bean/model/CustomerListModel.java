60b454a24b40382a023e010fec1e47f34e37e824
Brian Chan <brian.chan@liferay.com>
2010-02-27 19:40:46 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@47099 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/sample-icefaces-ipc-ajax-push-portlet/docroot/WEB-INF/src/com/liferay/sampleicefacesipc/bean/model/CustomerListModel.java
+++ portlets/sample-icefaces-ipc-ajax-push-portlet/docroot/WEB-INF/src/com/liferay/sampleicefacesipc/bean/model/CustomerListModel.java
@@ -1,44 +1,35 @@
 /**
  * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 package com.liferay.sampleicefacesipc.bean.model;
 
-import java.util.List;
-
-import javax.faces.event.ValueChangeEvent;
-
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-
-import com.liferay.sampleicefacesipc.service.CustomerService;
-import com.liferay.sampleicefacesipc.transfer.Customer;
-import com.liferay.sampleicefacesipc.util.PortletSessionUtil;
 import com.icesoft.faces.async.render.OnDemandRenderer;
-
 import com.icesoft.faces.async.render.RenderManager;
 import com.icesoft.faces.async.render.Renderable;
 import com.icesoft.faces.context.DisposableBean;
 import com.icesoft.faces.webapp.xmlhttp.PersistentFacesState;
 import com.icesoft.faces.webapp.xmlhttp.RenderingException;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.sampleicefacesipc.service.CustomerService;
+import com.liferay.sampleicefacesipc.transfer.Customer;
+import com.liferay.sampleicefacesipc.util.PortletSessionUtil;
+
+import java.util.List;
+
+import javax.faces.event.ValueChangeEvent;
 
 /**
  * <a href="CustomerListModel.java.html"><b><i>View Source</i></b></a>
@@ -131,7 +122,8 @@ public class CustomerListModel implements DisposableBean, Renderable {
 
 	private static final String CUSTOMER_RENDER_GROUP = "CUSTOMER_RENDER_GROUP";
 
-	private static final Log _log = LogFactoryUtil.getLog(CustomerListModel.class);
+	private static final Log _log = LogFactoryUtil.getLog(
+		CustomerListModel.class);
 
 	private CustomerService _customerService;
 	private PersistentFacesState _persistentFacesState;
