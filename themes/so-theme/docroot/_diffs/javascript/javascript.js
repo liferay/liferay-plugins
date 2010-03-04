2f0fc0095a34da827caffad6c43005ae443076e2
Ryan Park <ryan.park@liferay.com>
2010-03-04 01:25:24 +0000

    SOS-332 Theme update, add content
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@47404 05bdf26c-840f-0410-9ced-eb539d925f36

--- themes/so-theme/docroot/_diffs/javascript/javascript.js
+++ themes/so-theme/docroot/_diffs/javascript/javascript.js
@@ -1,11 +1,62 @@
 AUI().use(
-	'aui-base',
+	'aui-base', 'aui-dialog', 'aui-io-plugin',
 	function(A) {
 		LiferayInc = {
 			init: function() {
 				var instance = this;
 
+				instance.handleAddContent();
 				instance.handleMySitesDropDown();
+			},
+
+			handleAddContent: function() {
+				var instance = this;
+
+				var addContent = A.one('#soAddContent');
+
+				if (!addContent) {
+					return;
+				}
+
+				addContent.on(
+					'click',
+					function() {
+						if (!instance._addContentDialog) {
+							instance._addContentDialog = new A.Dialog(
+								{
+									title: Liferay.Language.get('add-content'),
+									width: 250,
+									xy: [15,15]
+								}
+							).plug(
+								A.Plugin.IO,
+								{
+									data: {
+										doAsUserId: themeDisplay.getDoAsUserIdEncoded(),
+										p_l_id: themeDisplay.getPlid(),
+										p_p_id: 87,
+										p_p_state: 'exclusive'
+									},
+									on: {
+										success: function() {
+											AUI().use(
+												'liferay-layout-configuration',
+												function(A) {
+													Liferay.LayoutConfiguration._dialogBody = instance._addContentDialog.bodyNode;
+
+													Liferay.LayoutConfiguration._loadContent();
+												}
+											);
+										}
+									},
+									uri: themeDisplay.getPathMain() + '/portal/render_portlet',
+								}
+							).render();
+						}
+
+						instance._addContentDialog.show();
+					}
+				);
 			},
 
 			handleMySitesDropDown: function() {
