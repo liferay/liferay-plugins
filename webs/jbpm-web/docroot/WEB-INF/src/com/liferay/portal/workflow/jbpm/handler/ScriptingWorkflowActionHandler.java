1988e73120b78430f6ba56e8094ef521be7df5b4
Michael Han <michael.han@liferay.com>
2010-03-18 23:04:20 +0000

    adding ability to have a scripting action in jbpm
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@48605 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ webs/jbpm-web/docroot/WEB-INF/src/com/liferay/portal/workflow/jbpm/handler/ScriptingWorkflowActionHandler.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.jbpm.handler;
+
+import com.liferay.portal.kernel.scripting.ScriptingUtil;
+import com.liferay.portal.kernel.workflow.ContextConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+import java.util.Map;
+
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.exe.ExecutionContext;
+import org.jbpm.taskmgmt.exe.TaskInstance;
+
+/**
+ * <a href="ScriptingWorkflowActionHandler.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Michael C. Han
+ */
+public class ScriptingWorkflowActionHandler implements ActionHandler {
+
+	public void execute(ExecutionContext executionContext) throws Exception {
+
+		Map<String, Object> inputObjects =
+			executionContext.getContextInstance().getVariables();
+
+		Long companyId = (Long)inputObjects.get(ContextConstants.COMPANY_ID);
+
+		TaskInstance taskInstance = executionContext.getTaskInstance();
+		if (taskInstance != null) {
+			inputObjects.put("assigneeClassPK", taskInstance.getActorId());
+			inputObjects.put("assigneeClassName", User.class.getName());
+
+			inputObjects.put("taskName", taskInstance.getName());
+			inputObjects.put("userId", taskInstance.getActorId());
+		}
+		else if (companyId != null) {
+			inputObjects.put(
+				"userId",
+				UserLocalServiceUtil.getDefaultUser(companyId).getUserId());
+		}
+
+		ScriptingUtil.exec(null, inputObjects, language, script);
+	}
+
+	private String language;
+	private String script;
+}
