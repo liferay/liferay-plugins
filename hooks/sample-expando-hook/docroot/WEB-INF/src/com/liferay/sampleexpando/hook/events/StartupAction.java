8e8c0d5c4391e5b730d4b95b64f6dd4868e480f2
Juan Fernandez <juan.fernandez@liferay.com>
2010-03-22 12:44:44 +0000

    LPS-8489 Adding the Sample Expando Hook
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@48786 05bdf26c-840f-0410-9ced-eb539d925f36

--- hooks/sample-expando-hook/docroot/WEB-INF/src/com/liferay/sampleexpando/hook/events/StartupAction.java
+++ hooks/sample-expando-hook/docroot/WEB-INF/src/com/liferay/sampleexpando/hook/events/StartupAction.java
@@ -27,6 +27,7 @@ import com.liferay.portlet.expando.service.ExpandoTableLocalServiceUtil;
 /**
  * <a href="StartupAction.java.html"><b><i>View Source</i></b></a>
  *
+ * @author Juan Fernández
  * @author Brian Wing Shun Chan
  */
 public class StartupAction extends SimpleAction {
