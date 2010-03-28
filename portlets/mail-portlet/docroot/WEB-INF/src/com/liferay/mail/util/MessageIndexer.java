4d2bb398d535db8b52f5e87963ceacc939bae307
Brian Chan <brian.chan@liferay.com>
2010-03-28 19:19:08 +0000

    LPS-8596 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@49323 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/util/MessageIndexer.java
+++ portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/util/MessageIndexer.java
@@ -112,8 +112,7 @@ public class MessageIndexer extends BaseIndexer {
 
 		Document document = getDocument(entry);
 
-		SearchEngineUtil.updateDocument(
-			entry.getCompanyId(), document);
+		SearchEngineUtil.updateDocument(entry.getCompanyId(), document);
 	}
 
 	protected void doReindex(String className, long classPK) throws Exception {
