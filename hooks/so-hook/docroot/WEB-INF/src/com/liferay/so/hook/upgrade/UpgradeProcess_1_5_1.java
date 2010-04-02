a739d5ff4a4cf5f09e8b8e1b3b4461de65d7c2ff
Ryan Park <ryan.park@liferay.com>
2010-04-02 21:37:55 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@49550 05bdf26c-840f-0410-9ced-eb539d925f36

--- hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/upgrade/UpgradeProcess_1_5_1.java
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/upgrade/UpgradeProcess_1_5_1.java
@@ -17,31 +17,8 @@
 
 package com.liferay.so.hook.upgrade;
 
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.model.Company;
-import com.liferay.portal.model.Group;
-import com.liferay.portal.model.GroupConstants;
-import com.liferay.portal.model.Layout;
-import com.liferay.portal.model.LayoutConstants;
-import com.liferay.portal.model.LayoutTemplate;
-import com.liferay.portal.model.LayoutTypePortlet;
-import com.liferay.portal.model.Portlet;
-import com.liferay.portal.service.CompanyLocalServiceUtil;
-import com.liferay.portal.service.GroupLocalServiceUtil;
-import com.liferay.portal.service.LayoutLocalServiceUtil;
-import com.liferay.portal.service.PortletPreferencesLocalServiceUtil;
-import com.liferay.portal.service.ServiceContext;
-import com.liferay.portal.util.PortalUtil;
-import com.liferay.portal.util.PortletKeys;
-import com.liferay.so.util.PortletPropsKeys;
-import com.liferay.util.portlet.PortletProps;
-
-import java.util.List;
-
-import javax.portlet.PortletPreferences;
+import com.liferay.so.hook.upgrade.v1_5_1.UpgradeGroup;
 
 /**
  * <a href="UpgradeProcess_1_5_1.java.html"><b><i>View Source</i></b></a>
@@ -55,156 +32,11 @@ public class UpgradeProcess_1_5_1 extends UpgradeProcess {
 	}
 
 	protected void doUpgrade() throws Exception {
-		if (isFirstRun()) {
+		if (UpgradeUtil.isFirstRun()) {
 			return;
 		}
 
-		updateGroups();
-	}
-
-	protected boolean isFirstRun() throws Exception {
-		List<Company> companies = CompanyLocalServiceUtil.getCompanies();
-
-		if (companies.isEmpty() || companies.size() > 1) {
-			return false;
-		}
-
-		Company company = companies.get(0);
-
-		long companyId = company.getCompanyId();
-
-		Group group = GroupLocalServiceUtil.getGroup(
-			companyId, GroupConstants.GUEST);
-
-		Layout layout = LayoutLocalServiceUtil.getLayout(
-			group.getGroupId(), false, 1);
-
-		LayoutTypePortlet layoutTypePortlet =
-			(LayoutTypePortlet)layout.getLayoutType();
-
-		if (!layoutTypePortlet.hasPortletId("47")) {
-			return false;
-		}
-
-		return true;
-	}
-
-	protected void updateGroups() throws Exception {
-		List<Group> groups = GroupLocalServiceUtil.getGroups(
-			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		for (Group group : groups) {
-			Layout layout = null;
-
-			try {
-				layout = LayoutLocalServiceUtil.getLayout(
-					group.getGroupId(), group.hasPrivateLayouts(), 1);
-			}
-			catch (Exception e) {
-				continue;
-			}
-
-			if (!layout.getFriendlyURL().equals("/home")) {
-				continue;
-			}
-
-			updatePortlets(group, layout);
-			updateLayouts(group);
-		}
-	}
-
-	protected void updateLayouts(Group group) throws Exception {
-		if (!group.isCommunity()) {
-			return;
-		}
-
-		if ((group.getType() == GroupConstants.TYPE_COMMUNITY_PRIVATE) &&
-			group.hasPrivateLayouts()) {
-
-			return;
-		}
-
-		if ((group.getType() == GroupConstants.TYPE_COMMUNITY_OPEN) &&
-			group.hasPublicLayouts()) {
-
-			return;
-		}
-
-		long companyId = group.getCompanyId();
-		long userId = PortalUtil.getValidUserId(
-			companyId, group.getCreatorUserId());
-		boolean privateLayout = group.hasPrivateLayouts();
-
-		List<Layout> sourceLayouts = LayoutLocalServiceUtil.getLayouts(
-			group.getGroupId(), privateLayout);
-
-		ServiceContext serviceContext = new ServiceContext();
-
-		for (Layout sourceLayout : sourceLayouts) {
-			Layout targetLayout = LayoutLocalServiceUtil.addLayout(
-				userId, group.getGroupId(), !privateLayout,
-				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
-				sourceLayout.getName(LocaleUtil.getDefault().toString()),
-				StringPool.BLANK, StringPool.BLANK,
-				LayoutConstants.TYPE_PORTLET, false,
-				sourceLayout.getFriendlyURL(), serviceContext);
-
-			LayoutLocalServiceUtil.updateLayout(
-				targetLayout.getGroupId(), targetLayout.isPrivateLayout(),
-				targetLayout.getLayoutId(), sourceLayout.getTypeSettings());
-
-			LayoutTypePortlet sourceLayoutTypePortlet =
-				(LayoutTypePortlet)sourceLayout.getLayoutType();
-
-			List<Portlet> sourcePortlets =
-				sourceLayoutTypePortlet.getAllPortlets();
-
-			for (Portlet sourcePortlet : sourcePortlets) {
-				String sourcePortletId = sourcePortlet.getPortletId();
-
-				PortletPreferencesLocalServiceUtil.getPreferences(
-					companyId, PortletKeys.PREFS_OWNER_ID_DEFAULT,
-					PortletKeys.PREFS_OWNER_TYPE_LAYOUT, targetLayout.getPlid(),
-					sourcePortletId);
-
-				PortletPreferences sourcePreferences =
-					PortletPreferencesLocalServiceUtil.getPreferences(
-						companyId, PortletKeys.PREFS_OWNER_ID_DEFAULT,
-						PortletKeys.PREFS_OWNER_TYPE_LAYOUT,
-						sourceLayout.getPlid(), sourcePortletId);
-
-				PortletPreferencesLocalServiceUtil.updatePreferences(
-					PortletKeys.PREFS_OWNER_ID_DEFAULT,
-					PortletKeys.PREFS_OWNER_TYPE_LAYOUT, targetLayout.getPlid(),
-					sourcePortletId, sourcePreferences);
-			}
-
-			LayoutLocalServiceUtil.deleteLayout(sourceLayout.getPlid());
-		}
-	}
-
-	protected void updatePortlets(Group group, Layout layout) throws Exception {
-		String prefix = PortletPropsKeys.SITE_LAYOUT_PORTLETS;
-
-		if (group.isUser()) {
-			prefix = PortletPropsKeys.USER_LAYOUT_PORTLETS;
-		}
-
-		LayoutTypePortlet layoutTypePortlet =
-			(LayoutTypePortlet)layout.getLayoutType();
-
-		LayoutTemplate layoutTemplate = layoutTypePortlet.getLayoutTemplate();
-
-		List<String> columns = layoutTemplate.getColumns();
-
-		for (String column : columns) {
-			layoutTypePortlet.setPortletIds(
-				column, PortletProps.get(prefix + column));
-		}
-
-		LayoutLocalServiceUtil.updateLayout(
-			layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
-			layout.getTypeSettings());
+		upgrade(UpgradeGroup.class);
 	}
 
 }
