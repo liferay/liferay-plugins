9e54f29b5b55f458add0527f6a00d1a3142135ac
Scott Lee <scott.lee@liferay.com>
2010-04-06 23:46:45 +0000

    Mail portlet, FolderLocalServiceImpl
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@49744 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/service/impl/FolderLocalServiceImpl.java
+++ portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/service/impl/FolderLocalServiceImpl.java
@@ -14,12 +14,71 @@
 
 package com.liferay.mail.service.impl;
 
+import com.liferay.mail.model.Folder;
 import com.liferay.mail.service.base.FolderLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.User;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * <a href="FolderLocalServiceImpl.java.html"><b><i>View Source</i></b></a>
  *
- * @author Brian Wing Shun Chan
+ * @author Scott Lee
  */
 public class FolderLocalServiceImpl extends FolderLocalServiceBaseImpl {
+
+	public Folder addFolder(
+			long userId, long accountId, String fullName, String displayName,
+			int remoteMessageCount)
+		throws PortalException, SystemException {
+
+		User user = userPersistence.findByPrimaryKey(userId);
+
+		Date now = new Date();
+
+		long folderId = counterLocalService.increment();
+
+		Folder folder = folderPersistence.create(folderId);
+
+		folder.setCompanyId(user.getCompanyId());
+		folder.setUserId(user.getUserId());
+		folder.setUserName(user.getFullName());
+		folder.setCreateDate(now);
+		folder.setModifiedDate(now);
+		folder.setAccountId(accountId);
+		folder.setFullName(fullName);
+		folder.setDisplayName(displayName);
+		folder.setRemoteMessageCount(remoteMessageCount);
+
+		return folderPersistence.update(folder, false);
+	}
+
+	public Folder getFolder(long accountId, String name)
+		throws PortalException, SystemException {
+
+		return folderPersistence.findByA_F(accountId, name);
+	}
+
+	public List<Folder> getFolders(long accountId) throws SystemException {
+		return folderPersistence.findByAccountId(accountId);
+	}
+
+	public Folder updateFolder(
+			long folderId, String fullName, String displayName,
+			int remoteMessageCount)
+		throws PortalException, SystemException {
+
+		Folder folder = folderPersistence.findByPrimaryKey(folderId);
+
+		folder.setModifiedDate(new Date());
+		folder.setFullName(fullName);
+		folder.setDisplayName(displayName);
+		folder.setRemoteMessageCount(remoteMessageCount);
+
+		return folderPersistence.update(folder, false);
+	}
+
 }
