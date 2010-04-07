51d35c0b2be245c885ebe53d16455e054f0b16ad
Scott Lee <scott.lee@liferay.com>
2010-04-07 18:46:02 +0000

    Mail Portlet, AttachmentLSI, service.xml
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@49809 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/service/impl/AttachmentLocalServiceImpl.java
+++ portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/service/impl/AttachmentLocalServiceImpl.java
@@ -14,12 +14,56 @@
 
 package com.liferay.mail.service.impl;
 
+import com.liferay.mail.model.Attachment;
+import com.liferay.mail.model.Folder;
+import com.liferay.mail.model.Message;
 import com.liferay.mail.service.base.AttachmentLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.User;
+
+import java.util.List;
 
 /**
  * <a href="AttachmentLocalServiceImpl.java.html"><b><i>View Source</i></b></a>
  *
- * @author Brian Wing Shun Chan
+ * @author Scott Lee
  */
 public class AttachmentLocalServiceImpl extends AttachmentLocalServiceBaseImpl {
+
+	public Attachment addAttachment(
+			long userId, long messageId, String contentPath, String fileName,
+			long size)
+		throws PortalException, SystemException {
+
+		User user = userPersistence.findByPrimaryKey(userId);
+		Message message = messagePersistence.findByPrimaryKey(messageId);
+
+		Folder folder = folderPersistence.findByPrimaryKey(
+			message.getFolderId());
+
+		long attachmentId = counterLocalService.increment();
+
+		Attachment attachment = attachmentPersistence.create(attachmentId);
+
+		attachment.setCompanyId(user.getCompanyId());
+		attachment.setUserId(user.getUserId());
+		attachment.setAccountId(folder.getAccountId());
+		attachment.setFolderId(folder.getFolderId());
+		attachment.setMessageId(messageId);
+		attachment.setContentPath(contentPath);
+		attachment.setFileName(fileName);
+		attachment.setSize(size);
+
+		attachmentPersistence.update(attachment, false);
+
+		return attachment;
+	}
+
+	public List<Attachment> getAttachments(long messageId)
+		throws SystemException {
+
+		return attachmentPersistence.findByMessageId(messageId);
+	}
+
 }
