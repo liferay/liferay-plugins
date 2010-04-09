2bb3cda1cdfda9f1a09d224e7ab00b3c1f541761
Peter Shin <peter.shin@liferay.com>
2010-04-09 21:39:37 +0000

    KB article add/update
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@50031 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/ArticleLocalServiceImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/ArticleLocalServiceImpl.java
@@ -14,17 +14,29 @@
 
 package com.liferay.knowledgebase.service.impl;
 
+import com.liferay.knowledgebase.ArticleContentException;
+import com.liferay.knowledgebase.ArticleTitleException;
 import com.liferay.knowledgebase.model.Article;
 import com.liferay.knowledgebase.service.base.ArticleLocalServiceBaseImpl;
+import com.liferay.knowledgebase.util.comparator.ArticlePriorityComparator;
+import com.liferay.knowledgebase.util.comparator.ArticleVersionComparator;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.portlet.PortletClassLoaderUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.MathUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +47,51 @@ import java.util.Map;
  * @author Brian Wing Shun Chan
  */
 public class ArticleLocalServiceImpl extends ArticleLocalServiceBaseImpl {
+
+	public Article addArticle(
+			String uuid, long userId, long parentResourcePrimKey, String title,
+			String content, String description, int priority,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		// Article
+
+		User user = userPersistence.findByPrimaryKey(userId);
+		long resourcePrimKey = counterLocalService.increment(
+			Article.class.getName());
+		long groupId = serviceContext.getScopeGroupId();
+		double version = 1.0;
+		Date now = new Date();
+
+		validate(title, content);
+
+		long articleId = counterLocalService.increment();
+
+		Article article = articlePersistence.create(articleId);
+
+		article.setUuid(uuid);
+		article.setResourcePrimKey(resourcePrimKey);
+		article.setGroupId(groupId);
+		article.setCompanyId(user.getCompanyId());
+		article.setUserId(user.getUserId());
+		article.setUserName(user.getFullName());
+		article.setCreateDate(now);
+		article.setModifiedDate(now);
+		article.setParentResourcePrimKey(parentResourcePrimKey);
+		article.setVersion(version);
+		article.setTitle(title);
+		article.setContent(content);
+		article.setDescription(description);
+		article.setPriority(priority);
+
+		articlePersistence.update(article, false);
+
+		// Articles
+
+		updateDisplayOrder(article, parentResourcePrimKey, priority);
+
+		return article;
+	}
 
 	public List<Article> getCompanyArticles(
 			long companyId, int start, int end,
@@ -109,6 +166,90 @@ public class ArticleLocalServiceImpl extends ArticleLocalServiceBaseImpl {
 		return dynamicQueryCount(getDynamicQuery(params));
 	}
 
+	public Article updateArticle(
+			long userId, long resourcePrimKey, long parentResourcePrimKey,
+			String title, String content, String description, int priority,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		// Article
+
+		Article oldArticle = articlePersistence.findByResourcePrimKey_First(
+			resourcePrimKey, new ArticleVersionComparator());
+		User user = userPersistence.findByPrimaryKey(userId);
+		double version = MathUtil.format(oldArticle.getVersion() + 0.1, 1, 1);
+		Date now = new Date();
+
+		validate(title, content);
+
+		long articleId = counterLocalService.increment();
+
+		Article article = articlePersistence.create(articleId);
+
+		article.setResourcePrimKey(oldArticle.getResourcePrimKey());
+		article.setGroupId(oldArticle.getGroupId());
+		article.setCompanyId(oldArticle.getCompanyId());
+		article.setUserId(user.getUserId());
+		article.setUserName(user.getFullName());
+		article.setCreateDate(oldArticle.getCreateDate());
+		article.setModifiedDate(now);
+		article.setParentResourcePrimKey(parentResourcePrimKey);
+		article.setVersion(version);
+		article.setTitle(title);
+		article.setContent(content);
+		article.setDescription(description);
+		article.setPriority(priority);
+
+		articlePersistence.update(article, false);
+
+		// Articles
+
+		updateDisplayOrder(article, parentResourcePrimKey, priority);
+
+		return article;
+	}
+
+	public Article updateDisplayOrder(
+			Article article, long parentResourcePrimKey, int priority)
+		throws SystemException {
+
+		List<Article> articles = ListUtil.copy(getGroupArticles(
+			article.getGroupId(), article.getParentResourcePrimKey(),
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+			new ArticlePriorityComparator(true)));
+
+		articles.remove(article);
+		articles.add(priority, article);
+
+		for (int i = 0; i < articles.size(); i++) {
+			Article curArticle = articles.get(i);
+
+			long curParentResourcePrimKey =
+				curArticle.getParentResourcePrimKey();
+
+			if (priority == i) {
+				curParentResourcePrimKey = parentResourcePrimKey;
+			}
+
+			List<Article> articles2 =
+				articlePersistence.findByResourcePrimKey(
+					curArticle.getResourcePrimKey());
+
+			for (Article curArticle2 : articles2) {
+				curArticle2.setParentResourcePrimKey(curParentResourcePrimKey);
+				curArticle2.setPriority(i);
+
+				articlePersistence.update(curArticle2, false);
+
+				if (article.getArticleId() == curArticle2.getArticleId()) {
+					article = curArticle2;
+				}
+			}
+		}
+
+		return article;
+	}
+
 	protected DynamicQuery getDynamicQuery(Map<String, Long> params) {
 		DynamicQuery subselectDynamicQuery = DynamicQueryFactoryUtil.forClass(
 			Article.class, "article2", PortletClassLoaderUtil.getClassLoader());
@@ -149,6 +290,18 @@ public class ArticleLocalServiceImpl extends ArticleLocalServiceBaseImpl {
 		}
 
 		return articles;
+	}
+
+	protected void validate(String title, String content)
+		throws PortalException {
+
+		if (Validator.isNull(title)) {
+			throw new ArticleTitleException();
+		}
+
+		if (Validator.isNull(content)) {
+			throw new ArticleContentException();
+		}
 	}
 
 }
