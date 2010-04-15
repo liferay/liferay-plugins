705a2ea86292913d0c11d94587b241c882cda2fa
Gregory Amerson <gregory.amerson@liferay.com>
2010-04-15 18:03:18 +0000

    LPS-9029  Added new hook plugin project template and also modified build scripts to add create shortcut support.
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@50358 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ hooks/create.bat
@@ -0,0 +1,24 @@
+@echo off
+
+if "" == "%1" goto errorCreate
+if "" == "%2" goto errorCreate
+
+call ant -Dhook.name=%1 -Dhook.display.name=%2 create
+
+rem call ant deploy
+
+goto end
+
+:errorCreate
+	echo.
+	echo Usage: create.bat hello-world "Hello World"
+	echo.
+	echo The first hello-world is your hook id. A new directory will be created based
+	echo on the hook id.
+	echo.
+	echo The second "Hello World" is the hook's display name. The quotation marks are
+	echo only needed because there is a space in the display name.
+
+	goto end
+
+:end
