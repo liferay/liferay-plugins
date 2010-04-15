8567f70d7b3f7cbbed52548666741f1f1e1a04b2
Brian Chan <brian.chan@liferay.com>
2010-04-15 19:01:11 +0000

    LPS-9029  Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@50365 05bdf26c-840f-0410-9ced-eb539d925f36

--- hooks/create.bat
+++ hooks/create.bat
@@ -13,8 +13,8 @@ goto end
 	echo.
 	echo Usage: create.bat hello-world "Hello World"
 	echo.
-	echo The first hello-world is your hook id. A new directory will be created based
-	echo on the hook id.
+	echo The first hello-world is your hook id. A new directory will be created based on
+	echo the hook id.
 	echo.
 	echo The second "Hello World" is the hook's display name. The quotation marks are
 	echo only needed because there is a space in the display name.
--- hooks/create.sh
+++ hooks/create.sh
@@ -6,8 +6,8 @@ if [ -z "$1" ]; then
 	echo
 	echo Usage: ./create.sh hello-world \"Hello World\"
 	echo
-	echo The first hello-world is your hook id. A new directory will be created based
-	echo on the hook id.
+	echo The first hello-world is your hook id. A new directory will be created based on
+	echo the hook id.
 	echo
 	echo The second \"Hello World\" is the hook\'s display name. The quotation marks are
 	echo only needed because there is a space in the display name.
