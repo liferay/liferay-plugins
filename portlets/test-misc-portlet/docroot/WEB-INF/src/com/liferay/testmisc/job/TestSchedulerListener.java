46aface7512914bafee4640f9d090091ee0a1d23
Minhchau Dang <minhchau.dang@liferay.com>
2010-04-15 23:31:59 +0000

    LPS-8841
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@50415 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/test-misc-portlet/docroot/WEB-INF/src/com/liferay/testmisc/job/TestSchedulerListener.java
+++ portlets/test-misc-portlet/docroot/WEB-INF/src/com/liferay/testmisc/job/TestSchedulerListener.java
@@ -14,37 +14,26 @@
 
 package com.liferay.testmisc.job;
 
-import com.liferay.portal.kernel.job.JobSchedulerUtil;
-import com.liferay.portal.kernel.job.Scheduler;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
 
 /**
- * <a href="TestScheduler.java.html"><b><i>View Source</i></b></a>
+ * <a href="TestIntervalJob.java.html"><b><i>View Source</i></b></a>
  *
  * @author Brian Wing Shun Chan
  *
  */
-public class TestScheduler implements Scheduler {
+public class TestSchedulerListener implements MessageListener {
 
-	public void schedule() {
+	public void receive(Message message) {
+
 		if (_log.isInfoEnabled()) {
-			_log.info("Schedule");
+			_log.info("Execute");
 		}
-
-		JobSchedulerUtil.schedule(_testIntervalJob);
 	}
 
-	public void unschedule() {
-		if (_log.isInfoEnabled()) {
-			_log.info("Unschedule");
-		}
-
-		JobSchedulerUtil.unschedule(_testIntervalJob);
-	}
-
-	private static Log _log = LogFactoryUtil.getLog(TestScheduler.class);
-
-	private TestIntervalJob _testIntervalJob = new TestIntervalJob();
+	private static Log _log = LogFactoryUtil.getLog(TestSchedulerListener.class);
 
 }
