ebd5fa8769a092876ed8014104c5ba952cdfd7e1
Brian Chan <brian.chan@liferay.com>
2010-04-23 10:09:09 +0000

    LPS-9217
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@50853 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/social-coding-portlet/docroot/WEB-INF/src/com/liferay/socialcoding/jira/messaging/SynchronizeJIRAMessageListener.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.socialcoding.jira.messaging;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.socialcoding.jira.util.JIRAConstants;
+import com.liferay.socialcoding.service.JIRAIssueLocalServiceUtil;
+
+/**
+ * <a href="SynchronizeJIRAMessageListener.java.html"><b><i>View Source</i></b>
+ * </a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class SynchronizeJIRAMessageListener implements MessageListener {
+
+	public void receive(Message message) {
+		try {
+			doReceive(message);
+		}
+		catch (Exception e) {
+			_log.error("Unable to process message " + message, e);
+		}
+	}
+
+	protected void doReceive(Message message) throws Exception {
+		JIRAIssueLocalServiceUtil.updateJIRAIssues(JIRAConstants.PROJECT_LEP);
+		JIRAIssueLocalServiceUtil.updateJIRAIssues(JIRAConstants.PROJECT_LPS);
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		SynchronizeJIRAMessageListener.class);
+
+}
