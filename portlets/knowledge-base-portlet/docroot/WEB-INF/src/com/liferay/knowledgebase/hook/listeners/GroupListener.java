707bfd76a5493d30c56b7f1d360e437e7e3b8bc5
Brian Chan <brian.chan@liferay.com>
2010-04-27 13:15:48 +0000

    KB make sure that deleting a community removes subscriptions to that community
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@51133 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/listeners/GroupListener.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.hook.listeners;
+
+import com.liferay.knowledgebase.model.Article;
+import com.liferay.portal.ModelListenerException;
+import com.liferay.portal.model.BaseModelListener;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.SubscriptionLocalServiceUtil;
+
+/**
+ * <a href="GroupListener.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class GroupListener extends BaseModelListener<Group> {
+
+	public void onBeforeRemove(Group group) throws ModelListenerException {
+		try {
+			SubscriptionLocalServiceUtil.deleteSubscriptions(
+				group.getCompanyId(), Article.class.getName(),
+				group.getGroupId());
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
+
+}
