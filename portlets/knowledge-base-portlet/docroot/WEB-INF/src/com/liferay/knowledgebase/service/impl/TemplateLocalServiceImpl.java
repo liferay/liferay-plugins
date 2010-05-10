4b5bb11dc9ab8683b5b38ec665ce00ca44777c15
Peter Shin <peter.shin@liferay.com>
2010-05-10 21:53:52 +0000

    KB template crud
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@52185 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/TemplateLocalServiceImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/TemplateLocalServiceImpl.java
@@ -14,7 +14,19 @@
 
 package com.liferay.knowledgebase.service.impl;
 
+import com.liferay.knowledgebase.TemplateContentException;
+import com.liferay.knowledgebase.TemplateTitleException;
+import com.liferay.knowledgebase.model.Template;
 import com.liferay.knowledgebase.service.base.TemplateLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * <a href="TemplateLocalServiceImpl.java.html"><b><i>View Source</i></b></a>
@@ -23,4 +35,108 @@ import com.liferay.knowledgebase.service.base.TemplateLocalServiceBaseImpl;
  * @author Brian Wing Shun Chan
  */
 public class TemplateLocalServiceImpl extends TemplateLocalServiceBaseImpl {
+
+	public Template addTemplate(
+			String uuid, long userId, String title, String content,
+			String description, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		// Template
+
+		User user = userPersistence.findByPrimaryKey(userId);
+		Date now = new Date();
+
+		validate(title, content);
+
+		long templateId = counterLocalService.increment();
+
+		Template template = templatePersistence.create(templateId);
+
+		template.setUuid(uuid);
+		template.setGroupId(serviceContext.getScopeGroupId());
+		template.setCompanyId(user.getCompanyId());
+		template.setUserId(user.getUserId());
+		template.setUserName(user.getFullName());
+		template.setCreateDate(serviceContext.getCreateDate(now));
+		template.setModifiedDate(serviceContext.getModifiedDate(now));
+		template.setTitle(title);
+		template.setContent(content);
+		template.setDescription(description);
+
+		templatePersistence.update(template, false);
+
+		return template;
+	}
+
+	public void deleteGroupTemplates(long groupId)
+		throws PortalException, SystemException {
+
+		List<Template> templates = templatePersistence.findByGroupId(groupId);
+
+		for (Template template : templates) {
+			deleteTemplate(template);
+		}
+	}
+
+	public void deleteTemplate(long templateId)
+		throws PortalException, SystemException {
+
+		Template template = templatePersistence.findByPrimaryKey(templateId);
+
+		deleteTemplate(template);
+	}
+
+	public void deleteTemplate(Template template) throws SystemException {
+
+		// Template
+
+		templatePersistence.remove(template);
+	}
+
+	public List<Template> getGroupTemplates(
+			long groupId, int start, int end,
+			OrderByComparator orderByComparator)
+		throws SystemException {
+
+		return templatePersistence.findByGroupId(
+			groupId, start, end, orderByComparator);
+	}
+
+	public int getGroupTemplatesCount(long groupId) throws SystemException {
+		return templatePersistence.countByGroupId(groupId);
+	}
+
+	public Template updateTemplate(
+			long templateId, String title, String content, String description,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		// Template
+
+		validate(title, content);
+
+		Template template = templatePersistence.findByPrimaryKey(templateId);
+
+		template.setModifiedDate(serviceContext.getModifiedDate(null));
+		template.setTitle(title);
+		template.setContent(content);
+		template.setDescription(description);
+
+		templatePersistence.update(template, false);
+
+		return template;
+	}
+
+	protected void validate(String title, String content)
+		throws PortalException {
+
+		if (Validator.isNull(title)) {
+			throw new TemplateTitleException();
+		}
+
+		if (Validator.isNull(content)) {
+			throw new TemplateContentException();
+		}
+	}
+
 }
