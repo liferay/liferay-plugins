3edf7818a7d44070a154d22b2051fd588fd8c927
Peter Shin <peter.shin@liferay.com>
2010-05-11 15:39:20 +0000

    KB template remote service
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@52301 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/TemplateServiceImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/TemplateServiceImpl.java
@@ -14,7 +14,19 @@
 
 package com.liferay.knowledgebase.service.impl;
 
+import com.liferay.knowledgebase.model.Template;
 import com.liferay.knowledgebase.service.base.TemplateServiceBaseImpl;
+import com.liferay.knowledgebase.service.permission.AdminPermission;
+import com.liferay.knowledgebase.service.permission.TemplatePermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * <a href="TemplateServiceImpl.java.html"><b><i>View Source</i></b></a>
@@ -23,4 +35,81 @@ import com.liferay.knowledgebase.service.base.TemplateServiceBaseImpl;
  * @author Brian Wing Shun Chan
  */
 public class TemplateServiceImpl extends TemplateServiceBaseImpl {
+
+	public Template addTemplate(
+			String title, String content, String description,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		AdminPermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			ActionKeys.ADD_TEMPLATE);
+
+		return templateLocalService.addTemplate(
+			null, getUserId(), title, content, description, serviceContext);
+	}
+
+	public void deleteTemplate(long templateId)
+		throws PortalException, SystemException {
+
+		TemplatePermission.check(
+			getPermissionChecker(), templateId, ActionKeys.DELETE);
+
+		templateLocalService.deleteTemplate(templateId);
+	}
+
+	public List<Template> getGroupTemplates(
+			long groupId, int start, int end,
+			OrderByComparator orderByComparator)
+		throws PortalException, SystemException {
+
+		List<Template> templates = templateLocalService.getGroupTemplates(
+			groupId, start, end, orderByComparator);
+
+		return filterTemplates(templates);
+	}
+
+	public int getGroupTemplatesCount(long groupId) throws SystemException {
+		return templateLocalService.getGroupTemplatesCount(groupId);
+	}
+
+	public Template getTemplate(long templateId)
+		throws PortalException, SystemException {
+
+		TemplatePermission.check(
+			getPermissionChecker(), templateId, ActionKeys.VIEW);
+
+		return templateLocalService.getTemplate(templateId);
+	}
+
+	public Template updateTemplate(
+			long templateId, String title, String content, String description,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		TemplatePermission.check(
+			getPermissionChecker(), templateId, ActionKeys.UPDATE);
+
+		return templateLocalService.updateTemplate(
+			templateId, title, content, description, serviceContext);
+	}
+
+	protected List<Template> filterTemplates(List<Template> templates)
+		throws PortalException {
+
+		templates = ListUtil.copy(templates);
+
+		Iterator<Template> itr = templates.iterator();
+
+		while (itr.hasNext()) {
+			if (!TemplatePermission.contains(
+					getPermissionChecker(), itr.next(), ActionKeys.VIEW)) {
+
+				itr.remove();
+			}
+		}
+
+		return templates;
+	}
+
 }
