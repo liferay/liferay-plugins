a2bcd567917436b8c40e699e4d2d3f9117ee4505
Brian Chan <brian.chan@liferay.com>
2010-05-17 17:15:06 +0000

    Regenerate
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@52908 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/http/ArticleJSONSerializer.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.service.http;
+
+import com.liferay.knowledgebase.model.Article;
+
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * <a href="ArticleJSONSerializer.java.html"><b><i>View Source</i></b></a>
+ *
+ * <p>
+ * ServiceBuilder generated this class. Modifications in this class will be
+ * overwritten the next time is generated.
+ * </p>
+ *
+ * <p>
+ * This class is used by {@link ArticleServiceJSON} to translate objects.
+ * </p>
+ *
+ * @author    Brian Wing Shun Chan
+ * @see       com.liferay.knowledgebase.service.http.ArticleServiceJSON
+ * @generated
+ */
+public class ArticleJSONSerializer {
+	public static JSONObject toJSONObject(Article model) {
+		JSONObject jsonObj = JSONFactoryUtil.createJSONObject();
+
+		jsonObj.put("uuid", model.getUuid());
+		jsonObj.put("articleId", model.getArticleId());
+		jsonObj.put("resourcePrimKey", model.getResourcePrimKey());
+		jsonObj.put("groupId", model.getGroupId());
+		jsonObj.put("companyId", model.getCompanyId());
+		jsonObj.put("userId", model.getUserId());
+		jsonObj.put("userName", model.getUserName());
+
+		Date createDate = model.getCreateDate();
+
+		String createDateJSON = StringPool.BLANK;
+
+		if (createDate != null) {
+			createDateJSON = String.valueOf(createDate.getTime());
+		}
+
+		jsonObj.put("createDate", createDateJSON);
+
+		Date modifiedDate = model.getModifiedDate();
+
+		String modifiedDateJSON = StringPool.BLANK;
+
+		if (modifiedDate != null) {
+			modifiedDateJSON = String.valueOf(modifiedDate.getTime());
+		}
+
+		jsonObj.put("modifiedDate", modifiedDateJSON);
+		jsonObj.put("parentResourcePrimKey", model.getParentResourcePrimKey());
+		jsonObj.put("version", model.getVersion());
+		jsonObj.put("title", model.getTitle());
+		jsonObj.put("content", model.getContent());
+		jsonObj.put("description", model.getDescription());
+		jsonObj.put("priority", model.getPriority());
+
+		return jsonObj;
+	}
+
+	public static JSONArray toJSONArray(
+		com.liferay.knowledgebase.model.Article[] models) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		for (Article model : models) {
+			jsonArray.put(toJSONObject(model));
+		}
+
+		return jsonArray;
+	}
+
+	public static JSONArray toJSONArray(
+		com.liferay.knowledgebase.model.Article[][] models) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		for (Article[] model : models) {
+			jsonArray.put(toJSONArray(model));
+		}
+
+		return jsonArray;
+	}
+
+	public static JSONArray toJSONArray(
+		List<com.liferay.knowledgebase.model.Article> models) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		for (Article model : models) {
+			jsonArray.put(toJSONObject(model));
+		}
+
+		return jsonArray;
+	}
+}
---
+++ portlets/knowledge-base-portlet/docroot/js/service.js
@@ -0,0 +1,34 @@
+Liferay.Service.register("Liferay.Service.KB", "com.liferay.knowledgebase.service");
+
+Liferay.Service.registerClass(
+	Liferay.Service.KB, "Article",
+	{
+		addArticle: true,
+		deleteArticle: true,
+		deleteAttachment: true,
+		getArticle: true,
+		getArticles: true,
+		getArticlesCount: true,
+		getCompanyArticles: true,
+		getCompanyArticlesCount: true,
+		getGroupArticles: true,
+		getGroupArticlesCount: true,
+		getLatestArticle: true,
+		subscribe: true,
+		unsubscribe: true,
+		updateArticle: true,
+		updateAttachments: true
+	}
+);
+
+Liferay.Service.registerClass(
+	Liferay.Service.KB, "Template",
+	{
+		addTemplate: true,
+		deleteTemplate: true,
+		getGroupTemplates: true,
+		getGroupTemplatesCount: true,
+		getTemplate: true,
+		updateTemplate: true
+	}
+);
