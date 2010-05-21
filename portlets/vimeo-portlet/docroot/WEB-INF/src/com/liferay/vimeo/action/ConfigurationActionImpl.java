e186db16ce9141ad29724172d1b395fea95c7f06
Brian Chan <brian.chan@liferay.com>
2010-05-21 22:48:05 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@53503 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/vimeo-portlet/docroot/WEB-INF/src/com/liferay/vimeo/action/ConfigurationActionImpl.java
+++ portlets/vimeo-portlet/docroot/WEB-INF/src/com/liferay/vimeo/action/ConfigurationActionImpl.java
@@ -31,7 +31,7 @@ import javax.portlet.RenderResponse;
  * <a href="ConfigurationActionImpl.java.html"><b><i>View Source</i></b></a>
  *
  * @author David Truong
- *
+ * @author Jonathan Neal
  */
 public class ConfigurationActionImpl implements ConfigurationAction {
 
