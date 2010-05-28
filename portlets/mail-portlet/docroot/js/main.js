f23ab5bcc6a96839edd97bc7545f4383bc3bc9ec
Ryan Park <ryan.park@liferay.com>
2010-05-28 00:02:02 +0000

    Mail Portlet, View folders
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@54040 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/mail-portlet/docroot/js/main.js
+++ portlets/mail-portlet/docroot/js/main.js
@@ -11,8 +11,22 @@ AUI().add(
 				instance.folderId = params.initialFolderId;
 
 				instance.accountsContainer = A.one('#accountsContainer');
+				instance.composeContainer = A.one('#composeContainer');
+				instance.controlContainer = A.one('#controlContainer');
+				instance.contentContainer = A.one('#contentContainer');
+				instance.foldersContainer = A.one('#foldersContainer');
+				instance.mailContainer = A.one('#mailContainer');
+				instance.manageFoldersContainer = A.one('#manageFoldersContainer');
+				instance.messageContainer = A.one('#messageContainer');
+				instance.messagesContainer = A.one('#messagesContainer');
 
 				instance._assignEvents();
+
+				if (instance.accountId == 0) {
+					return;
+				}
+
+				instance.loadFolders(instance.accountId);
 			},
 
 			addAccount: function () {
@@ -41,6 +55,49 @@ AUI().add(
 				instance.accountsContainer.io.start();
 			},
 
+			loadAccount: function(accountId, inboxFolderId) {
+				var instance = this;
+
+				instance.manageFoldersContainer.hide();
+				instance.messagesContainer.show();
+				instance.messageContainer.hide();
+				instance.composeContainer.hide();
+
+				A.io.request(
+					themeDisplay.getLayoutURL() + '/-/mail/password_saved',
+					{
+						data: {
+							accountId: accountId,
+							inboxFolderId: inboxFolderId
+						},
+						method: 'POST',
+						on: {
+							failure: function (event, id, obj) {
+								instance.setStatus('error', Liferay.Language.get('unable-to-connect-with-mail-server'));
+							},
+							success: function (event, id, obj) {
+								var results = this.get('responseData');
+
+								if (results == 'true') {
+									instance.loadFolders(accountId);
+								}
+								else {
+									instance.promptForPassword(accountId, inboxFolderId);
+								}
+							}
+						}
+					}
+				);
+			},
+
+			loadFolders: function(accountId) {
+				var instance = this;
+
+				instance.foldersContainer.io.set('data', {accountId: accountId});
+
+				instance.foldersContainer.io.start();
+			},
+
 			setStatus: function(type, message) {
 				var messageType = 'portlet-msg-error';
 
@@ -62,12 +119,33 @@ AUI().add(
 					{
 						uri: themeDisplay.getLayoutURL() + '/-/mail/view_accounts',
 						autoLoad: true,
-						data: {
-							accountId: instance.accountId
-						},
+						data: {accountId: instance.accountId},
 						showLoading: false,
 						method: 'POST'
 					}
+				);
+
+				instance.foldersContainer.plug(
+					A.Plugin.IO,
+					{
+						uri: themeDisplay.getLayoutURL() + '/-/mail/view_folders',
+						autoLoad: false,
+						method: 'POST'
+					}
+				);
+
+				instance.accountsContainer.delegate(
+					'click',
+					function(event) {
+						var link = event.currentTarget;
+
+						var accountId = link.getAttribute('data-accountId');
+						var inboxFolderId = link.getAttribute('data-inboxFolderId');
+
+						instance.loadAccounts(accountId);
+						instance.loadAccount(accountId, inboxFolderId);
+					},
+					'.folders-link'
 				);
 			},
 
