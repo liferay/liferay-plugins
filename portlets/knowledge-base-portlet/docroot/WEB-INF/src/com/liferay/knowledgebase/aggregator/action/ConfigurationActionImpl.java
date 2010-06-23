36e987165dc320754e7b8a8dd4e7ce6a85039489
Peter Shin <peter.shin@liferay.com>
2010-06-23 13:49:03 +0000

    KB config display settings
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@56191 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/aggregator/action/ConfigurationActionImpl.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.aggregator.action;
+
+import com.liferay.portal.kernel.portlet.BaseConfigurationAction;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletPreferences;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * <a href="ConfigurationActionImpl.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Peter Shin
+ * @author Brian Wing Shun Chan
+ */
+public class ConfigurationActionImpl extends BaseConfigurationAction {
+
+	public void processAction(
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		if (!cmd.equals(Constants.UPDATE)) {
+			return;
+		}
+
+		String portletResource = ParamUtil.getString(
+			actionRequest, "portletResource");
+
+		PortletPreferences preferences =
+			PortletPreferencesFactoryUtil.getPortletSetup(
+				actionRequest, portletResource);
+
+		String tabs2 = ParamUtil.getString(actionRequest, "tabs2");
+
+		if (tabs2.equals("display-settings")) {
+			updateDisplaySettings(actionRequest, preferences);
+		}
+
+		if (SessionErrors.isEmpty(actionRequest)) {
+			preferences.store();
+
+			SessionMessages.add(
+				actionRequest, portletConfig.getPortletName() + ".doConfigure");
+		}
+	}
+
+	public String render(
+			PortletConfig portletConfig, RenderRequest renderRequest,
+			RenderResponse renderResponse)
+		throws Exception {
+
+		return "/aggregator/configuration.jsp";
+	}
+
+	protected void updateDisplaySettings(
+			ActionRequest actionRequest, PortletPreferences preferences)
+		throws Exception {
+
+		String articlesTitle = ParamUtil.getString(
+			actionRequest, "articlesTitle");
+		int articlesDelta = ParamUtil.getInteger(
+			actionRequest, "articlesDelta");
+		String articlesDisplayStyle = ParamUtil.getString(
+			actionRequest, "articlesDisplayStyle");
+		String articleWindowState = ParamUtil.getString(
+			actionRequest, "articleWindowState");
+		String childArticlesDisplayStyle = ParamUtil.getString(
+			actionRequest, "childArticlesDisplayStyle");
+		boolean enableArticleComments = ParamUtil.getBoolean(
+			actionRequest, "enableArticleComments");
+		boolean enableArticleCommentRatings = ParamUtil.getBoolean(
+			actionRequest, "enableArticleCommentRatings");
+
+		preferences.setValue("articles-title", articlesTitle);
+		preferences.setValue("articles-delta", String.valueOf(articlesDelta));
+		preferences.setValue("articles-display-style", articlesDisplayStyle);
+		preferences.setValue("article-window-state", articleWindowState);
+		preferences.setValue(
+			"child-articles-display-style", childArticlesDisplayStyle);
+		preferences.setValue(
+			"enable-article-comments", String.valueOf(enableArticleComments));
+		preferences.setValue(
+			"enable-article-comment-ratings",
+			String.valueOf(enableArticleCommentRatings));
+	}
+
+}
