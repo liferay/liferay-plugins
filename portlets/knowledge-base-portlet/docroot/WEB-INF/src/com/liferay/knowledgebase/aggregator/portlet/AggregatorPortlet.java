541a24ebd4b24b6f3e3082711e7e054e3826f934
Peter Shin <peter.shin@liferay.com>
2010-06-23 14:02:48 +0000

    KB mvc aggregator
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@56193 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/aggregator/portlet/AggregatorPortlet.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/aggregator/portlet/AggregatorPortlet.java
@@ -14,7 +14,29 @@
 
 package com.liferay.knowledgebase.aggregator.portlet;
 
+import com.liferay.documentlibrary.service.DLLocalServiceUtil;
+import com.liferay.knowledgebase.NoSuchArticleException;
+import com.liferay.knowledgebase.model.Article;
+import com.liferay.knowledgebase.service.ArticleServiceUtil;
+import com.liferay.knowledgebase.util.WebKeys;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.model.CompanyConstants;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portlet.messageboards.NoSuchMessageException;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+import com.liferay.util.servlet.PortletResponseUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
 /**
  * <a href="AggregatorPortlet.java.html"><b><i>View Source</i></b></a>
@@ -23,4 +45,101 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
  * @author Brian Wing Shun Chan
  */
 public class AggregatorPortlet extends MVCPortlet {
+
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException, IOException {
+
+		try {
+			Article article = null;
+
+			long resourcePrimKey = ParamUtil.getLong(
+				renderRequest, "resourcePrimKey");
+
+			if (resourcePrimKey > 0) {
+				article = ArticleServiceUtil.getLatestArticle(resourcePrimKey);
+			}
+
+			renderRequest.setAttribute(WebKeys.KNOWLEDGE_BASE_ARTICLE, article);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchArticleException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass().getName());
+			}
+			else {
+				throw new PortletException(e);
+			}
+		}
+
+		super.render(renderRequest, renderResponse);
+	}
+
+	public void serveAttachment(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		long companyId = ParamUtil.getLong(resourceRequest, "companyId");
+		String fileName = ParamUtil.getString(resourceRequest, "fileName");
+
+		String shortFileName = FileUtil.getShortFileName(fileName);
+		InputStream is = DLLocalServiceUtil.getFileAsStream(
+			companyId, CompanyConstants.SYSTEM, fileName);
+		String contentType = MimeTypesUtil.getContentType(fileName);
+
+		PortletResponseUtil.sendFile(
+			resourceRequest, resourceResponse, shortFileName, is, contentType);
+	}
+
+	public void serveResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws IOException, PortletException {
+
+		try {
+			String resourceID = resourceRequest.getResourceID();
+
+			if (resourceID.equals("attachment")) {
+				serveAttachment(resourceRequest, resourceResponse);
+			}
+		}
+		catch (IOException ioe) {
+			throw ioe;
+		}
+		catch (PortletException pe) {
+			throw pe;
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
+	}
+
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if (SessionErrors.contains(
+				renderRequest, NoSuchArticleException.class.getName()) ||
+			SessionErrors.contains(
+				renderRequest, NoSuchMessageException.class.getName()) ||
+			SessionErrors.contains(
+				renderRequest, PrincipalException.class.getName())) {
+
+			include("/aggregator/error.jsp", renderRequest, renderResponse);
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
+	}
+
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof NoSuchArticleException ||
+			cause instanceof PrincipalException) {
+
+			return true;
+		}
+
+		return false;
+	}
+
 }
