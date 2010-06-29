475549edd4887487784f9f28090e2cf30b28d874
Peter Shin <peter.shin@liferay.com>
2010-06-29 03:35:49 +0000

    KB search mvc
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@56739 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/search/portlet/SearchPortlet.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.search.portlet;
+
+import com.liferay.knowledgebase.aggregator.portlet.AggregatorPortlet;
+
+import javax.portlet.ResourceRequest;
+
+/**
+ * <a href="SearchPortlet.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Peter Shin
+ * @author Brian Wing Shun Chan
+ */
+public class SearchPortlet extends AggregatorPortlet {
+
+	protected String getJspPath() {
+		return _JSP_PATH;
+	}
+
+	protected boolean isServeRSSMaximized(ResourceRequest resourceRequest) {
+		return _SERVE_RSS_MAXIMIZED;
+	}
+
+	private static final String _JSP_PATH = "/search/";
+
+	private static final boolean _SERVE_RSS_MAXIMIZED = true;
+
+}
