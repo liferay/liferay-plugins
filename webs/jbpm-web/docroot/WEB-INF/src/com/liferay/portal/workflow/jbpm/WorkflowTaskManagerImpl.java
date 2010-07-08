40e3b49efde395dd6c8c111c9dc7c0b8514df13f
Marcellus Tavares <marcellus.tavares@liferay.com>
2010-07-08 13:30:52 +0000

    Jbpm Fix compilation error
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@57742 05bdf26c-840f-0410-9ced-eb539d925f36

--- webs/jbpm-web/docroot/WEB-INF/src/com/liferay/portal/workflow/jbpm/WorkflowTaskManagerImpl.java
+++ webs/jbpm-web/docroot/WEB-INF/src/com/liferay/portal/workflow/jbpm/WorkflowTaskManagerImpl.java
@@ -266,23 +266,23 @@ public class WorkflowTaskManagerImpl implements WorkflowTaskManager {
 
 			CustomSession customSession = new CustomSession(jbpmContext);
 
-			for (TaskInstance taskInstance : taskInstances) {
+			for (TaskInstance taskInstance_ : taskInstances) {
 				TaskInstanceExtensionImpl taskInstanceExtensionImpl =
 					customSession.findTaskInstanceExtension(
-						taskInstance.getId());
+						taskInstance_.getId());
 
 				if (taskInstanceExtensionImpl == null) {
 					List<Assignee> assignees =
 						AssigneeRetrievalUtil.getAssignees(
-							companyId, groupId, taskInstance.getActorId(),
-							taskInstance.getPooledActors());
+							companyId, groupId, taskInstance_.getActorId(),
+							taskInstance_.getPooledActors());
 
 					String workflowContextJSON =
 						WorkflowContextUtil.convertToJSON(workflowContext);
 
 					taskInstanceExtensionImpl = new TaskInstanceExtensionImpl(
 						companyId, groupId, userId, assignees,
-						workflowContextJSON, taskInstance);
+						workflowContextJSON, taskInstance_);
 
 					session.save(taskInstanceExtensionImpl);
 				}
