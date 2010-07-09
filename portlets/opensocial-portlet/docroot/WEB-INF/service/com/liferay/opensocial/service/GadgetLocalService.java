39617da8a34e6308532186861d23362971acc770
Michael Han <michael.han@liferay.com>
2010-07-09 03:59:21 +0000

    regen services
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@57849 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/opensocial-portlet/docroot/WEB-INF/service/com/liferay/opensocial/service/GadgetLocalService.java
+++ portlets/opensocial-portlet/docroot/WEB-INF/service/com/liferay/opensocial/service/GadgetLocalService.java
@@ -31,7 +31,7 @@ import com.liferay.portal.kernel.exception.SystemException;
  * <p>
  * This interface defines the service. The default implementation is
  * {@link
- * com.liferay.opensocial.service.impl.GadgetLocalServiceImpl}}.
+ * com.liferay.opensocial.service.impl.GadgetLocalServiceImpl}.
  * Modify methods in that class and rerun ServiceBuilder to populate this class
  * and all other generated classes.
  * </p>
