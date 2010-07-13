a9ffb8ed27337fe768016eb047ec5cf13dff8907
Brian Chan <brian.chan@liferay.com>
2010-07-13 18:11:27 +0000

    Sample wrapper hook
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@58146 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ hooks/sample-wrapper-hook/docroot/WEB-INF/src/com/liferay/samplewrapper/hook/model/SampleUserImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.samplewrapper.hook.model;
+
+import com.liferay.portal.model.User;
+import com.liferay.portal.model.UserWrapper;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class SampleUserImpl extends UserWrapper {
+
+	public SampleUserImpl(User user) {
+		super(user);
+	}
+
+	public String getFavoriteColor() {
+		return "Green";
+	}
+
+	public String getFirstName() {
+		System.out.println("## Called getFirstName()");
+
+		return super.getFirstName();
+	}
+
+}
---
+++ hooks/sample-wrapper-hook/docroot/WEB-INF/src/com/liferay/samplewrapper/hook/service/SampleUserLocalServiceImpl.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.samplewrapper.hook.service;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalService;
+import com.liferay.portal.service.UserLocalServiceWrapper;
+import com.liferay.samplewrapper.hook.model.SampleUserImpl;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class SampleUserLocalServiceImpl extends UserLocalServiceWrapper {
+
+	public SampleUserLocalServiceImpl(UserLocalService userLocalService) {
+		super(userLocalService);
+	}
+
+	public User getUserById(long userId)
+		throws PortalException, SystemException {
+
+		User user = super.getUserById(userId);
+
+		return new SampleUserImpl(user);
+	}
+
+}
