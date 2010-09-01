c2d6e5c31f68d3ce08e3967db6ecb9245597480d
Scott Lee <scott.lee@liferay.com>
2010-09-01 22:24:41 +0000

    LPS-12211 PM - util class
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@61697 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/private-messaging-portlet/docroot/WEB-INF/src/com/liferay/privatemessaging/util/PrivateMessagingUtil.java
@@ -0,0 +1,191 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.privatemessaging.util;
+
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
+import com.liferay.portlet.messageboards.util.comparator.MessageCreateDateComparator;
+import com.liferay.privatemessaging.NoSuchUserThreadException;
+import com.liferay.privatemessaging.model.UserThread;
+import com.liferay.privatemessaging.service.UserThreadLocalServiceUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Scott Lee
+ */
+public class PrivateMessagingUtil {
+
+	public static MBMessage getThreadLastMessage(long userId, long mbThreadId)
+		throws PortalException, SystemException {
+
+		List<MBMessage> mbMessages = getThreadMessages(
+			userId, mbThreadId, QueryUtil.ALL_POS, QueryUtil.ALL_POS, false);
+
+		MBMessage lastMBMessage = mbMessages.get(0);
+
+		return lastMBMessage;
+	}
+
+	public static List<MBMessage> getThreadMessages(
+			long userId, long mbThreadId, int start, int end, boolean ascending)
+		throws PortalException, SystemException {
+
+		UserThread userThread = UserThreadLocalServiceUtil.getUserThread(
+			userId, mbThreadId);
+
+		MBMessage topMbMessage = MBMessageLocalServiceUtil.getMBMessage(
+			userThread.getTopMBMessageId());
+
+		List<MBMessage> mbMessages =
+			MBMessageLocalServiceUtil.getThreadMessages(
+				mbThreadId, WorkflowConstants.STATUS_ANY,
+				new MessageCreateDateComparator(ascending));
+
+		List<MBMessage> filteredMBMessages = new ArrayList<MBMessage>();
+
+		for (MBMessage mbMessage : mbMessages) {
+			if (DateUtil.compareTo(
+				topMbMessage.getCreateDate(), mbMessage.getCreateDate()) <= 0) {
+
+				filteredMBMessages.add(mbMessage);
+			}
+			else {
+				continue;
+			}
+		}
+
+		if (filteredMBMessages.isEmpty()) {
+			return filteredMBMessages;
+		}
+		else if ((start == QueryUtil.ALL_POS) || (end == QueryUtil.ALL_POS)) {
+			return filteredMBMessages;
+		}
+		else if (end > filteredMBMessages.size()) {
+			end = filteredMBMessages.size();
+		}
+
+		return filteredMBMessages.subList(start, end);
+	}
+
+	public static int getThreadMessageCount(
+			long userId, long mbThreadId)
+		throws PortalException, SystemException {
+
+		List<MBMessage> mbMessages = getThreadMessages(
+			userId, mbThreadId, QueryUtil.ALL_POS, QueryUtil.ALL_POS, true);
+
+		return mbMessages.size();
+	}
+
+	public static long getThreadRepresentative(long userId, long mbThreadId)
+		throws PortalException, SystemException {
+
+		List<MBMessage> mbMessages =
+			MBMessageLocalServiceUtil.getThreadMessages(
+				mbThreadId, WorkflowConstants.STATUS_ANY,
+				new MessageCreateDateComparator(false));
+
+		for (MBMessage mbMessage : mbMessages) {
+			if (userId != mbMessage.getUserId()) {
+				return mbMessage.getUserId();
+			}
+		}
+
+		return userId;
+	}
+
+	public static List<User> getThreadUsers(
+			long userId, long mbThreadId, boolean all)
+		throws PortalException, SystemException {
+
+		List<User> users = new ArrayList<User>();
+
+		if (all) {
+
+			// Users who can view the thread
+
+			List<UserThread> userThreads =
+				UserThreadLocalServiceUtil.getMBThreadUserThreads(mbThreadId);
+
+			for (UserThread userThread : userThreads) {
+				if (userId == userThread.getUserId()) {
+					continue;
+				}
+
+				User user = UserLocalServiceUtil.getUser(
+					userThread.getUserId());
+
+				if (!users.contains(user)) {
+					users.add(user);
+				}
+			}
+		}
+		else {
+
+			// Users who have contributed to the thread
+
+			List<MBMessage> mbMessages = getThreadMessages(
+				userId, mbThreadId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				false);
+
+			for (MBMessage mbMessage : mbMessages) {
+				if (userId == mbMessage.getUserId()) {
+					continue;
+				}
+
+				User user = UserLocalServiceUtil.getUser(mbMessage.getUserId());
+
+				if (!users.contains(user)) {
+					users.add(user);
+				}
+			}
+		}
+
+		return users;
+	}
+
+	public static String getThreadSubject(long mbThreadId)
+		throws PortalException, SystemException {
+
+		List<MBMessage> mbMessages =
+			MBMessageLocalServiceUtil.getThreadMessages(
+				mbThreadId, WorkflowConstants.STATUS_ANY, 0, 1);
+
+		return mbMessages.get(0).getSubject();
+	}
+
+	public static boolean isUserPartOfThread(long userId, long mbThreadId)
+		throws PortalException, SystemException {
+
+		try {
+			UserThreadLocalServiceUtil.getUserThread(userId, mbThreadId);
+
+			return true;
+		}
+		catch (NoSuchUserThreadException msute) {
+			return false;
+		}
+	}
+
+}
