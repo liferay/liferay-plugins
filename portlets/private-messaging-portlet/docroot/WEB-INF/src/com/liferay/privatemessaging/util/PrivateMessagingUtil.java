f9eed67b99db3198119d1dcea3cab6467b306ef4
Scott Lee <scott.lee@liferay.com>
2010-09-01 22:45:06 +0000

    LPS-12211 PM Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@61705 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/private-messaging-portlet/docroot/WEB-INF/src/com/liferay/privatemessaging/util/PrivateMessagingUtil.java
+++ portlets/private-messaging-portlet/docroot/WEB-INF/src/com/liferay/privatemessaging/util/PrivateMessagingUtil.java
@@ -17,7 +17,6 @@ package com.liferay.privatemessaging.util;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.UserLocalServiceUtil;
