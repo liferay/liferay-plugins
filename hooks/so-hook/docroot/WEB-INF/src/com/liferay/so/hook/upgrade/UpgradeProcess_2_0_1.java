cd322a97e7d2bffd7320e0c67a7a255ae144c790
Ryan Park <ryan.park@liferay.com>
2010-10-01 22:33:56 +0000

    SOS-472
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@63485 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/upgrade/UpgradeProcess_2_0_1.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2008-2010 Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.so.hook.upgrade;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.so.hook.upgrade.v2_0_1.UpgradeLayout;
+
+/**
+ * @author Ryan Park
+ */
+public class UpgradeProcess_2_0_1 extends UpgradeProcess {
+
+	public int getThreshold() {
+		return 201;
+	}
+
+	protected void doUpgrade() throws Exception {
+		if (UpgradeUtil.isFirstRun()) {
+			return;
+		}
+
+		upgrade(UpgradeLayout.class);
+	}
+
+}
---
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/upgrade/v2_0_1/UpgradeLayout.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2008-2010 Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.so.hook.upgrade.v2_0_1;
+
+import com.liferay.portal.NoSuchLayoutException;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+
+import java.util.List;
+
+/**
+ * @author Ryan Park
+ */
+public class UpgradeLayout extends UpgradeProcess {
+
+	protected void doUpgrade() throws Exception {
+		List<Group> groups = GroupLocalServiceUtil.getGroups(
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		for (Group group : groups) {
+			if (!group.isCommunity() ||
+				group.getName().equals(GroupConstants.CONTROL_PANEL) ||
+				group.getName().equals(GroupConstants.GUEST)) {
+
+				continue;
+			}
+
+			long userId = PortalUtil.getValidUserId(
+				group.getCompanyId(), group.getCreatorUserId());
+
+			Layout layout = null;
+
+			try {
+				layout = LayoutLocalServiceUtil.getFriendlyURLLayout(
+					group.getGroupId(), group.hasPrivateLayouts(),
+					"/documents");
+			}
+			catch (NoSuchLayoutException nsle) {
+				continue;
+			}
+
+			LayoutLocalServiceUtil.updateFriendlyURL(
+				layout.getPlid(), "/document");
+		}
+	}
+
+}
