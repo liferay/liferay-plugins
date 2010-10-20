4022ed13bde74c86093a42ae5241b93003828d91
Brian Chan <brian.chan@liferay.com>
2010-10-20 01:57:10 +0000

    LPS-13266 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@64483 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/util/UUIDUtil.java
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/util/UUIDUtil.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
@@ -21,21 +21,17 @@ import com.liferay.portal.kernel.util.StringUtil;
  * @author Michael C. Han
  */
 public class UUIDUtil {
-	public static String fromSafeUuid(String safeUuid) {
-		String uuid =
-			StringUtil.replace(safeUuid, _UNDERSCORE, _DASH);
 
-		return uuid;
+	public static String fromSafeUuid(String safeUuid) {
+		return StringUtil.replace(
+			safeUuid, _DOUBLE_UNDERSCORE, StringPool.DASH);
 	}
 
 	public static String toSafeUuid(String uuid) {
-		String safeName =
-			StringUtil.replace(uuid, _DASH, _UNDERSCORE);
-
-		return safeName;
+		return StringUtil.replace(uuid, StringPool.DASH, _DOUBLE_UNDERSCORE);
 	}
 
-	private static final String[] _DASH = new String[] {StringPool.DASH};
-	private static final String[] _UNDERSCORE =
-		new String[] {StringPool.UNDERLINE + StringPool.UNDERLINE};
+	private static final String _DOUBLE_UNDERSCORE =
+		StringPool.UNDERLINE.concat(StringPool.UNDERLINE);
+
 }
