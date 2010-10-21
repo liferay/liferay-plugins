49ef770a1e1b55398ae97691f9b043f42a0c3c14
Brian Chan <brian.chan@liferay.com>
2010-10-21 02:01:58 +0000

    LPS-12394 Remove unused indexes
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@64565 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/twitter-portlet/docroot/WEB-INF/sql/indexes.sql
+++ portlets/twitter-portlet/docroot/WEB-INF/sql/indexes.sql
@@ -1,4 +1,2 @@
 create index IX_59ECA762 on Twitter_Feed (companyId, twitterScreenName);
 create index IX_F5C00151 on Twitter_Feed (companyId, twitterUserId);
-create index IX_16979FAA on Twitter_Feed (twitterScreenName);
-create index IX_F025D99 on Twitter_Feed (twitterUserId);
