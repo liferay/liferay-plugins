29953cbc60bba1976571f4e754bcab89b5eacb77
Fernando Garcez <fernando.garcez@liferay.com>
2010-11-23 21:08:33 +0000

    LPS-12418
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@67071 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/messaging/AdminMessageListener.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/messaging/AdminMessageListener.java
@@ -210,7 +210,9 @@ public class AdminMessageListener extends BaseMessageListener {
 				continue;
 			}
 
-			if (!GroupLocalServiceUtil.hasUserGroup(userId, groupId)) {
+			if (!GroupLocalServiceUtil.hasUserGroup(
+					subscribedUserId, groupId)) {
+
 				if (_log.isInfoEnabled()) {
 					_log.info(
 						"Subscription " + subscription.getSubscriptionId() +
