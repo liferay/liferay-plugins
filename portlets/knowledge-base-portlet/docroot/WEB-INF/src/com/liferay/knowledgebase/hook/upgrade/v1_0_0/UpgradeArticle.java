25666b25cd4412681c33384394ebe6646dda51aa
Peter Shin <peter.shin@liferay.com>
2010-12-27 17:15:20 +0000

    Knowledge Base - enable upgrade process
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@68985 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/upgrade/v1_0_0/UpgradeArticle.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/upgrade/v1_0_0/UpgradeArticle.java
@@ -14,7 +14,13 @@
 
 package com.liferay.knowledgebase.hook.upgrade.v1_0_0;
 
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 /**
  * @author Peter Shin
@@ -22,6 +28,43 @@ import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 public class UpgradeArticle extends UpgradeProcess {
 
 	protected void doUpgrade() throws Exception {
+		if (hasColumn("status")) {
+			return;
+		}
+
+		upgradeArticles();
+	}
+
+	protected boolean hasColumn(String columnName) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement("select * from KB_Article");
+
+			rs = ps.executeQuery();
+
+			ResultSetMetaData rsmd = rs.getMetaData();
+
+			for (int i = 0; i < rsmd.getColumnCount(); i++) {
+				String curColumnName = rsmd.getColumnName(i + 1);
+
+				if (curColumnName.equals(columnName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+
+		return false;
+	}
+
+	protected void upgradeArticles() throws Exception {
 		runSQL("alter table KB_Article add status INTEGER");
 		runSQL("alter table KB_Article add statusByUserId LONG");
 		runSQL("alter table KB_Article add statusByUserName VARCHAR(75)");
