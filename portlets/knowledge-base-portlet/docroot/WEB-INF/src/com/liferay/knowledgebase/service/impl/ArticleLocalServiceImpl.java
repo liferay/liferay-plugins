63e620078293e1e595b73dc04e8b10cc0422ea87
Brian Chan <brian.chan@liferay.com>
2010-12-30 18:43:47 +0000

    LPS-14517 - source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@69242 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/ArticleLocalServiceImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/ArticleLocalServiceImpl.java
@@ -42,7 +42,6 @@ import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -1196,17 +1195,6 @@ public class ArticleLocalServiceImpl extends ArticleLocalServiceBaseImpl {
 				serviceContext.getPortalURL()
 			});
 
-		StringBundler sb = new StringBundler(6);
-
-		sb.append(StringPool.LESS_THAN);
-		sb.append("knowledge_base.article.");
-		sb.append(article.getResourcePrimKey());
-		sb.append(StringPool.AT);
-		sb.append(company.getMx());
-		sb.append(StringPool.GREATER_THAN);
-
-		String mailId = sb.toString();
-
 		SubscriptionSender subscriptionSender = new AdminSubscriptionSender(
 			article, serviceContext.getPortalURL());
 
@@ -1215,7 +1203,10 @@ public class ArticleLocalServiceImpl extends ArticleLocalServiceBaseImpl {
 		subscriptionSender.setFrom(fromAddress, fromName);
 		subscriptionSender.setGroupId(article.getGroupId());
 		subscriptionSender.setHtmlFormat(true);
-		subscriptionSender.setMailId(mailId);
+		subscriptionSender.setMailId(
+			StringPool.LESS_THAN + "knowledge_base.article." +
+				article.getResourcePrimKey() + StringPool.AT +
+					company.getMx() + StringPool.GREATER_THAN);
 		subscriptionSender.setReplyToAddress(fromAddress);
 		subscriptionSender.setSubject(subject);
 		subscriptionSender.setUserId(article.getUserId());
