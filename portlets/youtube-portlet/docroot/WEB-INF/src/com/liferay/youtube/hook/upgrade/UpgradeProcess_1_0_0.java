f0182aafd1c96b82a47d3aad87096aae36e9e064
Brian Chan <brian.chan@liferay.com>
2010-12-31 19:15:31 +0000

    LPS-14483
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@69302 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/youtube-portlet/docroot/WEB-INF/src/com/liferay/youtube/hook/upgrade/UpgradeProcess_1_0_0.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.youtube.hook.upgrade;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.youtube.hook.upgrade.v1_0_0.UpgradePortletPreferences;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class UpgradeProcess_1_0_0 extends UpgradeProcess {
+
+	public int getThreshold() {
+		return 100;
+	}
+
+	protected void doUpgrade() throws Exception {
+		upgrade(UpgradePortletPreferences.class);
+	}
+
+}
