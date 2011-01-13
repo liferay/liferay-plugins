9f714fba651d2ee6d9aa36b1560b1c0e2c9f0086
Brian Chan <brian.chan@liferay.com>
2011-01-13 23:41:13 +0000

    LPS-14730 Rename
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@70394 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/servlet/filters/WSRPHTTPSenderFilter.java
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/servlet/filters/WSRPHTTPSenderFilter.java
@@ -29,7 +29,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * @author Michael Young
  */
-public class WSRPFilter implements Filter {
+public class WSRPHTTPSenderFilter implements Filter {
 
 	public void destroy() {
 	}
