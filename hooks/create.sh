56cec5f6bc5e3a299b290a8485ed7be62b02f35c
Shinn Lok <shinn.lok@liferay.com>
2011-02-14 22:01:26 +0000

    LPS-15284 Line-break convert recommit
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@72953 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ hooks/create.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#set -x
+
+if [ -z "$1" ]; then
+	echo
+	echo Usage: ./create.sh hello-world \"Hello World\"
+	echo
+	echo The first hello-world is your hook id. A new directory will be created based on
+	echo the hook id.
+	echo
+	echo The second \"Hello World\" is the hook\'s display name. The quotation marks are
+	echo only needed because there is a space in the display name.
+
+	exit 127
+fi
+
+ant -Dhook.name=$1 -Dhook.display.name=\"$2\" create
+
+#ant deploy
+
+exit 0
