e51f9d7e5f7bb1c5af55e17c264afb1d4fb0dd5f
Gregory Amerson <gregory.amerson@liferay.com>
2011-03-05 05:05:55 +0000

    LPS-15671 Fixed compilation error in new vaading project Application.java file
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@74608 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/portlet_vaadin_tmpl/docroot/WEB-INF/src/Application.java
+++ tools/portlet_vaadin_tmpl/docroot/WEB-INF/src/Application.java
@@ -1,5 +1,10 @@
 package @portlet.java.package.name@;
 
+import com.vaadin.Application;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
 public class @portlet.java.class.name@Application extends Application {
 
 	public void init() {
