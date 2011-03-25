384b8cab3dcd8a666d8a9bd0c5474741388a7c4f
Peter Shin <peter.shin@liferay.com>
2011-03-25 17:05:59 +0000

    LPS-15988 - adding expandos and fixing logic for add resource
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@76179 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/custom/CalendarEventCustomAttributesDisplay.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.custom;
+
+import com.liferay.calendar.model.CalendarEvent;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.expando.model.BaseCustomAttributesDisplay;
+
+/**
+ * @author Eduardo Lundgren
+ * @author Michael C. Han
+ * @author Peter Shin
+ */
+public class CalendarEventCustomAttributesDisplay
+	extends BaseCustomAttributesDisplay {
+
+	public static final String CLASS_NAME = CalendarEvent.class.getName();
+
+	public String getClassName() {
+		return CLASS_NAME;
+	}
+
+	public String getIconPath(ThemeDisplay themeDisplay) {
+		return themeDisplay.getPathThemeImages() + "/common/date.png";
+	}
+
+}
