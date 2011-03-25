1ec29a14474679965e797f2553ae3ebc19e2c25c
Brian Chan <brian.chan@liferay.com>
2011-03-25 07:33:02 +0000

    LPS-15988 Extend AND reuse WorkflowConstants, so that changes to WorkflowConstants propagate to plugins
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@76155 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/workflow/CalendarBookingWorkflowConstants.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/workflow/CalendarBookingWorkflowConstants.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
@@ -26,54 +26,26 @@ public class CalendarBookingWorkflowConstants extends WorkflowConstants {
 	public static final String LABEL_DECLINED = "declined";
 
 	public static String toLabel(int status) {
-		if (status == STATUS_ANY) {
-			return LABEL_ANY;
-		}
-		else if (status == STATUS_APPROVED) {
+		if (status == STATUS_APPROVED) {
 			return LABEL_ACCEPTED;
 		}
 		else if (status == STATUS_DENIED) {
 			return LABEL_DECLINED;
 		}
-		else if (status == STATUS_DRAFT) {
-			return LABEL_DRAFT;
-		}
-		else if (status == STATUS_EXPIRED) {
-			return LABEL_EXPIRED;
-		}
-		else if (status == STATUS_PENDING) {
-			return LABEL_PENDING;
-		}
 		else {
-			return LABEL_ANY;
+			return WorkflowConstants.toLabel(status);
 		}
 	}
 
 	public static int toStatus(String label) {
-		if (label.equals(LABEL_ANY)) {
-			return STATUS_ANY;
-		}
-		else if (label.equals(LABEL_ACCEPTED) ||
-				 label.equals(LABEL_APPROVED)) {
-
+		if (label.equals(LABEL_ACCEPTED)) {
 			return STATUS_APPROVED;
 		}
-		else if (label.equals(LABEL_DECLINED) ||
-				 label.equals(LABEL_DENIED)) {
-
+		else if (label.equals(LABEL_DECLINED)) {
 			return STATUS_DENIED;
 		}
-		else if (label.equals(LABEL_DRAFT)) {
-			return STATUS_DRAFT;
-		}
-		else if (label.equals(LABEL_EXPIRED)) {
-			return STATUS_EXPIRED;
-		}
-		else if (label.equals(LABEL_PENDING)) {
-			return STATUS_PENDING;
-		}
 		else {
-			return STATUS_ANY;
+			return WorkflowConstants.toStatus(label);
 		}
 	}
 
