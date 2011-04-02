055333e6644daaf90f63ef3fb693eddc588edb4a
Brian Chan <brian.chan@liferay.com>
2011-04-02 09:12:16 +0000

    SOS-523 Rollback, because SVN copy in this case hides what was changed since it was copied from files that were not yet approved
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@77058 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/microblogs-portlet/docroot/WEB-INF/src/com/liferay/microblogs/service/impl/MicroblogsEntryLocalServiceImpl.java
+++ portlets/microblogs-portlet/docroot/WEB-INF/src/com/liferay/microblogs/service/impl/MicroblogsEntryLocalServiceImpl.java
@@ -14,7 +14,6 @@
 
 package com.liferay.microblogs.service.impl;
 
-import com.liferay.microblogs.UnsupportedMicroblogsEntryException;
 import com.liferay.microblogs.model.MicroblogsEntry;
 import com.liferay.microblogs.model.MicroblogsEntryConstants;
 import com.liferay.microblogs.service.base.MicroblogsEntryLocalServiceBaseImpl;
@@ -51,8 +50,6 @@ public class MicroblogsEntryLocalServiceImpl
 		}
 
 		Date now = new Date();
-
-		validate(type, receiverMicroblogsEntryId, socialRelationType);
 
 		long microblogsEntryId = counterLocalService.increment();
 
@@ -251,27 +248,6 @@ public class MicroblogsEntryLocalServiceImpl
 			serviceContext.getAssetTagNames());
 
 		return microblogsEntry;
-	}
-
-	protected void validate(
-			int type, long receiverMicroblogsEntryId, int socialRelationType)
-		throws PortalException, SystemException {
-
-		MicroblogsEntry microblogsEntry =
-			microblogsEntryPersistence.findByPrimaryKey(
-				receiverMicroblogsEntryId);
-
-		if (microblogsEntry.getSocialRelationType() ==
-				MicroblogsEntryConstants.TYPE_EVERYONE) {
-
-			return;
-		}
-
-		if ((type == MicroblogsEntryConstants.TYPE_REPLY) ||
-			(type == MicroblogsEntryConstants.TYPE_REPOST)) {
-
-			throw new UnsupportedMicroblogsEntryException();
-		}
 	}
 
 }
