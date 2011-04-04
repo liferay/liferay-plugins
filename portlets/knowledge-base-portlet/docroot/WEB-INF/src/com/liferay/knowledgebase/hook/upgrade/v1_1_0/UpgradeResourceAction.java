010437492bbec92915a3d1d4571a5811685c75b2
Brian Chan <brian.chan@liferay.com>
2011-04-04 08:25:25 +0000

    LPS-16264 Will this simplification work? I usually only put it into a sb.append if it's going to be more than 3 lines (when it comes to upgrades)
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@77107 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/upgrade/v1_1_0/UpgradeResourceAction.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/upgrade/v1_1_0/UpgradeResourceAction.java
@@ -14,15 +14,7 @@
 
 package com.liferay.knowledgebase.hook.upgrade.v1_1_0;
 
-import com.liferay.portal.kernel.dao.jdbc.DataAccess;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 /**
  * @author Peter Shin
@@ -30,93 +22,15 @@ import java.sql.ResultSet;
 public class UpgradeResourceAction extends UpgradeProcess {
 
 	protected void doUpgrade() throws Exception {
-		updateResourceActions();
+		runSQL(
+			"update ResourceAction set name = " +
+				"'com.liferay.knowledgebase.model.KBArticle' where name = " +
+					"'com.liferay.knowledgebase.model.Article'");
+
+		runSQL(
+			"update ResourceAction set name = " +
+				"'com.liferay.knowledgebase.model.KBTemplate' where name = " +
+					"'com.liferay.knowledgebase.model.Template'");
 	}
-
-	protected void updateResourceActions() throws Exception {
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			con = DataAccess.getConnection();
-
-			StringBundler sb = new StringBundler(5);
-
-			sb.append("select * from ResourceAction where name in ('");
-			sb.append(_ARTICLE_CLASS_NAME);
-			sb.append("', '");
-			sb.append(_TEMPLATE_CLASS_NAME);
-			sb.append("')");
-
-			ps = con.prepareStatement(sb.toString());
-
-			rs = ps.executeQuery();
-
-			while (rs.next()) {
-				long resourceActionId = rs.getLong("resourceActionId");
-				String name = rs.getString("name");
-				String actionId = rs.getString("actionId");
-				long bitwiseValue = rs.getLong("bitwiseValue");
-
-				String newName = null;
-
-				if (name.equals(_ARTICLE_CLASS_NAME)) {
-					newName = _KB_ARTICLE_CLASS_NAME;
-				}
-				else {
-					newName = _KB_TEMPLATE_CLASS_NAME;
-				}
-
-				sb = new StringBundler(7);
-
-				sb.append("delete from ResourceAction where name = '");
-				sb.append(newName);
-				sb.append("' and actionId = '");
-				sb.append(actionId);
-				sb.append("' and bitwiseValue = '");
-				sb.append(bitwiseValue);
-				sb.append("'");
-
-				if (_log.isDebugEnabled()) {
-					_log.debug(sb.toString());
-				}
-
-				runSQL(sb.toString());
-
-				sb = new StringBundler(5);
-
-				sb.append("update ResourceAction set name = '");
-				sb.append(newName);
-				sb.append("' where resourceActionId = '");
-				sb.append(resourceActionId);
-				sb.append("'");
-
-				if (_log.isDebugEnabled()) {
-					_log.debug(sb.toString());
-				}
-
-				runSQL(sb.toString());
-			}
-		}
-		finally {
-			DataAccess.cleanUp(con, ps, rs);
-		}
-	}
-
-	private static final String _ARTICLE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.Article";
-
-	private static final String _KB_ARTICLE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.KBArticle";
-
-	private static final String _KB_TEMPLATE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.KBTemplate";
-
-	private static final String _TEMPLATE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.Template";
-
-	private static Log _log = LogFactoryUtil.getLog(
-		UpgradeResourceAction.class);
 
 }
--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/upgrade/v1_1_0/UpgradeResourcePermission.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/hook/upgrade/v1_1_0/UpgradeResourcePermission.java
@@ -14,15 +14,7 @@
 
 package com.liferay.knowledgebase.hook.upgrade.v1_1_0;
 
-import com.liferay.portal.kernel.dao.jdbc.DataAccess;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 /**
  * @author Peter Shin
@@ -30,102 +22,15 @@ import java.sql.ResultSet;
 public class UpgradeResourcePermission extends UpgradeProcess {
 
 	protected void doUpgrade() throws Exception {
-		updateResourcePermissions();
+		runSQL(
+			"update ResourcePermission set name = " +
+				"'com.liferay.knowledgebase.model.KBArticle' where name = " +
+					"'com.liferay.knowledgebase.model.Article'");
+
+		runSQL(
+			"update ResourcePermission set name = " +
+				"'com.liferay.knowledgebase.model.KBTemplate' where name = " +
+					"'com.liferay.knowledgebase.model.Template'");
 	}
-
-	protected void updateResourcePermissions() throws Exception {
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			con = DataAccess.getConnection();
-
-			StringBundler sb = new StringBundler(5);
-
-			sb.append("select * from ResourcePermission where name in ('");
-			sb.append(_ARTICLE_CLASS_NAME);
-			sb.append("', '");
-			sb.append(_TEMPLATE_CLASS_NAME);
-			sb.append("')");
-
-			ps = con.prepareStatement(sb.toString());
-
-			rs = ps.executeQuery();
-
-			while (rs.next()) {
-				long resourcePermissionId = rs.getLong("resourcePermissionId");
-				long companyId = rs.getLong("companyId");
-				String name = rs.getString("name");
-				int scope = rs.getInt("scope");
-				String primKey = rs.getString("primKey");
-				long roleId = rs.getLong("roleId");
-				long actionIds = rs.getLong("actionIds");
-
-				String newName = null;
-
-				if (name.equals(_ARTICLE_CLASS_NAME)) {
-					newName = _KB_ARTICLE_CLASS_NAME;
-				}
-				else {
-					newName = _KB_TEMPLATE_CLASS_NAME;
-				}
-
-				sb = new StringBundler(13);
-
-				sb.append("delete from ResourcePermission where companyId = '");
-				sb.append(companyId);
-				sb.append("' and name = '");
-				sb.append(newName);
-				sb.append("' and scope = '");
-				sb.append(scope);
-				sb.append("' and primKey = '");
-				sb.append(primKey);
-				sb.append("' and roleId = '");
-				sb.append(roleId);
-				sb.append("' and actionIds = '");
-				sb.append(actionIds);
-				sb.append("'");
-
-				if (_log.isDebugEnabled()) {
-					_log.debug(sb.toString());
-				}
-
-				runSQL(sb.toString());
-
-				sb = new StringBundler(5);
-
-				sb.append("update ResourcePermission set name = '");
-				sb.append(newName);
-				sb.append("' where resourcePermissionId = '");
-				sb.append(resourcePermissionId);
-				sb.append("'");
-
-				if (_log.isDebugEnabled()) {
-					_log.debug(sb.toString());
-				}
-
-				runSQL(sb.toString());
-			}
-		}
-		finally {
-			DataAccess.cleanUp(con, ps, rs);
-		}
-	}
-
-	private static final String _ARTICLE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.Article";
-
-	private static final String _KB_ARTICLE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.KBArticle";
-
-	private static final String _KB_TEMPLATE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.KBTemplate";
-
-	private static final String _TEMPLATE_CLASS_NAME =
-		"com.liferay.knowledgebase.model.Template";
-
-	private static Log _log = LogFactoryUtil.getLog(
-		UpgradeResourcePermission.class);
 
 }
