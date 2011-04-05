eb48a28826f39b767b8d18e147ceb0826b62a7db
Peter Shin <peter.shin@liferay.com>
2011-04-05 19:03:33 +0000

    LPS-15988 init commit for front end
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@77264 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/portlet/CalendarPortlet.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.portlet;
+
+import com.liferay.calendar.CalendarEventDurationException;
+import com.liferay.calendar.CalendarEventEndDateException;
+import com.liferay.calendar.CalendarEventStartDateException;
+import com.liferay.calendar.CalendarEventTitleException;
+import com.liferay.calendar.DuplicateCalendarResourceException;
+import com.liferay.calendar.NoSuchBookingException;
+import com.liferay.calendar.NoSuchEventException;
+import com.liferay.calendar.NoSuchResourceException;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * @author Eduardo Lundgren
+ */
+public class CalendarPortlet extends MVCPortlet {
+
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof CalendarEventDurationException ||
+			cause instanceof CalendarEventEndDateException ||
+			cause instanceof CalendarEventStartDateException ||
+			cause instanceof CalendarEventTitleException ||
+			cause instanceof DuplicateCalendarResourceException ||
+			cause instanceof NoSuchBookingException ||
+			cause instanceof NoSuchEventException ||
+			cause instanceof NoSuchResourceException ||
+			cause instanceof PrincipalException) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+}
