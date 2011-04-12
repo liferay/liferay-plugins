f93c4d670d0644bd94ffd2c6e4aea3731855ac69
Brian Chan <brian.chan@liferay.com>
2011-04-12 18:42:37 +0000

    LPS-15805 Tests
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@77893 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/sample-php-portlet/docroot/navigation.php
+++ portlets/sample-php-portlet/docroot/navigation.php
@@ -4,6 +4,10 @@
 	Navigation:
 
 	<a href="index.php?foo=bar">Home</a> -
-	<a href="/quercus.php">Quercus</a> -
-	<a href="phpinfo.php">PHP Info</a>
+
+	<a href="quercus.php">Quercus</a> -
+
+	<a href="phpinfo.php">PHP Info</a> -
+
+	<a href="test_db.php">Test Database Connection</a>
 </div>
