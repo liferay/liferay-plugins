ee1b1a646845a06eff475a9553afa630f1266b66
Mika Koivisto <mika.koivisto@liferay.com>
2011-04-15 00:34:21 +0000

    LPS-16525 Add scheduler to automatically enable failed consumers
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@78192 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/messaging/WSRPConsumerCheckEventMessageListener.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wsrp.messaging;
+
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.wsrp.service.WSRPConsumerPortletLocalServiceUtil;
+
+/**
+ * @author Mika Koivisto
+ */
+public class WSRPConsumerCheckEventMessageListener extends BaseMessageListener {
+
+	protected void doReceive(Message message) throws Exception {
+		WSRPConsumerPortletLocalServiceUtil.initFailedWSRPConsumerPortlets();
+	}
+
+}
