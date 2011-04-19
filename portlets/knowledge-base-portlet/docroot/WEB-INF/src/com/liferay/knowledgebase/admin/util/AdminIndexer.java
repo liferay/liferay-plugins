f857c3dfd7af9ad9b04b9f9a03b6ee1f819ff18c
Peter Shin <peter.shin@liferay.com>
2011-04-19 14:31:59 +0000

    LPS-6422
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@78531 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/util/AdminIndexer.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/util/AdminIndexer.java
@@ -44,6 +44,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import javax.portlet.PortletURL;
 
@@ -144,7 +145,8 @@ public class AdminIndexer extends BaseIndexer {
 	}
 
 	protected Summary doGetSummary(
-		Document document, String snippet, PortletURL portletURL) {
+		Document document, Locale locale, String snippet,
+		PortletURL portletURL) {
 
 		String title = document.get(Field.TITLE);
 
