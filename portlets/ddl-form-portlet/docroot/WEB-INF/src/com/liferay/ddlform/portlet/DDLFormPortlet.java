25c0f978b7b27202569882559c7b43efbe7d0250
Marcellus Tavares <marcellus.tavares@liferay.com>
2011-06-13 20:35:57 +0000

    LPS-17789 Fix compilation error
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@83406 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/ddl-form-portlet/docroot/WEB-INF/src/com/liferay/ddlform/portlet/DDLFormPortlet.java
+++ portlets/ddl-form-portlet/docroot/WEB-INF/src/com/liferay/ddlform/portlet/DDLFormPortlet.java
@@ -67,7 +67,7 @@ public class DDLFormPortlet extends MVCPortlet {
 			DDLRecord.class.getName(), actionRequest);
 
 		DDLRecordServiceUtil.addRecord(
-			themeDisplay.getScopeGroupId(), recordSetId, fields, 0,
+			themeDisplay.getScopeGroupId(), recordSetId, 0, fields, 
 			serviceContext);
 	}
 
