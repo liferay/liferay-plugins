912e9af1e14f12ae3e7d2b747fb53d35cdcb9d15
Jonathan.Lee <Jonathan.Lee@liferay.com>
2011-06-21 11:35:14 -0700

    SOS-659

--- portlets/microblogs-portlet/docroot/WEB-INF/service/com/liferay/microblogs/service/permission/MicroblogsEntryPermission.java
+++ portlets/microblogs-portlet/docroot/WEB-INF/service/com/liferay/microblogs/service/permission/MicroblogsEntryPermission.java
@@ -76,18 +76,20 @@ public class MicroblogsEntryPermission {
 			return true;
 		}
 
+		if (microblogsEntry.getSocialRelationType() == 0) {
+			return true;
+		}
+
 		if ((microblogsEntry.getSocialRelationType() != 0) &&
 			(microblogsEntry.getUserId() != permissionChecker.getUserId()) &&
-			!SocialRelationLocalServiceUtil.hasRelation(
+			SocialRelationLocalServiceUtil.hasRelation(
 				permissionChecker.getUserId(), microblogsEntry.getUserId(),
 				microblogsEntry.getSocialRelationType())) {
 
-			return false;
+			return true;
 		}
 
-		return permissionChecker.hasPermission(
-			0, MicroblogsEntry.class.getName(),
-			microblogsEntry.getMicroblogsEntryId(), actionId);
+		return false;
 	}
 
 }
