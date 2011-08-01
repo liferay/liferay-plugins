1c87cad388ec786f4ab08c7965fe1cf886b3dfb7
Jonathan Lee <jonathan.lee@liferay.com>
2011-08-01 21:15:48 +0000

    LPS-18695 Update so-hook
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@86208 05bdf26c-840f-0410-9ced-eb539d925f36

--- hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/listeners/UserListener.java
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/listeners/UserListener.java
@@ -25,6 +25,7 @@ import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.LayoutSetLocalServiceUtil;
 import com.liferay.portal.service.RoleLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.UserLocalServiceUtil;
 
 /**
@@ -59,8 +60,12 @@ public class UserListener extends BaseModelListener<User> {
 
 		Group group = user.getGroup();
 
-		LayoutSetLocalServiceUtil.deleteLayoutSet(group.getGroupId(), false);
-		LayoutSetLocalServiceUtil.deleteLayoutSet(group.getGroupId(), true);
+		ServiceContext serviceContext = new ServiceContext();
+
+		LayoutSetLocalServiceUtil.deleteLayoutSet(
+			group.getGroupId(), false, serviceContext);
+		LayoutSetLocalServiceUtil.deleteLayoutSet(
+			group.getGroupId(), true, serviceContext);
 
 		LayoutSetLocalServiceUtil.addLayoutSet(group.getGroupId(), false);
 		LayoutSetLocalServiceUtil.addLayoutSet(group.getGroupId(), true);
