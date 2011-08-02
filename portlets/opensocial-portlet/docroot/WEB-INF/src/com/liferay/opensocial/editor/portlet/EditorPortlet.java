27e03a4e66182d2192298daaa3377aa0948af7e3
Brian Chan <brian.chan@liferay.com>
2011-08-02 16:51:19 -0700

    LPS-19525

--- portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/editor/portlet/EditorPortlet.java
+++ portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/editor/portlet/EditorPortlet.java
@@ -212,7 +212,7 @@ public class EditorPortlet extends AdminPortlet {
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
 		FileEntry fileEntry = DLAppServiceUtil.addFileEntry(
-			folder.getRepositoryId(), folderId, StringPool.BLANK,
+			folder.getRepositoryId(), folderId, fileEntryTitle,
 			resourceRequest.getContentType(), fileEntryTitle,
 			StringPool.BLANK, StringPool.BLANK, bytes, serviceContext);
 
