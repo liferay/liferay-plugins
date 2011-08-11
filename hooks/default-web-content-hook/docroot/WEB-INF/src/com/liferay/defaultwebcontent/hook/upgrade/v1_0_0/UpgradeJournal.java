c43961790d03ebf40d24a52d5acdac50cf6ee952
hhuijser <hugo.huijser@liferay.com>
2011-08-11 15:50:43 -0700

    LPS-16829, LPS-18907 default-web-content-hook does not compile

--- hooks/default-web-content-hook/docroot/WEB-INF/src/com/liferay/defaultwebcontent/hook/upgrade/v1_0_0/UpgradeJournal.java
+++ hooks/default-web-content-hook/docroot/WEB-INF/src/com/liferay/defaultwebcontent/hook/upgrade/v1_0_0/UpgradeJournal.java
@@ -16,6 +16,7 @@ package com.liferay.defaultwebcontent.hook.upgrade.v1_0_0;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.GroupLocalServiceUtil;
@@ -26,6 +27,10 @@ import com.liferay.portlet.journal.service.JournalStructureLocalServiceUtil;
 import com.liferay.portlet.journal.service.JournalTemplateLocalServiceUtil;
 
 import java.io.InputStream;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Ryan Park
@@ -40,43 +45,64 @@ public class UpgradeJournal extends UpgradeProcess {
 		serviceContext.setAddCommunityPermissions(true);
 		serviceContext.setAddGuestPermissions(true);
 
+		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
+		Map<Locale, String> nameMap = new HashMap<Locale, String>();
+
 		// Article
 
 		String structureId = "ARTICLE";
-		String name = "Article";
-		String description = "This structure accommodates article title, " +
-			"both main, and preview images, and the main article body.";
+
+		nameMap.put(LocaleUtil.getDefault(), "Article");
+		descriptionMap.put(
+			LocaleUtil.getDefault(),
+			"This structure accommodates article title, both main, and " +
+				"preview images, and the main article body.");
+
 		String xsd = getFileAsString("/structures/article.xml");
 
 		JournalStructureLocalServiceUtil.addStructure(
-			userId, groupId, structureId, false, StringPool.BLANK, name,
-			description, xsd, serviceContext);
+			userId, groupId, structureId, false, StringPool.BLANK, nameMap,
+			descriptionMap, xsd, serviceContext);
 
 		// Carousel
 
+		nameMap.clear();
+		descriptionMap.clear();
+
 		structureId = "MULTIPLE-ITEM-CAROUSEL";
-		name = "Carousel";
-		description = "This is a simple carousel structure designed to " +
-			"handle other necessary carousel configurations.";
+
+		nameMap.put(LocaleUtil.getDefault(), "Carousel");
+		descriptionMap.put(
+			LocaleUtil.getDefault(),
+			"This is a simple carousel structure designed to handle other " +
+				"necessary carousel configurations.");
+
 		xsd = getFileAsString(
 			"/structures/multiple_item_carousel.xml");
 
 		JournalStructureLocalServiceUtil.addStructure(
-			userId, groupId, structureId, false, StringPool.BLANK, name,
-			description, xsd, serviceContext);
+			userId, groupId, structureId, false, StringPool.BLANK, nameMap,
+			descriptionMap, xsd, serviceContext);
 
 		// Multiple Item
 
+		nameMap.clear();
+		descriptionMap.clear();
+
 		structureId = "MULTIPLE-ITEM";
-		name = "Multiple Item";
-		description = "This is a simple structure with a single repeatable " +
-			"element that includes an HTML field, and text-box for a title " +
-				"and url designation.";
+
+		nameMap.put(LocaleUtil.getDefault(), "Multiple Item");
+		descriptionMap.put(
+			LocaleUtil.getDefault(),
+			"This is a simple structure with a single repeatable element " +
+				"that includes an HTML field, and text-box for a title and " +
+					"url designation.");
+
 		xsd = getFileAsString("/structures/multiple_item.xml");
 
 		JournalStructureLocalServiceUtil.addStructure(
-			userId, groupId, structureId, false, StringPool.BLANK, name,
-			description, xsd, serviceContext);
+			userId, groupId, structureId, false, StringPool.BLANK, nameMap,
+			descriptionMap, xsd, serviceContext);
 	}
 
 	protected void addJournalTemplates(long groupId, long userId)
@@ -87,63 +113,90 @@ public class UpgradeJournal extends UpgradeProcess {
 		serviceContext.setAddCommunityPermissions(true);
 		serviceContext.setAddGuestPermissions(true);
 
+		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
+		Map<Locale, String> nameMap = new HashMap<Locale, String>();
+
 		// Regular Article Description
 
 		String templateId = "ARTICLE-DESCRIPTION";
 		String structureId = "ARTICLE";
-		String name = "Regular Article Description";
-		String description = "This template only displays brief descriptions " +
-			"of web content.";
+
+		nameMap.put(LocaleUtil.getDefault(), "Regular Article Description");
+		descriptionMap.put(
+			LocaleUtil.getDefault(),
+			"This template only displays brief descriptions of web content");
+
 		String type = "vm";
 		String xsl = getFileAsString(
 			"/templates/article_description.vm");
 
 		JournalTemplateLocalServiceUtil.addTemplate(
-			userId, groupId, templateId, false, structureId, name,
-			description, xsl, true, type, true, false, StringPool.BLANK, null,
-			serviceContext);
+			userId, groupId, templateId, false, structureId, nameMap,
+			descriptionMap, xsl, true, type, true, false, StringPool.BLANK,
+			null, serviceContext);
 
 		// Regular Article
 
+		nameMap.clear();
+		descriptionMap.clear();
+
 		templateId = "REGULAR-ARTICLE";
-		name = "Regular Article";
-		description = "This is the regular article template, it handles " +
-			"basic article content like, titles, main image, body, and " +
-				"author information.";
+
+		nameMap.put(LocaleUtil.getDefault(), "Regular Article");
+		descriptionMap.put(
+			LocaleUtil.getDefault(),
+			"This is the regular article template, it handles basic article " +
+				"content like, titles, main image, body, and author " +
+					"information.");
+
 		xsl = getFileAsString("/templates/regular_article.vm");
 
 		JournalTemplateLocalServiceUtil.addTemplate(
-			userId, groupId, templateId, false, structureId, name,
-			description, xsl, true, type, true, false, StringPool.BLANK, null,
-			serviceContext);
+			userId, groupId, templateId, false, structureId, nameMap,
+			descriptionMap, xsl, true, type, true, false, StringPool.BLANK,
+			null, serviceContext);
 
 		// Carousel
 
+		nameMap.clear();
+		descriptionMap.clear();
+
 		templateId = "MULTIPLE-ITEM-CAROUSEL";
 		structureId = "MULTIPLE-ITEM-CAROUSEL";
-		name = "Carousel";
-		description = "This is the carousel template that utilizes Alloy UI " +
-			"to display repeatable content as a slideshow.";
+
+		nameMap.put(LocaleUtil.getDefault(), "Carousel");
+		descriptionMap.put(
+			LocaleUtil.getDefault(),
+			"This is the carousel template that utilizes Alloy UI to display " +
+				"repeatable content as a slideshow.");
+
 		xsl = getFileAsString("/templates/multiple_item_carousel.vm");
 
 		JournalTemplateLocalServiceUtil.addTemplate(
-			userId, groupId, templateId, false, structureId, name,
-			description, xsl, true, type, true, false, StringPool.BLANK, null,
-			serviceContext);
+			userId, groupId, templateId, false, structureId, nameMap,
+			descriptionMap, xsl, true, type, true, false, StringPool.BLANK,
+			null, serviceContext);
 
 		// Featured Items
 
+		nameMap.clear();
+		descriptionMap.clear();
+
 		templateId = "MULTIPLE-ITEM-FEATURE";
 		structureId = "MULTIPLE-ITEM";
-		name = "Featured Items";
-		description = "This is a template that utilizes the Multiple Item " +
-			"Structure, and displays the data as Featured Items.";
+
+		nameMap.put(LocaleUtil.getDefault(), "Featured Items");
+		descriptionMap.put(
+			LocaleUtil.getDefault(),
+			"This is a template that utilizes the Multiple Item Structure, " +
+				"and displays the data as Featured Items.");
+
 		xsl = getFileAsString("/templates/multiple_item_feature.vm");
 
 		JournalTemplateLocalServiceUtil.addTemplate(
-			userId, groupId, templateId, false, structureId, name,
-			description, xsl, true, type, true, false, StringPool.BLANK, null,
-			serviceContext);
+			userId, groupId, templateId, false, structureId, nameMap,
+			descriptionMap, xsl, true, type, true, false, StringPool.BLANK,
+			null, serviceContext);
 	}
 
 	@Override
