63d5d662ae4ed5cc2ad3abcf44aeef11ca47fff2
Shinn Lok <shinn.lok@liferay.com>
2011-10-13 14:00:11 +0000

    LPS-22094 Regenerate
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@90681 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/social-networking-portlet/docroot/WEB-INF/service/com/liferay/socialnetworking/service/WallEntryLocalServiceWrapper.java
+++ portlets/social-networking-portlet/docroot/WEB-INF/service/com/liferay/socialnetworking/service/WallEntryLocalServiceWrapper.java
@@ -70,11 +70,13 @@ public class WallEntryLocalServiceWrapper implements WallEntryLocalService {
 	* Deletes the wall entry from the database. Also notifies the appropriate model listeners.
 	*
 	* @param wallEntry the wall entry
+	* @throws PortalException
 	* @throws SystemException if a system exception occurred
 	*/
 	public void deleteWallEntry(
 		com.liferay.socialnetworking.model.WallEntry wallEntry)
-		throws com.liferay.portal.kernel.exception.SystemException {
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
 		_wallEntryLocalService.deleteWallEntry(wallEntry);
 	}
 
@@ -255,7 +257,8 @@ public class WallEntryLocalServiceWrapper implements WallEntryLocalService {
 	}
 
 	public void deleteWallEntries(long groupId)
-		throws com.liferay.portal.kernel.exception.SystemException {
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
 		_wallEntryLocalService.deleteWallEntries(groupId);
 	}
 
