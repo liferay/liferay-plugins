4b8bd63b1541026ab3c6f3a43fc81adf9ed1d71d
Brian Chan <brian.chan@liferay.com>
2011-10-17 10:44:56 +0000

    LPS-22161 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@90849 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/sample-php-portlet/docroot/navigation.php
+++ portlets/sample-php-portlet/docroot/navigation.php
@@ -5,11 +5,11 @@
 
 	<a href="index.php?foo=bar">Home</a> -
 
-	<a href="portlet_url.php">A Custom Portlet URL</a> -
-
 	<a href="quercus.php">Quercus</a> -
 
 	<a href="phpinfo.php">PHP Info</a> -
+
+	<a href="portlet_url.php">Portlet URL</a> -
 
 	<a href="test_db.php">Test Database Connection</a>
 </div>
