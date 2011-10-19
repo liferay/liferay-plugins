d56dd725bace3b9ed7b5e524c59c8bd7563b4fe2
Ryan Park <ryan.park@liferay.com>
2011-10-19 08:23:15 +0000

    LPS-18813 Inital commit for Control Panel visibility
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@90993 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portlets/marketplace-portlet/docroot/WEB-INF/src/com/liferay/marketplace/store/portlet/StoreControlPanelEntry.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.marketplace.store.portlet;
+
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.BaseControlPanelEntry;
+
+/**
+ * @author Ryan Park
+ */
+public class StoreControlPanelEntry extends BaseControlPanelEntry {
+
+	public boolean isVisible(
+			Portlet portlet, String category, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		return isVisible(themeDisplay.getPermissionChecker(), portlet);
+	}
+
+	public boolean isVisible(
+			PermissionChecker permissionChecker, Portlet portlet)
+		throws Exception {
+
+		if (permissionChecker.isOmniadmin()) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
