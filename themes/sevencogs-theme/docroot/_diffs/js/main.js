32a31cd760e68cc50f081b1960e6c0ff8528ac13
Bradley Wood <bradley.wood@liferay.com>
2011-10-21 18:46:13 +0000

    LPS-22094 Added Templates and other styling fixes
    git-svn-id: svn://svn.liferay.com/repos/public/plugins/trunk@91232 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ themes/sevencogs-theme/docroot/_diffs/js/main.js
@@ -0,0 +1,38 @@
+if (!AUI().UA.ie || AUI().UA.ie >= 8) {
+	AUI().use('aui-viewport');
+}
+
+AUI().ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
+	function(A) {
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	node: the Alloy Node object of the current portlet
+	*/
+
+	function(portletId, node) {
+	}
+);
+
+Liferay.on(
+	'allPortletsReady',
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
