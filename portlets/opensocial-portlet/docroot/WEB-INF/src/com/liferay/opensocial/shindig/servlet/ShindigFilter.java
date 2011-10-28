426314920ed14d01ac989be9d58a93e8421fd4fa
Dennis Ju <dennis.ju@liferay.com>
2011-10-28 12:53:59 -0700

    LPS-22589

--- portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/shindig/servlet/ShindigFilter.java
+++ portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/shindig/servlet/ShindigFilter.java
@@ -15,7 +15,23 @@
 package com.liferay.opensocial.shindig.servlet;
 
 import com.liferay.opensocial.shindig.util.ShindigUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.CookieKeys;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.auth.AuthenticatedUserUUIDStoreUtil;
+import com.liferay.portal.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.security.permission.PermissionThreadLocal;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.util.CookieUtil;
+import com.liferay.util.Encryptor;
 
 import java.io.IOException;
 
@@ -23,11 +39,13 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shindig.common.servlet.InjectedFilter;
 
 /**
  * @author Michael Young
+ * @author Dennis Ju
  */
 public class ShindigFilter extends InjectedFilter {
 
@@ -39,6 +57,13 @@ public class ShindigFilter extends InjectedFilter {
 			FilterChain filterChain)
 		throws IOException, ServletException {
 
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (permissionChecker == null) {
+			setPermissionChecker(servletRequest);
+		}
+
 		String host = servletRequest.getServerName().concat(
 			StringPool.COLON).concat(
 				String.valueOf(servletRequest.getServerPort()));
@@ -47,5 +72,63 @@ public class ShindigFilter extends InjectedFilter {
 
 		filterChain.doFilter(servletRequest, servletResponse);
 	}
+
+	protected boolean setPermissionChecker(ServletRequest servletRequest) {
+		String companyIdString = CookieUtil.get(
+			(HttpServletRequest)servletRequest, CookieKeys.COMPANY_ID);
+
+		if (Validator.isNull(companyIdString)) {
+			return false;
+		}
+
+		long companyId = GetterUtil.getLong(companyIdString);
+
+		String userUUID = StringPool.BLANK;
+
+		try {
+			Company company = CompanyLocalServiceUtil.getCompany(
+				companyId);
+
+			String userUUIDString = CookieUtil.get(
+				(HttpServletRequest)servletRequest, CookieKeys.USER_UUID);
+
+			userUUID = GetterUtil.getString(
+				Encryptor.decrypt(company.getKeyObj(), userUUIDString));
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			return false;
+		}
+
+		if (!AuthenticatedUserUUIDStoreUtil.exists(userUUID)) {
+			return false;
+		}
+
+		String userIdString = userUUID.substring(
+			0, userUUID.indexOf(StringPool.PERIOD));
+
+		long userId = GetterUtil.getLong(userIdString);
+
+		try {
+			User user = UserLocalServiceUtil.getUserById(userId);
+
+			PrincipalThreadLocal.setName(userIdString);
+
+			PermissionChecker permissionChecker =
+				PermissionCheckerFactoryUtil.create(user, true);
+
+			PermissionThreadLocal.setPermissionChecker(permissionChecker);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(ShindigFilter.class);
 
 }
