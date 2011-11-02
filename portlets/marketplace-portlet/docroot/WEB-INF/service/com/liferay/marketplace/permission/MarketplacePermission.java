118e8b21b009872559fe3bb0695cbaab59fad2bb
Ryan Park <ryan.park@liferay.com>
2011-11-02 16:27:46 -0700

    LPS-18813 Basic Permissions

---
+++ portlets/marketplace-portlet/docroot/WEB-INF/service/com/liferay/marketplace/permission/MarketplacePermission.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.marketplace.service.permission;
+
+import com.liferay.marketplace.util.MarketplaceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.security.permission.PermissionChecker;
+
+/**
+ * @author Ryan Park
+ */
+public class MarketplacePermission {
+
+	public static void check(PermissionChecker permissionChecker)
+		throws PortalException {
+
+		if (!contains(permissionChecker)) {
+			throw new PrincipalException();
+		}
+	}
+
+	public static boolean contains(PermissionChecker permissionChecker) {
+		if (permissionChecker.isOmniadmin() &&
+			MarketplaceUtil.isMarketpalceAdmin(permissionChecker.getUserId())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+}
