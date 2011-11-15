c22f47ee0fd1fd506446e7f724f5d130286bb3b6
Brian Chan <brian.chan@liferay.com>
2011-11-14 16:59:07 -0800

    LPS-14419 Source formatting

--- webs/kaleo-web/docroot/WEB-INF/service/com/liferay/portal/workflow/kaleo/definition/Definition.java
+++ webs/kaleo-web/docroot/WEB-INF/service/com/liferay/portal/workflow/kaleo/definition/Definition.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.workflow.kaleo.definition;
 
-import com.liferay.portal.kernel.workflow.WorkflowException;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -35,7 +33,7 @@ public class Definition {
 		_version = version;
 	}
 
-	public void addNode(Node node) throws WorkflowException {
+	public void addNode(Node node) {
 		if (_nodesMap.containsKey(node.getName())) {
 			throw new IllegalArgumentException(
 				"Duplicate node " + node.getName());
