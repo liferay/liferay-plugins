cebc0b9aafcfa8957761e56fc787b995b2caa300
Aaron Delani <today0@gmail.com>
2011-12-19 16:00:04 -0800

    SOS-806 Removing Message Boards Customizations and relying on lighter CSS alternatives.

--- hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/util/InstanceUtil.java
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/util/InstanceUtil.java
@@ -288,7 +288,7 @@ public class InstanceUtil {
 
 		layout = LayoutUtil.addLayout(
 			group, true, LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "Forums",
-			"2_columns_iii");
+			"1_column");
 
 		LayoutUtil.addPortlets(
 			group, layout, "/forums", PortletPropsKeys.SITE_PROTOTYPE_PORTLETS);
