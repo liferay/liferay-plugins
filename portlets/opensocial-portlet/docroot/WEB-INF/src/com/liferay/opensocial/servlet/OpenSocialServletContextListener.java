b5186ffbd388f7811db5a10912d09bc577ed13d1
Dennis Ju <dennis.ju@liferay.com>
2012-01-20 18:16:33 -0800

    LPS-25039 OpenSocial's AppData Service is not storing/retrieving user data

--- portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/servlet/OpenSocialServletContextListener.java
+++ portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/servlet/OpenSocialServletContextListener.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.util.BasePortalLifecycle;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portlet.expando.NoSuchTableException;
@@ -58,6 +59,17 @@ public class OpenSocialServletContextListener
 			catch (NoSuchTableException nste) {
 				ExpandoTableLocalServiceUtil.addTable(
 					company.getCompanyId(), Layout.class.getName(),
+					ShindigUtil.getTableOpenSocial());
+			}
+
+			try {
+				ExpandoTableLocalServiceUtil.getTable(
+					company.getCompanyId(), User.class.getName(),
+					ShindigUtil.getTableOpenSocial());
+			}
+			catch (NoSuchTableException nste) {
+				ExpandoTableLocalServiceUtil.addTable(
+					company.getCompanyId(), User.class.getName(),
 					ShindigUtil.getTableOpenSocial());
 			}
 		}
