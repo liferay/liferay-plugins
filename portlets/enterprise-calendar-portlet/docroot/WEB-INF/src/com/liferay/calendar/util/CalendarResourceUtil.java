238d74c8fe2f42c3ecc9af2aa9a95ea6633fd276
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2012-01-24 17:31:51 -0300

    LPS-25061 Adding CalendarResourceLocalServiceImpl (manual)

---
+++ portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarResourceUtil.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/**
+ * @author Eduardo Lundgren
+ * @author Fabio Pezzutto
+ */
+package com.liferay.calendar.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+
+/**
+ * @author Eduardo Lundgren
+ * @author Fabio Pezzutto
+ */
+public class CalendarResourceUtil {
+
+	public static long getGlobalResourceUserId(String className, long classPK)
+		throws PortalException, SystemException {
+
+		long userId = 0;
+
+		if (className.equals(Group.class.getName())) {
+			Group group = GroupLocalServiceUtil.getGroup(classPK);
+
+			userId = group.getCreatorUserId();
+		}
+		else if (className.equals(User.class.getName())) {
+			userId = classPK;
+		}
+
+		return userId;
+	}
+
+	public static boolean isGlobalResource(String className, long classPK) {
+
+		return (className.equals(User.class.getName()) ||
+				className.equals(Group.class.getName()));
+	}
+
+}
