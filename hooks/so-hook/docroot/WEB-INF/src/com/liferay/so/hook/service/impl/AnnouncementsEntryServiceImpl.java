c8bf3b928c65959c1cce05d453f9d8333a5dbb9a
Jonathan Lee <Jonathan.Lee@liferay.com>
2012-01-26 16:02:00 -0800

    SOS-761

--- hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/service/impl/AnnouncementsEntryServiceImpl.java
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/service/impl/AnnouncementsEntryServiceImpl.java
@@ -19,11 +19,24 @@ package com.liferay.so.hook.service.impl;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.notifications.ChannelHubManagerUtil;
+import com.liferay.portal.kernel.notifications.NotificationEvent;
+import com.liferay.portal.kernel.notifications.NotificationEventFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.model.ClassName;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Organization;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.User;
+import com.liferay.portal.model.UserGroup;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.security.permission.PermissionThreadLocal;
+import com.liferay.portal.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.service.permission.PortletPermissionUtil;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PortletKeys;
@@ -31,6 +44,9 @@ import com.liferay.portlet.announcements.model.AnnouncementsEntry;
 import com.liferay.portlet.announcements.service.AnnouncementsEntryLocalServiceUtil;
 import com.liferay.portlet.announcements.service.AnnouncementsEntryService;
 import com.liferay.portlet.announcements.service.AnnouncementsEntryServiceWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Jonathan Lee
@@ -88,7 +104,68 @@ public class AnnouncementsEntryServiceImpl
 			}
 		}
 
+		if (announcementEntry != null) {
+			sendNotificationEvent(announcementEntry);
+		}
+
 		return announcementEntry;
+	}
+
+	protected void sendNotificationEvent(AnnouncementsEntry announcementEntry)
+		throws PortalException, SystemException {
+
+		JSONObject notificationEventJSON = JSONFactoryUtil.createJSONObject();
+
+		notificationEventJSON.put("body", announcementEntry.getTitle());
+		notificationEventJSON.put("entryId", announcementEntry.getEntryId());
+		notificationEventJSON.put("groupId", announcementEntry.getClassPK());
+		notificationEventJSON.put("portletId", PortletKeys.ANNOUNCEMENTS);
+		notificationEventJSON.put("title", "x-sent-a-new-announcement");
+		notificationEventJSON.put("userId", announcementEntry.getUserId());
+
+		NotificationEvent notificationEvent =
+			NotificationEventFactoryUtil.createNotificationEvent(
+				System.currentTimeMillis(), "6_WAR_soportlet",
+				notificationEventJSON);
+
+		notificationEvent.setDeliveryRequired(0);
+
+		List<User> users = new ArrayList<User>();
+
+		if (announcementEntry.getClassNameId() == 0) {
+			users = UserLocalServiceUtil.getUsers(
+				QueryUtil.ALL_POS,  QueryUtil.ALL_POS);
+		}
+		else {
+			ClassName className = ClassNameLocalServiceUtil.getClassName(
+				announcementEntry.getClassNameId());
+
+			if (className.getClassName().equals(Group.class.getName())) {
+				users = UserLocalServiceUtil.getGroupUsers(
+					announcementEntry.getClassPK());
+			}
+			else if (className.getClassName().equals(
+				Organization.class.getName())) {
+
+				users = UserLocalServiceUtil.getOrganizationUsers(
+					announcementEntry.getClassPK());
+			}
+			else if (className.getClassName().equals(Role.class.getName())) {
+				users = UserLocalServiceUtil.getRoleUsers(
+					announcementEntry.getClassPK());
+			}
+			else if (className.getClassName().equals(
+				UserGroup.class.getName())) {
+
+				users = UserLocalServiceUtil.getUserGroupUsers(
+					announcementEntry.getClassPK());
+			}
+		}
+
+		for (User user : users) {
+			ChannelHubManagerUtil.sendNotificationEvent(
+				user.getCompanyId(), user.getUserId(), notificationEvent);
+		}
 	}
 
 }
