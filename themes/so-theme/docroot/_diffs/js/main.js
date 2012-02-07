435d350122bdcd084593db14ec0fd64fbc8b2ea0
Ryan Park <ryan.park@liferay.com>
2012-02-07 13:26:15 -0800

    SOS-832 Source formatting

--- themes/so-theme/docroot/_diffs/js/main.js
+++ themes/so-theme/docroot/_diffs/js/main.js
@@ -2,31 +2,21 @@ AUI().ready(
 	'aui-io-request',
 	function(A) {
 		var body = A.getBody();
+
 		A.one('#toggleFluid').on(
 			'click',
 			function (event) {
-			event.preventDefault();
+				body.toggleClass('so-layout-fluid');
 
-			var toggleFluid = 'so-layout-fluid'
-
-			var isFluidLayout = body.hasClass('so-layout-fluid');
-
-			body.toggleClass('so-layout-fluid');
-
-			if (!isFluidLayout) {
-				var toggleFluidClass = true;
-			} else {
-				var toggleFluidClass = false;
-			};
-
-			A.io.request(
-				themeDisplay.getPathMain() + '/portal/session_click',
-				{
-					data: {
-							'so-layout-fluid': toggleFluidClass
+				A.io.request(
+					themeDisplay.getPathMain() + '/portal/session_click',
+					{
+						data: {
+							'so-layout-fluid': body.hasClass('so-layout-fluid')
 						}
-				}
-			);
-		});
+					}
+				);
+			}
+		);
 	}
 );
