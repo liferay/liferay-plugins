2b88dbf298dc8125e9192bf3b4207a39487bb292
Aaron Delani <today0@gmail.com>
2012-02-08 17:45:50 -0800

    SOS-842 Added a color scheme that an admin can set to make certain pages only fluid.

--- themes/so-theme/docroot/_diffs/js/main.js
+++ themes/so-theme/docroot/_diffs/js/main.js
@@ -6,16 +6,18 @@ AUI().ready(
 		A.one('#toggleFluid').on(
 			'click',
 			function (event) {
-				body.toggleClass('so-layout-fluid');
+				if (!body.hasClass('so-layout-fluid-ad')) {
+					body.toggleClass('so-layout-fluid');
 
-				A.io.request(
-					themeDisplay.getPathMain() + '/portal/session_click',
-					{
-						data: {
-							'so-layout-fluid': body.hasClass('so-layout-fluid')
+					A.io.request(
+						themeDisplay.getPathMain() + '/portal/session_click',
+						{
+							data: {
+								'so-layout-fluid': body.hasClass('so-layout-fluid')
+							}
 						}
-					}
-				);
+					);
+				};
 			}
 		);
 	}
