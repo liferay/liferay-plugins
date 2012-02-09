cd6db108d72ac61b8c25c031770e3accba82ae18
fabiopezzutto <fabio.pezzutto@smc.it>
2012-02-09 10:47:06 +0100

    Add CalendarBooking remote service

--- portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarBookingServiceImpl.java
+++ portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarBookingServiceImpl.java
@@ -14,10 +14,93 @@
 
 package com.liferay.calendar.service.impl;
 
+import com.liferay.calendar.model.CalendarBooking;
 import com.liferay.calendar.service.base.CalendarBookingServiceBaseImpl;
+import com.liferay.calendar.service.permission.CalendarBookingPermission;
+import com.liferay.calendar.service.permission.CalendarPermission;
+import com.liferay.calendar.util.ActionKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Eduardo Lundgren
+ * @author Fabio Pezzutto
  */
 public class CalendarBookingServiceImpl extends CalendarBookingServiceBaseImpl {
+
+	public CalendarBooking addCalendarBooking(
+			long calendarId, long parentCalendarBookingId,
+			Map<Locale, String> titleMap, Map<Locale, String> descriptionMap,
+			Map<Locale, String> locationMap, String type, int startDateMonth,
+			int startDateDay, int startDateYear, int startDateHour,
+			int startDateMinute, int endDateMonth, int endDateDay,
+			int endDateYear, int endDateHour, int endDateMinute,
+			boolean allDay, String recurrence, int priority,
+			boolean outOfOffice, int firstReminder, int secondReminder,
+			boolean required, String requestMessage, String responseMessage,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		CalendarPermission.check(
+			getPermissionChecker(), calendarId, ActionKeys.ADD_BOOKING);
+
+		return calendarBookingLocalService.addCalendarBooking(
+			getUserId(), calendarId, parentCalendarBookingId, titleMap,
+			descriptionMap, locationMap, type, startDateMonth, startDateDay,
+			startDateYear, startDateHour, startDateMinute, endDateMonth,
+			endDateDay, endDateYear, endDateHour, endDateMinute, allDay,
+			recurrence, priority, outOfOffice, firstReminder, secondReminder,
+			required, requestMessage, responseMessage, serviceContext);
+	}
+
+	public void deleteCalendarBooking(
+			long calendarBookingId)
+		throws PortalException, SystemException {
+
+		CalendarBookingPermission.check(
+			getPermissionChecker(), calendarBookingId, ActionKeys.DELETE);
+
+		calendarBookingLocalService.deleteCalendarBooking(calendarBookingId);
+	}
+
+	public CalendarBooking getCalendarBooking(long calendarBookingId)
+		throws PortalException, SystemException {
+
+		CalendarBookingPermission.check(
+			getPermissionChecker(), calendarBookingId, ActionKeys.VIEW);
+
+		return calendarBookingLocalService.getCalendarBooking(
+			calendarBookingId);
+	}
+
+	public CalendarBooking updateCalendarBooking(
+			long calendarBookingId, long calendarId,
+			Map<Locale, String> titleMap, Map<Locale, String> descriptionMap,
+			Map<Locale, String> locationMap, String type, int status,
+			int startDateMonth, int startDateDay, int startDateYear,
+			int startDateHour, int startDateMinute, int endDateMonth,
+			int endDateDay, int endDateYear, int endDateHour, int endDateMinute,
+			boolean allDay, String recurrence, int priority,
+			boolean outOfOffice, int firstReminder, int secondReminder,
+			boolean required, String requestMessage, String responseMessage,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		CalendarBookingPermission.check(
+			getPermissionChecker(), calendarBookingId, ActionKeys.UPDATE);
+
+		return calendarBookingLocalService.updateCalendarBooking(
+			getUserId(), calendarBookingId, calendarId, titleMap,
+			descriptionMap, locationMap, type, status, startDateMonth,
+			startDateDay, startDateYear, startDateHour, startDateMinute,
+			endDateMonth, endDateDay, endDateYear, endDateHour, endDateMinute,
+			allDay, recurrence, priority, outOfOffice, firstReminder,
+			secondReminder, required, requestMessage, responseMessage,
+			serviceContext);
+	}
+
 }
