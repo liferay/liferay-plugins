b59e66ccf3042dd66d7e096471a38439d343817f
fabiopezzutto <fabio.pezzutto@smc.it>
2012-02-09 10:33:04 +0100

    Add getters to CalendarResourceLocalService

--- portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarResourceLocalServiceImpl.java
+++ portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarResourceLocalServiceImpl.java
@@ -18,8 +18,20 @@ import com.liferay.calendar.DuplicateCalendarResourceException;
 import com.liferay.calendar.model.CalendarResource;
 import com.liferay.calendar.service.base.CalendarResourceLocalServiceBaseImpl;
 import com.liferay.calendar.util.CalendarResourceUtil;
+import com.liferay.calendar.util.CalendarUtil;
+import com.liferay.portal.kernel.dao.orm.Criterion;
+import com.liferay.portal.kernel.dao.orm.Disjunction;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.Junction;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.User;
@@ -27,6 +39,8 @@ import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.util.PortalUtil;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -121,6 +135,40 @@ public class CalendarResourceLocalServiceImpl
 		deleteCalendarResource(calendarResource);
 	}
 
+	public CalendarResource getCalendarResource(long calendarResourceId)
+		throws PortalException, SystemException {
+
+		return calendarResourcePersistence.findByPrimaryKey(
+			calendarResourceId);
+	}
+
+	public List<CalendarResource> search(
+			long groupId, long classNameId, long classPK, String code,
+			String name, String description, String type, Boolean active,
+			boolean andOperator, int start, int end,
+			OrderByComparator orderByComparator)
+		throws SystemException {
+
+		DynamicQuery dynamicQuery = buildDynamicQuery(
+			groupId, classNameId, classPK, code, name, description, type,
+			active, andOperator);
+
+		return dynamicQuery(dynamicQuery, start, end, orderByComparator);
+	}
+
+	public long searchCount(
+			long groupId, long classNameId, long classPK, String code,
+			String name, String description, String type, Boolean active,
+			boolean andOperator)
+		throws SystemException {
+
+		DynamicQuery dynamicQuery = buildDynamicQuery(
+			groupId, classNameId, classPK, code, name, description, type,
+			active, andOperator);
+
+		return dynamicQueryCount(dynamicQuery);
+	}
+
 	public CalendarResource updateCalendarResource(
 			long calendarResourceId, String code, Map<Locale, String> nameMap,
 			Map<Locale, String> descriptionMap, String type, boolean active,
@@ -147,6 +195,85 @@ public class CalendarResourceLocalServiceImpl
 			calendarResource, serviceContext);
 
 		return calendarResource;
+	}
+
+	protected DynamicQuery buildDynamicQuery(
+		long groupId, long classNameId, long classPK, String code, String name,
+		String description, String type, Boolean active, boolean andOperator) {
+
+		Junction junction = null;
+
+		if (andOperator) {
+			junction = RestrictionsFactoryUtil.conjunction();
+		}
+		else {
+			junction = RestrictionsFactoryUtil.disjunction();
+		}
+
+		Map<String, String> terms = new HashMap<String, String>();
+
+		if (Validator.isNotNull(code)) {
+			terms.put("code", code);
+		}
+
+		if (Validator.isNotNull(name)) {
+			terms.put("name", name);
+		}
+
+		if (Validator.isNotNull(description)) {
+			terms.put("description", description);
+		}
+
+		for (Map.Entry<String, String> entry : terms.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+
+			Disjunction disjunction = RestrictionsFactoryUtil.disjunction();
+
+			for (String keyword : CalendarUtil.splitKeywords(value)) {
+				Criterion criterion = RestrictionsFactoryUtil.ilike(
+					key, StringUtil.quote(keyword, StringPool.PERCENT));
+
+				disjunction.add(criterion);
+			}
+
+			junction.add(disjunction);
+		}
+
+		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(
+			CalendarResource.class, getClass().getClassLoader());
+
+		if (groupId > 0) {
+			Property property = PropertyFactoryUtil.forName("groupId");
+
+			dynamicQuery.add(property.eq(groupId));
+		}
+
+		if (classNameId > 0) {
+			Property property = PropertyFactoryUtil.forName("classNameId");
+
+			dynamicQuery.add(property.eq(classNameId));
+		}
+
+		if (classPK > 0) {
+			Property property = PropertyFactoryUtil.forName("classPK");
+
+			dynamicQuery.add(property.eq(classPK));
+		}
+
+		if (Validator.isNotNull(type)) {
+			Property property = PropertyFactoryUtil.forName("type");
+
+			dynamicQuery.add(property.eq(type));
+		}
+
+		if (active != null) {
+			Property property = PropertyFactoryUtil.forName("active");
+
+			dynamicQuery.add(property.eq(active));
+		}
+
+		return dynamicQuery.add(junction);
 	}
 
 	protected void validate(long classNameId, long classPK)
