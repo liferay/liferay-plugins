1b5a5b9b5dc6444798838d1f652080554feb5330
Wesley Gong <wesley.gong@liferay.com>
2012-02-16 18:23:42 -0800

    LPS-25623 LPS-25259 (reverse-merged from commit 61e6fa0903e8a1a785dc7f3dc94ffe78149ac85e)

--- webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/service/persistence/KaleoTaskInstanceTokenFinderImpl.java
+++ webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/service/persistence/KaleoTaskInstanceTokenFinderImpl.java
@@ -34,7 +34,6 @@ import com.liferay.portal.service.UserGroupRoleLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
 import com.liferay.portal.workflow.kaleo.model.KaleoTaskInstanceToken;
-import com.liferay.portal.workflow.kaleo.model.impl.KaleoTaskInstanceTokenImpl;
 import com.liferay.portal.workflow.kaleo.util.RoleUtil;
 import com.liferay.util.dao.orm.CustomSQLUtil;
 
@@ -102,9 +101,24 @@ public class KaleoTaskInstanceTokenFinderImpl
 			SQLQuery q = buildKaleoTaskInstanceTokenQuerySQL(
 				kaleoTaskInstanceTokenQuery, false, session);
 
-			return (List<KaleoTaskInstanceToken>)QueryUtil.list(
+			List<KaleoTaskInstanceToken> kaleoTaskInstanceTokens =
+				new ArrayList<KaleoTaskInstanceToken>();
+
+			Iterator<Long> itr = (Iterator<Long>)QueryUtil.iterate(
 				q, getDialect(), kaleoTaskInstanceTokenQuery.getStart(),
 				kaleoTaskInstanceTokenQuery.getEnd());
+
+			while (itr.hasNext()) {
+				long kaleoTaskInstanceTokenId = itr.next();
+
+				KaleoTaskInstanceToken kaleoTaskInstanceToken =
+					KaleoTaskInstanceTokenUtil.findByPrimaryKey(
+						kaleoTaskInstanceTokenId);
+
+				kaleoTaskInstanceTokens.add(kaleoTaskInstanceToken);
+			}
+
+			return kaleoTaskInstanceTokens;
 		}
 		catch (Exception e) {
 			throw new SystemException(e);
@@ -246,8 +260,7 @@ public class KaleoTaskInstanceTokenFinderImpl
 			q.addScalar(COUNT_COLUMN_NAME, Type.LONG);
 		}
 		else {
-			q.addEntity(
-				"KaleoTaskInstanceToken", KaleoTaskInstanceTokenImpl.class);
+			q.addScalar("KaleoTaskInstanceTokenId", Type.LONG);
 		}
 
 		QueryPos qPos = QueryPos.getInstance(q);
