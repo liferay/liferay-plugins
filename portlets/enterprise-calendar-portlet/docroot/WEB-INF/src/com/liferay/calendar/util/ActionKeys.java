ffb06830f1d9f73580243e1f29b35d8de3007684
Brian Chan <brian.chan@liferay.com>
2012-02-17 11:29:08 -0800

    LPS-25061 Sort

--- portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/ActionKeys.java
+++ portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/ActionKeys.java
@@ -23,11 +23,11 @@ package com.liferay.calendar.util;
 public class ActionKeys
 	extends com.liferay.portal.security.permission.ActionKeys {
 
-	public static final String MANAGE_BOOKINGS = "MANAGE_BOOKINGS";
-
 	public static final String ADD_CALENDAR = "ADD_CALENDAR";
 
 	public static final String ADD_RESOURCE = "ADD_RESOURCE";
+
+	public static final String MANAGE_BOOKINGS = "MANAGE_BOOKINGS";
 
 	public static final String VIEW_BOOKING_DETAILS = "VIEW_BOOKING_DETAILS";
 
