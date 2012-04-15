e60eb20fadb5fb8d024d23792c4ebd863e9d8b05
Brian Chan <brian.chan@liferay.com>
2012-04-15 16:53:14 -0700

    LPS-26321 Protect against hook servlet-filter

---
+++ portlets/test-pacl-portlet/docroot/WEB-INF/src/com/liferay/testpacl/hook/filter/TestFilter.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.testpacl.hook.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class TestFilter implements Filter {
+
+	public void destroy() {
+	}
+
+	public void doFilter(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			FilterChain filterChain)
+		throws IOException, ServletException {
+
+		if (true) {
+			throw new ServletException(
+				"Hook servlet filters should not have been allowed");
+		}
+
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+}
