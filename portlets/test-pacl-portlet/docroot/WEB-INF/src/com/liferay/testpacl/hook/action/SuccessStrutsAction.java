3df79d491ae2169854ce4550cd3a2c780defcf9b
Brian Chan <brian.chan@liferay.com>
2012-04-15 23:09:27 -0700

    LPS-26321 Protect against hook struts-action

---
+++ portlets/test-pacl-portlet/docroot/WEB-INF/src/com/liferay/testpacl/hook/action/SuccessStrutsAction.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.testpacl.hook.action;
+
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class SuccessStrutsAction extends BaseStrutsAction {
+
+	public static boolean isInstantiated() {
+		return _instantiated;
+	}
+
+	public SuccessStrutsAction() {
+		_instantiated = true;
+	}
+
+	@Override
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		return null;
+	}
+
+	private static boolean _instantiated;
+
+}
