b84a58258466226ae6efcd1ab00c4257cdd99bd1
Neil Griffin <neil.griffin.scm@gmail.com>
2012-04-25 12:26:05 -0400

    LPS-26874 Support Ajax-based file upload for WSRP

--- portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/bind/V2MarkupServiceImpl.java
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/bind/V2MarkupServiceImpl.java
@@ -268,6 +268,44 @@ public class V2MarkupServiceImpl
 
 		ResourceParams resourceParams = getResource.getResourceParams();
 
+		UploadContext[] uploadContexts = resourceParams.getUploadContexts();
+
+		if (uploadContexts != null) {
+			for (UploadContext uploadContext : uploadContexts) {
+				NamedString mimeAttribute = uploadContext.getMimeAttributes(0);
+
+				String[] mimeAttributeValues = StringUtil.split(
+					mimeAttribute.getValue(), StringPool.SEMICOLON);
+
+				String name = StringUtil.replace(
+					mimeAttributeValues[1], "name=", StringPool.BLANK);
+
+				name = StringUtil.trim(name);
+
+				String fileName = StringUtil.replace(
+					mimeAttributeValues[2], "filename=", StringPool.BLANK);
+
+				fileName = StringUtil.trim(fileName);
+
+				String contentType = uploadContext.getMimeType();
+
+				String charSet = null;
+
+				if (contentType.contains(StringPool.SEMICOLON)) {
+					int pos = contentType.indexOf(StringPool.SEMICOLON);
+
+					charSet = contentType.substring(pos + 1);
+					charSet = StringUtil.trim(charSet);
+
+					contentType = contentType.substring(0, pos);
+				}
+
+				httpOptions.addFilePart(
+					name, fileName, uploadContext.getUploadData(), contentType,
+					charSet);
+			}
+		}
+
 		NamedString[] formParameters = resourceParams.getFormParameters();
 
 		if (formParameters != null) {
