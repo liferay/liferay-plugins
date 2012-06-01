945614e3e3ae92daa35f187d4171c47717c27b2a
Brian Chan <brian.chan@liferay.com>
2012-06-01 14:11:16 -0700

    LPS-27668 Source formatting

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/KBCommentServiceImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/KBCommentServiceImpl.java
@@ -23,18 +23,7 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.service.ServiceContext;
 
 /**
- * The implementation of the k b comment remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.knowledgebase.service.KBCommentService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see com.liferay.knowledgebase.service.base.KBCommentServiceBaseImpl
- * @see com.liferay.knowledgebase.service.KBCommentServiceUtil
  */
 public class KBCommentServiceImpl extends KBCommentServiceBaseImpl {
 
