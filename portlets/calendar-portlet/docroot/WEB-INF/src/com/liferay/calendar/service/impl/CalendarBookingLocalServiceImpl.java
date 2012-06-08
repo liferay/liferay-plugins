dacc834cd9181dfa07a03a7d8754c86bad12afba
fabiopezzutto <fabio.pezzutto@smc.it>
2012-06-09 00:36:45 +0200

    LPS-27469 Fix reminder subject and body text

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
@@ -573,7 +573,8 @@ public class CalendarBookingLocalServiceImpl
 				new String[] {
 					calendarBooking.getLocation(),
 					dateFormatDateTime.format(startDate.getTime()),
-					calendarBooking.getTitle(), fromAddress, fromName,
+					calendarBooking.getTitle(user.getLocale()), fromAddress,
+					fromName,
 					company.getPortalURL(calendarBooking.getGroupId()),
 					HtmlUtil.escape(toAddress),
 					HtmlUtil.escape(toName),
@@ -584,8 +585,7 @@ public class CalendarBookingLocalServiceImpl
 				new String[] {
 					"[$BOOKING_LOCATION$]", "[$BOOKING_START_DATE$]",
 					"[$BOOKING_TITLE$]", "[$FROM_ADDRESS$]", "[$FROM_NAME$]",
-					"[$PORTAL_URL$]", "[$PORTLET_NAME$]", "[$TO_ADDRESS$]",
-					"[$TO_NAME$]"
+					"[$PORTAL_URL$]", "[$TO_ADDRESS$]", "[$TO_NAME$]"
 				},
 				new String[] {
 					calendarBooking.getLocation(),
