fed0c6bfa99a9efd41cd08a8ff9fe6d68e883749
Jonathan Lee <Jonathan.Lee@liferay.com>
2012-06-21 19:38:04 -0700

    SOS-815 upgrade contacts portlet

---
+++ portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/hook/upgrade/UpgradeProcess_2_0_0.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contacts.hook.upgrade;
+
+import com.liferay.contacts.hook.upgrade.v2_0_0.UpgradeEntry;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Jonathan Lee
+ */
+public class UpgradeProcess_2_0_0 extends UpgradeProcess {
+
+	@Override
+	public int getThreshold() {
+		return 200;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		upgrade(UpgradeEntry.class);
+	}
+
+}
