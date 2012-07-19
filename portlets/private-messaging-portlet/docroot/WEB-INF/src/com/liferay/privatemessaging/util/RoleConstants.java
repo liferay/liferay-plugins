84ca85db795cdb549df1caa4bf68664c96f4d33a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2012-07-19 14:13:30 +0200

    SOS-1134

---
+++ portlets/private-messaging-portlet/docroot/WEB-INF/src/com/liferay/privatemessaging/util/RoleConstants.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.privatemessaging.util;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class RoleConstants extends com.liferay.portal.model.RoleConstants {
+
+	public static final String SOCIAL_OFFICE_USER = "Social Office User";
+
+}
