bbdb3c048af9b032129eb76a947c94809752ec2b
Brian Chan <brian.chan@liferay.com>
2012-07-20 17:26:50 -0700

    LPS-28356 Remote service is not needed? bug is already fixed?

--- portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/model/impl/EntryModelImpl.java
+++ portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/model/impl/EntryModelImpl.java
@@ -16,11 +16,9 @@ package com.liferay.contacts.model.impl;
 
 import com.liferay.contacts.model.Entry;
 import com.liferay.contacts.model.EntryModel;
-import com.liferay.contacts.model.EntrySoap;
 
 import com.liferay.portal.kernel.bean.AutoEscapeBeanHandler;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.json.JSON;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -37,10 +35,8 @@ import java.io.Serializable;
 
 import java.sql.Types;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -56,7 +52,6 @@ import java.util.Map;
  * @see com.liferay.contacts.model.EntryModel
  * @generated
  */
-@JSON(strict = true)
 public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 	/*
 	 * NOTE FOR DEVELOPERS:
@@ -94,54 +89,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 			true);
 	public static long EMAILADDRESS_COLUMN_BITMASK = 1L;
 	public static long USERID_COLUMN_BITMASK = 2L;
-
-	/**
-	 * Converts the soap model instance into a normal model instance.
-	 *
-	 * @param soapModel the soap model instance to convert
-	 * @return the normal model instance
-	 */
-	public static Entry toModel(EntrySoap soapModel) {
-		if (soapModel == null) {
-			return null;
-		}
-
-		Entry model = new EntryImpl();
-
-		model.setEntryId(soapModel.getEntryId());
-		model.setGroupId(soapModel.getGroupId());
-		model.setCompanyId(soapModel.getCompanyId());
-		model.setUserId(soapModel.getUserId());
-		model.setUserName(soapModel.getUserName());
-		model.setCreateDate(soapModel.getCreateDate());
-		model.setModifiedDate(soapModel.getModifiedDate());
-		model.setFullName(soapModel.getFullName());
-		model.setEmailAddress(soapModel.getEmailAddress());
-		model.setComments(soapModel.getComments());
-
-		return model;
-	}
-
-	/**
-	 * Converts the soap model instances into normal model instances.
-	 *
-	 * @param soapModels the soap model instances to convert
-	 * @return the normal model instances
-	 */
-	public static List<Entry> toModels(EntrySoap[] soapModels) {
-		if (soapModels == null) {
-			return null;
-		}
-
-		List<Entry> models = new ArrayList<Entry>(soapModels.length);
-
-		for (EntrySoap soapModel : soapModels) {
-			models.add(toModel(soapModel));
-		}
-
-		return models;
-	}
-
 	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(com.liferay.util.service.ServiceProps.get(
 				"lock.expiration.time.com.liferay.contacts.model.Entry"));
 
@@ -253,7 +200,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		}
 	}
 
-	@JSON
 	public long getEntryId() {
 		return _entryId;
 	}
@@ -262,7 +208,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		_entryId = entryId;
 	}
 
-	@JSON
 	public long getGroupId() {
 		return _groupId;
 	}
@@ -271,7 +216,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		_groupId = groupId;
 	}
 
-	@JSON
 	public long getCompanyId() {
 		return _companyId;
 	}
@@ -280,7 +224,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		_companyId = companyId;
 	}
 
-	@JSON
 	public long getUserId() {
 		return _userId;
 	}
@@ -309,7 +252,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		return _originalUserId;
 	}
 
-	@JSON
 	public String getUserName() {
 		if (_userName == null) {
 			return StringPool.BLANK;
@@ -323,7 +265,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		_userName = userName;
 	}
 
-	@JSON
 	public Date getCreateDate() {
 		return _createDate;
 	}
@@ -332,7 +273,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		_createDate = createDate;
 	}
 
-	@JSON
 	public Date getModifiedDate() {
 		return _modifiedDate;
 	}
@@ -341,7 +281,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		_modifiedDate = modifiedDate;
 	}
 
-	@JSON
 	public String getFullName() {
 		if (_fullName == null) {
 			return StringPool.BLANK;
@@ -357,7 +296,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		_fullName = fullName;
 	}
 
-	@JSON
 	public String getEmailAddress() {
 		if (_emailAddress == null) {
 			return StringPool.BLANK;
@@ -381,7 +319,6 @@ public class EntryModelImpl extends BaseModelImpl<Entry> implements EntryModel {
 		return GetterUtil.getString(_originalEmailAddress);
 	}
 
-	@JSON
 	public String getComments() {
 		if (_comments == null) {
 			return StringPool.BLANK;
