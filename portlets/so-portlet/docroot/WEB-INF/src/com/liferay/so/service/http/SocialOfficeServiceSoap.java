8db82524e6c0054512e793c1d09e8f27f9ded830
Brian Chan <brian.chan@liferay.com>
2012-07-23 13:33:40 -0700

    LPS-28052 Regenerate

--- portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/service/http/SocialOfficeServiceSoap.java
+++ portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/service/http/SocialOfficeServiceSoap.java
@@ -38,9 +38,8 @@ import java.rmi.RemoteException;
  * </p>
  *
  * <p>
- * You can see a list of services at
- * http://localhost:8080/api/secure/axis. Set the property
- * <b>axis.servlet.hosts.allowed</b> in portal.properties to configure
+ * You can see a list of services at http://localhost:8080/api/axis. Set the
+ * property <b>axis.servlet.hosts.allowed</b> in portal.properties to configure
  * security.
  * </p>
  *
