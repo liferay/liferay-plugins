ec9ee97184ff7db6b76cae7dfaa5d7b02af40608
Brian Chan <brian.chan@liferay.com>
2012-07-26 17:40:08 -0700

    Typo

--- webs/wurfl-web/docroot/WEB-INF/src/com/liferay/portal/mobile/device/wurfl/util/PortletPropsKeys.java
+++ webs/wurfl-web/docroot/WEB-INF/src/com/liferay/portal/mobile/device/wurfl/util/PortletPropsKeys.java
@@ -7,7 +7,7 @@
  *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  */
 
