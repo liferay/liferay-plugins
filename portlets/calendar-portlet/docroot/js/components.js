07fe045945d7af09b7879289c747a0007ad8a796
Marcellus Tavares <marcellus.tavares@liferay.com>
2012-08-22 17:20:18 -0300

    LPS-28447 GPR Calendar - Add deletion support for recurring events

--- portlets/calendar-portlet/docroot/js/components.js
+++ portlets/calendar-portlet/docroot/js/components.js
@@ -905,6 +905,8 @@
 		'liferay-calendar-recurrence-util',
 		function(A) {
 			Liferay.RecurrenceUtil = {
+				confirmationPanel: null,
+
 				FREQUENCY: {
 					DAILY: 'DAILY',
 					WEEKLY: 'WEEKLY',
@@ -982,6 +984,91 @@
 					);
 
 					return A.Lang.String.capitalize(summary);
+				},
+				
+				hideConfirmationPanel: function() {
+					var instance = this;
+					
+					if (instance.confirmationPanel) {
+						instance.confirmationPanel.hide();
+					}
+				},
+				
+				openConfirmationPanel: function(schedulerEvent, actionName, onlyThisInstanceFn, allFollowingFn, allEventsInFn) {
+					var instance = this;
+
+					var content = [];
+					var title;
+
+					if (actionName === 'delete') {
+						title = A.Lang.sub(
+							'<h4>{title}</h4>', 
+							{ 
+								title: Liferay.Language.get('delete-recurring-event')
+							}
+						)
+
+						content.push(Liferay.Language.get('would-you-like-to-delete-only-this-event-all-events-in-the-series-or-this-and-all-future-events-in-the-series'));
+					}
+					else {
+						title = A.Lang.sub(
+							'<h4>{title}</h4>', 
+							{ 
+								title: Liferay.Language.get('change-recurring-event')
+							}
+						)
+
+						content.push(Liferay.Language.get('would-you-like-to-change-only-this-event-all-events-in-the-series-or-this-and-all-future-events-in-the-series'));
+					}
+					
+					if (schedulerEvent.isMasterBooking()) {
+						content.push(
+							A.Lang.sub(
+							'<br/><br/><b>{cancelMeetingMessage}</b>', 
+							{ 
+								cancelMeetingMessage: Liferay.Language.get('deleting-this-event-will-cancel-the-meeting-with-your-guests')
+							}
+						));
+					}
+
+					if (!instance.confirmationPanel) {
+						instance.confirmationPanel = new A.Panel({
+							bodyContent: content.join(''),
+							headerContent: title,
+							buttons: [
+								{
+									value: 'Only this instance',
+									action: 'onlyThisInstanceFn'
+								},
+
+								{
+									value: 'All following',
+									action: 'allFollowingFn'
+								},
+
+								{
+									value: 'All events in the series',
+									action: 'allEventsInFn'
+								},
+
+								{
+									value: 'Cancel this change',
+									action: 'hide'
+								}
+							],
+							centered: true,
+							modal: true,
+							plugins: [A.Plugin.Drag],
+							visible: false,
+							width: 550,
+							zIndex: 1000
+						});
+					}
+
+					instance.confirmationPanel.onlyThisInstanceFn = onlyThisInstanceFn;
+					instance.confirmationPanel.allFollowingFn = allFollowingFn;
+					instance.confirmationPanel.allEventsInFn = allEventsInFn;
+					instance.confirmationPanel.render().show();
 				}
 			};
 		},
