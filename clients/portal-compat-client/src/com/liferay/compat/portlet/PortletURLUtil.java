b6b1c40f03d41a60573e6ebc514f9ab163d6829f
Brian Chan <brian.chan@liferay.com>
2012-09-07 13:51:54 -0700

    LPS-29742 Compat

---
+++ clients/portal-compat-client/src/com/liferay/compat/portlet/PortletURLUtil.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.compat.portlet;
+
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+
+import java.util.Enumeration;
+
+import javax.portlet.PortletURL;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class PortletURLUtil extends com.liferay.portlet.PortletURLUtil {
+
+	public static PortletURL getCurrent(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		PortletURL portletURL = liferayPortletResponse.createRenderURL();
+
+		Enumeration<String> enu = liferayPortletRequest.getParameterNames();
+
+		while (enu.hasMoreElements()) {
+			String param = enu.nextElement();
+			String[] values = liferayPortletRequest.getParameterValues(param);
+
+			boolean addParam = true;
+
+			// Don't set paramter values that are over 32 kb. See LEP-1755.
+
+			for (int i = 0; i < values.length; i++) {
+				if (values[i].length() > _CURRENT_URL_PARAMETER_THRESHOLD) {
+					addParam = false;
+
+					break;
+				}
+			}
+
+			if (addParam) {
+				portletURL.setParameter(param, values);
+			}
+		}
+
+		return portletURL;
+	}
+
+	private static final int _CURRENT_URL_PARAMETER_THRESHOLD = 32768;
+
+}
