01f36f4a3cc8d6d2a66965ffa4de7e3e33dd1169
Brian Chan <brian.chan@liferay.com>
2012-09-12 18:42:27 -0700

    LPS-29813 Compatibility for ee-6.1.x Marketplace

---
+++ clients/portal-compat-client/src/com/liferay/portal/kernel/transaction/TransactionCommitCallbackRegistryUtil.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.transaction;
+
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+
+import java.lang.reflect.Method;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author Michael C. Han
+ */
+public class TransactionCommitCallbackRegistryUtil {
+
+	public static void registerCallback(Callable<?> callable) {
+		try {
+			ClassLoader classLoader = PortalClassLoaderUtil.getClassLoader();
+
+			Class<?> clazz = classLoader.loadClass(
+				"com.liferay.portal.spring.transaction." +
+					"TransactionCommitCallbackUtil");
+
+			Method method = clazz.getMethod("registerCallback", Callable.class);
+
+			method.setAccessible(true);
+
+			method.invoke(null, callable);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
