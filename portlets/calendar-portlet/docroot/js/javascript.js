be27b10119027597043d8456bbd773a1c362337a
Bruno Basto <bruno.basto@liferay.com>
2012-09-14 20:16:26 -0300

    LPS-29912 GPR Calendar - Show icons for repeated events, meetings and events with reminders

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -1022,7 +1022,7 @@ AUI.add(
 
 					firstReminder: {
 						setter: toInt,
-						value: 3600000
+						value: 0
 					},
 
 					firstReminderType: {
@@ -1048,6 +1048,22 @@ AUI.add(
 					recurrence: {
 						validator: isString,
 						value: STR_BLANK
+					},
+
+					reminder: {
+						getter: function() {
+							var instance = this;
+
+							return (instance.get('firstReminder') > 0) || (instance.get('secondReminder') > 0);
+						}
+					},
+
+					repeated: {
+						getter: function() {
+							var instance = this;
+
+							return instance.isRecurring();
+						}
 					},
 
 					secondReminder: {
