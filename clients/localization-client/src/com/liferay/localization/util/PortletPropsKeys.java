40102a052d40b585b68a12ba38d87a402970f100
Brian Chan <brian.chan@liferay.com>
2012-09-19 19:12:05 -0700

    LPS-29576

---
+++ clients/localization-client/src/com/liferay/localization/util/PortletPropsKeys.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.localization.util;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public interface PortletPropsKeys {
+
+	public static final String LANGUAGE_ID = "language.id";
+
+	public static final String ROLE_ADMINISTRATOR_DESCRIPTION =
+		"role.administrator.description";
+
+	public static final String ROLE_ADMINISTRATOR_NAME =
+		"role.administrator.name";
+
+	public static final String ROLE_GUEST_DESCRIPTION =
+		"role.guest.description";
+
+	public static final String ROLE_GUEST_NAME = "role.guest.name";
+
+	public static final String ROLE_ORGANIZATION_ADMINISTRATOR_DESCRIPTION =
+		"role.organization.administrator.description";
+
+	public static final String ROLE_ORGANIZATION_ADMINISTRATOR_NAME =
+		"role.organization.administrator.name";
+
+	public static final String ROLE_ORGANIZATION_OWNER_DESCRIPTION =
+		"role.organization.owner.description";
+
+	public static final String ROLE_ORGANIZATION_OWNER_NAME =
+		"role.organization.owner.name";
+
+	public static final String ROLE_ORGANIZATION_USER_DESCRIPTION =
+		"role.organization.owner.description";
+
+	public static final String ROLE_ORGANIZATION_USER_NAME =
+		"role.organization.owner.name";
+
+	public static final String ROLE_OWNER_DESCRIPTION =
+		"role.owner.description";
+
+	public static final String ROLE_OWNER_NAME = "role.owner.name";
+
+	public static final String ROLE_POWER_USER_DESCRIPTION =
+		"role.power.user.description";
+
+	public static final String ROLE_POWER_USER_NAME = "role.power.user.name";
+
+	public static final String ROLE_SITE_ADMINISTRATOR_DESCRIPTION =
+		"role.site.administrator.description";
+
+	public static final String ROLE_SITE_ADMINISTRATOR_NAME =
+		"role.site.administrator.name";
+
+	public static final String ROLE_SITE_MEMBER_DESCRIPTION =
+		"role.site.member.description";
+
+	public static final String ROLE_SITE_MEMBER_NAME = "role.site.member.name";
+
+	public static final String ROLE_SITE_OWNER_DESCRIPTION =
+		"role.site.owner.description";
+
+	public static final String ROLE_SITE_OWNER_NAME = "role.site.owner.name";
+
+	public static final String ROLE_USER_DESCRIPTION = "role.user.description";
+
+	public static final String ROLE_USER_NAME = "role.user.name";
+
+}
---
+++ clients/localization-client/src/com/liferay/localization/util/PortletPropsValues.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.localization.util;
+
+import com.liferay.util.portlet.PortletProps;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class PortletPropsValues {
+
+	public static final String LANGUAGE_ID = PortletProps.get(
+		PortletPropsKeys.LANGUAGE_ID);
+
+	public static final String ROLE_ADMINISTRATOR_DESCRIPTION =
+		PortletProps.get(PortletPropsKeys.ROLE_ADMINISTRATOR_DESCRIPTION);
+
+	public static final String ROLE_ADMINISTRATOR_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_ADMINISTRATOR_NAME);
+
+	public static final String ROLE_GUEST_DESCRIPTION = PortletProps.get(
+		PortletPropsKeys.ROLE_GUEST_DESCRIPTION);
+
+	public static final String ROLE_GUEST_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_GUEST_NAME);
+
+	public static final String ROLE_ORGANIZATION_ADMINISTRATOR_DESCRIPTION =
+		PortletProps.get(
+			PortletPropsKeys.ROLE_ORGANIZATION_ADMINISTRATOR_DESCRIPTION);
+
+	public static final String ROLE_ORGANIZATION_ADMINISTRATOR_NAME =
+		PortletProps.get(PortletPropsKeys.ROLE_ORGANIZATION_ADMINISTRATOR_NAME);
+
+	public static final String ROLE_ORGANIZATION_OWNER_DESCRIPTION =
+		PortletProps.get(PortletPropsKeys.ROLE_ORGANIZATION_OWNER_DESCRIPTION);
+
+	public static final String ROLE_ORGANIZATION_OWNER_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_ORGANIZATION_OWNER_NAME);
+
+	public static final String ROLE_ORGANIZATION_USER_DESCRIPTION =
+		PortletProps.get(PortletPropsKeys.ROLE_ORGANIZATION_USER_DESCRIPTION);
+
+	public static final String ROLE_ORGANIZATION_USER_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_ORGANIZATION_USER_NAME);
+
+	public static final String ROLE_OWNER_DESCRIPTION = PortletProps.get(
+		PortletPropsKeys.ROLE_OWNER_DESCRIPTION);
+
+	public static final String ROLE_OWNER_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_OWNER_NAME);
+
+	public static final String ROLE_POWER_USER_DESCRIPTION = PortletProps.get(
+		PortletPropsKeys.ROLE_POWER_USER_DESCRIPTION);
+
+	public static final String ROLE_POWER_USER_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_POWER_USER_NAME);
+
+	public static final String ROLE_SITE_ADMINISTRATOR_DESCRIPTION =
+		PortletProps.get(PortletPropsKeys.ROLE_SITE_ADMINISTRATOR_DESCRIPTION);
+
+	public static final String ROLE_SITE_ADMINISTRATOR_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_SITE_ADMINISTRATOR_NAME);
+
+	public static final String ROLE_SITE_MEMBER_DESCRIPTION = PortletProps.get(
+		PortletPropsKeys.ROLE_SITE_MEMBER_DESCRIPTION);
+
+	public static final String ROLE_SITE_MEMBER_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_SITE_MEMBER_NAME);
+
+	public static final String ROLE_SITE_OWNER_DESCRIPTION = PortletProps.get(
+		PortletPropsKeys.ROLE_SITE_OWNER_DESCRIPTION);
+
+	public static final String ROLE_SITE_OWNER_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_SITE_OWNER_NAME);
+
+	public static final String ROLE_USER_DESCRIPTION = PortletProps.get(
+		PortletPropsKeys.ROLE_USER_DESCRIPTION);
+
+	public static final String ROLE_USER_NAME = PortletProps.get(
+		PortletPropsKeys.ROLE_USER_NAME);
+
+}
