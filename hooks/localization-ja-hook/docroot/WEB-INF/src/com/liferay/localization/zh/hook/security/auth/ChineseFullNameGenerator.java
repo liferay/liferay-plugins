e1d1d7c1d2ac9787c39e1525bc21cb0e53035097
samuel.kong <samuel.kong@liferay.com>
2012-09-29 09:09:53 +0800

    LPS-29577 Copy from localization-zh-hook at e09fc19d0f45bc7c1043247e3a16537f0d25be14

---
+++ hooks/localization-ja-hook/docroot/WEB-INF/src/com/liferay/localization/zh/hook/security/auth/ChineseFullNameGenerator.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.localization.zh.hook.security.auth;
+
+import com.liferay.localization.zh.util.LocalizationZHUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.auth.FamilyNameFirstFullNameGenerator;
+
+/**
+ * @author Andrew Yang
+ * @author Samuel Kong
+ */
+public class ChineseFullNameGenerator extends FamilyNameFirstFullNameGenerator {
+
+	@Override
+	public String[] splitFullName(String fullName) {
+		if (!LocalizationZHUtil.isCJKUnifiedIdeographString(fullName)) {
+			return super.splitFullName(fullName);
+		}
+
+		String firstName = StringPool.BLANK;
+		String middleName = StringPool.BLANK;
+		String lastName = StringPool.BLANK;
+
+		if (Validator.isNotNull(fullName)) {
+			if (fullName.length() == 1) {
+				firstName = fullName;
+			}
+
+			firstName = fullName.substring(1);
+			lastName = fullName.substring(0, 1);
+		}
+
+		return new String[] {firstName, middleName, lastName};
+	}
+
+	@Override
+	protected String buildFullName(
+		String firstName, String middleName, String lastName,
+		boolean useInitials) {
+
+		if (!LocalizationZHUtil.isCJKUnifiedIdeographString(firstName) &&
+			!LocalizationZHUtil.isCJKUnifiedIdeographString(middleName) &&
+			!LocalizationZHUtil.isCJKUnifiedIdeographString(lastName)) {
+
+			return super.buildFullName(
+				firstName, middleName, lastName, useInitials);
+		}
+
+		if (Validator.isNull(lastName)) {
+			return firstName;
+		}
+
+		return lastName + firstName;
+	}
+
+}
