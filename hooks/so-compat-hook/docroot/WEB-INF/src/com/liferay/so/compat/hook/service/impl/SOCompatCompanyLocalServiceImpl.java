8ad15430b955dfec69fb173260b869b6cf086b63
Brian Chan <brian.chan@liferay.com>
2012-10-03 08:56:29 -0700

    LPS-30228 Add ExpandoBridge when "checkCompany" is called. This method is called on startup for every existing company in PortalInstances (in core) and every time addCompany is called, so is the single place for adding this logic.

---
+++ hooks/so-compat-hook/docroot/WEB-INF/src/com/liferay/so/compat/hook/service/impl/SOCompatCompanyLocalServiceImpl.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.so.compat.hook.service.impl;
+
+import com.liferay.compat.portlet.documentlibrary.util.DLUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.service.CompanyLocalService;
+import com.liferay.portal.service.CompanyLocalServiceWrapper;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.expando.model.ExpandoBridge;
+import com.liferay.portlet.expando.model.ExpandoColumnConstants;
+import com.liferay.portlet.expando.util.ExpandoBridgeFactoryUtil;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class SOCompatCompanyLocalServiceImpl
+	extends CompanyLocalServiceWrapper {
+
+	public SOCompatCompanyLocalServiceImpl(
+		CompanyLocalService companyLocalService) {
+
+		super(companyLocalService);
+	}
+
+	@Override
+	public Company checkCompany(String webId)
+		throws PortalException, SystemException {
+
+		Company company = super.checkCompany(webId);
+
+		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
+			company.getCompanyId(), DLFileEntry.class.getName());
+
+		if (!expandoBridge.hasAttribute(DLUtil.MANUAL_CHECK_IN_REQUIRED)) {
+			expandoBridge.addAttribute(
+				DLUtil.MANUAL_CHECK_IN_REQUIRED, ExpandoColumnConstants.BOOLEAN,
+				false);
+		}
+
+		return company;
+	}
+
+}
