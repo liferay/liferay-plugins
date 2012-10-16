6af017ef4806945c1515c4c6c5889399ca8efb47
Evan Thibodeau <evan.thibodeau@liferay.com>
2012-10-15 17:10:01 -0700

    SOS-1189 SO Upgrade: Upgrading from SO 1.5 to 2.0 causes Bookmarks to not be present

---
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/hook/upgrade/v2_0_3/UpgradeBookmarks.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.so.hook.upgrade.v2_0_3;
+
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.LayoutSet;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.LayoutSetLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.bookmarks.model.BookmarksFolder;
+import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
+
+import java.util.List;
+
+/**
+ * @author Evan Thibodeau
+ */
+public class UpgradeBookmarks extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		List<Group> groups = GroupLocalServiceUtil.getGroups(
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		for (Group group : groups) {
+			boolean privateLayout = group.hasPrivateLayouts();
+
+			LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
+				group.getGroupId(), privateLayout);
+
+			String themeId = layoutSet.getThemeId();
+
+			if (!themeId.equals("so_WAR_sotheme")) {
+				continue;
+			}
+
+			List<BookmarksFolder> folders =
+				BookmarksFolderLocalServiceUtil.getFolders(group.getGroupId());
+
+			for (BookmarksFolder folder : folders) {
+				String folderName = folder.getName();
+
+				if (folderName.equals("Bookmarks")) {
+					BookmarksFolderLocalServiceUtil.updateFolder(
+						folder.getFolderId(), folder.getParentFolderId(),
+						folderName, folder.getDescription(), true,
+						new ServiceContext());
+				}
+			}
+		}
+	}
+
+}
