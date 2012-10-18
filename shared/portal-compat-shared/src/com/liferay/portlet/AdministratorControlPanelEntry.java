67c38ae18d7bae0668769c377e2847b8598ec0d5
Brian Chan <brian.chan@liferay.com>
2012-10-18 14:51:21 -0700

    LPS-30247

---
+++ shared/portal-compat-shared/src/com/liferay/portlet/AdministratorControlPanelEntry.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class AdministratorControlPanelEntry extends BaseControlPanelEntry {
+
+	public boolean isVisible(
+			PermissionChecker permissionChecker, Portlet portlet)
+		throws Exception {
+
+		return permissionChecker.isCompanyAdmin();
+	}
+
+	@Override
+	public boolean isVisible(
+			Portlet portlet, String category, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		PermissionChecker permissionChecker =
+			themeDisplay.getPermissionChecker();
+
+		return isVisible(permissionChecker, portlet);
+	}
+
+}
