4cf7ea744157d6fb2200958a6b208eefa02d3884
Brian Chan <brian.chan@liferay.com>
2012-10-26 20:26:06 -0700

    LPS-30665 Make it compatible in ee-6.1.x

---
+++ shared/portal-compat-shared/src/com/liferay/portal/kernel/messaging/HotDeployMessageListener.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.messaging;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class HotDeployMessageListener extends BaseMessageListener {
+
+	public HotDeployMessageListener(String servletContextName) {
+		_servletContextName = servletContextName;
+	}
+
+	@Override
+	protected void doReceive(Message message) throws Exception {
+		String servletContextName = GetterUtil.getString(
+			message.getString("servletContextName"));
+
+		if (!servletContextName.equals(_servletContextName)) {
+			return;
+		}
+
+		String command = GetterUtil.getString(message.getString("command"));
+
+		if (command.equals("deploy")) {
+			onDeploy();
+		}
+		else if (command.equals("undeploy")) {
+			onUndeploy();
+		}
+	}
+
+	protected void onDeploy() throws Exception {
+	}
+
+	protected void onUndeploy() throws Exception {
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		HotDeployMessageListener.class);
+
+	private String _servletContextName;
+
+}
