b52fdc9371b79ec0037a9fbfe4ab32f91f688eec
Brian Chan <brian.chan@liferay.com>
2012-10-28 21:31:09 -0700

    SOS-1281 Source formatting

--- hooks/portal-compat-hook/docroot/WEB-INF/src/com/liferay/compat/hook/service/impl/CompatDLFileEntryServiceImpl.java
+++ hooks/portal-compat-hook/docroot/WEB-INF/src/com/liferay/compat/hook/service/impl/CompatDLFileEntryServiceImpl.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -38,8 +38,8 @@ public class CompatDLFileEntryServiceImpl extends DLFileEntryServiceWrapper {
 	public DLFileVersion cancelCheckOut(long fileEntryId)
 		throws PortalException, SystemException {
 
-		DLFileEntry dlFileEntry =
-			DLFileEntryLocalServiceUtil.fetchDLFileEntry(fileEntryId);
+		DLFileEntry dlFileEntry = DLFileEntryLocalServiceUtil.fetchDLFileEntry(
+			fileEntryId);
 
 		PermissionChecker permissionChecker =
 			PermissionThreadLocal.getPermissionChecker();
