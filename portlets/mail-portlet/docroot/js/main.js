aef7f1c441ad17f3b96f82aecedb126b004f2e6a
Brian Chan <brian.chan@liferay.com>
2012-11-06 13:08:38 -0800

    Source formatting

--- portlets/mail-portlet/docroot/js/main.js
+++ portlets/mail-portlet/docroot/js/main.js
@@ -152,10 +152,10 @@ AUI.add(
 						dataType: 'json',
 						method: 'POST',
 						on: {
-							failure: function (event, id, obj) {
+							failure: function(event, id, obj) {
 								instance.setStatus('error', Liferay.Language.get('unable-to-connect-with-mail-server'));
 							},
-							success: function (event, id, obj) {
+							success: function(event, id, obj) {
 								var responseData = this.get('responseData');
 
 								instance.setStatus(responseData.status, responseData.message);
@@ -326,10 +326,10 @@ AUI.add(
 						dataType: 'json',
 						method: 'POST',
 						on: {
-							failure: function (event, id, obj) {
+							failure: function(event, id, obj) {
 								instance.setStatus('error', Liferay.Language.get('unable-to-connect-with-mail-server'));
 							},
-							success: function (event, id, obj) {
+							success: function(event, id, obj) {
 								var responseData = this.get('responseData');
 
 								instance.setStatus(responseData.status, responseData.message);
