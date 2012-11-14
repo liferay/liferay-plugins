7974700b188d83340468cd0581dca527556b59e3
Chas Austin <charles.austin@liferay.com>
2012-11-14 15:59:55 -0800

    plugins master copyright fix

--- portlets/so-portlet/docroot/activities/css/main.css
+++ portlets/so-portlet/docroot/activities/css/main.css
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
  *
  * This file is part of Liferay Social Office. Liferay Social Office is free
  * software: you can redistribute it and/or modify it under the terms of the GNU
