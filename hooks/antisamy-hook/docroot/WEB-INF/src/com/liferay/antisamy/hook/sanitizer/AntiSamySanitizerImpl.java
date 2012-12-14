b1f01b86935265a03502e71fdf167f2df8453c11
Julio Camarero <julio.camarero@liferay.com>
2012-12-14 10:52:43 -0800

    LPS-25974 have the message in the exception to use it in the future

--- hooks/antisamy-hook/docroot/WEB-INF/src/com/liferay/antisamy/hook/sanitizer/AntiSamySanitizerImpl.java
+++ hooks/antisamy-hook/docroot/WEB-INF/src/com/liferay/antisamy/hook/sanitizer/AntiSamySanitizerImpl.java
@@ -105,7 +105,7 @@ public class AntiSamySanitizerImpl implements Sanitizer {
 		catch (Exception e) {
 			_log.error("Unable to sanitize input", e);
 
-			throw new SanitizerException(e);
+			throw new SanitizerException(e.getMessage());
 		}
 	}
 
