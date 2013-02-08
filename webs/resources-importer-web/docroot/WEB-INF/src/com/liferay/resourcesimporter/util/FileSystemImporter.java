0e56f50a170ca74e483ab30ad1e0a98f4a39513c
Shinn Lok <shinn.lok@liferay.com>
2013-02-07 16:17:32 -0800

    LPS-32853 Clear if no json object, otherwise previous tag would append to asset

--- webs/resources-importer-web/docroot/WEB-INF/src/com/liferay/resourcesimporter/util/FileSystemImporter.java
+++ webs/resources-importer-web/docroot/WEB-INF/src/com/liferay/resourcesimporter/util/FileSystemImporter.java
@@ -704,6 +704,8 @@ public class FileSystemImporter extends BaseImporter {
 		JSONObject assetJSONObject = _assetJSONObjectMap.get(name);
 
 		if (assetJSONObject == null) {
+			serviceContext.setAssetTagNames(null);
+
 			return;
 		}
 
