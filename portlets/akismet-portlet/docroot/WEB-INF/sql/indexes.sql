77f468722a01cbe6ab5205837148d0edb395830d
Peter Shin <peter.shin@liferay.com>
2013-02-13 18:01:16 -0600

    LPS-32639 Upgrade

--- portlets/akismet-portlet/docroot/WEB-INF/sql/indexes.sql
+++ portlets/akismet-portlet/docroot/WEB-INF/sql/indexes.sql
@@ -1,3 +1,2 @@
 create index IX_8AAFDB76 on Akismet_AkismetData (classNameId, classPK);
-create index IX_4D57A41C on Akismet_AkismetData (mbMessageId);
 create index IX_1642346C on Akismet_AkismetData (modifiedDate);
