c94e1854e3f1c643945311e1296c633932871be8
Brian Chan <brian.chan@liferay.com>
2013-02-20 11:27:42 -0800

    LPS-32149 Disable site exclusion since it cannot work in 6.1.x without a patch in core at LPS-31246 in f1f373890d55fb83ba61f9144ad4d585fc6f0630 (portal master)

--- portlets/chat-portlet/docroot/WEB-INF/src/com/liferay/chat/util/PortletPropsValues.java
+++ portlets/chat-portlet/docroot/WEB-INF/src/com/liferay/chat/util/PortletPropsValues.java
@@ -25,9 +25,7 @@ public class PortletPropsValues {
 	public static final int BUDDY_LIST_MAX_BUDDIES = GetterUtil.getInteger(
 		PortletProps.get(PortletPropsKeys.BUDDY_LIST_MAX_BUDDIES));
 
-	public static final String[] BUDDY_LIST_SITE_EXCLUDES =
-		PortletProps.getArray(
-			PortletPropsKeys.BUDDY_LIST_SITE_EXCLUDES);
+	public static final String[] BUDDY_LIST_SITE_EXCLUDES = new String[0];
 
 	public static final String BUDDY_LIST_STRATEGY = GetterUtil.getString(
 		PortletProps.get(PortletPropsKeys.BUDDY_LIST_STRATEGY));
