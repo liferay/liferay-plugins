9a5243d622f3821bdfa016f0e27c383bd30770c7
Brian Chan <brian.chan@liferay.com>
2013-02-21 17:29:28 -0800

    LPS-33212 Search result sorting should not be case sensitive

--- shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/search/DocumentImpl.java
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/search/DocumentImpl.java
@@ -14,9 +14,12 @@
 
 package com.liferay.compat.portal.kernel.search;
 
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Set;
 
@@ -26,6 +29,30 @@ import java.util.Set;
  */
 public class DocumentImpl
 	extends com.liferay.portal.kernel.search.DocumentImpl implements Cloneable {
+
+	@Override
+	public void addText(String name, String value) {
+		if (Validator.isNull(value)) {
+			return;
+		}
+
+		Field field = new Field(name, value);
+
+		field.setTokenized(true);
+
+		add(field);
+
+		if (_sortableTextFields.contains(name)) {
+			String truncatedValue = value;
+
+			if (value.length() > _SORTABLE_TEXT_FIELDS_TRUNCATED_LENGTH) {
+				truncatedValue = value.substring(
+					0, _SORTABLE_TEXT_FIELDS_TRUNCATED_LENGTH);
+			}
+
+			addKeyword(getSortableFieldName(name), truncatedValue, true);
+		}
+	}
 
 	@Override
 	public Object clone() {
@@ -47,6 +74,11 @@ public class DocumentImpl
 	protected void setSortableTextFields(Set<String> sortableTextFields) {
 		_sortableTextFields = sortableTextFields;
 	}
+
+	private static final int _SORTABLE_TEXT_FIELDS_TRUNCATED_LENGTH =
+		GetterUtil.getInteger(
+			PropsUtil.get(
+				PropsKeys.INDEX_SORTABLE_TEXT_FIELDS_TRUNCATED_LENGTH));
 
 	private static Set<String> _defaultSortableTextFields = SetUtil.fromArray(
 		PropsUtil.getArray(PropsKeys.INDEX_SORTABLE_TEXT_FIELDS));
