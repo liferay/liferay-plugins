a5591a18aa7e27babc88ff3c6ee56383afc0df3c
Brian Chan <brian.chan@liferay.com>
2013-04-08 21:33:02 -0700

    SOS-1454 Move interperters to one dir

--- hooks/so-activities-hook/docroot/WEB-INF/src/com/liferay/so/activities/hook/social/CalendarActivityInterpreter.java
+++ hooks/so-activities-hook/docroot/WEB-INF/src/com/liferay/so/activities/hook/social/CalendarActivityInterpreter.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.so.activities.portlet.calendar.social;
+package com.liferay.so.activities.hook.social;
 
 import com.liferay.compat.portal.service.ServiceContext;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
