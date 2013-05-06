1d0130f4bcce67d82d8ded436adc32cba484c4fe
Raymond Auge <raymond.auge@liferay.com>
2013-05-06 16:46:12 -0400

    LPS-35006 Include a test jar and source files into the plugins SDK for OSGi framework testing

---
+++ shared/test-module-framework-shared/src/com/liferay/test/module/framework/TestComponent.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.test.module.framework;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
+
+/**
+ * @author Raymond Augé
+ */
+@Component
+public class TestComponent {
+
+	@Activate
+	public void activate() {
+		System.out.println("TestComponent activated!");
+	}
+
+	@Deactivate
+	public void deactivate() {
+		System.out.println("TestComponent is no longer activated!");
+	}
+
+}
