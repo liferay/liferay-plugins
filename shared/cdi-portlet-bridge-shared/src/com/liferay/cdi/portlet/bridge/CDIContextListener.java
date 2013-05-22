f3e33ef9eedb65166db7fc7c581b80d2ba19dca4
Neil Griffin <neil.griffin.scm@gmail.com>
2013-05-22 16:45:15 -0400

    LPS-35560 CDI Portlet Bridge is unable to discover CDI/Weld BeanManager on JBoss AS 7

--- shared/cdi-portlet-bridge-shared/src/com/liferay/cdi/portlet/bridge/CDIContextListener.java
+++ shared/cdi-portlet-bridge-shared/src/com/liferay/cdi/portlet/bridge/CDIContextListener.java
@@ -14,7 +14,15 @@
 
 package com.liferay.cdi.portlet.bridge;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.enterprise.inject.spi.BeanManager;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
+import javax.naming.NamingException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -41,11 +49,43 @@ public class CDIContextListener implements ServletContextListener {
 				_ATTRIBUTE_NAME_BEAN_MANAGER);
 		}
 
+		if (beanManager == null) {
+
+			try {
+				Context initialContext = new InitialContext();
+
+				try {
+					beanManager = (BeanManager)initialContext.lookup(
+						_BEAN_MANAGER_JNDI_NAME1);
+				}
+				catch (NameNotFoundException e) {
+					beanManager = (BeanManager)initialContext.lookup(
+						_BEAN_MANAGER_JNDI_NAME2);
+				}
+			}
+			catch (NamingException e) {
+				_log.log(Level.SEVERE, e.getMessage(), e);
+			}
+		}
+
+		if (beanManager == null) {
+			_log.log(Level.SEVERE, "Unable to get CDI BeanManager instance");
+		}
+
 		CDIBeanManagerUtil.setBeanManager(beanManager);
 	}
 
 	private static final String _ATTRIBUTE_NAME_BEAN_MANAGER =
 		"org.jboss.weld.environment.servlet.javax.enterprise.inject.spi." +
 			"BeanManager";
+
+	private static final String _BEAN_MANAGER_JNDI_NAME1 =
+		"java:comp/BeanManager";
+
+	private static final String _BEAN_MANAGER_JNDI_NAME2 =
+		"java:comp/env/BeanManager";
+
+	private static final Logger _log = Logger.getLogger(
+		CDIContextListener.class.getName());
 
 }
