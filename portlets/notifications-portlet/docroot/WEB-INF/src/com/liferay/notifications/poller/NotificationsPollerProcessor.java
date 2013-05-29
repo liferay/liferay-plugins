d89185f1aab411384f32b7cb9c29be0258225748
Jonathan Lee <Jonathan.Lee@liferay.com>
2013-05-29 16:18:38 -0700

    LPS-35156 PollerrPocessor and backend logic

---
+++ portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/poller/NotificationsPollerProcessor.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.notifications.poller;
+
+import com.liferay.portal.kernel.poller.BasePollerProcessor;
+import com.liferay.portal.kernel.poller.PollerRequest;
+import com.liferay.portal.kernel.poller.PollerResponse;
+import com.liferay.portal.service.UserNotificationEventLocalServiceUtil;
+
+/**
+ * @author Jonathan Lee
+ */
+public class NotificationsPollerProcessor extends BasePollerProcessor {
+
+	@Override
+	protected void doReceive(
+			PollerRequest pollerRequest, PollerResponse pollerResponse)
+		throws Exception {
+
+		getNewUserNotificationsCount(pollerRequest, pollerResponse);
+	}
+
+	@Override
+	protected void doSend(PollerRequest pollerRequest) throws Exception {
+	}
+
+	protected void getNewUserNotificationsCount(
+			PollerRequest pollerRequest, PollerResponse pollerResponse)
+		throws Exception {
+
+		int count =
+			UserNotificationEventLocalServiceUtil.
+				getUserNotificationEventsByDeliveredCount(
+					pollerRequest.getUserId(), false);
+
+		pollerResponse.setParameter("count", String.valueOf(count));
+	}
+
+}
---
+++ portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/portlet/NotificationsPortlet.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.notifications.portlet;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.UserNotificationEvent;
+import com.liferay.portal.service.UserNotificationEventLocalServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+/**
+ * @author Jonathan Lee
+ */
+public class NotificationsPortlet extends MVCPortlet {
+
+	public JSONObject markAsRead(
+			ActionRequest actionRequest, ActionResponse actionResponse) {
+
+		long userNotificationEventId = ParamUtil.getLong(
+			actionRequest, "userNotificationEventId");
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			UserNotificationEvent userNotificationEvent =
+				UserNotificationEventLocalServiceUtil.getUserNotificationEvent(
+					userNotificationEventId);
+
+			userNotificationEvent.setArchived(true);
+
+			UserNotificationEventLocalServiceUtil.updateUserNotificationEvent(
+				userNotificationEvent);
+
+			jsonObject.put("success", true);
+		}
+		catch (Exception e) {
+			jsonObject.put("success", false);
+		}
+
+		return jsonObject;
+	}
+
+	public void processAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws PortletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (!themeDisplay.isSignedIn()) {
+			return;
+		}
+
+		try {
+			String actionName = ParamUtil.getString(
+				actionRequest, ActionRequest.ACTION_NAME);
+
+			if (actionName.equals("markAsRead")) {
+				JSONObject jsonObject = markAsRead(
+					actionRequest, actionResponse);
+
+				writeJSON(actionRequest, actionResponse, jsonObject);
+			}
+			else if (actionName.equals("setDelivered")) {
+				JSONObject jsonObject = setDelivered(
+					actionRequest, actionResponse);
+
+				writeJSON(actionRequest, actionResponse, jsonObject);
+			}
+			else {
+				super.processAction(actionRequest, actionResponse);
+			}
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
+	}
+
+	public JSONObject setDelivered(
+		ActionRequest actionRequest, ActionResponse actionResponse) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			List<UserNotificationEvent> userNotificationEvents =
+				UserNotificationEventLocalServiceUtil.
+					getUserNotificationEventsByDelivered(
+						themeDisplay.getUserId(), false);
+
+			for (UserNotificationEvent userNotificationEvent :
+					userNotificationEvents) {
+
+				userNotificationEvent.setDelivered(true);
+
+				UserNotificationEventLocalServiceUtil.
+					updateUserNotificationEvent(userNotificationEvent);
+			}
+
+			jsonObject.put("success", true);
+		}
+		catch (Exception e) {
+			jsonObject.put("success", false);
+		}
+
+		return jsonObject;
+	}
+
+}
