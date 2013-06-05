ed5651351148533b61f5f96a632eeaccba701c04
Miguel Pastor <miguelinlas3@gmail.com>
2013-06-05 22:16:42 +0200

    LPS-36004 Unit tests for the WebXMLDefinitionLoader

---
+++ shared/http-service-shared/test/unit/com/liferay/httpservice/internal/definition/WebXMLDefinitionLoaderTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.httpservice.internal.definition;
+
+import com.liferay.http.service.test.MockBundle;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.xml.DocumentException;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
+import com.liferay.portal.xml.SAXReaderImpl;
+
+import java.util.List;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContextListener;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+
+import org.osgi.framework.Bundle;
+
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Miguel Pastor
+ */
+@RunWith(PowerMockRunner.class)
+public class WebXMLDefinitionLoaderTest {
+
+	@BeforeClass
+	public static void setUp() {
+		new SAXReaderUtil().setSAXReader(SAXReaderImpl.getInstance());
+	}
+
+	public WebXMLDefinitionLoaderTest() throws DocumentException {
+		_webXMLDefinitionLoader = new WebXMLDefinitionLoader();
+	}
+
+	@Test
+	public void testLoadDefaultDependencies() throws Exception {
+		WebXMLDefinition webXMLDefinition = _webXMLDefinitionLoader.loadWebXML(
+			_bundle);
+
+		List<ListenerDefinition> listenerDefinitions =
+			webXMLDefinition.getListenerDefinitions();
+
+		Assert.assertEquals(
+			_LISTENER_DEFAULT_CLASSES.length, listenerDefinitions.size());
+
+		for (ListenerDefinition listenerDefinition : listenerDefinitions) {
+			Object listener = listenerDefinition.getListener();
+
+			Assert.assertTrue(listener instanceof ServletContextListener);
+		}
+	}
+
+	private static final String[] _LISTENER_DEFAULT_CLASSES = {
+		"org.eclipse.jetty.servlet.listener.ELContextCleaner",
+		"org.eclipse.jetty.servlet.listener.IntrospectorCleaner"
+	};
+
+	private static final String[] _SERVLET_DEFAULT_CLASSES = {
+		"com.liferay.httpservice.servlet.ResourceServlet",
+		"org.apache.jasper.servlet.JspServlet"
+	};
+
+	private Bundle _bundle = new CustomClassLoaderMockBundle();
+
+	@Mock
+	private Servlet _servlet;
+
+	@Mock
+	private ServletContextListener _servletContextListener;
+
+	private WebXMLDefinitionLoader _webXMLDefinitionLoader;
+
+	private class CustomClassLoaderMockBundle extends MockBundle {
+
+		@Override
+		public Class<?> loadClass(String s) throws ClassNotFoundException {
+			if (ArrayUtil.contains(_LISTENER_DEFAULT_CLASSES, s)) {
+				return _servletContextListener.getClass();
+			}
+			else if (ArrayUtil.contains(_SERVLET_DEFAULT_CLASSES, s)) {
+				return _servlet.getClass();
+			}
+			else {
+				return super.loadClass(s);
+			}
+		}
+
+	}
+
+}
