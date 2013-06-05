50c21ae4a5c41a6682d3171756be8faf461dcaf5
Brian Chan <brian.chan@liferay.com>
2013-06-05 12:03:13 -0700

    LPS-36000

---
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/util/Time.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.compat.portal.kernel.util;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
+
+import java.text.Format;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class Time extends com.liferay.portal.kernel.util.Time {
+
+	public static String getRelativeTimeDescription(
+		long milliseconds, Locale locale, TimeZone timeZone) {
+
+		Format timeFormat = FastDateFormatFactoryUtil.getTime(locale, timeZone);
+
+		int daysBetween = DateUtil.getDaysBetween(
+			new Date(milliseconds), new Date(), timeZone);
+
+		long millisAgo = System.currentTimeMillis() - milliseconds;
+
+		if (millisAgo <= Time.MINUTE) {
+			return LanguageUtil.get(locale, "about-a-minute-ago");
+		}
+		else if (millisAgo < Time.HOUR) {
+			return LanguageUtil.format(
+				locale, "x-minutes-ago", (millisAgo / Time.MINUTE));
+		}
+		else if ((millisAgo / Time.HOUR) == 1) {
+			return LanguageUtil.get(locale, "about-an-hour-ago");
+		}
+		else if ((millisAgo < Time.DAY) || (daysBetween == 0)) {
+			return LanguageUtil.format(
+				locale, "x-hours-ago", (millisAgo / Time.HOUR));
+		}
+		else if (daysBetween == 1) {
+			return LanguageUtil.format(
+				locale, "yesterday-at-x", timeFormat.format(milliseconds));
+		}
+
+		Format dateFormat = FastDateFormatFactoryUtil.getSimpleDateFormat(
+			"EEEE, MMMMM dd, yyyy", locale, timeZone);
+
+		return dateFormat.format(milliseconds);
+	}
+
+}
