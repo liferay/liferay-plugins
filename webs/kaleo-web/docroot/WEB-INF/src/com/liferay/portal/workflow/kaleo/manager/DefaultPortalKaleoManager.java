52a476c732dfafe6d9d3702f9473b9deed8c8618
Michael Han <michael.han@liferay.com>
2013-06-07 21:37:01 -0500

    LPS-36096 update kaleo to use new API

--- webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/manager/DefaultPortalKaleoManager.java
+++ webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/manager/DefaultPortalKaleoManager.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.workflow.kaleo.manager;
 
 import com.liferay.portal.NoSuchRoleException;
-import com.liferay.portal.NoSuchWorkflowDefinitionLinkException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
@@ -27,6 +26,7 @@ import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.User;
+import com.liferay.portal.model.WorkflowDefinitionLink;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.RoleLocalServiceUtil;
@@ -232,14 +232,13 @@ public class DefaultPortalKaleoManager
 			String assetClassName, String workflowDefinitionName)
 		throws PortalException, SystemException {
 
-		try {
+		WorkflowDefinitionLink workflowDefinitionLink =
 			WorkflowDefinitionLinkLocalServiceUtil.
-				getDefaultWorkflowDefinitionLink(
+				fetchDefaultWorkflowDefinitionLink(
 					company.getCompanyId(), assetClassName, 0, 0);
 
+		if (workflowDefinitionLink != null) {
 			return;
-		}
-		catch (NoSuchWorkflowDefinitionLinkException nswdle) {
 		}
 
 		List<WorkflowDefinition> workflowDefinitions =
