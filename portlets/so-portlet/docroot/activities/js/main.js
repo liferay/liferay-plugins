aad3ff77dc901023681188cb63043d991fe384df
Evan Thibodeau <evan.thibodeau@liferay.com>
2013-06-13 15:19:29 -0700

    SOS-1594 calculate and set default container height for future smooth transition

--- portlets/so-portlet/docroot/activities/js/main.js
+++ portlets/so-portlet/docroot/activities/js/main.js
@@ -9,8 +9,16 @@ AUI().use(
 
 				var entry = A.one('#' + portletNamespace + entryId);
 
+				var bodyContainer = entry.one('.grouped-activity-body-container');
+				var subentryHeight = entry.one('.activity-subentry').outerHeight();
+
+				var minHeight = (subentryHeight * 3) + 'px';
+
 				if (entry.hasClass('toggler-content-collapsed')) {
 					entry.removeClass('toggler-content-collapsed');
+
+					bodyContainer.setStyle('height', minHeight);
+					bodyContainer.setStyle('max-height', 'none');
 				}
 				else {
 					entry.addClass('toggler-content-collapsed');
