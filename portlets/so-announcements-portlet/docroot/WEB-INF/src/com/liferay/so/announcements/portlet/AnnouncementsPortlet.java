ad0a165d81e0f9f9a4e76294a5731641c7a834f6
Evan Thibodeau <evan.thibodeau@liferay.com>
2013-06-18 16:08:40 -0700

    SOS-1595 copied from portal

---
+++ portlets/so-announcements-portlet/docroot/WEB-INF/src/com/liferay/so/announcements/portlet/AnnouncementsPortlet.java
@@ -0,0 +1,214 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.announcements.action;
+
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.struts.PortletAction;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.announcements.EntryContentException;
+import com.liferay.portlet.announcements.EntryDisplayDateException;
+import com.liferay.portlet.announcements.EntryExpirationDateException;
+import com.liferay.portlet.announcements.EntryTitleException;
+import com.liferay.portlet.announcements.EntryURLException;
+import com.liferay.portlet.announcements.NoSuchEntryException;
+import com.liferay.portlet.announcements.service.AnnouncementsEntryServiceUtil;
+
+import java.util.Calendar;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author Raymond Augé
+ */
+public class EditEntryAction extends PortletAction {
+
+	@Override
+	public void processAction(
+		ActionMapping mapping, ActionForm form, PortletConfig portletConfig,
+		ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		try {
+			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
+				updateEntry(actionRequest);
+			}
+			else if (cmd.equals(Constants.DELETE)) {
+				deleteEntry(actionRequest);
+			}
+
+			String redirect = PortalUtil.escapeRedirect(
+				ParamUtil.getString(actionRequest, "redirect"));
+
+			if (Validator.isNotNull(redirect)) {
+				actionResponse.sendRedirect(redirect);
+			}
+		}
+		catch (Exception e) {
+			if (e instanceof EntryContentException ||
+				e instanceof EntryDisplayDateException ||
+				e instanceof EntryExpirationDateException ||
+				e instanceof EntryTitleException ||
+				e instanceof EntryURLException ||
+				e instanceof NoSuchEntryException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	@Override
+	public ActionForward render(
+		ActionMapping mapping, ActionForm form, PortletConfig portletConfig,
+		RenderRequest renderRequest, RenderResponse renderResponse)
+		throws Exception {
+
+		try {
+			ActionUtil.getEntry(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchEntryException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return mapping.findForward("portlet.announcements.error");
+			}
+			else {
+				throw e;
+			}
+		}
+
+		return mapping.findForward(
+			getForward(renderRequest, "portlet.announcements.edit_entry"));
+	}
+
+	protected void deleteEntry(ActionRequest actionRequest) throws Exception {
+		long entryId = ParamUtil.getLong(actionRequest, "entryId");
+
+		AnnouncementsEntryServiceUtil.deleteEntry(entryId);
+	}
+
+	protected void updateEntry(ActionRequest actionRequest) throws Exception {
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long entryId = ParamUtil.getLong(actionRequest, "entryId");
+
+		String[] distributionScopeParts = StringUtil.split(
+			ParamUtil.getString(actionRequest, "distributionScope"));
+
+		long classNameId = 0;
+		long classPK = 0;
+
+		if (distributionScopeParts.length == 2) {
+			classNameId = GetterUtil.getLong(distributionScopeParts[0]);
+
+			if (classNameId > 0) {
+				classPK = GetterUtil.getLong(distributionScopeParts[1]);
+			}
+		}
+
+		String title = ParamUtil.getString(actionRequest, "title");
+		String content = ParamUtil.getString(actionRequest, "content");
+		String url = ParamUtil.getString(actionRequest, "url");
+		String type = ParamUtil.getString(actionRequest, "type");
+
+		int displayDateMonth = ParamUtil.getInteger(
+			actionRequest, "displayDateMonth");
+		int displayDateDay = ParamUtil.getInteger(
+			actionRequest, "displayDateDay");
+		int displayDateYear = ParamUtil.getInteger(
+			actionRequest, "displayDateYear");
+		int displayDateHour = ParamUtil.getInteger(
+			actionRequest, "displayDateHour");
+		int displayDateMinute = ParamUtil.getInteger(
+			actionRequest, "displayDateMinute");
+		int displayDateAmPm = ParamUtil.getInteger(
+			actionRequest, "displayDateAmPm");
+
+		if (displayDateAmPm == Calendar.PM) {
+			displayDateHour += 12;
+		}
+
+		boolean displayImmediately = ParamUtil.getBoolean(
+			actionRequest, "displayImmediately");
+
+		int expirationDateMonth = ParamUtil.getInteger(
+			actionRequest, "expirationDateMonth");
+		int expirationDateDay = ParamUtil.getInteger(
+			actionRequest, "expirationDateDay");
+		int expirationDateYear = ParamUtil.getInteger(
+			actionRequest, "expirationDateYear");
+		int expirationDateHour = ParamUtil.getInteger(
+			actionRequest, "expirationDateHour");
+		int expirationDateMinute = ParamUtil.getInteger(
+			actionRequest, "expirationDateMinute");
+		int expirationDateAmPm = ParamUtil.getInteger(
+			actionRequest, "expirationDateAmPm");
+
+		if (expirationDateAmPm == Calendar.PM) {
+			expirationDateHour += 12;
+		}
+
+		int priority = ParamUtil.getInteger(actionRequest, "priority");
+		boolean alert = ParamUtil.getBoolean(actionRequest, "alert");
+
+		if (entryId <= 0) {
+
+			// Add entry
+
+			AnnouncementsEntryServiceUtil.addEntry(
+				themeDisplay.getPlid(), classNameId, classPK, title, content,
+				url, type, displayDateMonth, displayDateDay, displayDateYear,
+				displayDateHour, displayDateMinute, displayImmediately,
+				expirationDateMonth, expirationDateDay, expirationDateYear,
+				expirationDateHour, expirationDateMinute, priority, alert);
+		}
+		else {
+
+			// Update entry
+
+			AnnouncementsEntryServiceUtil.updateEntry(
+				entryId, title, content, url, type, displayDateMonth,
+				displayDateDay, displayDateYear, displayDateHour,
+				displayDateMinute, expirationDateMonth, expirationDateDay,
+				expirationDateYear, expirationDateHour, expirationDateMinute,
+				priority);
+		}
+	}
+
+}
