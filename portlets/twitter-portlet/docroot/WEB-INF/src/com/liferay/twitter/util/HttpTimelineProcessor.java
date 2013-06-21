9794a4c470d5803e16cf5fbb411f4a30b1870919
Shinn Lok <shinn.lok@liferay.com>
2013-06-21 12:58:28 -0700

    LPS-36613 Update regex, it is fragile and we may have to keep updating it but currently we don't have a choice

--- portlets/twitter-portlet/docroot/WEB-INF/src/com/liferay/twitter/util/HttpTimelineProcessor.java
+++ portlets/twitter-portlet/docroot/WEB-INF/src/com/liferay/twitter/util/HttpTimelineProcessor.java
@@ -127,7 +127,8 @@ public class HttpTimelineProcessor implements TimelineProcessor {
 		"EEE MMM d hh:mm:ss Z yyyy", Locale.US);
 	private Pattern _pattern = Pattern.compile(
 		"data-item-id=\"([0-9]+)\".*?data-user-id=\"([0-9]+)\".*?data-time=\"" +
-		"([0-9]+)\".*?<p class=\"js-tweet-text\">(.*?)</p>", Pattern.DOTALL);
+			"([0-9]+)\".*?<p class=\"js-tweet-text tweet-text\">(.*?)</p>",
+		Pattern.DOTALL);
 	private Random _random = new Random();
 
 }
