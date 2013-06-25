fb6faecc7661e2fc361de7bb31d483c63ad6af04
Ryan Park <ryan.park@liferay.com>
2013-06-25 10:58:58 -0700

    LPS-36684 Upload WAR\LPKG for install (copy methods from core)

--- portlets/marketplace-portlet/docroot/WEB-INF/src/com/liferay/marketplace/appmanager/portlet/AppManagerPortlet.java
+++ portlets/marketplace-portlet/docroot/WEB-INF/src/com/liferay/marketplace/appmanager/portlet/AppManagerPortlet.java
@@ -17,22 +17,47 @@ package com.liferay.marketplace.appmanager.portlet;
 import com.liferay.marketplace.service.AppServiceUtil;
 import com.liferay.portal.kernel.deploy.DeployManagerUtil;
 import com.liferay.portal.kernel.servlet.ServletContextPool;
+import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.upload.UploadException;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.LayoutTemplate;
 import com.liferay.portal.model.Plugin;
 import com.liferay.portal.model.PluginSetting;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.Theme;
+import com.liferay.portal.plugin.PluginPackageUtil;
+import com.liferay.portal.security.lang.DoPrivilegedBean;
 import com.liferay.portal.service.PluginSettingLocalServiceUtil;
 import com.liferay.portal.service.PluginSettingServiceUtil;
 import com.liferay.portal.service.PortletServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.tools.deploy.BaseDeployer;
+import com.liferay.portal.upload.ProgressInputStream;
+import com.liferay.portal.util.HttpImpl;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portal.util.WebKeys;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import java.util.Arrays;
 import java.util.List;
@@ -41,6 +66,11 @@ import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 
 /**
  * @author Ryan Park
@@ -165,6 +195,262 @@ public class AppManagerPortlet extends MVCPortlet {
 						themeDisplay.getCompanyId(), theme.getThemeId(),
 						Plugin.TYPE_THEME, pluginSetting.getRoles(), active);
 				}
+			}
+		}
+	}
+
+	protected void localDeploy(ActionRequest actionRequest) throws Exception {
+		UploadPortletRequest uploadPortletRequest =
+			PortalUtil.getUploadPortletRequest(actionRequest);
+
+		String fileName = null;
+
+		String deploymentContext = ParamUtil.getString(
+			actionRequest, "deploymentContext");
+
+		if (Validator.isNotNull(deploymentContext)) {
+			fileName =
+				BaseDeployer.DEPLOY_TO_PREFIX + deploymentContext + ".war";
+		}
+		else {
+			fileName = GetterUtil.getString(
+				uploadPortletRequest.getFileName("file"));
+
+			int pos = fileName.lastIndexOf(CharPool.PERIOD);
+
+			if (pos != -1) {
+				deploymentContext = fileName.substring(0, pos);
+			}
+		}
+
+		File file = uploadPortletRequest.getFile("file");
+
+		byte[] bytes = FileUtil.getBytes(file);
+
+		if ((bytes == null) || (bytes.length == 0)) {
+			SessionErrors.add(actionRequest, UploadException.class.getName());
+
+			return;
+		}
+
+		try {
+			PluginPackageUtil.registerPluginPackageInstallation(
+				deploymentContext);
+
+			String source = file.toString();
+
+			String deployDir = PrefsPropsUtil.getString(
+				PropsKeys.AUTO_DEPLOY_DEPLOY_DIR,
+				PropsValues.AUTO_DEPLOY_DEPLOY_DIR);
+
+			String destination = deployDir + StringPool.SLASH + fileName;
+
+			FileUtil.copyFile(source, destination);
+
+			SessionMessages.add(actionRequest, "pluginUploaded");
+		}
+		finally {
+			PluginPackageUtil.endPluginPackageInstallation(deploymentContext);
+		}
+	}
+
+	protected void remoteDeploy(ActionRequest actionRequest) throws Exception {
+		try {
+			String url = ParamUtil.getString(actionRequest, "url");
+
+			URL urlObj = new URL(url);
+
+			String host = urlObj.getHost();
+
+			if (host.endsWith(".sf.net") || host.endsWith(".sourceforge.net")) {
+				remoteDeploySourceForge(urlObj.getPath(), actionRequest);
+			}
+			else {
+				remoteDeploy(url, urlObj, actionRequest, true);
+			}
+		}
+		catch (MalformedURLException murle) {
+			SessionErrors.add(actionRequest, "invalidUrl", murle);
+		}
+	}
+
+	protected int remoteDeploy(
+			String url, URL urlObj, ActionRequest actionRequest,
+			boolean failOnError)
+		throws Exception {
+
+		int responseCode = HttpServletResponse.SC_OK;
+
+		GetMethod getMethod = null;
+
+		String deploymentContext = ParamUtil.getString(
+			actionRequest, "deploymentContext");
+
+		try {
+			HttpImpl httpImpl = null;
+
+			Object httpObject = HttpUtil.getHttp();
+
+			if (httpObject instanceof DoPrivilegedBean) {
+				DoPrivilegedBean doPrivilegedBean =
+					(DoPrivilegedBean)httpObject;
+
+				httpImpl = (HttpImpl)doPrivilegedBean.getActualBean();
+			}
+			else {
+				httpImpl = (HttpImpl)httpObject;
+			}
+
+			HostConfiguration hostConfiguration = httpImpl.getHostConfiguration(
+				url);
+
+			HttpClient httpClient = httpImpl.getClient(hostConfiguration);
+
+			getMethod = new GetMethod(url);
+
+			String fileName = null;
+
+			if (Validator.isNotNull(deploymentContext)) {
+				fileName =
+					BaseDeployer.DEPLOY_TO_PREFIX + deploymentContext + ".war";
+			}
+			else {
+				fileName = url.substring(url.lastIndexOf(CharPool.SLASH) + 1);
+
+				int pos = fileName.lastIndexOf(CharPool.PERIOD);
+
+				if (pos != -1) {
+					deploymentContext = fileName.substring(0, pos);
+				}
+			}
+
+			PluginPackageUtil.registerPluginPackageInstallation(
+				deploymentContext);
+
+			responseCode = httpClient.executeMethod(
+				hostConfiguration, getMethod);
+
+			if (responseCode != HttpServletResponse.SC_OK) {
+				if (failOnError) {
+					SessionErrors.add(
+						actionRequest, "errorConnectingToUrl",
+						new Object[] {String.valueOf(responseCode)});
+				}
+
+				return responseCode;
+			}
+
+			long contentLength = getMethod.getResponseContentLength();
+
+			String progressId = ParamUtil.getString(
+				actionRequest, Constants.PROGRESS_ID);
+
+			ProgressInputStream pis = new ProgressInputStream(
+				actionRequest, getMethod.getResponseBodyAsStream(),
+				contentLength, progressId);
+
+			String deployDir = PrefsPropsUtil.getString(
+				PropsKeys.AUTO_DEPLOY_DEPLOY_DIR,
+				PropsValues.AUTO_DEPLOY_DEPLOY_DIR);
+
+			String tmpFilePath =
+				deployDir + StringPool.SLASH + _DOWNLOAD_DIR +
+					StringPool.SLASH + fileName;
+
+			File tmpFile = new File(tmpFilePath);
+
+			if (!tmpFile.getParentFile().exists()) {
+				tmpFile.getParentFile().mkdirs();
+			}
+
+			FileOutputStream fos = new FileOutputStream(tmpFile);
+
+			try {
+				pis.readAll(fos);
+
+				if (_log.isInfoEnabled()) {
+					_log.info(
+						"Downloaded plugin from " + urlObj + " has " +
+							pis.getTotalRead() + " bytes");
+				}
+			}
+			finally {
+				pis.clearProgress();
+			}
+
+			getMethod.releaseConnection();
+
+			if (pis.getTotalRead() > 0) {
+				String destination = deployDir + StringPool.SLASH + fileName;
+
+				File destinationFile = new File(destination);
+
+				boolean moved = FileUtil.move(tmpFile, destinationFile);
+
+				if (!moved) {
+					FileUtil.copyFile(tmpFile, destinationFile);
+					FileUtil.delete(tmpFile);
+				}
+
+				SessionMessages.add(actionRequest, "pluginDownloaded");
+			}
+			else {
+				if (failOnError) {
+					SessionErrors.add(
+						actionRequest, UploadException.class.getName());
+				}
+
+				responseCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+			}
+		}
+		catch (MalformedURLException murle) {
+			SessionErrors.add(actionRequest, "invalidUrl", murle);
+		}
+		catch (IOException ioe) {
+			SessionErrors.add(actionRequest, "errorConnectingToUrl", ioe);
+		}
+		finally {
+			if (getMethod != null) {
+				getMethod.releaseConnection();
+			}
+
+			PluginPackageUtil.endPluginPackageInstallation(deploymentContext);
+		}
+
+		return responseCode;
+	}
+
+	protected void remoteDeploySourceForge(
+			String path, ActionRequest actionRequest)
+		throws Exception {
+
+		String[] sourceForgeMirrors = getSourceForgeMirrors();
+
+		for (int i = 0; i < sourceForgeMirrors.length; i++) {
+			try {
+				String url = sourceForgeMirrors[i] + path;
+
+				if (_log.isDebugEnabled()) {
+					_log.debug("Downloading from SourceForge mirror " + url);
+				}
+
+				URL urlObj = new URL(url);
+
+				boolean failOnError = false;
+
+				if ((i + 1) == sourceForgeMirrors.length) {
+					failOnError = true;
+				}
+
+				int responseCode = remoteDeploy(
+					url, urlObj, actionRequest, failOnError);
+
+				if (responseCode == HttpServletResponse.SC_OK) {
+					return;
+				}
+			}
+			catch (MalformedURLException murle) {
+				SessionErrors.add(actionRequest, "invalidUrl", murle);
 			}
 		}
 	}
