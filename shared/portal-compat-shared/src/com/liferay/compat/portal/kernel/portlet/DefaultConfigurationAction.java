3c23b5650054a4898b40d43986c7c26f65971b96
Hugo Huijser <hugo.huijser@liferay.com>
2013-06-26 11:28:25 -0700

    LPS-32066 Add changes in DefaultConfigurationAction to portal-compat-shared

---
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/portlet/DefaultConfigurationAction.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.compat.portal.kernel.portlet;
+
+import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.permission.PortletPermissionUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletPreferences;
+import javax.portlet.ValidatorException;
+
+/**
+ * @author Hugo Huijser
+ */
+public class DefaultConfigurationAction
+	extends com.liferay.portal.kernel.portlet.DefaultConfigurationAction {
+
+	@Override
+	public void processAction(
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		if (!cmd.equals(Constants.UPDATE)) {
+			return;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		UnicodeProperties properties = PropertiesParamUtil.getProperties(
+			actionRequest, PREFERENCES_PREFIX);
+
+		String portletResource = ParamUtil.getString(
+			actionRequest, "portletResource");
+
+		PortletPermissionUtil.check(
+			themeDisplay.getPermissionChecker(), themeDisplay.getLayout(),
+			portletResource, ActionKeys.CONFIGURATION);
+
+		PortletPreferences portletPreferences = actionRequest.getPreferences();
+
+		for (Map.Entry<String, String> entry : properties.entrySet()) {
+			String name = entry.getKey();
+			String value = entry.getValue();
+
+			portletPreferences.setValue(name, value);
+		}
+
+		Map<String, String[]> portletPreferencesMap =
+			(Map<String, String[]>)actionRequest.getAttribute(
+				WebKeys.PORTLET_PREFERENCES_MAP);
+
+		if (portletPreferencesMap != null) {
+			for (Map.Entry<String, String[]> entry :
+					portletPreferencesMap.entrySet()) {
+
+				String name = entry.getKey();
+				String[] values = entry.getValue();
+
+				portletPreferences.setValues(name, values);
+			}
+		}
+
+		if (SessionErrors.isEmpty(actionRequest)) {
+			try {
+				portletPreferences.store();
+			}
+			catch (ValidatorException ve) {
+				SessionErrors.add(
+					actionRequest, ValidatorException.class.getName(), ve);
+
+				return;
+			}
+
+			LiferayPortletConfig liferayPortletConfig =
+				(LiferayPortletConfig)portletConfig;
+
+			SessionMessages.add(
+				actionRequest,
+				liferayPortletConfig.getPortletId() +
+					SessionMessages.KEY_SUFFIX_REFRESH_PORTLET,
+				portletResource);
+
+			SessionMessages.add(
+				actionRequest,
+				liferayPortletConfig.getPortletId() +
+					SessionMessages.KEY_SUFFIX_UPDATED_CONFIGURATION);
+		}
+	}
+
+}
