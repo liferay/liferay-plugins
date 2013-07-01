1af60ce7bc2e85b47e44981a69f31964e8f47ffa
Marcellus Tavares <marcellus.tavares@liferay.com>
2013-07-01 19:18:32 -0300

    LPS-35264 Update CalendarActivityInterpreter to handle recycle bin messages

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/social/CalendarActivityInterpreter.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/social/CalendarActivityInterpreter.java
@@ -26,6 +26,7 @@ import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.PortletURLFactoryUtil;
 import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
 import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityConstants;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
@@ -84,6 +85,24 @@ public class CalendarActivityInterpreter extends BaseSocialActivityInterpreter {
 			}
 			else {
 				return "activity-calendar-booking-add-booking-in";
+			}
+		}
+		else if (activityType == SocialActivityConstants.TYPE_MOVE_TO_TRASH) {
+			if (Validator.isNull(groupName)) {
+				return "activity-calendar-booking-move-to-trash";
+			}
+			else {
+				return "activity-calendar-booking-move-to-trash-in";
+			}
+		}
+		else if (activityType ==
+					SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
+
+			if (Validator.isNull(groupName)) {
+				return "activity-calendar-booking-restore-from-trash";
+			}
+			else {
+				return "activity-calendar-booking-restore-from-trash-in";
 			}
 		}
 		else if (activityType == CalendarActivityKeys.UPDATE_CALENDAR_BOOKING) {
