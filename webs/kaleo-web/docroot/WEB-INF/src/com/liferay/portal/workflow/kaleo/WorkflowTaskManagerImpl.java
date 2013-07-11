0dbf4159938b5e4f63bf01555ea0c88499a05167
Brian Chan <brian.chan@liferay.com>
2013-07-10 22:41:28 -0700

    LPS-37466 SF

--- webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/WorkflowTaskManagerImpl.java
+++ webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/WorkflowTaskManagerImpl.java
@@ -189,18 +189,17 @@ public class WorkflowTaskManagerImpl implements WorkflowTaskManager {
 			for (KaleoTaskAssignment kaleoTaskAssignment :
 					kaleoTaskAssignments) {
 
-				long roleId = kaleoTaskAssignment.getAssigneeClassPK();
-
-				Role role = RoleLocalServiceUtil.getRole(roleId);
+				Role role = RoleLocalServiceUtil.getRole(
+					kaleoTaskAssignment.getAssigneeClassPK());
 
 				if ((role.getType() == RoleConstants.TYPE_SITE) ||
 					(role.getType() == RoleConstants.TYPE_ORGANIZATION)) {
 
-					long groupId = kaleoTaskInstanceToken.getGroupId();
-
 					List<UserGroupRole> userGroupRoles =
 						UserGroupRoleLocalServiceUtil.
-							getUserGroupRolesByGroupAndRole(groupId, roleId);
+							getUserGroupRolesByGroupAndRole(
+								kaleoTaskInstanceToken.getGroupId(),
+								kaleoTaskAssignment.getAssigneeClassPK());
 
 					for (UserGroupRole userGroupRole : userGroupRoles) {
 						pooledActors.add(userGroupRole.getUserId());
@@ -209,15 +208,15 @@ public class WorkflowTaskManagerImpl implements WorkflowTaskManager {
 					List<UserGroupGroupRole> userGroupGroupRoles =
 						UserGroupGroupRoleLocalServiceUtil.
 							getUserGroupGroupRolesByGroupAndRole(
-								groupId, roleId);
+								kaleoTaskInstanceToken.getGroupId(),
+								kaleoTaskAssignment.getAssigneeClassPK());
 
 					for (UserGroupGroupRole userGroupGroupRole :
 							userGroupGroupRoles) {
 
-						long userGroupId = userGroupGroupRole.getUserGroupId();
-
 						List<User> userGroupUsers =
-							UserLocalServiceUtil.getUserGroupUsers(userGroupId);
+							UserLocalServiceUtil.getUserGroupUsers(
+								userGroupGroupRole.getUserGroupId());
 
 						for (User user : userGroupUsers) {
 							pooledActors.add(user.getUserId());
@@ -226,7 +225,7 @@ public class WorkflowTaskManagerImpl implements WorkflowTaskManager {
 				}
 				else {
 					long[] userIds = UserLocalServiceUtil.getRoleUserIds(
-						roleId);
+						kaleoTaskAssignment.getAssigneeClassPK());
 
 					pooledActors.addAll(userIds);
 				}
