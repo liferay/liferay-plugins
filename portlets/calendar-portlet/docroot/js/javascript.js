237e2e7bae4d1ad53acd02623c0271e0fd94528c
Marcellus Tavares <marcellus.tavares@liferay.com>
2013-07-18 19:31:17 -0300

    LPS-37602 "Add" buttons in calendar are inconsistent

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -42,7 +42,7 @@ AUI.add(
 		var ICON_CREATE_EVENT_NODE = 'iconCreateEventNode';
 
 		var TPL_ICON_CREATE_EVENT_NODE = '<button type="button" class="btn btn-primary calendar-create-event-btn">' +
-											'<i class="icon-plus icon-white"></i> ' + Liferay.Language.get('new-calendar-booking') +
+											Liferay.Language.get('add-calendar-booking') +
 										 '</button>';
 
 		var COMPANY_ID = toInt(themeDisplay.getCompanyId());
