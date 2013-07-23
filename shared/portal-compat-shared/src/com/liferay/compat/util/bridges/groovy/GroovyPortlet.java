24b40dcdb111771928916e8caac8fd76ac68d837
Shinn Lok <shinn.lok@liferay.com>
2013-07-23 11:44:59 -0700

    LPS-36641 Copy from portal

---
+++ shared/portal-compat-shared/src/com/liferay/compat/util/bridges/groovy/GroovyPortlet.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.util.bridges.groovy;
+
+import com.liferay.util.bridges.scripting.ScriptingPortlet;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+
+/**
+ * @author Jorge Ferrer
+ * @author Alberto Montero
+ * @author Brian Wing Shun Chan
+ */
+public class GroovyPortlet extends ScriptingPortlet {
+
+	@Override
+	public void init() throws PortletException {
+		super.init();
+
+		language = _LANGUAGE;
+	}
+
+	@Override
+	protected String getFileName(RenderRequest renderRequest) {
+		return renderRequest.getParameter("groovyFile");
+	}
+
+	private static final String _LANGUAGE = "groovy";
+
+}
