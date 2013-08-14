5895062788c32418364d920fdb723693e4708be5
driesco <daniela.riesco@liferay.com>
2013-08-14 17:46:08 -0500

    LPS-32041 SolrSpellCheckIndexWriter adapted to new portal abstraction

---
+++ webs/solr4-web/docroot/WEB-INF/src/com/liferay/portal/search/solr/suggest/SolrSpellCheckIndexWriter.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr.suggest;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.BaseGenericSpellCheckIndexWriter;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.util.PortletKeys;
+
+import org.apache.solr.client.solrj.SolrServer;
+
+/**
+ * @author Daniela Zapata Riesco
+ * @author David Gonzalez
+ * @author Michael C. Han
+ */
+public class SolrSpellCheckIndexWriter
+	extends BaseGenericSpellCheckIndexWriter {
+
+	public static final long GLOBAL_GROUP_ID = 0L;
+
+	@Override
+	public void clearQuerySuggestionDictionaryIndexes(
+			SearchContext searchContext)
+		throws SearchException {
+
+		StringBundler sb = getDeleteQuery(searchContext, QUERY_SUGGESTION_TYPE);
+
+		try {
+			_solrServer.deleteByQuery(sb.toString());
+
+			if (_commit) {
+				_solrServer.commit();
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new SearchException(e.getMessage());
+		}
+	}
+
+	@Override
+	public void clearSpellCheckerDictionaryIndexes(SearchContext searchContext)
+		throws SearchException {
+
+		StringBundler sb = getDeleteQuery(searchContext, SPELL_CHECKER_TYPE);
+
+		try {
+			_solrServer.deleteByQuery(sb.toString());
+
+			if (_commit) {
+				_solrServer.commit();
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new SearchException(e.getMessage());
+		}
+
+	}
+
+	public void setCommit(boolean commit) {
+		_commit = commit;
+	}
+
+	public void setSolrServer(SolrServer solrServer) {
+		_solrServer = solrServer;
+	}
+
+	protected StringBundler addQuerySeparator(StringBundler sb) {
+		sb.append(StringPool.SPACE);
+		sb.append(StringPool.PLUS);
+
+		return sb;
+	}
+
+	protected StringBundler addTypeQuery(StringBundler sb, String type) {
+
+		sb.append(Field.TYPE);
+		sb.append(StringPool.COLON);
+		sb.append(type);
+
+		return sb;
+	}
+
+	protected StringBundler getDeleteQuery(
+		SearchContext searchContext, String type) {
+
+		StringBundler sb = new StringBundler(14);
+
+		sb.append(StringPool.PLUS);
+		sb.append(Field.COMPANY_ID);
+		sb.append(StringPool.COLON);
+		sb.append(searchContext.getCompanyId());
+		addQuerySeparator(sb);
+		sb.append(Field.PORTLET_ID);
+		sb.append(StringPool.COLON);
+		sb.append(PortletKeys.SEARCH);
+		addQuerySeparator(sb);
+
+		addTypeQuery(sb, type);
+
+		return sb;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		SolrSpellCheckIndexWriter.class);
+
+	private boolean _commit;
+	private SolrServer _solrServer;
+
+}
