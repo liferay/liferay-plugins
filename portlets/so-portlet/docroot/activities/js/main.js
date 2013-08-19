8c8b75da1022c3b4072357e8bbf591f1d1f78996
Jonathan Lee <Jonathan.Lee@liferay.com>
2013-08-19 16:24:50 -0700

    SOS-1478 adding Lazy load javascript component

--- portlets/so-portlet/docroot/activities/js/main.js
+++ portlets/so-portlet/docroot/activities/js/main.js
@@ -1,3 +1,180 @@
+AUI.add(
+	'liferay-so-scroll',
+	function(A) {
+		var Lang = A.Lang,
+			isNumber = Lang.isNumber,
+			isString = Lang.isString,
+
+			HOST = 'host',
+			NAME = 'scroll',
+
+			BOTTOM ='bottom',
+			DELAY = 'delay',
+			DOWN = 'down',
+			EDGE_PROXIMITY = 'edgeProximity',
+			LAST_STATE = 'lastState',
+			MAX_COORDINATE = 'maxCoordinate',
+			
+			AVAILABLE = '-available',
+			EDGE = '-edge',
+			START = '-start',
+
+			SCROLL = NAME;
+		
+		var Scroll = A.Component.create(
+			{
+				NAME: NAME,
+
+				NS: NAME,
+
+				ATTRS: {
+					delay: {
+						validator: isNumber,
+						value: null
+					},
+
+					edgeProximity: {
+						setter: function(val) {
+							var instance = this;
+
+							var value = 0;
+
+							if (isNumber(val)) {
+								value = val;
+							}
+							else if (isString(val)) {
+								value = (Lang.toInt(val) / 100);
+							}
+
+							return value;
+						},
+						value: null
+					},
+
+					lastState: {
+						value: {
+							scrollTop: 0
+						}
+					},
+
+					maxCoordinate: {
+						value: null
+					}
+				},
+
+				EXTENDS: A.Plugin.Base,
+
+				prototype: {
+					initializer: function(config) {
+						var instance = this;
+
+						var host = A.one(config.host);
+
+						instance._host = host;
+
+						instance._reset();
+
+						host.on(SCROLL, A.bind(instance._onScroll, instance));
+					},
+
+					_onScroll: function(event) {
+						var instance = this;
+
+						window.e = event;
+
+						var edgeProximity = instance.get(EDGE_PROXIMITY);
+						var lastState = instance.get(LAST_STATE);
+						var maxCoordinate = instance.get(MAX_COORDINATE);
+
+						var node = instance._host._node;
+
+						var scrollTop = node.scrollTop || node.scrollY;
+
+						window.l = lastState;
+
+						var maxCoordinateY = maxCoordinate.y;
+
+						var isPercentage = (edgeProximity % 1) !== 0;
+
+						var edgeProximityY = isPercentage ? (maxCoordinateY * edgeProximity) : edgeProximity;
+
+						var scrolledDown = (scrollTop > lastState.scrollTop);
+						
+						var availableScrollY = (scrollTop - maxCoordinateY);
+
+						var state = {
+							availableScrollY: availableScrollY,
+							scrollTop: scrollTop,
+							scrolledDown: scrolledDown
+						};
+
+						if (scrolledDown) {
+							instance.fire(DOWN, state);
+
+							if ((availableScrollY + edgeProximityY) >= 0) {
+								instance.fire(BOTTOM + EDGE, state);
+							}
+
+							if (availableScrollY > 0) {
+								instance.fire(DOWN + AVAILABLE, state);
+
+								if (lastState.availableScrollY < 1) {
+									instance.fire(DOWN + AVAILABLE + START, state);
+								}
+							}
+
+							if (!lastState.scrolledDown) {
+								instance.fire(DOWN + START, state);
+							}
+						}
+
+						if ((availableScrollY > 0) || (scrollTop < 0) ) {
+							instance._reset();
+						}
+
+						instance.set(LAST_STATE, state);
+
+						clearTimeout(instance._delay);
+
+						instance._delay = setTimeout(instance._reset.bind(instance), instance.get(DELAY));
+					},
+
+					_reset: function() {
+						var instance = this;
+
+						var lastState = instance.get(LAST_STATE);
+
+						lastState.availableScrollY = 0;
+
+						instance.set(LAST_STATE, lastState);
+
+						var host = instance._host;
+
+						var node = host._node;
+
+						var scrollY = node.scrollHeight || A.config.doc.documentElement.scrollHeight - A.config.doc.documentElement.clientHeight;
+
+						instance.set(
+							MAX_COORDINATE,
+							{
+								y: (scrollY)
+							}
+						);
+					}
+				}
+			}
+		);
+
+		Liferay.namespace('SO');
+
+		Liferay.SO.Scroll = Scroll;
+	},
+	'',
+	{
+		requires:['aui-base']
+	}
+);
+
 AUI().use(
 	'aui-base',
 	'transition',
