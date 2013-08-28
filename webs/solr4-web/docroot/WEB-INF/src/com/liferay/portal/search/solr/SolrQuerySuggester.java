f53a6f73703e01bb2815b20e1816028af9a8ed27
Brian Chan <brian.chan@liferay.com>
2013-08-27 17:50:38 -0700

    LPS-32041 SF

--- webs/solr4-web/docroot/WEB-INF/src/com/liferay/portal/search/solr/SolrQuerySuggester.java
+++ webs/solr4-web/docroot/WEB-INF/src/com/liferay/portal/search/solr/SolrQuerySuggester.java
@@ -35,7 +35,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
 
 import org.apache.solr.client.solrj.SolrQuery;
