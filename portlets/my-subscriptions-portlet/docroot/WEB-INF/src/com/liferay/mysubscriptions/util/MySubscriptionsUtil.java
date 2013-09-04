113f8614b3e4942851730d062655eb1b2004f325
Hugo Huijser <hugo.huijser@liferay.com>
2013-09-04 12:19:00 +0800

    LPS-38648 Source formatting

--- portlets/my-subscriptions-portlet/docroot/WEB-INF/src/com/liferay/mysubscriptions/util/MySubscriptionsUtil.java
+++ portlets/my-subscriptions-portlet/docroot/WEB-INF/src/com/liferay/mysubscriptions/util/MySubscriptionsUtil.java
@@ -14,6 +14,7 @@
 
 package com.liferay.mysubscriptions.util;
 
+import com.liferay.knowledgebase.util.PortletKeys;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -25,7 +26,6 @@ import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.Portal;
 import com.liferay.portal.util.PortalUtil;
-import com.liferay.knowledgebase.util.PortletKeys;
 import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
 import com.liferay.portlet.asset.model.AssetRenderer;
 import com.liferay.portlet.asset.model.AssetRendererFactory;
