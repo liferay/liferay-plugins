fa680fa0ce1cc8a001d4400d1f22866b6c6ce5b7
Brian Chan <brian.chan@liferay.com>
2013-09-04 22:41:26 -0700

    LPS-39103

--- shared/portal-compat-shared/src/com/liferay/compat/util/bridges/alloy/BaseAlloyControllerImpl.java
+++ shared/portal-compat-shared/src/com/liferay/compat/util/bridges/alloy/BaseAlloyControllerImpl.java
@@ -123,6 +123,16 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 	}
 
 	public void execute() throws Exception {
+		if (permissioned &&
+			!AlloyPermission.contains(
+				themeDisplay.getPermissionChecker(),
+				themeDisplay.getScopeGroupId(), portlet.getRootPortletId(),
+				controllerPath, actionPath)) {
+
+			renderError(
+				"you-do-not-have-permission-to-access-the-requested-resource");
+		}
+
 		Method method = getMethod(actionPath);
 
 		if (method == null) {
@@ -785,6 +795,10 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 		return search(null, keywords, sorts);
 	}
 
+	protected void setPermissioned(boolean permissioned) {
+		this.permissioned = permissioned;
+	}
+
 	protected String translate(String pattern, Object... arguments) {
 		return LanguageUtil.format(locale, pattern, arguments);
 	}
@@ -892,6 +906,7 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 	protected Map<String, Method> methodsMap;
 	protected MimeResponse mimeResponse;
 	protected PageContext pageContext;
+	protected boolean permissioned;
 	protected Portlet portlet;
 	protected PortletContext portletContext;
 	protected PortletRequest portletRequest;
