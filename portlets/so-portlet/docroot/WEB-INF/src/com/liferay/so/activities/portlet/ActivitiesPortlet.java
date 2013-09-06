247c6bddf27f980ae5378c2fb792405fc72a8395
matthewkong <matthew.kong@liferay.com>
2013-09-06 15:23:47 -0700

    SOS-1715  Comments made in Activities for documents are placed in Previous Comments

--- portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/activities/portlet/ActivitiesPortlet.java
+++ portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/activities/portlet/ActivitiesPortlet.java
@@ -39,6 +39,8 @@ import com.liferay.portal.service.ServiceContextFactory;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFileVersion;
+import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalServiceUtil;
 import com.liferay.portlet.messageboards.model.MBMessage;
 import com.liferay.portlet.messageboards.model.MBMessageDisplay;
 import com.liferay.portlet.messageboards.model.MBThread;
@@ -48,6 +50,7 @@ import com.liferay.portlet.messageboards.service.MBMessageServiceUtil;
 import com.liferay.portlet.messageboards.util.comparator.MessageCreateDateComparator;
 import com.liferay.portlet.social.model.SocialActivitySet;
 import com.liferay.portlet.social.service.SocialActivitySetLocalServiceUtil;
+import com.liferay.so.activities.util.SocialActivityKeyConstants;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import java.io.IOException;
@@ -81,11 +84,23 @@ public class ActivitiesPortlet extends MVCPortlet {
 		String className = activitySet.getClassName();
 		long classPK = activitySet.getClassPK();
 
-		if ((activitySet.getActivityCount() > 1 ) &&
-			className.equals(DLFileEntry.class.getName())) {
+		if (className.equals(DLFileEntry.class.getName())) {
+			if ((activitySet.getActivityCount() > 1 ) &&
+				(activitySet.getType() ==
+					SocialActivityKeyConstants.DL_ADD_FILE_ENTRY)) {
 
-			className = SocialActivitySet.class.getName();
-			classPK = activitySet.getActivitySetId();
+				className = SocialActivitySet.class.getName();
+				classPK = activitySet.getActivitySetId();
+			}
+			else {
+				className = DLFileVersion.class.getName();
+
+				DLFileVersion dlFileVersion =
+					DLFileVersionLocalServiceUtil.getLatestFileVersion(
+						classPK, false);
+
+				classPK = dlFileVersion.getFileVersionId();
+			}
 		}
 
 		MBMessageDisplay mbMessageDisplay =
