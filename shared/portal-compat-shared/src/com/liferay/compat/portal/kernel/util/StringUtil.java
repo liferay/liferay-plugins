dc10e7f7ad7d14f121ca585d8aa47530a30a9445
Hugo Huijser <hugo.huijser@liferay.com>
2013-09-11 14:10:25 +0800

    LPS-39780 - Add method StringUtil.equalsIgnoreCase to portal-compat

--- shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/util/StringUtil.java
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/util/StringUtil.java
@@ -21,6 +21,42 @@ import java.util.Locale;
  */
 public class StringUtil extends com.liferay.portal.kernel.util.StringUtil {
 
+	public static boolean equalsIgnoreCase(String s1, String s2) {
+		if (s1 == s2) {
+			return true;
+		}
+
+		if ((s1 == null) || (s2 == null)) {
+			return false;
+		}
+
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+
+		for (int i = 0; i < s1.length(); i++) {
+			char c1 = s1.charAt(i);
+
+			char c2 = s2.charAt(i);
+
+			if (c1 == c2) {
+				continue;
+			}
+
+			if ((c1 > 127) || (c2 > 127)) {
+				return s1.equalsIgnoreCase(s2);
+			}
+
+			int delta = c1 - c2;
+
+			if ((delta != 32) && (delta != -32)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	public static String toLowerCase(String s) {
 		return toLowerCase(s, null);
 	}
