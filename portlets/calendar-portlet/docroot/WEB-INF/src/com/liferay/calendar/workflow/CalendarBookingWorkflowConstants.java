c98e0ad4632eab89f5831534d4a9adb1d834108e
Marcellus Tavares <marcellus.tavares@liferay.com>
2013-09-19 11:38:23 -0300

    LPS-40176 SF

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/workflow/CalendarBookingWorkflowConstants.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/workflow/CalendarBookingWorkflowConstants.java
@@ -23,4 +23,5 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 public class CalendarBookingWorkflowConstants extends WorkflowConstants {
 
 	public static final int STATUS_MAYBE = 9;
+
 }
