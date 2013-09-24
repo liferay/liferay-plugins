06d87e9e831a2df681976d40f820428aa50cf7c2
Brian Chan <brian.chan@liferay.com>
2013-09-23 22:25:01 -0700

    LPS-40171 SF

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarICalDataHandler.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarICalDataHandler.java
@@ -605,32 +605,28 @@ public class CalendarICalDataHandler implements CalendarDataHandler {
 
 		// Dates
 
-		boolean isMultipleDayEvent = false;
+		boolean multipleDayEvent = false;
 
 		if (calendarBooking.getDuration() > Time.DAY) {
-			isMultipleDayEvent = true;
+			multipleDayEvent = true;
 		}
 
-		if (isMultipleDayEvent && calendarBooking.isAllDay()) {
+		if (calendarBooking.isAllDay()) {
 			DtStart dtStart = new DtStart(
 				new Date(calendarBooking.getStartTime()));
 
 			propertyList.add(dtStart);
 
-			java.util.Calendar endJCalendar = JCalendarUtil.getJCalendar(
-				calendarBooking.getEndTime());
+			if (calendarBooking.getDuration() > Time.DAY) {
+				java.util.Calendar endJCalendar = JCalendarUtil.getJCalendar(
+					calendarBooking.getEndTime());
 
-			endJCalendar.add(java.util.Calendar.DAY_OF_MONTH, 1);
+				endJCalendar.add(java.util.Calendar.DAY_OF_MONTH, 1);
 
-			DtEnd dtEnd = new DtEnd(new Date(endJCalendar.getTime()));
+				DtEnd dtEnd = new DtEnd(new Date(endJCalendar.getTime()));
 
-			propertyList.add(dtEnd);
-		}
-		else if (calendarBooking.isAllDay()) {
-			DtStart dtStart = new DtStart(
-				new Date(calendarBooking.getStartTime()));
-
-			propertyList.add(dtStart);
+				propertyList.add(dtEnd);
+			}
 		}
 		else {
 			DtStart dtStart = new DtStart(
