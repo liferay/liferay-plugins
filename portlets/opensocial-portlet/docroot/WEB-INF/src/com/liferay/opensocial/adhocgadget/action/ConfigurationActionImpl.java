eac67f0548057731ada1a550d8e0dbf129e746bd
Brian Chan <brian.chan@liferay.com>
2013-09-24 21:33:56 -0700

    LPS-37928 Apply every where

--- portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/adhocgadget/action/ConfigurationActionImpl.java
+++ portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/adhocgadget/action/ConfigurationActionImpl.java
@@ -18,10 +18,10 @@ import com.liferay.opensocial.gadget.action.BaseConfigurationAction;
 import com.liferay.opensocial.model.Gadget;
 import com.liferay.opensocial.shindig.util.ShindigUtil;
 import com.liferay.opensocial.util.WebKeys;
-import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.util.PortalUtil;
 
 import java.util.Map;
 
@@ -52,21 +52,18 @@ public class ConfigurationActionImpl extends BaseConfigurationAction {
 		if (tabs2.equals("manage-oauth")) {
 			ShindigUtil.updateOAuthConsumers(actionRequest, actionResponse);
 
-			LiferayPortletConfig liferayPortletConfig =
-				(LiferayPortletConfig)portletConfig;
-
 			String portletResource = ParamUtil.getString(
 				actionRequest, "portletResource");
 
 			SessionMessages.add(
 				actionRequest,
-				liferayPortletConfig.getPortletId() +
+				PortalUtil.getPortletId(actionRequest) +
 					SessionMessages.KEY_SUFFIX_REFRESH_PORTLET,
 				portletResource);
 
 			SessionMessages.add(
 				actionRequest,
-				liferayPortletConfig.getPortletId() +
+				PortalUtil.getPortletId(actionRequest) +
 					SessionMessages.KEY_SUFFIX_UPDATED_CONFIGURATION);
 		}
 		else if (tabs2.equals("preferences")) {
