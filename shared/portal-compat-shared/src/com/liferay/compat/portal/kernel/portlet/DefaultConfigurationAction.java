82ea00201ba50a1d707dd8a96ef3a3c04a5b7b27
Brian Chan <brian.chan@liferay.com>
2013-09-24 22:09:56 -0700

    LPS-37928 Apply every where

--- shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/portlet/DefaultConfigurationAction.java
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/portlet/DefaultConfigurationAction.java
@@ -14,7 +14,6 @@
 
 package com.liferay.compat.portal.kernel.portlet;
 
-import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.Constants;
@@ -25,6 +24,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.service.permission.PortletPermissionUtil;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 
 import java.util.Map;
 
@@ -100,18 +100,15 @@ public class DefaultConfigurationAction
 				return;
 			}
 
-			LiferayPortletConfig liferayPortletConfig =
-				(LiferayPortletConfig)portletConfig;
-
 			SessionMessages.add(
 				actionRequest,
-				liferayPortletConfig.getPortletId() +
+				PortalUtil.getPortletId(actionRequest) +
 					SessionMessages.KEY_SUFFIX_REFRESH_PORTLET,
 				portletResource);
 
 			SessionMessages.add(
 				actionRequest,
-				liferayPortletConfig.getPortletId() +
+				PortalUtil.getPortletId(actionRequest) +
 					SessionMessages.KEY_SUFFIX_UPDATED_CONFIGURATION);
 		}
 	}
