70f951e6ce3c8dcc7c2eae600227e3421ab15a79
Michael Han <michael.han@liferay.com>
2013-09-25 09:45:54 -0500

    LPS-40584 ensure context changes are stored for conditoin nodes

--- webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/runtime/condition/ScriptingConditionEvaluator.java
+++ webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/runtime/condition/ScriptingConditionEvaluator.java
@@ -20,6 +20,9 @@ import com.liferay.portal.kernel.scripting.ScriptingUtil;
 import com.liferay.portal.workflow.kaleo.model.KaleoCondition;
 import com.liferay.portal.workflow.kaleo.runtime.ExecutionContext;
 import com.liferay.portal.workflow.kaleo.runtime.util.ScriptingContextBuilderUtil;
+import com.liferay.portal.workflow.kaleo.util.WorkflowContextUtil;
+
+import java.io.Serializable;
 
 import java.util.HashSet;
 import java.util.Map;
@@ -44,6 +47,13 @@ public class ScriptingConditionEvaluator implements ConditionEvaluator {
 			kaleoCondition.getScriptLanguage(), kaleoCondition.getScript(),
 			classLoaders);
 
+		Map<String, Serializable> resultsWorkflowContext =
+			(Map<String, Serializable>)results.get(
+				WorkflowContextUtil.WORKFLOW_CONTEXT_NAME);
+
+		WorkflowContextUtil.mergeWorkflowContexts(
+			executionContext, resultsWorkflowContext);
+
 		String returnValue = (String)results.get(_RETURN_VALUE);
 
 		if (returnValue != null) {
@@ -61,6 +71,7 @@ public class ScriptingConditionEvaluator implements ConditionEvaluator {
 
 	static {
 		_outputNames.add(_RETURN_VALUE);
+		_outputNames.add(WorkflowContextUtil.WORKFLOW_CONTEXT_NAME);
 	}
 
 }
