d9ae40de6e4123e8e6f069dae14b41ad51395c09
Shinn Lok <shinn.lok@liferay.com>
2013-09-26 15:02:11 -0700

    LRDCOM-6116 Compat

---
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/util/ClassUtil.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.compat.portal.kernel.util;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ClassUtil extends com.liferay.portal.kernel.util.ClassUtil {
+
+	public static String getClassName(Object object) {
+		if (object == null) {
+			return null;
+		}
+
+		Class<?> clazz = object.getClass();
+
+		return clazz.getName();
+	}
+
+}
