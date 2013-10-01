c2f4e152d094e7b189b090412d8bfb69440d886b
shuyangzhou <shuyang.zhou@liferay.com>
2013-10-01 10:53:11 -0700

    LPS-40805 Make wsrp portlet portal resiliency compatible

---
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/messaging/WSRPHotDeployMessageListener.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wsrp.messaging;
+
+import com.liferay.portal.kernel.messaging.HotDeployMessageListener;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.wsrp.service.WSRPConsumerPortletLocalServiceUtil;
+import com.liferay.wsrp.util.ExtensionHelperUtil;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class WSRPHotDeployMessageListener extends HotDeployMessageListener {
+
+	public WSRPHotDeployMessageListener(String... servletContextNames) {
+		super(servletContextNames);
+	}
+
+	@Override
+	protected void onDeploy(Message message) throws Exception {
+		ExtensionHelperUtil.initialize();
+
+		WSRPConsumerPortletLocalServiceUtil.destroyWSRPConsumerPortlets();
+
+		WSRPConsumerPortletLocalServiceUtil.initWSRPConsumerPortlets();
+	}
+
+	@Override
+	protected void onUndeploy(Message message) throws Exception {
+		WSRPConsumerPortletLocalServiceUtil.destroyWSRPConsumerPortlets();
+	}
+
+}
