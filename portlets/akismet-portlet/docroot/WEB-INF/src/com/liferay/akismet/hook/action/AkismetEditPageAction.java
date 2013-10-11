879adbca70ef0aa7d82c338210474a15205b4cad
Roberto Díaz <roberto.diaz@liferay.com>
2013-10-11 11:05:07 +0200

    LPS-41112 Avoid wiki pages to have two heads

--- portlets/akismet-portlet/docroot/WEB-INF/src/com/liferay/akismet/hook/action/AkismetEditPageAction.java
+++ portlets/akismet-portlet/docroot/WEB-INF/src/com/liferay/akismet/hook/action/AkismetEditPageAction.java
@@ -163,6 +163,13 @@ public class AkismetEditPageAction extends BaseStrutsPortletAction {
 		if (spam) {
 			pattern = "version-x-was-marked-as-spam";
 
+			// Selected version
+
+			wikiPage.setStatus(WorkflowConstants.STATUS_APPROVED);
+			wikiPage.setSummary(AkismetConstants.WIKI_PAGE_PENDING_APPROVAL);
+
+			wikiPage = WikiPageLocalServiceUtil.updateWikiPage(wikiPage);
+
 			// Latest version
 
 			if (wikiPage.getVersion() >= latestVersion) {
@@ -181,13 +188,6 @@ public class AkismetEditPageAction extends BaseStrutsPortletAction {
 				}
 			}
 
-			// Selected version
-
-			wikiPage.setStatus(WorkflowConstants.STATUS_APPROVED);
-			wikiPage.setSummary(AkismetConstants.WIKI_PAGE_PENDING_APPROVAL);
-
-			wikiPage = WikiPageLocalServiceUtil.updateWikiPage(wikiPage);
-
 			// Akismet
 
 			if (AkismetUtil.isWikiEnabled(themeDisplay.getCompanyId())) {
@@ -196,6 +196,13 @@ public class AkismetEditPageAction extends BaseStrutsPortletAction {
 		}
 		else {
 			pattern = "version-x-was-marked-as-not-spam";
+
+			// Selected version
+
+			wikiPage.setStatus(WorkflowConstants.STATUS_APPROVED);
+			wikiPage.setSummary(StringPool.BLANK);
+
+			wikiPage = WikiPageLocalServiceUtil.updateWikiPage(wikiPage);
 
 			// Latest version
 
@@ -209,13 +216,6 @@ public class AkismetEditPageAction extends BaseStrutsPortletAction {
 			else {
 				SessionMessages.add(actionRequest, "anotherUserHasMadeChanges");
 			}
-
-			// Selected version
-
-			wikiPage.setStatus(WorkflowConstants.STATUS_APPROVED);
-			wikiPage.setSummary(StringPool.BLANK);
-
-			wikiPage = WikiPageLocalServiceUtil.updateWikiPage(wikiPage);
 
 			// Akismet
 
