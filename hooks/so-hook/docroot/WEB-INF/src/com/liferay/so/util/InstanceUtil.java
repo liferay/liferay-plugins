4805795d4a6ddc572f3f9afe486b97e2adc486ab
Jonathan Lee <Jonathan.Lee@liferay.com>
2013-10-15 16:28:20 -0700

    SOS-1832 Making so-strata-theme the default theme for Social Office.

--- hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/util/InstanceUtil.java
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/util/InstanceUtil.java
@@ -255,7 +255,7 @@ public class InstanceUtil {
 			group.getGroupId(), true, new ServiceContext());
 
 		LayoutSetLocalServiceUtil.updateLookAndFeel(
-			group.getGroupId(), "so_WAR_sotheme", "01", "", false);
+			group.getGroupId(), "sostrata_WAR_sostratatheme", "01", "", false);
 
 		// Home
 
@@ -374,7 +374,7 @@ public class InstanceUtil {
 			group.getGroupId(), true, new ServiceContext());
 
 		LayoutSetLocalServiceUtil.updateLookAndFeel(
-			group.getGroupId(), "so_WAR_sotheme", "01", "", false);
+			group.getGroupId(), "sostrata_WAR_sostratatheme", "01", "", false);
 
 		// Dashboard
 
@@ -481,7 +481,7 @@ public class InstanceUtil {
 			group.getGroupId(), true, new ServiceContext());
 
 		LayoutSetLocalServiceUtil.updateLookAndFeel(
-			group.getGroupId(), "so_WAR_sotheme", "01", "", false);
+			group.getGroupId(), "sostrata_WAR_sostratatheme", "01", "", false);
 
 		// Profile
 
