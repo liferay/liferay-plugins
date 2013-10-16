d62bd779dbf2be5502663a2bd613de6565d0f8f0
Brian Chan <brian.chan@liferay.com>
2013-10-16 08:26:55 -0700

    LPS-40873 Just backend changes from Philippe Proulx's pull at https://github.com/brianchandotcom/liferay-plugins/pull/2183, except that I renamed WebRtc* to WebRTC*

---
+++ portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/poller/ChatVideoPollerProcessor.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2013 Savoir-faire Linux Inc.
+ *     Philippe Proulx <philippe.proulx@savoirfairelinux.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by Savoir-faire Linux.  The name of Savoir-faire Linux
+ * may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+package com.liferay.chat.video.poller;
+
+import com.liferay.portal.NoSuchUserException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.poller.BasePollerProcessor;
+import com.liferay.portal.kernel.poller.PollerRequest;
+import com.liferay.portal.kernel.poller.PollerResponse;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.model.ContactConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.chat.video.WebRtcClient;
+import com.liferay.chat.video.WebRtcManager;
+import com.liferay.chat.video.WebRtcClient.Mailbox;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Philippe Proulx <philippe.proulx@savoirfairelinux.com>
+ */
+public class ChatVideoPollerProcessor extends BasePollerProcessor {
+	private final WebRtcManager webRtcManager = new WebRtcManager();
+
+    protected String getWebRtcMessageType(PollerRequest req) {
+        return this.getString(req, "webrtcMsgType");
+    }
+
+    protected long getWebRtcDstUserId(PollerRequest req) {
+        if (req.getParameterMap().containsKey("webrtcDstUserId")) {
+            return this.getLong(req, "webrtcDstUserId");
+        } else {
+            return -1;
+        }
+    }
+
+    protected void processWebRtcMessage(PollerRequest req) {
+        long srcUserId = req.getUserId();
+        String webRtcMsgType = this.getWebRtcMessageType(req);
+        long dstUserId = this.getWebRtcDstUserId(req);
+
+        // process message
+        if (webRtcMsgType.equals("setAvailability")) {
+            boolean isAvailable = getBoolean(req, "webrtcIsAvailable");
+            this.webRtcManager.processMsgSetAvailability(srcUserId, isAvailable);
+        } else if (webRtcMsgType.equals("call")) {
+            this.webRtcManager.processMsgCall(srcUserId, dstUserId);
+        } else if (webRtcMsgType.equals("answer")) {
+            boolean acceptAnswer = getBoolean(req, "webrtcAccept");
+            this.webRtcManager.processMsgAnswer(srcUserId, dstUserId, acceptAnswer);
+        } else if (webRtcMsgType.equals("sdp")) {
+            String jsonSdp = getString(req, "webrtcSdp");
+            this.webRtcManager.processMsgShareSdp(srcUserId, dstUserId, jsonSdp);
+        } else if (webRtcMsgType.equals("ice")) {
+            String jsonIceCandidate = getString(req, "webrtcIce");
+            this.webRtcManager.processMsgShareIceCandidate(srcUserId, dstUserId, jsonIceCandidate);
+        } else if (webRtcMsgType.equals("hangUp")) {
+            this.webRtcManager.processMsgHangUp(srcUserId, dstUserId);
+        } else if (webRtcMsgType.equals("reset")) {
+            this.webRtcManager.processMsgReset(srcUserId);
+        } else if (webRtcMsgType.equals("updatePresence")) {
+            this.webRtcManager.updatePresence(srcUserId);
+        } else {
+            // safely ignore unknown messages
+            // TODO: error
+        }
+    }
+
+    protected void getWebRtcData(PollerRequest req, PollerResponse resp) throws Exception {
+        // get WebRtc client
+        WebRtcClient client = this.webRtcManager.getClient(req.getUserId());
+
+        // initialize response
+        JSONObject webrtcObj = JSONFactoryUtil.createJSONObject();
+        JSONArray mailsArray = JSONFactoryUtil.createJSONArray();
+        JSONArray clientsArray = JSONFactoryUtil.createJSONArray();
+
+        // get client mails
+        if (client != null) {
+            List<WebRtcClient.Mailbox.Mail> clientMails = client.getOugoingMailbox().popAll();
+            for (Mailbox.Mail mail : clientMails) {
+                String type = mail.getMsgType();
+                String jsonMessage = mail.getJsonMessage();
+                JSONObject mailObj = JSONFactoryUtil.createJSONObject();
+                mailObj.put("type", type);
+                mailObj.put("msg", jsonMessage);
+                mailObj.put("fromUserId", mail.getFromUserId());
+                mailsArray.put(mailObj);
+            }
+        }
+
+        // copy list of connected clients (except me) to outgoing array
+        for (Long uid : this.webRtcManager.getAvailableClientsIds()) {
+            if (uid != req.getUserId()) {
+                clientsArray.put(uid);
+            }
+        }
+
+        // set parameters
+        webrtcObj.put("mails", mailsArray);
+        webrtcObj.put("clients", clientsArray);
+        resp.setParameter("webrtc", webrtcObj);
+    }
+
+	@Override
+	protected void doReceive(PollerRequest pollerRequest, PollerResponse pollerResponse) throws Exception {
+        JSONObject yallObj = JSONFactoryUtil.createJSONObject();
+
+        this.getWebRtcData(pollerRequest, pollerResponse);
+	}
+
+	@Override
+	protected void doSend(PollerRequest pollerRequest) throws Exception {
+        this.processWebRtcMessage(pollerRequest);
+	}
+}
---
+++ portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/scheduler/WebRTCPresenceScheduler.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2013 Savoir-faire Linux Inc.
+ *     Philippe Proulx <philippe.proulx@savoirfairelinux.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by Savoir-faire Linux.  The name of Savoir-faire Linux
+ * may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+package com.liferay.chat.video.scheduler;
+
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.chat.video.WebRtcManager;
+
+/**
+ * WebRTC presence scheduler
+ *
+ * This class is instantiated by Liferay and checks at a regular
+ * interval all the client presences of all the WebRTC managers.
+ *
+ * @author Philippe Proulx <philippe.proulx@savoirfairelinux.com>
+ */
+public class WebRtcPresenceScheduler extends BaseMessageListener {
+    @Override
+    protected void doReceive(Message message) throws Exception {
+        WebRtcManager.checkAllManagersPresences();
+    }
+}
