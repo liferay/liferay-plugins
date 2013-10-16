6dcd8d816bc4f50e0056151e5db85b009720b211
Yu Hai <hai.yu@liferay.com>
2013-10-16 11:32:26 +0800

    LPS-34833 Regenerate (there was a regenerate commit for this ticket, but some files were not committed)

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/base/CalendarBookingServiceBaseImpl.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/base/CalendarBookingServiceBaseImpl.java
@@ -725,6 +725,25 @@ public abstract class CalendarBookingServiceBaseImpl extends BaseServiceImpl
 	}
 
 	/**
+	 * Returns the social activity remote service.
+	 *
+	 * @return the social activity remote service
+	 */
+	public com.liferay.portlet.social.service.SocialActivityService getSocialActivityService() {
+		return socialActivityService;
+	}
+
+	/**
+	 * Sets the social activity remote service.
+	 *
+	 * @param socialActivityService the social activity remote service
+	 */
+	public void setSocialActivityService(
+		com.liferay.portlet.social.service.SocialActivityService socialActivityService) {
+		this.socialActivityService = socialActivityService;
+	}
+
+	/**
 	 * Returns the social activity persistence.
 	 *
 	 * @return the social activity persistence
@@ -985,6 +1004,8 @@ public abstract class CalendarBookingServiceBaseImpl extends BaseServiceImpl
 	protected RatingsStatsPersistence ratingsStatsPersistence;
 	@BeanReference(type = com.liferay.portlet.social.service.SocialActivityLocalService.class)
 	protected com.liferay.portlet.social.service.SocialActivityLocalService socialActivityLocalService;
+	@BeanReference(type = com.liferay.portlet.social.service.SocialActivityService.class)
+	protected com.liferay.portlet.social.service.SocialActivityService socialActivityService;
 	@BeanReference(type = SocialActivityPersistence.class)
 	protected SocialActivityPersistence socialActivityPersistence;
 	@BeanReference(type = com.liferay.portlet.social.service.SocialActivityCounterLocalService.class)
