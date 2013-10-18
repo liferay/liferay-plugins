dc85393b9ac24d1ec26235c7fdae126f8ad41407
Jonathan Lee <Jonathan.Lee@liferay.com>
2013-10-17 17:38:08 -0700

    SOS-1837 Adding McroblogsUserNotificationHandler

---
+++ portlets/microblogs-portlet/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/notifications/MicroblogsUserNotificationHandler.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.microblogs.microblogs.notifications;
+
+import com.liferay.microblogs.model.MicroblogsEntry;
+import com.liferay.microblogs.model.MicroblogsEntryConstants;
+import com.liferay.microblogs.service.MicroblogsEntryLocalServiceUtil;
+import com.liferay.microblogs.util.PortletKeys;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.notifications.BaseUserNotificationHandler;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.UserNotificationEvent;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserNotificationEventLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
+import com.liferay.portlet.asset.model.AssetRenderer;
+import com.liferay.portlet.asset.model.AssetRendererFactory;
+
+/**
+ * @author Jonathan Lee
+ */
+public class MicroblogsUserNotificationHandler
+	extends BaseUserNotificationHandler {
+
+	public MicroblogsUserNotificationHandler() {
+		setPortletId(PortletKeys.MICROBLOGS);
+	}
+
+	@Override
+	protected String getBody(
+			UserNotificationEvent userNotificationEvent,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			userNotificationEvent.getPayload());
+
+		long microblogsEntryId = jsonObject.getLong("microblogsEntryId");
+
+		MicroblogsEntry microblogsEntry =
+			MicroblogsEntryLocalServiceUtil.fetchMicroblogsEntry(
+				microblogsEntryId);
+
+		if (microblogsEntry == null) {
+			UserNotificationEventLocalServiceUtil.deleteUserNotificationEvent(
+				userNotificationEvent.getUserNotificationEventId());
+
+			return null;
+		}
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("<div class=\"title\">");
+
+		if (microblogsEntry.getType() == MicroblogsEntryConstants.TYPE_REPLY) {
+			String userFullName = PortalUtil.getUserName(
+				microblogsEntry.getUserId(), StringPool.BLANK);
+
+			sb.append(
+				serviceContext.translate(
+					"x-commented-on-your-post", userFullName));
+		}
+
+		sb.append("</div><div class=\"body\">");
+		sb.append(HtmlUtil.escape(microblogsEntry.getContent()));
+		sb.append("</div>");
+
+		return sb.toString();
+	}
+
+	@Override
+	protected String getLink(
+			UserNotificationEvent userNotificationEvent,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			userNotificationEvent.getPayload());
+
+		long microblogsEntryId = jsonObject.getLong("microblogsEntryId");
+
+		AssetRendererFactory assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				MicroblogsEntry.class.getName());
+
+		AssetRenderer assetRenderer = assetRendererFactory.getAssetRenderer(
+			microblogsEntryId);
+
+		return assetRenderer.getURLViewInContext(
+			serviceContext.getLiferayPortletRequest(),
+			serviceContext.getLiferayPortletResponse(), null);
+	}
+
+}
