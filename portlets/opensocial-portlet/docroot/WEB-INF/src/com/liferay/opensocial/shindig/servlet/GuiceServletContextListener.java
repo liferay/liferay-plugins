5527fc99ab888f016b07b0c240d0189c9579fe42
Michael Han <michael.han@liferay.com>
2013-10-31 21:46:54 -0500

    LPS-41825 listen to hot deploy event to initialize

--- portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/shindig/servlet/GuiceServletContextListener.java
+++ portlets/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/shindig/servlet/GuiceServletContextListener.java
@@ -14,8 +14,17 @@
 
 package com.liferay.opensocial.shindig.servlet;
 
+import com.liferay.opensocial.service.ClpSerializer;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
 import com.liferay.portal.kernel.portlet.PortletClassLoaderUtil;
 import com.liferay.portal.kernel.util.BasePortalLifecycle;
+import com.liferay.portal.kernel.util.GetterUtil;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -28,6 +37,8 @@ public class GuiceServletContextListener extends BasePortalLifecycle
 
 	@Override
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		portalDestroy();
+
 		_guiceServletContextListener.contextDestroyed(servletContextEvent);
 	}
 
@@ -40,10 +51,45 @@ public class GuiceServletContextListener extends BasePortalLifecycle
 
 	@Override
 	protected void doPortalDestroy() throws Exception {
+		MessageBusUtil.unregisterMessageListener(
+			DestinationNames.HOT_DEPLOY, _messageListener);
 	}
 
 	@Override
 	protected void doPortalInit() throws Exception {
+		_messageListener = new MessageListener() {
+
+			@Override
+			public void receive(Message message)
+				throws MessageListenerException {
+
+				String servletContextName = GetterUtil.getString(
+					message.getString("servletContextName"));
+
+				String command = GetterUtil.getString(
+					message.getString("command"));
+
+				if (command.equals("deploy") &&
+					ClpSerializer.getServletContextName().equals(
+						servletContextName)) {
+
+					try {
+						initialize();
+					}
+					catch (Exception e) {
+						_log.error("Unable to initialize open-social", e);
+
+					}
+				}
+			}
+
+		};
+
+		MessageBusUtil.registerMessageListener(
+			DestinationNames.HOT_DEPLOY, _messageListener);
+	}
+
+	protected void initialize() throws Exception {
 		ClassLoader portletClassLoader =
 			PortletClassLoaderUtil.getClassLoader();
 
@@ -66,8 +112,12 @@ public class GuiceServletContextListener extends BasePortalLifecycle
 		}
 	}
 
+	private static Log _log = LogFactoryUtil.getLog(
+		GuiceServletContextListener.class);
+
 	private ServletContextListener _guiceServletContextListener =
 		new org.apache.shindig.common.servlet.GuiceServletContextListener();
 	private ServletContextEvent _initializedServletContextEvent;
+	private MessageListener _messageListener;
 
 }
