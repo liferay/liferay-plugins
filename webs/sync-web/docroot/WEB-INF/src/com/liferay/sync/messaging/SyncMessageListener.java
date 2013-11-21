e5443a81f3a1f996baf8677f63e730f33e283ebf
Brian Chan <brian.chan@liferay.com>
2013-11-20 18:28:28 -0800

    LPS-42493 Rename

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/messaging/SyncMessageListener.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/messaging/SyncMessageListener.java
@@ -94,7 +94,7 @@ public class SyncMessageListener extends BaseMessageListener {
 				return;
 			}
 
-			if (!SyncUtil.validateFolder(folder)) {
+			if (!SyncUtil.isSupportedFolder(folder)) {
 				return;
 			}
 
