5123c0002b569b9f4145f5ec8ca30601c3ee9592
Jonathan Lee <Jonathan.Lee@liferay.com>
2013-11-21 16:50:14 -0800

    LPS-42530 Implement the same fix from BaseUserNOtificationHandler in portal for portal-compat-shared.

---
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/notifications/BaseUserNotificationHandler.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.compat.portal.kernel.notifications;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.notifications.UserNotificationFeedEntry;
+import com.liferay.portal.model.UserNotificationEvent;
+import com.liferay.portal.service.ServiceContext;
+
+/**
+ * @author Jonathan Lee
+ */
+public abstract class BaseUserNotificationHandler
+	extends com.liferay.portal.kernel.notifications.
+		BaseUserNotificationHandler {
+
+	@Override
+	public UserNotificationFeedEntry interpret(
+			UserNotificationEvent userNotificationEvent,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		try {
+			UserNotificationFeedEntry userNotificationFeedEntry = doInterpret(
+				userNotificationEvent, serviceContext);
+
+			if (userNotificationEvent != null) {
+				userNotificationFeedEntry.setPortletId(getPortletId());
+			}
+
+			return userNotificationFeedEntry;
+		}
+		catch (Exception e) {
+			_log.error("Unable to interpret notification", e);
+		}
+
+		return null;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		BaseUserNotificationHandler.class);
+
+}
