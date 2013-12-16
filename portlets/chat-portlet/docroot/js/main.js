1985f5b7b7412d8ca2e619a064287ef8829d72f9
michael.chen <michael.chen@liferay.com>
2013-12-16 14:33:01 +0800

    LPS-42944 Correct the openPanelId judgement

--- portlets/chat-portlet/docroot/js/main.js
+++ portlets/chat-portlet/docroot/js/main.js
@@ -1251,12 +1251,8 @@ AUI().use(
 				if (instance._initialRequest && response.initialRequest) {
 					instance._loadCache(entries);
 
-					if (instance._openPanelId.length) {
-						var openPanelId = parseInt(instance._openPanelId, 10);
-
-						if (!isNaN(openPanelId)) {
-							instance._createChatFromUser(instance._openPanelId);
-						}
+					if (instance._openPanelId && !isNaN(instance._openPanelId)) {
+						instance._createChatFromUser(instance._openPanelId);
 					}
 
 					instance._restoreMinimizedPanels();
