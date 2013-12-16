39751078b13e57b2ce4bc979002789e18e3c7cc3
Shinn Lok <shinn.lok@liferay.com>
2013-12-16 15:01:52 -0800

    LPS-42458 Same fix as Currency Converter in portal

--- portlets/stocks-portlet/docroot/WEB-INF/src/com/liferay/stocks/portlet/StocksPortlet.java
+++ portlets/stocks-portlet/docroot/WEB-INF/src/com/liferay/stocks/portlet/StocksPortlet.java
@@ -21,6 +21,8 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.stocks.util.PortletKeys;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import java.io.IOException;
@@ -56,7 +58,17 @@ public class StocksPortlet extends MVCPortlet {
 			return;
 		}
 
-		PortletPreferences preferences = actionRequest.getPreferences();
+		PortletPreferences portletPreferences = null;
+
+		try {
+			portletPreferences =
+				PortletPreferencesFactoryUtil.getPortletPreferences(
+					PortalUtil.getHttpServletRequest(actionRequest),
+					PortletKeys.STOCKS);
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
 
 		String[] symbols = StringUtil.split(
 			StringUtil.toUpperCase(
@@ -65,10 +77,10 @@ public class StocksPortlet extends MVCPortlet {
 
 		Arrays.sort(symbols);
 
-		preferences.setValues("symbols", symbols);
+		portletPreferences.setValues("symbols", symbols);
 
 		try {
-			preferences.store();
+			portletPreferences.store();
 		}
 		catch (ValidatorException ve) {
 			SessionErrors.add(
