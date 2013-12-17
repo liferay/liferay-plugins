ba8cca45a7473fab3bcdd575ccf8d70a2c82fbf5
Eric Chin <eric.chin@liferay.com>
2013-12-17 08:13:05 -0800

    LPS-42880 Add Shibboleth auto login hook and logout action with configuration settings for portal

---
+++ hooks/shibboleth-hook/docroot/WEB-INF/src/com/liferay/shibboleth/util/ShibbolethUtil.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.shibboleth.util;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+
+/**
+ * @author Eric Chin
+ */
+public class ShibbolethUtil {
+
+	public static boolean isEnabled(long companyId) throws SystemException {
+		if (PrefsPropsUtil.getBoolean(
+				companyId, PropsKeys.SHIBBOLETH_AUTH_ENABLED,
+				PropsValues.SHIBBOLETH_AUTH_ENABLED)) {
+
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
