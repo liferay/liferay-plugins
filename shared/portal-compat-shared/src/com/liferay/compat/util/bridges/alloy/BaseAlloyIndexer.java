e637c77ecfc0a623c263801ad4ea97e98a3b8c9e
Ethan Bustad <ethan.bustad@liferay.com>
2013-12-18 17:51:13 -0800

    LPS-42994 BaseAlloyIndexer reindex only models in this company (portal-compat)

--- shared/portal-compat-shared/src/com/liferay/compat/util/bridges/alloy/BaseAlloyIndexer.java
+++ shared/portal-compat-shared/src/com/liferay/compat/util/bridges/alloy/BaseAlloyIndexer.java
@@ -112,7 +112,8 @@ public abstract class BaseAlloyIndexer extends BaseIndexer {
 	}
 
 	protected void reindexModels(long companyId) throws Exception {
-		int count = alloyServiceInvoker.getModelsCount();
+		int count = (int)alloyServiceInvoker.executeDynamicQueryCount(
+			new Object[] {"companyId", companyId});
 
 		int pages = count / Indexer.DEFAULT_INTERVAL;
 
@@ -127,7 +128,8 @@ public abstract class BaseAlloyIndexer extends BaseIndexer {
 	protected void reindexModels(long companyId, int start, int end)
 		throws Exception {
 
-		List<Object> models = alloyServiceInvoker.getModels(start, end);
+		List<Object> models = alloyServiceInvoker.executeDynamicQuery(
+			new Object[] {"companyId", companyId}, start, end);
 
 		if (models.isEmpty()) {
 			return;
