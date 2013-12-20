630313e875fce0c4feffb896c009392d3c22147e
Brian Chan <brian.chan@liferay.com>
2013-12-20 10:18:29 -0800

    LPS-42880 SF

--- hooks/shibboleth-hook/docroot/WEB-INF/src/com/liferay/shibboleth/util/PropsValues.java
+++ hooks/shibboleth-hook/docroot/WEB-INF/src/com/liferay/shibboleth/util/PropsValues.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.shibboleth.util;
 
 import com.liferay.portal.kernel.util.GetterUtil;
