5ec387c01dcea174d06aed69ec893bd6aebce99e
Philippe Proulx <philippe.proulx@savoirfairelinux.com>
2014-01-06 13:43:20 -0500

    LPS-40873 Add WebRTC mailbox and mail

---
+++ portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/WebRTCMailbox.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.chat.video;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Philippe Proulx
+ */
+public class WebRTCMailbox {
+
+	public List<WebRTCMail> popAllWebRTCMails() {
+		List<WebRTCMail> allWebRTCMails = new ArrayList<WebRTCMail>(_webRTCMails);
+
+		_webRTCMails.clear();
+
+		return allWebRTCMails;
+	}
+
+	public void pushWebRTCMail(WebRTCMail webRTCMail) {
+		_webRTCMails.add(webRTCMail);
+	}
+
+	private List<WebRTCMail> _webRTCMails = new ArrayList<WebRTCMail>();
+
+}
