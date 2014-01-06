b965357cbf03c8e8015628050a3e9beab1090e5f
Kevin Fong <kevin.fong@liferay.com>
2014-01-06 14:47:04 -0800

    Theme Project 1.2

---
+++ themes/deep-blue-theme/docroot/_diffs/js/main.js
@@ -0,0 +1,40 @@
+AUI().ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+	'liferay-sign-in-modal',
+	function(A) {
+		var signIn = A.one('#sign-in');
+
+		if (signIn && signIn.getData('redirect') !== 'true') {
+			signIn.plug(Liferay.SignInModal);
+		}
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	node: the Alloy Node object of the current portlet
+	*/
+
+	function(portletId, node) {
+	}
+);
+
+Liferay.on(
+	'allPortletsReady',
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
