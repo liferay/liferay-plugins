71f7c60e9fb8d53f96f696ae42f40d4578ce8458
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2014-01-20 16:51:54 -0300

    LPS-43434 SF

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarImporterLocalServiceImpl.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarImporterLocalServiceImpl.java
@@ -536,7 +536,6 @@ public class CalendarImporterLocalServiceImpl
 
 		return CalendarResourceUtil.getGroupCalendarResource(
 			groupId, serviceContext);
-		}
 	}
 
 	protected String getRecurrence(TZSRecurrence tzsRecurrence) {
