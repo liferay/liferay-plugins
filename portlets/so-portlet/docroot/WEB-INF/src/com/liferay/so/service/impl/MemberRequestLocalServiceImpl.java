7136cb23835a4e1799479fefbfc33ca250add499
Jozsef Illes <jozsef.illes@liferay.com>
2014-01-21 12:58:47 +0100

    SOS-2042 Separate login URL in Member Invitation mail

--- portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/service/impl/MemberRequestLocalServiceImpl.java
+++ portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/service/impl/MemberRequestLocalServiceImpl.java
@@ -19,6 +19,7 @@ package com.liferay.so.service.impl;
 
 import com.liferay.mail.service.MailServiceUtil;
 import com.liferay.portal.NoSuchUserException;
+import com.liferay.portal.NoSuchWorkflowDefinitionLinkException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -40,6 +41,7 @@ import com.liferay.portal.model.User;
 import com.liferay.portal.model.UserNotificationDeliveryConstants;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.UserNotificationEventLocalServiceUtil;
+import com.liferay.portal.service.WorkflowDefinitionLinkLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.so.MemberRequestAlreadyUsedException;
 import com.liferay.so.MemberRequestInvalidUserException;
@@ -274,7 +276,8 @@ public class MemberRequestLocalServiceImpl
 	}
 
 	protected String getCreateAccountURL(
-		MemberRequest memberRequest, ServiceContext serviceContext) {
+			MemberRequest memberRequest, ServiceContext serviceContext)
+		throws PortalException, SystemException {
 
 		String createAccountURL = (String)serviceContext.getAttribute(
 			"createAccountURL");
@@ -283,13 +286,20 @@ public class MemberRequestLocalServiceImpl
 			createAccountURL = serviceContext.getPortalURL();
 		}
 
-		String redirectURL = getRedirectURL(serviceContext);
+		try {
+			WorkflowDefinitionLinkLocalServiceUtil.
+				getDefaultWorkflowDefinitionLink(
+					memberRequest.getCompanyId(), User.class.getName(), 0, 0);
+		}
+		catch (NoSuchWorkflowDefinitionLinkException nswdle) {
+			String redirectURL = getRedirectURL(serviceContext);
 
 		redirectURL = addParameterWithPortletNamespace(
 			redirectURL, "key", memberRequest.getKey());
 
 		createAccountURL = addParameterWithPortletNamespace(
 			createAccountURL, "redirect", redirectURL);
+		}
 
 		return createAccountURL;
 	}
