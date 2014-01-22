84e2f84b1d6c6ee2f338f446bcb6b28b11404bc7
Jonathan Lee <Jonathan.Lee@liferay.com>
2014-01-21 16:58:33 -0800

    SOS-2036 Upgrade old table created from the previous version.

---
+++ hooks/so-activities-hook/docroot/WEB-INF/src/com/liferay/so/activities/hook/upgrade/v1_0_1/UpgradeSocial.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.so.activities.hook.upgrade.v1_0_1;
+
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringBundler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Matthew Kong
+ */
+public class UpgradeSocial extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		updateSOSocialActivities();
+	}
+
+	protected void updateSOSocialActivities() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"select activityId, activitySetId from SO_SocialActivity");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long activityId = rs.getLong("activityId");
+				long activitySetId = rs.getLong("activitySetId");
+
+				StringBundler sb = new StringBundler(4);
+
+				sb.append("update SocialActivity set activitySetId = ");
+				sb.append(activitySetId);
+				sb.append(" where activityId = ");
+				sb.append(activityId);
+
+				runSQL(sb.toString());
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+
+		runSQL("drop table SO_SocialActivity");
+	}
+
+}
