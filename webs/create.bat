066ef35af5bb239013d7c62ba288dff8b0f2615b
Terry Jia <terry.jia@liferay.com>
2014-01-23 16:09:52 +0800

    LPS-43682 Support creating web-type plugins in PluginsSDK

---
+++ webs/create.bat
@@ -0,0 +1,25 @@
+@echo off
+
+if "" == "%1" goto errorCreate
+
+if "" == "%2" goto errorCreate
+
+call ant -Dweb.name=%1 -Dweb.display.name=%2 create
+
+rem call ant deploy
+
+goto end
+
+:errorCreate
+	echo.
+	echo Usage: create.bat hello-world "Hello World"
+	echo.
+	echo The first hello-world is your web id. A new directory will be created based on
+	echo the web id.
+	echo.
+	echo The second "Hello World" is the web's display name. The quotation marks are
+	echo only needed because there is a space in the display name.
+
+	goto end
+
+:end
---
+++ webs/create.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#set -x
+
+if [ $# -lt 2 ]; then
+	echo
+	echo Usage: ./create.sh hello-world \"Hello World\"
+	echo
+	echo The first hello-world is your web id. A new directory will be created based on
+	echo the web id.
+	echo
+	echo The second \"Hello World\" is the web\'s display name. The quotation marks are
+	echo only needed because there is a space in the display name.
+
+	exit 127
+fi
+
+ant -Dweb.name=$1 -Dweb.display.name=\"$2\" create
+
+#ant deploy
+
+exit 0
