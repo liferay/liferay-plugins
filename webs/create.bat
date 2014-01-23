825f8310f756d36733111099347334859798adf6
Brian Chan <brian.chan@liferay.com>
2014-01-23 10:01:08 -0800

    LPS-43682 Match col 81

--- webs/create.bat
+++ webs/create.bat
@@ -17,8 +17,8 @@ goto end
 	echo The first hello-world is your web id. A new directory will be created based on
 	echo the web id.
 	echo.
-	echo The second "Hello World" is the web's display name. The quotation marks are
-	echo only needed because there is a space in the display name.
+	echo The second "Hello World" is the web's display name. The quotation marks are only
+	echo needed because there is a space in the display name.
 
 	goto end
 
