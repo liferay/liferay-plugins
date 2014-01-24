4f1cea7b3096fde87ee7e7e0c252efe9d3f313e8
Roberto Díaz <roberto.diaz@liferay.com>
2014-01-24 12:48:13 +0100

    LPS-43687 Add javadoc to ExtRepository.java

--- shared/dl-repository-shared/src/com/liferay/repository/external/ExtRepository.java
+++ shared/dl-repository-shared/src/com/liferay/repository/external/ExtRepository.java
@@ -31,25 +31,78 @@ import java.util.List;
  */
 public interface ExtRepository {
 
+	/**
+	 * Adds a file entry and associated metadata based on an {@link InputStream}
+	 * object.
+	 *
+	 * @param  extRepositoryParentFolderKey the primary key of the folder
+	 * @param  mimeType the file's MIME type
+	 * @param  title the name to be assigned to the file
+	 * @param  description the file's description
+	 * @param  changeLog the file's version change log
+	 * @param  inputStream the file's data (optionally <code>null</code>)
+	 * @return the file entry
+	 * @throws PortalException if the parent folder could not be found or if the
+	 *         file entry's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public ExtRepositoryFileEntry addExtRepositoryFileEntry(
 			String extRepositoryParentFolderKey, String mimeType, String title,
 			String description, String changeLog, InputStream inputStream)
 		throws PortalException, SystemException;
 
+	/**
+	 * Adds a folder.
+	 *
+	 * @param  extRepositoryParentFolderKey the repository primary key of the
+	 *         folder
+	 * @param  name the folder's name
+	 * @param  description the folder's description
+	 * @return the folder
+	 * @throws PortalException if the parent folder could not be found or if the
+	 *         new folder's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public ExtRepositoryFolder addExtRepositoryFolder(
 			String extRepositoryParentFolderKey, String name,
 			String description)
 		throws PortalException, SystemException;
 
+	/**
+	 * Cancels the file's checkout.
+	 *
+	 * @param  extRepositoryFileEntryKey the repository primary key of the file
+	 *         entry
+	 * @return the file version
+	 * @throws PortalException if file entry's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public ExtRepositoryFileVersion cancelCheckOut(
 			String extRepositoryFileEntryKey)
 		throws PortalException, SystemException;
 
+	/**
+	 * Checks in the file.
+	 *
+	 * @param  extRepositoryFileEntryKey the primary key of the file entry
+	 * @param  createMajorVersion
+	 * @param  changeLog the file's version change log
+	 * @throws PortalException if the file entry's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void checkInExtRepositoryFileEntry(
 			String extRepositoryFileEntryKey, boolean createMajorVersion,
 			String changeLog)
 		throws PortalException, SystemException;
 
+	/**
+	 * Checks out the file.
+	 *
+	 * @param  extRepositoryFileEntryKey the repository primary key of the file
+	 * @return the file entry
+	 * @throws PortalException if the file entry's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public ExtRepositoryFileEntry checkOutExtRepositoryFileEntry(
 			String extRepositoryFileEntryKey)
 		throws PortalException, SystemException;
@@ -60,6 +113,14 @@ public interface ExtRepository {
 			String newTitle)
 		throws PortalException, SystemException;
 
+	/**
+	 * Deletes a file, a folder or a version.
+	 *
+	 * @param  extRepositoryObjectType the type of the object
+	 * @param  extRepositoryObjectKey the repository primary key of the object
+	 * @throws PortalException if the object's information was invalidd
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void deleteExtRepositoryObject(
 			ExtRepositoryObjectType<? extends ExtRepositoryObject>
 				extRepositoryObjectType,
@@ -68,14 +129,40 @@ public interface ExtRepository {
 
 	public String getAuthType();
 
+	/**
+	 * Returns the content stream of the current file version.
+	 *
+	 * @param  extRepositoryFileEntry the repository file entry primary key
+	 * @return the content stream of the current file version.
+	 * @throws PortalException if the file entry's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public InputStream getContentStream(
 			ExtRepositoryFileEntry extRepositoryFileEntry)
 		throws PortalException, SystemException;
 
+	/**
+	 * Returns the content stream of the file version.
+	 *
+	 * @param  extRepositoryFileVersion the repository file version primary key
+	 * @return the content stream of the file version
+	 * @throws PortalException if the file version's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public InputStream getContentStream(
 			ExtRepositoryFileVersion extRepositoryFileVersion)
 		throws PortalException, SystemException;
 
+	/**
+	 * Returns the file version.
+	 *
+	 * @param  extRepositoryFileEntry the repository file entry primary key
+	 * @param  version the file's version
+	 * @return the file version
+	 * @throws PortalException if the file entry's or file version's information
+	 *         was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public ExtRepositoryFileVersion getExtRepositoryFileVersion(
 			ExtRepositoryFileEntry extRepositoryFileEntry, String version)
 		throws PortalException, SystemException;
@@ -84,31 +171,89 @@ public interface ExtRepository {
 		getExtRepositoryFileVersionDescriptor(
 			String extRepositoryFileVersionKey);
 
+	/**
+	 * Returns a list of file versions of the file entry.
+	 *
+	 * @param  extRepositoryFileEntry the repository file entry primary key
+	 * @return a list of the file versions of the file entry
+	 * @throws SystemException if a system exception occurred
+	 */
 	public List<ExtRepositoryFileVersion> getExtRepositoryFileVersions(
 			ExtRepositoryFileEntry extRepositoryFileEntry)
 		throws PortalException, SystemException;
 
+	/**
+	 * Returns a file, a folder or a version.
+	 *
+	 * @param  extRepositoryObjectType the type of the object
+	 * @param  extRepositoryObjectKey the repository primary key of the object
+	 * @return a file, a folder or a version
+	 * @throws PortalException if the object's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public <T extends ExtRepositoryObject> T getExtRepositoryObject(
 			ExtRepositoryObjectType<T> extRepositoryObjectType,
 			String extRepositoryObjectKey)
 		throws PortalException, SystemException;
 
+	/**
+	 * Returns a file, a folder or a version with the specified title int the
+	 * contained folder.
+	 *
+	 * @param  extRepositoryObjectType the type of the object
+	 * @param  extRepositoryFolderKey the repository primaty key of the folder
+	 * @param  title
+	 * @return a file, a folder or a version with the specified title int the
+	 *         contained folder
+	 * @throws PortalException if the folder's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public <T extends ExtRepositoryObject> T getExtRepositoryObject(
 			ExtRepositoryObjectType<T> extRepositoryObjectType,
 			String extRepositoryFolderKey, String title)
 		throws PortalException, SystemException;
 
+	/**
+	 * Returns a list of the specified elements with the specified title
+	 * contained in the specified folder.
+	 *
+	 * @param  extRepositoryObjectType the type of the object
+	 * @param  extRepositoryFolderKey the repository primary key of the folder
+	 * @return a list of the specified elements with the specified title
+	 *         contained in the specified folder
+	 * @throws PortalException if the folder's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public <T extends ExtRepositoryObject> List<T> getExtRepositoryObjects(
 			ExtRepositoryObjectType<T> extRepositoryObjectType,
 			String extRepositoryFolderKey)
 		throws PortalException, SystemException;
 
+	/**
+	 * Returns a list of the specified elements contained in the specified
+	 * folder.
+	 *
+	 * @param  extRepositoryObjectType the type of the object
+	 * @param  extRepositoryFolderKey the repository primaty key of the folder
+	 * @return a list of the specified elements contained in the specified
+	 *         folder
+	 * @throws PortalException if the folder's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public int getExtRepositoryObjectsCount(
 			ExtRepositoryObjectType<? extends ExtRepositoryObject>
 				extRepositoryObjectType,
 			String extRepositoryFolderKey)
 		throws PortalException, SystemException;
 
+	/**
+	 * Returns the parent folder of the object.
+	 *
+	 * @param  extRepositoryObject the repository object
+	 * @return the parent folder of the object
+	 * @throws PortalException if the object's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public ExtRepositoryFolder getExtRepositoryParentFolder(
 			ExtRepositoryObject extRepositoryObject)
 		throws PortalException, SystemException;
@@ -117,6 +262,15 @@ public interface ExtRepository {
 
 	public String getRootFolderKey() throws PortalException, SystemException;
 
+	/**
+	 * Returns the list of the subfolders of the specified folder
+	 *
+	 * @param  extRepositoryFolderKey the repository primary key of the folder
+	 * @param  recurse whether to recurse through each subfolder
+	 * @return the list of the subfolders of the specified folder
+	 * @throws PortalException if the folder's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public List<String> getSubfolderKeys(
 			String extRepositoryFolderKey, boolean recurse)
 		throws PortalException, SystemException;
@@ -125,11 +279,32 @@ public interface ExtRepository {
 
 	public String[][] getSupportedParameters();
 
+	/**
+	 * Checks the conectivity between liferay and a repository with the current
+	 * liferay user credentials. This method is called the first time the
+	 * repository is used.
+	 *
+	 * @param  typeSettingsProperties the type settings properties
+	 * @param  credentialsProvider the liferay user and pass
+	 * @throws PortalException if a portal exception occurred
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void initRepository(
 			UnicodeProperties typeSettingsProperties,
 			CredentialsProvider credentialsProvider)
 		throws PortalException, SystemException;
 
+	/**
+	 * Moves an object
+	 *
+	 * @param  extRepositoryObjectType the type of the object
+	 * @param  extRepositoryObjectKey the repository primary key of the object
+	 * @param  newExtRepositoryFolderKey the target repository of the object
+	 * @param  newTitle the new title of the object
+	 * @return the new object
+	 * @throws PortalException
+	 * @throws SystemException if a system exception occurred
+	 */
 	public <T extends ExtRepositoryObject> T moveExtRepositoryObject(
 			ExtRepositoryObjectType<T> extRepositoryObjectType,
 			String extRepositoryObjectKey, String newExtRepositoryFolderKey,
@@ -141,6 +316,15 @@ public interface ExtRepository {
 			ExtRepositoryQueryMapper extRepositoryQueryMapper)
 		throws PortalException, SystemException;
 
+	/**
+	 * Updates a file entry
+	 *
+	 * @param  mimeType the file's MIME type
+	 * @param  inputStream the file's data (optionally <code>null</code>)
+	 * @return the updated file entry
+	 * @throws PortalException if file entry's information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public ExtRepositoryFileEntry updateExtRepositoryFileEntry(
 			String extRepositoryFileEntryKey, String mimeType,
 			InputStream inputStream)
