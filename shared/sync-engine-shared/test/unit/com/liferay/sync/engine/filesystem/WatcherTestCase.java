ef5e2a5b8b5c4499f81b85008c9bb88a31ded527
Michael Young <michael.young@liferay.com>
2014-01-24 16:07:26 -0800

    sync-engine-shared - watcher test

---
+++ shared/sync-engine-shared/test/unit/com/liferay/sync/engine/filesystem/WatcherTestCase.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.filesystem;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.liferay.sync.engine.BaseTestCase;
+import com.liferay.sync.engine.model.SyncAccount;
+import com.liferay.sync.engine.service.SyncAccountService;
+
+/**
+ * @author Michael Young
+ */
+@RunWith(PowerMockRunner.class)
+public class WatcherTestCase extends BaseTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_filePath = System.getProperty("user.home") + "/liferay-sync-test";
+
+		_file = new File(_filePath);
+		
+		_file.mkdir();
+		
+		_syncAccount = SyncAccountService.addSyncAccount(
+			_filePath, "test@liferay.com", "test",
+			"http://localhost:8080/api/jsonws");
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		FileUtils.deleteDirectory(_file);
+
+		SyncAccountService.deleteSyncAccount(_syncAccount.getSyncAccountId());
+	}
+	
+	@Test
+	public void testRun() throws Exception {
+		WatchEventListener watchEventListener = new SyncSiteWatchEventListener(
+			_syncAccount.getSyncAccountId());
+		
+		Path filePath = Paths.get(_syncAccount.getFilePath());
+		
+		Watcher watcher = new Watcher(filePath, true, watchEventListener);
+		
+		watcher.run();
+	}
+
+	private File _file;
+	private String _filePath;
+	private SyncAccount _syncAccount;
+
+}
