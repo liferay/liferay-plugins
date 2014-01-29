5f1c59e30e68fe4298f225fa35d52564d85cc896
Hugo Huijser <hugo.huijser@liferay.com>
2014-01-29 08:40:25 +0800

    LPS-43586 Add missing import

--- webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/service/impl/KaleoTimerInstanceTokenLocalServiceImpl.java
+++ webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/service/impl/KaleoTimerInstanceTokenLocalServiceImpl.java
@@ -45,6 +45,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
