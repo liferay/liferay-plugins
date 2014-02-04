136820a52f21b78daf52da5630b55977615fa834
Michael Han <michael.han@liferay.com>
2014-02-03 22:17:11 -0600

    LPS-44006 comply with 6.2 staging arch

---
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/admin/lar/WSRPConsumerPortletStagedModelDataHandler.java
@@ -0,0 +1,161 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wsrp.admin.lar;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
+import com.liferay.portal.kernel.lar.ExportImportPathUtil;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.wsrp.model.WSRPConsumer;
+import com.liferay.wsrp.model.WSRPConsumerPortlet;
+import com.liferay.wsrp.service.WSRPConsumerLocalServiceUtil;
+import com.liferay.wsrp.service.WSRPConsumerPortletLocalServiceUtil;
+
+import java.util.Map;
+
+/**
+ * @author Michael C. Han
+ */
+public class WSRPConsumerPortletStagedModelDataHandler
+	extends BaseStagedModelDataHandler<WSRPConsumerPortlet> {
+
+	public static final String[] CLASS_NAMES = {
+		WSRPConsumerPortlet.class.getName()
+	};
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException, SystemException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	public String getDisplayName(WSRPConsumerPortlet wsrpConsumerPortlet) {
+		return wsrpConsumerPortlet.getName();
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext,
+			WSRPConsumerPortlet wsrpConsumerPortlet)
+		throws Exception {
+
+		WSRPConsumer wsrpConsumer =
+			WSRPConsumerLocalServiceUtil.getWSRPConsumer(
+				wsrpConsumerPortlet.getWsrpConsumerId());
+
+		StagedModelDataHandlerUtil.exportReferenceStagedModel(
+			portletDataContext, wsrpConsumerPortlet, wsrpConsumer,
+			PortletDataContext.REFERENCE_TYPE_STRONG);
+
+		Element entryElement = portletDataContext.getExportDataElement(
+			wsrpConsumerPortlet);
+
+		portletDataContext.addClassedModel(
+			entryElement,
+			ExportImportPathUtil.getModelPath(wsrpConsumerPortlet),
+			wsrpConsumerPortlet);
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext,
+			WSRPConsumerPortlet wsrpConsumerPortlet)
+		throws Exception {
+
+		StagedModelDataHandlerUtil.importReferenceStagedModel(
+			portletDataContext, wsrpConsumerPortlet, WSRPConsumer.class,
+			wsrpConsumerPortlet.getWsrpConsumerId());
+
+		Map<Long, Long> wsrpConsumerIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				WSRPConsumer.class);
+
+		long wsrpConsumerId = MapUtil.getLong(
+			wsrpConsumerIds, wsrpConsumerPortlet.getWsrpConsumerId(),
+			wsrpConsumerPortlet.getWsrpConsumerId());
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			wsrpConsumerPortlet);
+
+		WSRPConsumerPortlet importedWSRPConsumerPortlet = null;
+
+		if (portletDataContext.isDataStrategyMirror()) {
+			WSRPConsumerPortlet existingWSRPConsumerPortlet =
+				WSRPConsumerPortletLocalServiceUtil.
+					fetchWSRPConsumerPortletByUuidAndCompanyId(
+						wsrpConsumerPortlet.getUuid(),
+						portletDataContext.getCompanyId());
+
+			if (existingWSRPConsumerPortlet == null) {
+				serviceContext.setUuid(wsrpConsumerPortlet.getUuid());
+
+				importedWSRPConsumerPortlet =
+					WSRPConsumerPortletLocalServiceUtil.addWSRPConsumerPortlet(
+						wsrpConsumerId, wsrpConsumerPortlet.getName(),
+						wsrpConsumerPortlet.getPortletHandle(), serviceContext);
+			}
+			else {
+				existingWSRPConsumerPortlet.setWsrpConsumerId(wsrpConsumerId);
+				existingWSRPConsumerPortlet.setName(
+					wsrpConsumerPortlet.getName());
+				existingWSRPConsumerPortlet.setPortletHandle(
+					wsrpConsumerPortlet.getPortletHandle());
+
+				importedWSRPConsumerPortlet =
+					WSRPConsumerPortletLocalServiceUtil.
+						updateWSRPConsumerPortlet(existingWSRPConsumerPortlet);
+			}
+		}
+		else {
+			importedWSRPConsumerPortlet =
+				WSRPConsumerPortletLocalServiceUtil.addWSRPConsumerPortlet(
+					wsrpConsumerId, wsrpConsumerPortlet.getName(),
+					wsrpConsumerPortlet.getPortletHandle(), serviceContext);
+		}
+
+		portletDataContext.importClassedModel(
+			wsrpConsumerPortlet, importedWSRPConsumerPortlet);
+	}
+
+	@Override
+	protected boolean validateMissingReference(
+			String uuid, long companyId, long groupId)
+		throws Exception {
+
+		WSRPConsumerPortlet wsrpConsumerPortlet =
+			WSRPConsumerPortletLocalServiceUtil.
+				fetchWSRPConsumerPortletByUuidAndCompanyId(uuid, companyId);
+
+		if (wsrpConsumerPortlet == null) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
