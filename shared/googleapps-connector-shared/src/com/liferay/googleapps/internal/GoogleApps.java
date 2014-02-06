303094f07e5f731d3a22b54ceb1692b6fff99c05
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-02-06 10:52:40 +0100

    LPS-44083 no-logic-changes. Move logic from portal-impl to internal

---
+++ shared/googleapps-connector-shared/src/com/liferay/googleapps/internal/GoogleApps.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.googleapps.internal;
+
+import com.liferay.portal.kernel.googleapps.GEmailSettingsManager;
+import com.liferay.portal.kernel.googleapps.GGroupManager;
+import com.liferay.portal.kernel.googleapps.GNicknameManager;
+import com.liferay.portal.kernel.googleapps.GUserManager;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class GoogleApps {
+
+	public GoogleApps(long companyId) {
+		_gAuthenticator = new GAuthenticator(companyId);
+
+		init();
+	}
+
+	public GoogleApps(String domain, String userName, String password) {
+		_gAuthenticator = new GAuthenticator(domain, userName, password);
+
+		init();
+	}
+
+	public GAuthenticator getGAuthenticator() {
+		return _gAuthenticator;
+	}
+
+	public GEmailSettingsManager getGEmailSettingsManager() {
+		return _gEmailSettingsManager;
+	}
+
+	public GGroupManager getGGroupManager() {
+		return _gGroupManager;
+	}
+
+	public GNicknameManager getGNicknameManager() {
+		return _gNicknameManager;
+	}
+
+	public GUserManager getGUserManager() {
+		return _gUserManager;
+	}
+
+	protected void init() {
+		_gEmailSettingsManager = new GEmailSettingsManagerImpl(this);
+		_gGroupManager = new GGroupManagerImpl(this);
+		_gNicknameManager = new GNicknameManagerImpl(this);
+		_gUserManager = new GUserManagerImpl(this);
+	}
+
+	private GAuthenticator _gAuthenticator;
+	private GEmailSettingsManager _gEmailSettingsManager;
+	private GGroupManager _gGroupManager;
+	private GNicknameManager _gNicknameManager;
+	private GUserManager _gUserManager;
+
+}
---
+++ shared/googleapps-connector-shared/src/com/liferay/googleapps/internal/GoogleAppsFactoryImpl.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.googleapps.internal;
+
+import com.liferay.portal.kernel.googleapps.GEmailSettingsManager;
+import com.liferay.portal.kernel.googleapps.GGroupManager;
+import com.liferay.portal.kernel.googleapps.GNicknameManager;
+import com.liferay.portal.kernel.googleapps.GUserManager;
+import com.liferay.portal.kernel.googleapps.GoogleAppsFactory;
+import com.liferay.portal.kernel.security.pacl.DoPrivileged;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@DoPrivileged
+public class GoogleAppsFactoryImpl implements GoogleAppsFactory {
+
+	@Override
+	public GEmailSettingsManager getGEmailSettingsManager(long companyId) {
+		return getGoogleApps(companyId).getGEmailSettingsManager();
+	}
+
+	@Override
+	public GGroupManager getGGroupManager(long companyId) {
+		return getGoogleApps(companyId).getGGroupManager();
+	}
+
+	@Override
+	public GNicknameManager getGNicknameManager(long companyId) {
+		return getGoogleApps(companyId).getGNicknameManager();
+	}
+
+	@Override
+	public GUserManager getGUserManager(long companyId) {
+		return getGoogleApps(companyId).getGUserManager();
+	}
+
+	protected GoogleApps getGoogleApps(long companyId) {
+		GoogleApps googleApps = _googleAppsMap.get(companyId);
+
+		if (googleApps == null) {
+			googleApps = new GoogleApps(companyId);
+
+			_googleAppsMap.put(companyId, googleApps);
+		}
+
+		return googleApps;
+	}
+
+	private static Map<Long, GoogleApps> _googleAppsMap =
+		new ConcurrentHashMap<Long, GoogleApps>();
+
+}
