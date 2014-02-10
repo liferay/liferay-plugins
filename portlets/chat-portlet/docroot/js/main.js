3a9df40c9981d97f183199d962382d47eac25eff
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-02-10 10:29:28 +0100

    LPS-42835 Chat Portlet - Time is not localized in the chat panel

--- portlets/chat-portlet/docroot/js/main.js
+++ portlets/chat-portlet/docroot/js/main.js
@@ -2,6 +2,7 @@ AUI().use(
 	'anim-color',
 	'anim-easing',
 	'aui-base',
+	'aui-datatype',
 	'aui-live-search-deprecated',
 	'liferay-poller',
 	'stylesheet',
@@ -76,27 +77,15 @@ AUI().use(
 
 				time = new Date(time);
 
-				var meridian = 'am';
-				var hour = time.getHours();
-				var minute = time.getMinutes();
+				var formattedTime = A.DataType.Date.format(
+					time,
+					{
+						format: '%X',
+						locale: themeDisplay.getLanguageId()
+					}
+				);
 
-				if (hour >= 12) {
-					meridian = 'pm';
-				}
-
-				if (hour > 12) {
-					hour -= 12;
-				}
-
-				if (hour === 0) {
-					hour += 12;
-				}
-
-				if (minute < 10) {
-					minute = '0' + minute;
-				}
-
-				return hour + ':' + minute + ' ' + meridian;
+				return formattedTime;
 			},
 
 			getCurrentTimestamp: function() {
