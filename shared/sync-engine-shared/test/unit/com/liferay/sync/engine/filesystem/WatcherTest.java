129282cc4e10621e9010b0116aab94ef82d8d993
Shinn Lok <shinn.lok@liferay.com>
2014-02-12 16:21:34 -0800

    sync-engine-shared - Delay 12s for OSX, there is nothing to be fixed in the engine, events will be fired ~10 seconds after because that is when the file system/watcher notifies us.

--- shared/sync-engine-shared/test/unit/com/liferay/sync/engine/filesystem/WatcherTest.java
+++ shared/sync-engine-shared/test/unit/com/liferay/sync/engine/filesystem/WatcherTest.java
@@ -24,6 +24,7 @@ import com.liferay.sync.engine.service.SyncSiteService;
 import com.liferay.sync.engine.service.SyncWatchEventService;
 import com.liferay.sync.engine.util.FilePathNameUtil;
 import com.liferay.sync.engine.util.FileUtil;
+import com.liferay.sync.engine.util.OSDetector;
 
 import java.io.BufferedWriter;
 
@@ -106,7 +107,7 @@ public class WatcherTest extends BaseTestCase {
 
 		Files.createFile(filePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		_syncFiles = SyncFileService.findSyncFiles(
 			syncAccount.getSyncAccountId());
@@ -122,7 +123,7 @@ public class WatcherTest extends BaseTestCase {
 
 		Files.createFile(filePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		_syncFiles = SyncFileService.findSyncFiles(
 			syncAccount.getSyncAccountId());
@@ -138,11 +139,11 @@ public class WatcherTest extends BaseTestCase {
 
 		Files.createFile(filePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		Files.delete(filePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		_syncFiles = SyncFileService.findSyncFiles(
 			syncAccount.getSyncAccountId());
@@ -162,7 +163,7 @@ public class WatcherTest extends BaseTestCase {
 
 		Files.createFile(filePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		BufferedWriter bufferedWriter = Files.newBufferedWriter(
 			filePath, StandardCharsets.UTF_8);
@@ -171,7 +172,7 @@ public class WatcherTest extends BaseTestCase {
 
 		bufferedWriter.close();
 
-		Thread.sleep(1000);
+		sleep();
 
 		_syncFiles = SyncFileService.findSyncFiles(
 			syncAccount.getSyncAccountId());
@@ -201,13 +202,13 @@ public class WatcherTest extends BaseTestCase {
 
 		Files.createDirectory(destinationFilePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		Files.move(
 			sourceFilePath,
 			destinationFilePath.resolve(sourceFilePath.getFileName()));
 
-		Thread.sleep(1000);
+		sleep();
 
 		_syncFiles = SyncFileService.findSyncFiles(
 			syncAccount.getSyncAccountId());
@@ -228,14 +229,14 @@ public class WatcherTest extends BaseTestCase {
 
 		Files.createFile(sourceFilePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		Path destinationFilePath = Paths.get(
 			_syncSite.getFilePathName() + "/test2.txt");
 
 		Files.move(sourceFilePath, destinationFilePath);
 
-		Thread.sleep(1000);
+		sleep();
 
 		_syncFiles = SyncFileService.findSyncFiles(
 			syncAccount.getSyncAccountId());
@@ -245,6 +246,15 @@ public class WatcherTest extends BaseTestCase {
 			SyncFileService.fetchSyncFile(
 				FilePathNameUtil.getFilePathName(destinationFilePath),
 				syncAccount.getSyncAccountId()));
+	}
+
+	protected void sleep() throws InterruptedException {
+		if (OSDetector.isApple()) {
+			Thread.sleep(12000);
+		}
+		else {
+			Thread.sleep(1000);
+		}
 	}
 
 	private List<SyncFile> _syncFiles;
