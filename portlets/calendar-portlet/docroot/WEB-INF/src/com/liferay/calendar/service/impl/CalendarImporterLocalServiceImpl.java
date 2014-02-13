a685efd65f6febc31a758da27392a75ebbeb9145
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-02-13 14:17:02 -0300

    LPS-41363 Asset link will be removed after deleting the calEvent

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarImporterLocalServiceImpl.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/service/impl/CalendarImporterLocalServiceImpl.java
@@ -645,8 +645,6 @@ public class CalendarImporterLocalServiceImpl
 			linkId, assetLink.getCompanyId(), assetLink.getUserId(),
 			assetLink.getUserName(), assetLink.getCreateDate(), entryId1,
 			entryId2, assetLink.getType(), assetLink.getWeight());
-
-		assetLinkLocalService.deleteAssetLink(assetLink);
 	}
 
 	protected void importAssets(CalEvent calEvent, long calendarBookingId)
