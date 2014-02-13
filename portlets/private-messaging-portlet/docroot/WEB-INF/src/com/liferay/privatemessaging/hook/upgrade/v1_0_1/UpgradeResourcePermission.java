ed246236108a54b3444c5a1fdc00c469960a9367
sherryre <sherry.yang@liferay.com>
2014-02-12 18:08:43 -0800

    SOS-2030 Cannot view a private message attachment without admin role

---
+++ portlets/private-messaging-portlet/docroot/WEB-INF/src/com/liferay/privatemessaging/hook/upgrade/v1_0_1/UpgradeResourcePermission.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.privatemessaging.hook.upgrade.v1_0_1;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ResourceLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
+
+import java.util.List;
+
+/**
+ * @author Sherry Yang
+ */
+public class UpgradeResourcePermission extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		List<Company> companies = CompanyLocalServiceUtil.getCompanies();
+
+		for (Company company : companies) {
+			Group group = GroupLocalServiceUtil.getCompanyGroup(
+				company.getCompanyId());
+
+			int count =
+				ResourcePermissionLocalServiceUtil.getResourcePermissionsCount(
+					company.getCompanyId(), _DL_RESOURCE_NAME,
+					ResourceConstants.SCOPE_INDIVIDUAL,
+					String.valueOf(group.getGroupId()));
+
+			if (count > 0) {
+				return;
+			}
+
+			ResourceLocalServiceUtil.addResources(
+				company.getCompanyId(), group.getGroupId(), 0,
+				_DL_RESOURCE_NAME, group.getGroupId(), false, true, true);
+		}
+	}
+
+	private static final String _DL_RESOURCE_NAME =
+		"com.liferay.portlet.documentlibrary";
+
+}
