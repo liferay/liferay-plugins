9ea23e6c0e2695b2ded9b35241db9b74689da04e
Roberto Díaz <roberto.diaz@liferay.com>
2014-02-24 09:54:18 +0100

    LPS-43332 Do not strip data in asset renderers. It should be escaped where it's been used.

--- portlets/microblogs-portlet/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/asset/MicroblogsEntryAssetRenderer.java
+++ portlets/microblogs-portlet/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/asset/MicroblogsEntryAssetRenderer.java
@@ -75,12 +75,12 @@ public class MicroblogsEntryAssetRenderer extends BaseAssetRenderer {
 
 	@Override
 	public String getSummary(Locale locale) {
-		return HtmlUtil.stripHtml(_entry.getContent());
+		return _entry.getContent();
 	}
 
 	@Override
 	public String getTitle(Locale locale) {
-		return HtmlUtil.stripHtml(_entry.getContent());
+		return _entry.getContent();
 	}
 
 	@Override
