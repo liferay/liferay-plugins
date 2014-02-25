753037e3981cd27db9272e47731a2b1e4078184c
Lin Cui <lin.cui@liferay.com>
2014-02-24 16:59:52 -0800

    SOS-2059 Add notifications-shared project

---
+++ shared/notifications-shared/src/com/liferay/notifications/util/PortletKeys.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.notifications.util;
+
+/**
+ * @author Lin Cui
+ */
+public class PortletKeys extends com.liferay.portal.util.PortletKeys {
+
+	public static final String CONTACTS_CENTER =
+		com.liferay.contacts.util.PortletKeys.CONTACTS_CENTER;
+
+	public static final String DOCKBAR_NOTIFICATIONS =
+		"2_WAR_notificationsportlet";
+
+	public static final String NOTIFICATIONS = "1_WAR_notificationsportlet";
+
+	public static final String SO_ANNOUNCEMENTS =
+		com.liferay.so.announcements.util.PortletKeys.SO_ANNOUNCEMENTS;
+
+	public static final String SO_INVITE_MEMBERS =
+		com.liferay.so.util.PortletKeys.SO_INVITE_MEMBERS;
+
+	public static final String SO_NOTIFICATION =
+		com.liferay.so.util.PortletKeys.SO_NOTIFICATION;
+
+}
