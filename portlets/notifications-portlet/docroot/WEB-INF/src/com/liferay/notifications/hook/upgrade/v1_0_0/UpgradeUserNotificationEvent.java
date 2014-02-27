93ea600656550eca308bfbdad607bac8ad25cdca
Jonathan Lee <Jonathan.Lee@liferay.com>
2014-02-27 15:26:26 -0800

    SOS-2122 The upgrade script needs to reference to the old portlet keys that we are not using anymore.

--- portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/hook/upgrade/v1_0_0/UpgradeUserNotificationEvent.java
+++ portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/hook/upgrade/v1_0_0/UpgradeUserNotificationEvent.java
@@ -72,7 +72,7 @@ public class UpgradeUserNotificationEvent extends UpgradeProcess {
 				"select userNotificationEventId, payload from " +
 					"UserNotificationEvent where type_ = ?");
 
-			ps.setString(1, PortletKeys.NOTIFICATIONS);
+			ps.setString(1, "6_WAR_soportlet");
 
 			rs = ps.executeQuery();
 
