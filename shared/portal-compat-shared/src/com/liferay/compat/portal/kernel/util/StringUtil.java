b44e605238600eea97430e89b7c475e6578a51c5
Brian Chan <brian.chan@liferay.com>
2014-02-27 14:50:37 -0800

    LPS-44480

--- shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/util/StringUtil.java
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/util/StringUtil.java
@@ -86,7 +86,7 @@ public class StringUtil extends com.liferay.portal.kernel.util.StringUtil {
 					locale = LocaleUtil.getDefault();
 				}
 
-				return s.toLowerCase(locale);
+				return s.toUpperCase(locale);
 			}
 
 			if ((c >= 'a') && (c <= 'z')) {
