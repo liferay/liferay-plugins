0faeb15a3b62b0f5a16123b98f8d0b40ee93971a
Philippe Proulx <philippe.proulx@savoirfairelinux.com>
2014-03-04 13:28:50 -0500

    LPS-40873 Poller: implement doReceive()

--- portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/poller/ChatVideoPollerProcessor.java
+++ portlets/chat-video-portlet/docroot/WEB-INF/src/com/liferay/chat/video/poller/ChatVideoPollerProcessor.java
@@ -14,9 +14,18 @@
 
 package com.liferay.chat.video.poller;
 
+import com.liferay.chat.video.WebRTCClient;
+import com.liferay.chat.video.WebRTCMail;
+import com.liferay.chat.video.WebRTCManager;
+import com.liferay.chat.video.WebRTCManagerFactory;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.poller.BasePollerProcessor;
 import com.liferay.portal.kernel.poller.PollerRequest;
 import com.liferay.portal.kernel.poller.PollerResponse;
+
+import java.util.List;
 
 /**
  * @author Philippe Proulx
@@ -27,10 +36,58 @@ public class ChatVideoPollerProcessor extends BasePollerProcessor {
 	protected void doReceive(
 			PollerRequest pollerRequest, PollerResponse pollerResponse)
 		throws Exception {
+
+		receiveWebRTCData(pollerRequest, pollerResponse);
 	}
 
 	@Override
 	protected void doSend(PollerRequest pollerRequest) throws Exception {
 	}
+
+	protected void receiveWebRTCData(
+			PollerRequest pollerRequest, PollerResponse pollerResponse)
+		throws Exception {
+
+		WebRTCClient webRTCClient = _webRTCManager.getWebRTCClient(
+			pollerRequest.getUserId());
+
+		JSONArray webRTCMailsJSONArray = JSONFactoryUtil.createJSONArray();
+
+		if (webRTCClient != null) {
+			List<WebRTCMail> webRTCMails =
+				webRTCClient.getOutgoingWebRTCMailbox().popWebRTCMails();
+
+			for (WebRTCMail webRTCMail : webRTCMails) {
+				String messageType = webRTCMail.getMessageType();
+				String messageJSON = webRTCMail.getMessageJSON();
+				long sourceUserId = webRTCMail.getSourceUserId();
+				JSONObject mailJSONObject = JSONFactoryUtil.createJSONObject();
+
+				mailJSONObject.put("type", messageType);
+				mailJSONObject.put("msg", messageJSON);
+				mailJSONObject.put("source_user_id", sourceUserId);
+
+				webRTCMailsJSONArray.put(mailJSONObject);
+			}
+		}
+
+		JSONArray webRTCClientsJSONArray = JSONFactoryUtil.createJSONArray();
+
+		for (Long userId : _webRTCManager.getAvailableWebRTCClientIds()) {
+			if (userId != pollerRequest.getUserId()) {
+				webRTCClientsJSONArray.put(userId);
+			}
+		}
+
+		JSONObject webRTCJSONObject = JSONFactoryUtil.createJSONObject();
+
+		webRTCJSONObject.put("mails", webRTCMailsJSONArray);
+		webRTCJSONObject.put("clients", webRTCClientsJSONArray);
+
+		pollerResponse.setParameter("webrtc", webRTCJSONObject);
+	}
+
+	private WebRTCManager _webRTCManager =
+		WebRTCManagerFactory.createWebRTCManager();
 
 }
