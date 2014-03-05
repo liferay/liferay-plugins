24b06cb07b6658981fd2ed70b712eea89a53794b
Hugo Huijser <hugo.huijser@liferay.com>
2014-03-05 14:02:35 +0800

    LPS-43332 Source formatting

--- portlets/microblogs-portlet/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/asset/MicroblogsEntryAssetRenderer.java
+++ portlets/microblogs-portlet/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/asset/MicroblogsEntryAssetRenderer.java
@@ -22,7 +22,6 @@ import com.liferay.microblogs.service.permission.MicroblogsEntryPermission;
 import com.liferay.microblogs.util.WebKeys;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
-import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.permission.ActionKeys;
