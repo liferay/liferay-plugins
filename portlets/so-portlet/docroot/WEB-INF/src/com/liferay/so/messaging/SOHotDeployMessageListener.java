d0292da243aa22d64b42ad54de9eb4b82ec8685d
Jonathan Lee <Jonathan.Lee@liferay.com>
2014-03-10 17:00:03 -0700

    SOS-2057 Follow ContactsHotDeployMessageListener

--- portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/messaging/SOHotDeployMessageListener.java
+++ portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/messaging/SOHotDeployMessageListener.java
@@ -46,15 +46,24 @@ public class SOHotDeployMessageListener extends HotDeployMessageListener {
 	}
 
 	protected void registerContactsExtension() throws Exception {
+		if (_registerMethodKey == null) {
+			try {
+				_registerMethodKey = new MethodKey(
+					ClassResolverUtil.resolveByPortletClassLoader(
+						"com.liferay.contacts.util.ContactsExtensionsUtil",
+						"contacts-portlet"),
+					"register", String.class, String.class);
+			}
+			catch (RuntimeException re) {
+				return;
+			}
+		}
+
 		PortletClassInvoker.invoke(
 			"1_WAR_contactsportlet", _registerMethodKey,
 			ClpSerializer.getServletContextName(), "/contacts/projects.jsp");
 	}
 
-	private MethodKey _registerMethodKey = new MethodKey(
-		ClassResolverUtil.resolveByPortletClassLoader(
-			"com.liferay.contacts.util.ContactsExtensionsUtil",
-			"contacts-portlet"),
-		"register", String.class, String.class);
+	private MethodKey _registerMethodKey;
 
 }
