f97907775225a6ebcba7aa11f2307dd22371f608
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-03-11 14:23:41 +0100

    LPS-45047 no logic changes - Instead of having AutoCompleteUserAction as a portal action we need a regular portlet URL where we can have more information about the context (siteGroupId)

--- portlets/mentions-portlet/docroot/WEB-INF/src/com/liferay/mentions/portlet/AutoCompleteUserAction.java
+++ portlets/mentions-portlet/docroot/WEB-INF/src/com/liferay/mentions/portlet/AutoCompleteUserAction.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.mentions.hook.action;
+package com.liferay.mentions.portlet;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
