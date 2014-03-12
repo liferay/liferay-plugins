d37da5b6cadea9e4458a28daf67f2e532d43fd96
Brian Chan <brian.chan@liferay.com>
2014-03-12 15:30:48 -0700

    LPS-44653 SF

--- portlets/elasticsearch-portlet/docroot/WEB-INF/src/com/liferay/portal/search/elasticsearch/util/LogUtil.java
+++ portlets/elasticsearch-portlet/docroot/WEB-INF/src/com/liferay/portal/search/elasticsearch/util/LogUtil.java
@@ -31,23 +31,23 @@ public class LogUtil {
 	public static void logActionResponse(Log log, ActionResponse actionResponse)
 		throws IOException {
 
-		if (log.isInfoEnabled()) {
-			StringOutputStream stringOutputStream = new StringOutputStream();
-
-			actionResponse.writeTo(
-				new OutputStreamStreamOutput(stringOutputStream));
-
-			log.info(stringOutputStream);
+		if (!log.isInfoEnabled()) {
+			return;
 		}
+
+		StringOutputStream stringOutputStream = new StringOutputStream();
+
+		actionResponse.writeTo(
+			new OutputStreamStreamOutput(stringOutputStream));
+
+		log.info(stringOutputStream);
 	}
 
 	public static void logActionResponse(Log log, BulkResponse bulkResponse)
 		throws IOException {
 
 		if (bulkResponse.hasFailures()) {
-			if (log.isErrorEnabled()) {
-				log.error(bulkResponse.buildFailureMessage());
-			}
+			log.error(bulkResponse.buildFailureMessage());
 		}
 
 		logActionResponse(log, (ActionResponse)bulkResponse);
