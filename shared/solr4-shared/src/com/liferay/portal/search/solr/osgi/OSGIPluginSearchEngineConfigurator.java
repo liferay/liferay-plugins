1a19855828a93e0c60960a7cc666c5920a0af616
Raymond Auge <raymond.auge@liferay.com>
2014-03-13 17:41:22 -0400

    LPS-45091 create an impl of AbstractSearchEngineConfigurator for use in osgi/blueprint configuration (do not use the plugin one)

---
+++ shared/solr4-shared/src/com/liferay/portal/search/solr/osgi/OSGIPluginSearchEngineConfigurator.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr.osgi;
+
+import com.liferay.portal.kernel.messaging.MessageBus;
+import com.liferay.portal.kernel.search.AbstractSearchEngineConfigurator;
+import com.liferay.portal.kernel.search.IndexSearcher;
+import com.liferay.portal.kernel.search.IndexWriter;
+
+/**
+ * @author Raymond Augé
+ */
+public class OSGIPluginSearchEngineConfigurator
+	extends AbstractSearchEngineConfigurator {
+
+	@Override
+	protected String getDefaultSearchEngineId() {
+		return _defaultSearchEngineId;
+	}
+
+	@Override
+	protected IndexSearcher getIndexSearcher() {
+		return _indexSearcher;
+	}
+
+	@Override
+	protected IndexWriter getIndexWriter() {
+		return _indexWriter;
+	}
+
+	@Override
+	protected MessageBus getMessageBus() {
+		return _messageBus;
+	}
+
+	@Override
+	protected ClassLoader getOperatingClassloader() {
+		return getClass().getClassLoader();
+	}
+
+	public void setDefaultSearchEngineId(String defaultSearchEngineId) {
+		_defaultSearchEngineId = defaultSearchEngineId;
+	}
+
+	public void setIndexSearcher(IndexSearcher indexSearcher) {
+		_indexSearcher = indexSearcher;
+	}
+
+	public void setIndexWriter(IndexWriter indexWriter) {
+		_indexWriter = indexWriter;
+	}
+
+	public void setMessageBus(MessageBus messageBus) {
+		_messageBus = messageBus;
+	}
+
+	private String _defaultSearchEngineId;
+	private IndexSearcher _indexSearcher;
+	private IndexWriter _indexWriter;
+	private MessageBus _messageBus;
+
+}
