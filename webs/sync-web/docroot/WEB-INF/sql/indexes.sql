0d32c081914ade75742d62fe90c8303dc479aa75
Dennis Ju <dennis.ju@liferay.com>
2014-03-18 13:11:59 -0700

    SYNC-1199 Remove unused index

--- webs/sync-web/docroot/WEB-INF/sql/indexes.sql
+++ webs/sync-web/docroot/WEB-INF/sql/indexes.sql
@@ -1,6 +1,5 @@
 create index IX_A9B43C55 on SyncDLFileVersionDiff (expirationDate);
-create unique index IX_CA006E0C on SyncDLFileVersionDiff (fileEntryId, sourceFileVersionId, destinationFileVersionId);
-create unique index IX_7F4E8964 on SyncDLFileVersionDiff (syncDLFileVersionDiffId, sourceFileVersionId, destinationFileVersionId);
+create unique index IX_AC4C7667 on SyncDLFileVersionDiff (fileEntryId, sourceFileVersionId, targetFileVersionId);
 
 create index IX_7F996123 on SyncDLObject (companyId, modifiedTime, repositoryId);
 create unique index IX_69ADEDD1 on SyncDLObject (typePK);
