12585b2c3a9418217f5948cfe804494a1c4e8a2d
Brian Chan <brian.chan@liferay.com>
2014-03-19 11:43:01 -0700

    LPS-45248 Instead of hard coding which CustomSQL to initialize, take the current name, and strip of 4 chars (Util) and then instantiate that.

--- shared/custom-sql-shared/src/com/liferay/customsql/CustomSQLUtil.java
+++ shared/custom-sql-shared/src/com/liferay/customsql/CustomSQLUtil.java
@@ -17,6 +17,7 @@ package com.liferay.customsql;
 import com.liferay.portal.kernel.dao.orm.QueryDefinition;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.InstanceFactory;
 import com.liferay.portal.kernel.util.OrderByComparator;
 
 import java.sql.SQLException;
@@ -136,7 +137,12 @@ public class CustomSQLUtil {
 
 	private CustomSQLUtil() {
 		try {
-			_customSQL = new CustomSQL();
+			Class<?> clazz = getClass();
+
+			String className = clazz.getName();
+
+			_customSQL = (CustomSQL)InstanceFactory.newInstance(
+				className.substring(0, className.length() - 4));
 		}
 		catch (Exception e) {
 			_log.error(e, e);
