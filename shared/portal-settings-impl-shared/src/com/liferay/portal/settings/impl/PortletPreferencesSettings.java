cd925da7e217ccd241ba3385d92c4bee8e94de5d
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-03-20 14:13:41 +0100

    LPS-45273 Rewrite message of error logs to make it more descriptive

--- shared/portal-settings-impl-shared/src/com/liferay/portal/settings/impl/PortletPreferencesSettings.java
+++ shared/portal-settings-impl-shared/src/com/liferay/portal/settings/impl/PortletPreferencesSettings.java
@@ -80,7 +80,9 @@ public class PortletPreferencesSettings implements Settings {
 			_portletPreferences.setValue(key, value);
 		}
 		catch (ReadOnlyException roe) {
-			_log.error(roe, roe);
+			_log.error(
+				"When preferences are used as the backend for settings they " +
+				"should never be read only", roe);
 		}
 
 		return this;
@@ -92,7 +94,9 @@ public class PortletPreferencesSettings implements Settings {
 			_portletPreferences.setValues(key, values);
 		}
 		catch (ReadOnlyException roe) {
-			_log.error(roe, roe);
+			_log.error(
+				"When preferences are used as the backend for settings they " +
+				"should never be read only", roe);
 		}
 
 		return this;
