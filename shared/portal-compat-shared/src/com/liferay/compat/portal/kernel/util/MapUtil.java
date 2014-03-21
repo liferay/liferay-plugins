2c1efbdb0bcfd5652e9cb31fe54c333708e8a22f
Brian Chan <brian.chan@liferay.com>
2014-03-21 16:25:27 -0700

    LPS-44864 add MapUtil to compat

---
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/util/MapUtil.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.compat.portal.kernel.util;
+
+import java.util.Map;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class MapUtil extends com.liferay.portal.kernel.util.MapUtil {
+
+	public static boolean isEmpty(Map<?, ?> map) {
+		if ((map == null) || map.isEmpty()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isNotEmpty(Map<?, ?> map) {
+		return !isEmpty(map);
+	}
+
+}
