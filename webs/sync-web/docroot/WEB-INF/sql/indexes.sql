7f90735ef1e8c95dc5c7d76d929b0d0de91a32cd
Dennis Ju <dennis.ju@liferay.com>
2014-03-25 10:56:41 -0700

    SYNC-1229 Unique constraint on type and typePK to allow pwc types with same fileEntryId

--- webs/sync-web/docroot/WEB-INF/sql/indexes.sql
+++ webs/sync-web/docroot/WEB-INF/sql/indexes.sql
@@ -2,4 +2,4 @@ create index IX_A9B43C55 on SyncDLFileVersionDiff (expirationDate);
 create unique index IX_AC4C7667 on SyncDLFileVersionDiff (fileEntryId, sourceFileVersionId, targetFileVersionId);
 
 create index IX_7F996123 on SyncDLObject (companyId, modifiedTime, repositoryId);
-create unique index IX_69ADEDD1 on SyncDLObject (typePK);
+create unique index IX_E3F57BD6 on SyncDLObject (type_, typePK);
