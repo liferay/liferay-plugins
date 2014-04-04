bf9fc36499521081bee93308e942cbf039a6822a
Brian Chan <brian.chan@liferay.com>
2014-04-04 10:32:10 -0700

    CLDSVCS-655 add to 6.1.x

---
+++ shared/json-web-service-client-shared/src/com/liferay/jsonwebserviceclient/BaseJSONWebServiceClientHandler.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jsonwebserviceclient;
+
+import com.liferay.portal.kernel.json.JSONDeserializer;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.security.auth.login.CredentialException;
+
+/**
+ * @author Igor Beslic
+ */
+public abstract class BaseJSONWebServiceClientHandler {
+
+	public abstract JSONWebServiceClient getJSONWebServiceClient();
+
+	protected String doGet(String url, String... parametersArray)
+		throws CredentialException, IOException {
+
+		Map<String, String> parameters = new HashMap<String, String>();
+
+		for (int i = 0; i < parametersArray.length; i += 2) {
+			parameters.put(parametersArray[i], parametersArray[i + 1]);
+		}
+
+		return getJSONWebServiceClient().doGet(url, parameters);
+	}
+
+	protected <T> List<T> doGetToList(
+			Class<T> clazz, String url, String... parametersArray)
+		throws Exception {
+
+		String json = doGet(url, parametersArray);
+
+		if ((json == null) || json.equals("{}") || json.equals("[]")) {
+			return Collections.emptyList();
+		}
+
+		if (json.contains("exception")) {
+			throw new Exception(getExceptionMessage(json));
+		}
+
+		JSONDeserializer<List<T>> jsonDeserializer =
+			JSONFactoryUtil.createJSONDeserializer();
+
+		jsonDeserializer.use("values", clazz);
+
+		return jsonDeserializer.deserialize(json);
+	}
+
+	protected <T> T doGetToObject(
+			Class<T> clazz, String url, String... parametersArray)
+		throws Exception {
+
+		String json = doGet(url, parametersArray);
+
+		if (json.contains("exception")) {
+			throw new Exception(getExceptionMessage(json));
+		}
+
+		return JSONFactoryUtil.looseDeserialize(json, clazz);
+	}
+
+	protected <T> T doGetToObject(String url, String... parametersArray)
+		throws Exception {
+
+		String json = doGet(url, parametersArray);
+
+		if (json.contains("exception")) {
+			throw new Exception(getExceptionMessage(json));
+		}
+
+		JSONDeserializer<T> jsonDeserializer =
+			JSONFactoryUtil.createJSONDeserializer();
+
+		return jsonDeserializer.deserialize(json);
+	}
+
+	protected void doPost(String url, String... parametersArray)
+		throws CredentialException, IOException {
+
+		Map<String, String> parameters = new HashMap<String, String>();
+
+		for (int i = 0; i < parametersArray.length; i += 2) {
+			parameters.put(parametersArray[i], parametersArray[i + 1]);
+		}
+
+		getJSONWebServiceClient().doPost(url, parameters);
+	}
+
+	protected String getExceptionMessage(String json) {
+		int exceptionMessageStart = json.indexOf("exception\":\"") + 12;
+
+		int exceptionMessageEnd = json.indexOf("\"", exceptionMessageStart);
+
+		return json.substring(exceptionMessageStart, exceptionMessageEnd);
+	}
+
+}
---
+++ shared/json-web-service-client-shared/src/com/liferay/jsonwebserviceclient/JSONWebServiceClient.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jsonwebserviceclient;
+
+import java.io.IOException;
+
+import java.util.Map;
+
+import javax.security.auth.login.CredentialException;
+
+/**
+ * @author Ivica Cardic
+ * @author Igor Beslic
+ */
+public interface JSONWebServiceClient {
+
+	public String doGet(String url, Map<String, String> parameters)
+		throws CredentialException, IOException;
+
+	public String doPost(String url, Map<String, String> parameters)
+		throws CredentialException, IOException;
+
+	public void resetHttpClient();
+
+	public void setLogin(String login);
+
+	public void setPassword(String password);
+
+}
