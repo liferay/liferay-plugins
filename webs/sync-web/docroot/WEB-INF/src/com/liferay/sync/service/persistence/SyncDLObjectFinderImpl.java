b04c2b9ffd883b2eb3aa9047e7fa18b988588079
Dennis Ju <dennis.ju@liferay.com>
2014-04-17 16:49:19 -0700

    SYNC-1231 Generated package differs between 7.0 and 6.2

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/persistence/SyncDLObjectFinderImpl.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/persistence/SyncDLObjectFinderImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.sync.service.persistence.impl;
+package com.liferay.sync.service.persistence;
 
 import com.liferay.portal.kernel.dao.orm.QueryPos;
 import com.liferay.portal.kernel.dao.orm.SQLQuery;
@@ -26,7 +26,6 @@ import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFolder;
 import com.liferay.sync.model.SyncDLObject;
 import com.liferay.sync.model.impl.SyncDLObjectImpl;
-import com.liferay.sync.service.persistence.SyncDLObjectFinder;
 import com.liferay.util.dao.orm.CustomSQLUtil;
 
 import java.util.List;
