2fe4a5b22e4009dc232d3afa8408d038cf97d140
Bruno Farache <bruno.farache@liferay.com>
2014-04-17 16:31:28 -0300

    LPS-46043 A very simple message listener, we will use that to send notifications via Google and Apple

---
+++ portlets/push-notifications-portlet/docroot/WEB-INF/src/com/liferay/pushnotifications/messaging/PushNotificationsMessageListener.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.pushnotifications.messaging;
+
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
+
+/**
+ * @author Silvio Santos
+ * @author Bruno Farache
+ */
+public class PushNotificationsMessageListener implements MessageListener {
+
+	@Override
+	public void receive(Message message) throws MessageListenerException {
+		JSONObject payload = (JSONObject)message.getPayload();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Received message " + payload);
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		PushNotificationsMessageListener.class);
+
+}
