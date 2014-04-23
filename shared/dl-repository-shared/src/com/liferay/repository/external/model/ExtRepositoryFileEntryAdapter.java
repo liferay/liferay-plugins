4b7f67d663f9ce73e866b07f624d1c715122b976
Máté Thurzó <mate.thurzo@liferay.com>
2014-04-23 10:54:14 +0200

    LPS-46042 Update dl-repository-shared too

--- shared/dl-repository-shared/src/com/liferay/repository/external/model/ExtRepositoryFileEntryAdapter.java
+++ shared/dl-repository-shared/src/com/liferay/repository/external/model/ExtRepositoryFileEntryAdapter.java
@@ -272,7 +272,7 @@ public class ExtRepositoryFileEntryAdapter
 
 	@Override
 	public StagedModelType getStagedModelType() {
-		return new StagedModelType(FileEntry.class);
+		return new StagedModelType(DLFileEntryConstants.getClassName());
 	}
 
 	@Override
