04e51689efedc9787f44d3aab80806aa322cdca5
Wesley Gong <wesley.gong@liferay.com>
2014-04-30 17:50:38 -0700

    LPS-46084 Change package to shorter name

--- shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/AlloyFriendlyURLMapper.java
+++ shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/AlloyFriendlyURLMapper.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.alloy.mvc.util.bridges.alloy;
+package com.liferay.alloy.mvc;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
