a91d8e6c94d53686440719332689f713538ecf97
Máté Thurzó <mate.thurzo@liferay.com>
2014-05-06 16:11:28 +0200

    LPS-45909 Add portlet class

---
+++ portlets/sample-lar-portlet/docroot/WEB-INF/src/com/liferay/samplelar/portlet/SampleLARPortlet.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.samplelar.portlet;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.security.auth.CompanyThreadLocal;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.samplelar.service.SampleLARBookingLocalServiceUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+/**
+ * @author Mate Thurzo
+ */
+public class SampleLARPortlet extends MVCPortlet {
+
+	public void addSampleData(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long userId = UserLocalServiceUtil.getDefaultUserId(
+			CompanyThreadLocal.getCompanyId());
+		long groupId = ParamUtil.getLong(actionRequest, "groupId");
+
+		for (int i = 0; i <= 1; i++) {
+			String bookingNumber =
+				"LR" + GetterUtil.getInteger(Math.random() * 100);
+
+			SampleLARBookingLocalServiceUtil.addSampleLARBooking(
+				userId, groupId, bookingNumber, new ServiceContext());
+		}
+	}
+
+	public void deleteSampleLARBooking(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long sampleLARBookingId = ParamUtil.getLong(
+			actionRequest, "sampleLARBookingId");
+
+		SampleLARBookingLocalServiceUtil.deleteSampleLARBooking(
+			sampleLARBookingId);
+	}
+
+}
