d5a6b504545461ccd6b13594c614360104a9a4f8
Rebecca Dai <rebecca.dai@liferay.com>
2014-05-09 20:07:38 -0700

    LPS-46723 Sample json Portlet SF

--- portlets/sample-json-portlet/docroot/click_image.js
+++ portlets/sample-json-portlet/docroot/click_image.js
@@ -1,6 +1,6 @@
 toString : function() {
-	var main = document.createElement("div");
-	var clickMe = document.createElement("span");
+	var main = document.createElement('div');
+	var clickMe = document.createElement('span');
 	var id;
 
 	if (this.id) {
@@ -11,21 +11,21 @@ toString : function() {
 		id = "" + date.getTime();
 	}
 
-	clickMe.innerHTML = "Click me.";
-	clickMe.setAttribute("onclick", "document.getElementById('" + id + "image').style.display = 'inline'; this.style.display = 'none'");
-	clickMe.id = id + "link";
-	clickMe.style.cursor = "pointer";
+	clickMe.innerHTML = 'Click me.';
+	clickMe.setAttribute('onclick', 'document.getElementById("' + id + 'image").style.display = "inline"; this.style.display = "none"');
+	clickMe.id = id + 'link';
+	clickMe.style.cursor = 'pointer';
 
-	var image = document.createElement("img");
+	var image = document.createElement('img');
 
-	image.src = "http://www.liferay.com/image/company_logo?img_id=liferay.com";
-	image.setAttribute("onclick", "document.getElementById('" + id + "link').style.display = 'inline'; this.style.display = 'none'");
-	image.style.cursor = "pointer";
-	image.id = id + "image";
-	image.style.display = "none";
+	image.src = 'http://www.liferay.com/image/company_logo?img_id=liferay.com';
+	image.setAttribute('onclick', 'document.getElementById("' + id + 'link").style.display = "inline"; this.style.display = "none"');
+	image.style.cursor = 'pointer';
+	image.id = id + 'image';
+	image.style.display = 'none';
 
 	main.appendChild(clickMe);
-	main.appendChild(document.createElement("br"));
+	main.appendChild(document.createElement('br'));
 	main.appendChild(image);
 
 	return(main.innerHTML);
