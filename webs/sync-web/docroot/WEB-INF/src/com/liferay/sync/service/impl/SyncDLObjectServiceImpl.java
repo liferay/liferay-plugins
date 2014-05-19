71c2e6594a341aec8f6d117b8e02bfafbd70e270
Michael Young <michael.young@liferay.com>
2014-05-19 12:11:28 -0700

    sync-engine-shared - apply SF to sync-web

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectServiceImpl.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectServiceImpl.java
@@ -519,14 +519,14 @@ public class SyncDLObjectServiceImpl extends SyncDLObjectServiceBaseImpl {
 		PortletPreferences portletPreferences = PrefsPropsUtil.getPreferences(
 			companyId);
 
-		int syncClientPollInterval = PrefsPropsUtil.getInteger(
+		int pollInterval = PrefsPropsUtil.getInteger(
 			portletPreferences, companyId,
 			PortletPropsKeys.SYNC_CLIENT_POLL_INTERVAL,
 			PortletPropsValues.SYNC_CLIENT_POLL_INTERVAL);
 
 		portletPreferencesMap.put(
 			PortletPropsKeys.SYNC_CLIENT_POLL_INTERVAL,
-			String.valueOf(syncClientPollInterval));
+			String.valueOf(pollInterval));
 
 		return portletPreferencesMap;
 	}
