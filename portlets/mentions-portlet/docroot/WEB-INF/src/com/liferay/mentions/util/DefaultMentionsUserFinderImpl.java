dcda87de22dce698231bf89f9d5b9dc7c87d5fa5
adolfo.perez <adolfo.perez@liferay.com>
2014-05-23 13:46:49 +0200

    LPS-46132 use new `searchSocial' methods when looking up candidate users

--- portlets/mentions-portlet/docroot/WEB-INF/src/com/liferay/mentions/util/DefaultMentionsUserFinderImpl.java
+++ portlets/mentions-portlet/docroot/WEB-INF/src/com/liferay/mentions/util/DefaultMentionsUserFinderImpl.java
@@ -14,17 +14,18 @@
 
 package com.liferay.mentions.util;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.WildcardMode;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.comparator.UserScreenNameComparator;
 import com.liferay.portlet.social.util.SocialInteractionsConfiguration;
-import com.liferay.portlet.usersadmin.util.UsersAdminUtil;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -38,11 +39,80 @@ public class DefaultMentionsUserFinderImpl implements MentionsUserFinder {
 			SocialInteractionsConfiguration configuration)
 		throws PortalException, SystemException {
 
-		Hits hits = UserLocalServiceUtil.search(
-			companyId, query, query, query, query, StringPool.BLANK,
-			WorkflowConstants.STATUS_APPROVED, null, false, 0, 100, (Sort)null);
+		if (!configuration.isSocialInteractionsEnabled()) {
+			return Collections.emptyList();
+		}
 
-		return UsersAdminUtil.getUsers(hits);
+		if (configuration.isSocialInteractionsAnyUserEnabled()) {
+			LinkedHashMap<String, Object> params =
+				new LinkedHashMap<String, Object>();
+
+			params.put("wildcardMode", WildcardMode.TRAILING);
+
+			return UserLocalServiceUtil.search(
+				companyId, query, WorkflowConstants.STATUS_APPROVED, params, 0,
+				_MAX_USERS, new UserScreenNameComparator());
+		}
+
+		int[] types = getRelationTypes(configuration);
+
+		long[] groupIds = getGroupIds(userId, configuration);
+
+		boolean searchBySocialRelation =
+			configuration.isSocialInteractionsSocialRelationTypesEnabled() &&
+			types.length > 0;
+
+		boolean searchByGroup =
+			configuration.isSocialInteractionsSitesEnabled() &&
+				groupIds.length > 0;
+
+		if (searchBySocialRelation && searchByGroup) {
+			return UserLocalServiceUtil.searchSocial(
+				groupIds, userId, types, query, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+		}
+
+		if (searchByGroup) {
+			return UserLocalServiceUtil.searchSocial(
+				companyId, groupIds, query, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+		}
+
+		if (searchBySocialRelation) {
+			return UserLocalServiceUtil.searchSocial(
+				userId, types, query, QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		}
+
+		return Collections.emptyList();
 	}
+
+	protected int[] getRelationTypes(
+		SocialInteractionsConfiguration configuration) {
+
+		if (!configuration.isSocialInteractionsSocialRelationTypesEnabled()) {
+			return _EMPTY_TYPES;
+		}
+
+		return configuration.getSocialInteractionsSocialRelationTypesArray();
+	}
+
+	private long[] getGroupIds(
+			long userId, SocialInteractionsConfiguration configuration)
+		throws PortalException, SystemException {
+
+		if (!configuration.isSocialInteractionsSitesEnabled()) {
+			return _EMPTY_GROUPS;
+		}
+
+		User user = UserLocalServiceUtil.getUser(userId);
+
+		return user.getGroupIds();
+	}
+
+	private static long[] _EMPTY_GROUPS = {};
+
+	private static int[] _EMPTY_TYPES = {};
+
+	private static int _MAX_USERS = 100;
 
 }
