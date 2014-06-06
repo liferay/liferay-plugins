234131e404378806adefeb8feb74773cd544d7c0
Tibor Lipusz <tibor.lipusz@liferay.com>
2014-06-06 15:43:07 +0200

    SOS-2283 Move listener to the correct package like in so-hook

--- portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/hook/listeners/UserListener.java
+++ portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/hook/listeners/UserListener.java
@@ -15,7 +15,7 @@
  * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
  */
 
-package com.liferay.so.hook.listener;
+package com.liferay.so.hook.listeners;
 
 import com.liferay.portal.ModelListenerException;
 import com.liferay.portal.kernel.util.HttpUtil;
