a7757b47f1c4885b37d67214443bc71221736f44
Noelle Fa-Kaji <noelle.fa-kaji@liferay.com>
2014-06-09 14:24:00 -0700

    Stylized navigation bar with children menus, scroll js on homepage

---
+++ themes/noelle-portfolio-theme/docroot/js/scroll.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+// http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
