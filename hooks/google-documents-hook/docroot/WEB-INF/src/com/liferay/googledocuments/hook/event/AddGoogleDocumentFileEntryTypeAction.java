6256c90151bd177ea52ae6864845921c85f8ad77
Ivan Zaera Avellon <izaera@gmail.com>
2014-06-17 16:55:45 +0200

    LPS-47710 Declare an startup action to create the "Google Document" D&M document type

---
+++ hooks/google-documents-hook/docroot/WEB-INF/src/com/liferay/googledocuments/hook/event/AddGoogleDocumentFileEntryTypeAction.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.googledocuments.hook.event;
+
+import com.liferay.googledocuments.hook.util.Constants;
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.events.SimpleAction;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryType;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryTypeLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryTypeServiceUtil;
+import com.liferay.util.ContentUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Iván Zaera
+ */
+public class AddGoogleDocumentFileEntryTypeAction extends SimpleAction {
+
+	@Override
+	public void run(String[] ids) throws ActionException {
+		try {
+			doRun(GetterUtil.getLong(ids[0]));
+		}
+		catch (Exception e) {
+			throw new ActionException(e);
+		}
+	}
+
+	protected long _getCompanyGroupId(long companyId)
+		throws PortalException, SystemException {
+
+		Company company = CompanyLocalServiceUtil.getCompanyById(companyId);
+
+		return company.getGroupId();
+	}
+
+	protected void doRun(long companyId)
+		throws PortalException, SystemException {
+
+		long groupId = _getCompanyGroupId(companyId);
+
+		if (!_existsGoogleDriveDocumentFileEntryType(groupId)) {
+			long defaultUserId = UserLocalServiceUtil.getDefaultUserId(
+				companyId);
+
+			_createGoogleDriveDocumentFileEntryType(defaultUserId, groupId);
+		}
+	}
+
+	private void _createGoogleDriveDocumentFileEntryType(
+			long userId, long groupId)
+		throws PortalException, SystemException {
+
+		Locale defaultLocale = LocaleUtil.getDefault();
+
+		Map<Locale, String> nameMap = new HashMap<Locale, String>();
+
+		nameMap.put(defaultLocale, Constants.GOOGLE_DOCUMENT_NAME);
+
+		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
+
+		descriptionMap.put(defaultLocale, Constants.GOOGLE_DOCUMENT_NAME);
+
+		long[] ddmStructureIds = new long[] {};
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		String xsd = ContentUtil.get(
+			getClass().getClassLoader(),
+			"com/liferay/googledocuments/hook/event/" +
+			"GoogleDocument_DDM_XSD.xml");
+
+		serviceContext.setAttribute("xsd", xsd);
+
+		DLFileEntryTypeLocalServiceUtil.addFileEntryType(
+			userId, groupId, Constants.GOOGLE_DOCUMENT_FILE_ENTRY_TYPE_KEY,
+			nameMap, descriptionMap, ddmStructureIds, serviceContext);
+	}
+
+	private boolean _existsGoogleDriveDocumentFileEntryType(long groupId)
+		throws SystemException {
+
+		long[] groupIds = new long[] { groupId };
+
+		List<DLFileEntryType> fileEntryTypes =
+			DLFileEntryTypeServiceUtil.getFileEntryTypes(groupIds);
+
+		for (DLFileEntryType fileEntryType : fileEntryTypes) {
+			if (fileEntryType.getFileEntryTypeKey().equals(
+					Constants.GOOGLE_DOCUMENT_FILE_ENTRY_TYPE_KEY)) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+}
