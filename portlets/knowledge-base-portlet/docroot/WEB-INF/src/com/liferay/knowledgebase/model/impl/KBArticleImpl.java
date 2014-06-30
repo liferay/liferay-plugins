a3255b3aba19ab6d7f5bd1a91c1aa7e709b999a0
adolfo.perez <adolfo.perez@liferay.com>
2014-06-30 11:01:46 +0200

    LPS-48076 add methods to retrieve next and previous KB article

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/model/impl/KBArticleImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/model/impl/KBArticleImpl.java
@@ -18,6 +18,7 @@ import com.liferay.knowledgebase.article.util.KBArticleAttachmentsUtil;
 import com.liferay.knowledgebase.model.KBArticle;
 import com.liferay.knowledgebase.model.KBArticleConstants;
 import com.liferay.knowledgebase.service.KBArticleLocalServiceUtil;
+import com.liferay.knowledgebase.service.KBArticleServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
@@ -114,6 +115,13 @@ public class KBArticleImpl extends KBArticleBaseImpl {
 	}
 
 	@Override
+	public KBArticle getNextKBArticle() throws PortalException {
+		return KBArticleServiceUtil.fetchSuccessorKBArticle(
+			getGroupId(), getParentResourcePrimKey(), getResourcePrimKey(),
+			getStatus());
+	}
+
+	@Override
 	public KBArticle getParentKBArticle()
 		throws PortalException, SystemException {
 
@@ -125,6 +133,13 @@ public class KBArticleImpl extends KBArticleBaseImpl {
 
 		return KBArticleLocalServiceUtil.getLatestKBArticle(
 			parentResourcePrimKey, WorkflowConstants.STATUS_APPROVED);
+	}
+
+	@Override
+	public KBArticle getPreviousKBArticle() throws PortalException {
+		return KBArticleServiceUtil.fetchPredecessorKBArticle(
+			getGroupId(), getParentResourcePrimKey(), getResourcePrimKey(),
+			getStatus());
 	}
 
 	@Override
