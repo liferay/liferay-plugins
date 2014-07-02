4829bd323a28836aa8410f239dc8009a75cad708
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2014-07-02 10:37:42 -0300

    LPS-48182 Submitting instance index to view page

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -2088,6 +2088,7 @@ AUI.add(
 						var schedulerEvent = instance.get('event');
 
 						data.calendarBookingId = schedulerEvent.get('calendarBookingId');
+						data.instanceIndex = schedulerEvent.get('instanceIndex');
 
 						Liferay.Util.openWindow(
 							{
