9fd0b02a7fbe32f07027ff21a38b5ad5ebc79a02
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-10 11:31:42 -0300

    LPS-45783 Use AlloyUI alert to display calendar messages

--- portlets/calendar-portlet/docroot/js/components.js
+++ portlets/calendar-portlet/docroot/js/components.js
@@ -1167,24 +1167,21 @@
 					return confirmationPanel.render().show();
 				},
 
-				showMessage: function(messageContainer, message, timeDisplayed) {
-					timeDisplayed = timeDisplayed || 5000;
-
-					messageContainer.set('text', message);
-					messageContainer.show();
-
-					setTimeout(
-						function() {
-							messageContainer.hide();
-						},
-						timeDisplayed
-					);
+				showAlert: function(container, message) {
+					new A.Alert({
+						animated: true,
+						bodyContent: message,
+						closeable: true,
+						cssClass: 'alert-success',
+						destroyOnHide: true,
+						duration: 1
+					}).render(container);
 				}
 			};
 		},
 		'',
 		{
-			requires: ['liferay-util-window']
+			requires: ['aui-alert', 'liferay-util-window']
 		}
 	);
 }());
