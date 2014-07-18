7850cee701b50db87d0cfd373a8bdef2be5f469d
Dustin Ryerson <dustin.ryerson@liferay.com>
2014-07-18 15:11:12 -0700

    Mail Portlet, indexing - Source formatting

--- portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/service/impl/MessageLocalServiceImpl.java
+++ portlets/mail-portlet/docroot/WEB-INF/src/com/liferay/mail/service/impl/MessageLocalServiceImpl.java
@@ -136,7 +136,9 @@ public class MessageLocalServiceImpl extends MessageLocalServiceBaseImpl {
 		return (int)dynamicQueryCount(dynamicQuery);
 	}
 
-	public List<Message> getCompanyMessages(long companyId, int start, int end) {
+	public List<Message> getCompanyMessages(
+		long companyId, int start, int end) {
+
 		return messagePersistence.findByCompanyId(companyId, start, end);
 	}
 
