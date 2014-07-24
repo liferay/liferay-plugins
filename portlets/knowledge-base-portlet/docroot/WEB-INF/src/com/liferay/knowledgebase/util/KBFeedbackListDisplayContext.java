fb7d4050ab743b34c3defda20dcb69fa335d9065
adolfo.perez <adolfo.perez@liferay.com>
2014-07-24 13:16:29 +0200

    LPS-48765 move location dependent code into display context

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/KBFeedbackListDisplayContext.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.util;
+
+import com.liferay.knowledgebase.model.KBArticle;
+import com.liferay.knowledgebase.model.KBCommentConstants;
+import com.liferay.knowledgebase.service.KBCommentLocalServiceUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class KBFeedbackListDisplayContext {
+
+	public KBFeedbackListDisplayContext(
+		KBArticle kbArticle, String selectedNavItem) {
+
+		_kbArticle = kbArticle;
+		_selectedNavItem = selectedNavItem;
+	}
+
+	public KBFeedbackListDisplayContext(long groupId, String selectedNavItem) {
+		_groupId = groupId;
+		_selectedNavItem = selectedNavItem;
+	}
+
+	public int getCompletedSuggestionsCount() throws SystemException {
+		return getKBCommentsCountByStatus(KBCommentConstants.STATUS_COMPLETED);
+	}
+
+	public int getInProgressSuggestionsCount() throws SystemException {
+		return getKBCommentsCountByStatus(
+			KBCommentConstants.STATUS_IN_PROGRESS);
+	}
+
+	public int getNewSuggestionsCount() throws SystemException {
+		return getKBCommentsCountByStatus(KBCommentConstants.STATUS_NEW);
+	}
+
+	public String getSelectedNavItem() {
+		return _selectedNavItem;
+	}
+
+	public String getViewFeedbackByStatusURL(
+		RenderResponse renderResponse, String navItem) {
+
+		PortletURL viewFeedbackByStatusURL = renderResponse.createRenderURL();
+
+		viewFeedbackByStatusURL.setParameter("navItem", navItem);
+
+		if (_kbArticle == null) {
+			viewFeedbackByStatusURL.setParameter(
+				"mvcPath", "/admin/view_feedback.jsp");
+		}
+		else {
+			viewFeedbackByStatusURL.setParameter(
+				"resourcePrimKey",
+				String.valueOf(_kbArticle.getResourcePrimKey()));
+		}
+
+		return viewFeedbackByStatusURL.toString();
+	}
+
+	protected int getKBCommentsCountByStatus(int status)
+		throws SystemException {
+
+		int kbCommentCount = 0;
+
+		if (_kbArticle == null) {
+			kbCommentCount = KBCommentLocalServiceUtil.getKBCommentsCount(
+				_groupId, status);
+		}
+		else {
+			kbCommentCount = KBCommentLocalServiceUtil.getKBCommentsCount(
+				KBArticle.class.getName(), _kbArticle.getClassPK(), status);
+		}
+
+		return kbCommentCount;
+	}
+
+	private long _groupId;
+	private KBArticle _kbArticle;
+	private String _selectedNavItem;
+
+}
