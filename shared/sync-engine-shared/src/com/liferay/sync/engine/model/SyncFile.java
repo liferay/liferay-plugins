134c5cd7eb7226dfe906a4679ab9b38df690992b
Shinn Lok <shinn.lok@liferay.com>
2014-07-28 11:54:35 -0700

    sync-engine-shared - Namespace index name to avoid conflicts

--- shared/sync-engine-shared/src/com/liferay/sync/engine/model/SyncFile.java
+++ shared/sync-engine-shared/src/com/liferay/sync/engine/model/SyncFile.java
@@ -364,13 +364,13 @@ public class SyncFile extends StateAwareModel {
 	@DatabaseField(useGetSet = true)
 	protected long parentFolderId;
 
-	@DatabaseField(indexName = "R_S_T", useGetSet = true)
+	@DatabaseField(indexName = "SyncFile_R_S_T", useGetSet = true)
 	protected long repositoryId;
 
 	@DatabaseField(useGetSet = true)
 	protected long size;
 
-	@DatabaseField(indexName = "R_S_T", useGetSet = true)
+	@DatabaseField(indexName = "SyncFile_R_S_T", useGetSet = true)
 	protected long syncAccountId;
 
 	@DatabaseField(generatedId = true, useGetSet = true)
@@ -379,7 +379,7 @@ public class SyncFile extends StateAwareModel {
 	@DatabaseField(useGetSet = true)
 	protected String type;
 
-	@DatabaseField(indexName = "R_S_T", useGetSet = true)
+	@DatabaseField(indexName = "SyncFile_R_S_T", useGetSet = true)
 	protected long typePK;
 
 	@DatabaseField(useGetSet = true)
