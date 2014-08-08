f9c5b161a810bb8bf82fc363974fda2c478514f7
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-08-08 10:58:48 -0700

    LPS-48464 - AUI Templates have a variable of LString automatically injected into them that points to Lang.String

--- portlets/calendar-portlet/docroot/js/components.js
+++ portlets/calendar-portlet/docroot/js/components.js
@@ -303,7 +303,7 @@
 				'<tpl for="calendars">',
 					'<div class="', CSS_CALENDAR_LIST_ITEM, '">',
 						'<div class="', CSS_CALENDAR_LIST_ITEM_COLOR, '" {[ parent.calendars[$i].get("visible") ? ', '\'style="background-color:\'', STR_PLUS, 'parent.calendars[$i].get("color")', STR_PLUS, '";border-color:"', STR_PLUS, 'parent.calendars[$i].get("color")', STR_PLUS, '";\\""', ' : \'', STR_BLANK, '\' ]}></div>',
-							'<span class="', CSS_CALENDAR_LIST_ITEM_LABEL, '">{[ AUI().Lang.String.escapeHTML(parent.calendars[$i].getDisplayName()) ]}</span>',
+							'<span class="', CSS_CALENDAR_LIST_ITEM_LABEL, '">{[LString.escapeHTML(parent.calendars[$i].getDisplayName())]}</span>',
 						'<div class="', CSS_CALENDAR_LIST_ITEM_ARROW, '"><i class="', CSS_ICON_CARET_DOWN, '"></i></div>',
 					'</div>',
 				'</tpl>'
