02a42561ca8d0210baeae200e929d9bd058c0251
Jonathan McCann <jonathan.mccann@liferay.com>
2014-08-14 08:16:12 -0700

    SOS-2376 Source Formatting

--- portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/util/ContactsUtil.java
+++ portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/util/ContactsUtil.java
@@ -195,7 +195,7 @@ public class ContactsUtil {
 
 			ListType listType = address.getType();
 
-			sb.append(StringUtil.toUpperCase(_getVcardType(listType)));
+			sb.append(StringUtil.toUpperCase(_getVCardListTypeName(listType)));
 
 			sb.append(StringPool.COLON);
 			sb.append(StringPool.SEMICOLON);
@@ -390,7 +390,7 @@ public class ContactsUtil {
 
 			ListType listType = phone.getType();
 
-			sb.append(StringUtil.toUpperCase(_getVcardType(listType)));
+			sb.append(StringUtil.toUpperCase(_getVCardListTypeName(listType)));
 
 			sb.append(StringPool.COLON);
 			sb.append(phone.getNumber());
@@ -400,6 +400,19 @@ public class ContactsUtil {
 		}
 
 		return sb.toString();
+	}
+
+	private static String _getVCardListTypeName(ListType listType) {
+		String listTypeName = listType.getName();
+
+		if (StringUtil.equalsIgnoreCase(listTypeName, "business")) {
+			listTypeName = "work";
+		}
+		else if (StringUtil.equalsIgnoreCase(listTypeName, "personal")) {
+			listTypeName = "home";
+		}
+
+		return listTypeName;
 	}
 
 	private static String _getWebsites(User user) throws Exception {
@@ -413,7 +426,7 @@ public class ContactsUtil {
 
 			ListType listType = website.getType();
 
-			sb.append(StringUtil.toUpperCase(_getVcardType(listType)));
+			sb.append(StringUtil.toUpperCase(_getVCardListTypeName(listType)));
 
 			sb.append(StringPool.COLON);
 
@@ -425,19 +438,6 @@ public class ContactsUtil {
 		}
 
 		return sb.toString();
-	}
-
-	private static String _getVcardType(ListType listType) {
-		String listTypeName = listType.getName();
-
-		if (listTypeName.equalsIgnoreCase("business")) {
-			listTypeName = "work";
-		}
-		else if (listTypeName.equalsIgnoreCase("personal")) {
-			listTypeName = "home";
-		}
-
-		return listTypeName;
 	}
 
 }
