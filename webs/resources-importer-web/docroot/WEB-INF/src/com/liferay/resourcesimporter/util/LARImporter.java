3aa7558432af151743eeab822c69f617c871671e
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-08-27 10:16:05 +0200

    LPS-49570 Set non-boolean import parameters to their correct values

--- webs/resources-importer-web/docroot/WEB-INF/src/com/liferay/resourcesimporter/util/LARImporter.java
+++ webs/resources-importer-web/docroot/WEB-INF/src/com/liferay/resourcesimporter/util/LARImporter.java
@@ -15,6 +15,7 @@
 package com.liferay.resourcesimporter.util;
 
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
+import com.liferay.portal.kernel.lar.UserIdStrategy;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
@@ -88,7 +89,9 @@ public class LARImporter extends BaseImporter {
 			new String[] {Boolean.TRUE.toString()});
 		parameters.put(
 			PortletDataHandlerKeys.LAYOUTS_IMPORT_MODE,
-			new String[] {Boolean.TRUE.toString()});
+			new String[] {
+				PortletDataHandlerKeys.
+					LAYOUTS_IMPORT_MODE_MERGE_BY_LAYOUT_UUID});
 		parameters.put(
 			PortletDataHandlerKeys.LOGO,
 			new String[] {Boolean.TRUE.toString()});
@@ -118,13 +121,13 @@ public class LARImporter extends BaseImporter {
 			new String[] {Boolean.TRUE.toString()});
 		parameters.put(
 			PortletDataHandlerKeys.PORTLETS_MERGE_MODE,
-			new String[] {Boolean.TRUE.toString()});
+			new String[] {PortletDataHandlerKeys.PORTLETS_MERGE_MODE_REPLACE});
 		parameters.put(
 			PortletDataHandlerKeys.THEME_REFERENCE,
 			new String[] {Boolean.TRUE.toString()});
 		parameters.put(
 			PortletDataHandlerKeys.USER_ID_STRATEGY,
-			new String[] {Boolean.TRUE.toString()});
+			new String[] {UserIdStrategy.CURRENT_USER_ID});
 
 		return parameters;
 	}
