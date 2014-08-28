bd39469a6b3225d110881f0fe8755ab449979e36
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2014-08-28 19:36:07 -0300

    LPS-49606 Persisting positional weekday info

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/portlet/CalendarPortlet.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/portlet/CalendarPortlet.java
@@ -751,6 +751,20 @@ public class CalendarPortlet extends MVCPortlet {
 					positionalWeekdays.add(new PositionalWeekday(weekday, 0));
 				}
 			}
+		} else if ((frequency == Frequency.MONTHLY) ||
+			(frequency == Frequency.YEARLY)) {
+
+			boolean repeatOnWeekday = ParamUtil.getBoolean(
+				actionRequest, "repeatOnWeekday");
+
+			if (repeatOnWeekday) {
+				int position = ParamUtil.getInteger(actionRequest, "position");
+				Weekday weekday = Weekday.parse(
+					ParamUtil.getString(actionRequest, "weekday"));
+
+				positionalWeekdays.add(
+					new PositionalWeekday(weekday, position));
+			}
 		}
 
 		recurrence.setPositionalWeekdays(positionalWeekdays);
