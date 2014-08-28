7ce6f50d284c0c552bafcb2cf8be06eca9ab6ec1
Michael Han <michael.han@liferay.com>
2014-08-27 19:36:50 -0500

    LPS-49586 refactor score processing

--- shared/solr4-shared/src/com/liferay/portal/search/solr/SolrIndexSearcher.java
+++ shared/solr4-shared/src/com/liferay/portal/search/solr/SolrIndexSearcher.java
@@ -230,6 +230,27 @@ public class SolrIndexSearcher extends BaseIndexSearcher {
 		}
 	}
 
+	protected float addScore(
+		SolrDocument solrDocument, List<Float> scores, float maxScore,
+		boolean scoreEnabled) {
+
+		if (scoreEnabled) {
+			float score = GetterUtil.getFloat(
+				String.valueOf(solrDocument.getFieldValue("score")));
+
+			if (score > maxScore) {
+				maxScore = score;
+			}
+
+			scores.add(score);
+		}
+		else {
+			scores.add(maxScore);
+		}
+
+		return maxScore;
+	}
+
 	protected void addSelectedFields(
 		SolrQuery solrQuery, QueryConfig queryConfig) {
 
@@ -451,19 +472,8 @@ public class SolrIndexSearcher extends BaseIndexSearcher {
 				}
 			}
 
-			if (queryConfig.isScoreEnabled()) {
-				float score = GetterUtil.getFloat(
-					String.valueOf(solrDocument.getFieldValue("score")));
-
-				if (score > maxScore) {
-					maxScore = score;
-				}
-
-				scores.add(score);
-			}
-			else {
-				scores.add(maxScore);
-			}
+			maxScore = addScore(
+				solrDocument, scores, maxScore, queryConfig.isScoreEnabled());
 
 			subsetTotal++;
 		}
