202168e689f0ef488cb71dacccdabe950bdf3c5e
Shinn Lok <shinn.lok@liferay.com>
2014-09-04 17:58:29 -0700

    SYNC-1254 Use token so we aren't bogged down by the slowness of encrypting a password during authentication

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/hook/security/auth/SyncAuthVerifier.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/hook/security/auth/SyncAuthVerifier.java
@@ -14,9 +14,14 @@
 
 package com.liferay.sync.hook.security.auth;
 
+import com.google.common.collect.Lists;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.auth.AccessControlContext;
 import com.liferay.portal.security.auth.AuthException;
 import com.liferay.portal.security.auth.AuthVerifier;
@@ -24,13 +29,28 @@ import com.liferay.portal.security.auth.AuthVerifierResult;
 import com.liferay.portal.security.auth.AutoLoginException;
 import com.liferay.portal.security.auth.BaseAutoLogin;
 import com.liferay.sync.util.PortletPropsValues;
+import com.liferay.util.PwdGenerator;
 
 import java.lang.reflect.Method;
 
+import java.util.List;
 import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.oauth.jsontoken.Checker;
+import net.oauth.jsontoken.JsonToken;
+import net.oauth.jsontoken.JsonTokenParser;
+import net.oauth.jsontoken.crypto.HmacSHA256Signer;
+import net.oauth.jsontoken.crypto.HmacSHA256Verifier;
+import net.oauth.jsontoken.crypto.SignatureAlgorithm;
+import net.oauth.jsontoken.crypto.Signer;
+import net.oauth.jsontoken.crypto.Verifier;
+import net.oauth.jsontoken.discovery.VerifierProvider;
+import net.oauth.jsontoken.discovery.VerifierProviders;
+
+import org.joda.time.Instant;
 
 /**
  * @author Michael Young
@@ -41,6 +61,27 @@ public class SyncAuthVerifier extends BaseAutoLogin implements AuthVerifier {
 	@Override
 	public String getAuthType() {
 		return SyncAuthVerifier.class.getSimpleName();
+	}
+
+	public String getUserId(String tokenString) {
+		try {
+			JsonTokenParser jsonTokenParser = getJsonTokenParser();
+
+			JsonToken jsonToken = jsonTokenParser.verifyAndDeserialize(
+				tokenString);
+
+			JsonPrimitive userIdJsonPrimitive = jsonToken.getParamAsPrimitive(
+				"userId");
+
+			if (userIdJsonPrimitive == null) {
+				return null;
+			}
+
+			return userIdJsonPrimitive.getAsString();
+		}
+		catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
@@ -68,10 +109,49 @@ public class SyncAuthVerifier extends BaseAutoLogin implements AuthVerifier {
 		}
 	}
 
+	protected String createToken(long userId) {
+		Signer signer = null;
+
+		try {
+			signer = getSigner();
+		}
+		catch (Exception e) {
+			return null;
+		}
+
+		JsonToken jsonToken = new JsonToken(signer);
+
+		Instant instant = new Instant();
+
+		jsonToken.setExpiration(instant.plus(_expiration));
+		jsonToken.setIssuedAt(instant);
+
+		JsonObject payloadJsonObject = jsonToken.getPayloadAsJsonObject();
+
+		payloadJsonObject.addProperty("userId", userId);
+
+		try {
+			return jsonToken.serializeAndSign();
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
 	@Override
 	protected String[] doLogin(
 			HttpServletRequest request, HttpServletResponse response)
 		throws Exception {
+
+		String token = request.getHeader(_TOKEN_HEADER);
+
+		if (Validator.isNotNull(token)) {
+			String userId = getUserId(token);
+
+			if (userId != null) {
+				return new String[] {userId, null};
+			}
+		}
 
 		Thread currentThread = Thread.currentThread();
 
@@ -96,6 +176,12 @@ public class SyncAuthVerifier extends BaseAutoLogin implements AuthVerifier {
 					object, request, response);
 
 				if (credentials != null) {
+					token = createToken(Long.valueOf(credentials[0]));
+
+					if (token != null) {
+						response.addHeader(_TOKEN_HEADER, token);
+					}
+
 					return credentials;
 				}
 			}
@@ -107,7 +193,62 @@ public class SyncAuthVerifier extends BaseAutoLogin implements AuthVerifier {
 		return null;
 	}
 
+	protected JsonTokenParser getJsonTokenParser() throws Exception {
+		if (_jsonTokenParser != null) {
+			return _jsonTokenParser;
+		}
+
+		final Verifier verifier = new HmacSHA256Verifier(_secret.getBytes());
+
+		VerifierProvider verifierProvider = new VerifierProvider() {
+
+			@Override
+			public List<Verifier> findVerifier(String signerId, String keyId) {
+				return Lists.newArrayList(verifier);
+			}
+
+		};
+
+		VerifierProviders verifyProviders = new VerifierProviders();
+
+		verifyProviders.setVerifierProvider(
+			SignatureAlgorithm.HS256, verifierProvider);
+
+		Checker checker = new Checker() {
+
+			@Override
+			public void check(JsonObject jsonObject) {
+			}
+
+		};
+
+		_jsonTokenParser = new JsonTokenParser(verifyProviders, checker);
+
+		return _jsonTokenParser;
+	}
+
+	protected Signer getSigner() {
+		if (_signer != null) {
+			return _signer;
+		}
+
+		try {
+			_signer = new HmacSHA256Signer(null, null, _secret.getBytes());
+
+			return _signer;
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	private static final String _TOKEN_HEADER = "Sync-JWT";
+
 	private static String[] _autoLoginClassNames = StringUtil.split(
 		PortletPropsValues.SYNC_AUTH_VERIFIER_PIPELINE);
+	private static long _expiration = 60000L;
+	private static JsonTokenParser _jsonTokenParser;
+	private static String _secret = PwdGenerator.getPassword();
+	private static Signer _signer;
 
 }
