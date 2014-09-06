720806f02d5f2eed83681b61dbc57c8acad53816
matthewkong <matthew.kong@liferay.com>
2014-09-05 17:25:13 -0700

    LRDCOM-11488 Properties

---
+++ hooks/google-groups-hook/docroot/WEB-INF/src/com/liferay/google/groups/util/PortletPropsKeys.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.google.groups.util;
+
+/**
+ * @author Matthew Kong
+ */
+public class PortletPropsKeys {
+
+	public static final String GOOGLE_GROUPS_EMAIL_PERMISSION =
+		"google.groups.email.permission";
+
+	public static final String GOOGLE_GROUPS_EMAIL_PREFIX =
+		"google.groups.email.prefix";
+
+	public static final String GOOGLE_GROUPS_SYNC_ON_STARTUP =
+		"google.groups.sync.on.startup";
+
+}
