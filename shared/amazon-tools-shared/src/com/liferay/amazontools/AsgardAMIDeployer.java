6e1a3c43e1eec12f6bbdb030eb542ca59c536ff5
Ivica Cardic <ivica.cardic@liferay.com>
2014-09-09 14:35:43 +0200

    CLDSVCS-892 Number of available elastic IP address doesn't have to be the as the number of available instances

--- shared/amazon-tools-shared/src/com/liferay/amazontools/AsgardAMIDeployer.java
+++ shared/amazon-tools-shared/src/com/liferay/amazontools/AsgardAMIDeployer.java
@@ -134,7 +134,9 @@ public class AsgardAMIDeployer extends BaseAMITool {
 
 		String[] elasticIpAddresses = elasticIpAddressesString.split(",");
 
-		for (int i = 0; i < elasticIpAddresses.length; i++) {
+		for (int i = 0; (i < elasticIpAddresses.length) &&
+				(i < instanceIds.size()); i++) {
+
 			System.out.println(
 				"Associating IP address " + elasticIpAddresses[i] +
 					" with instance " + instanceIds.get(i));
