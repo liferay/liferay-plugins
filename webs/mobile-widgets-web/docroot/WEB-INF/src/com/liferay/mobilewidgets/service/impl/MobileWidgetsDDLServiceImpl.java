ea8f422271d73370974ed9f5510131b64491ce78
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-09-10 15:19:52 +0200

    LPS-49908 SF

--- webs/mobile-widgets-web/docroot/WEB-INF/src/com/liferay/mobilewidgets/service/impl/MobileWidgetsDDLServiceImpl.java
+++ webs/mobile-widgets-web/docroot/WEB-INF/src/com/liferay/mobilewidgets/service/impl/MobileWidgetsDDLServiceImpl.java
@@ -23,33 +23,15 @@ import com.liferay.portlet.dynamicdatamapping.storage.FieldConstants;
 import com.liferay.portlet.dynamicdatamapping.storage.Fields;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * The implementation of the mobile widgets d d l remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.mobilewidgets.service.MobileWidgetsDDLService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
  * @author Jose Manuel Navarro
- * @see com.liferay.mobilewidgets.service.base.MobileWidgetsDDLServiceBaseImpl
- * @see com.liferay.mobilewidgets.service.MobileWidgetsDDLServiceUtil
  */
 public class MobileWidgetsDDLServiceImpl
 	extends MobileWidgetsDDLServiceBaseImpl {
-
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this interface directly. Always use {@link com.liferay.mobilewidgets.service.MobileWidgetsDDLServiceUtil} to access the mobile widgets d d l remote service.
-	 */
 
 	public Map<String, String> getDDLRecordValues(long recordId, Locale locale)
 		throws PortalException, SystemException {
@@ -66,25 +48,25 @@ public class MobileWidgetsDDLServiceImpl
 			locale = fields.getDefaultLocale();
 		}
 
-		Iterator<Field> fieldsIterator = fields.iterator();
+		for (Field field : fields) {
+			String fieldValue = String.valueOf(field.getValue(locale));
 
-		while (fieldsIterator.hasNext()) {
-			Field currentField = fieldsIterator.next();
-
-			String fieldType = currentField.getType();
-			String fieldValue;
-
-			if (fieldType.indexOf(FieldConstants.DATE) != -1) {
-				fieldValue = currentField.getRenderedValue(locale);
-			}
-			else {
-				fieldValue = String.valueOf(currentField.getValue(locale));
+			if (isDateField(field.getType())) {
+				fieldValue = field.getRenderedValue(locale);
 			}
 
-			recordValues.put(currentField.getName(), fieldValue);
+			recordValues.put(field.getName(), fieldValue);
 		}
 
 		return recordValues;
+	}
+
+	protected boolean isDateField(String fieldType) {
+		if (fieldType.indexOf(FieldConstants.DATE) != -1) {
+			return true;
+		}
+
+		return false;
 	}
 
 }
