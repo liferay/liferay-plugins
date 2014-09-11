3c91bf32bf17bfd7a2454cd22ac4f4d60cef5550
Brian Chan <brian.chan@liferay.com>
2014-09-11 14:16:23 -0700

    LPS-50043 SF

--- portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/hook/events/StartupAction.java
+++ portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/hook/events/StartupAction.java
@@ -14,7 +14,6 @@
 
 package com.liferay.notifications.hook.events;
 
-import com.liferay.compat.portal.util.PortalUtil;
 import com.liferay.notifications.util.PortletPropsKeys;
 import com.liferay.notifications.util.PortletPropsValues;
 import com.liferay.portal.kernel.configuration.Filter;
@@ -33,6 +32,7 @@ import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.util.JavaFieldsParser;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.util.ContentUtil;
 import com.liferay.util.portlet.PortletProps;
 
