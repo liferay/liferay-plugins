ebed1e7928d8805562ceffef8f558e5a4bc90375
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-09-11 12:44:40 -0700

    LPS-50022 - General source formatting

--- portlets/calendar-portlet/docroot/js/components.js
+++ portlets/calendar-portlet/docroot/js/components.js
@@ -790,7 +790,7 @@
 				'</label>' +
 				'<label class="reminder-type" for="{portletNamespace}reminder{i}">' +
 					'<input id="{portletNamespace}reminderType{i}" name="{portletNamespace}reminderType{i}" type="hidden" value="email" />' +
-					'{email}'+
+					'{email}' +
 				'</label>' +
 				'<input class="input-mini reminder-value" name="{portletNamespace}reminderValue{i}" type="text" size="5" value="{time.value}" <tpl if="disabled">disabled="disabled"</tpl> /> ' +
 				'<select class="reminder-duration span2" name="{portletNamespace}reminderDuration{i}" <tpl if="disabled">disabled="disabled"</tpl>>' +
