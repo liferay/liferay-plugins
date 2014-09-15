2b1a4def6ff328f2c6060c52ce405b236f73de5c
Dennis Ju <dennis.ju@liferay.com>
2014-09-15 16:51:59 -0700

    LPS-50101 - Add lock info to approved SyncDLObject

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectLocalServiceImpl.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectLocalServiceImpl.java
@@ -64,6 +64,19 @@ public class SyncDLObjectLocalServiceImpl
 			syncDLObject.setType(type);
 			syncDLObject.setTypePK(typePK);
 			syncDLObject.setTypeUuid(typeUuid);
+
+			if (type.equals(SyncConstants.TYPE_PRIVATE_WORKING_COPY)) {
+				SyncDLObject approvedSyncDLObject =
+					syncDLObjectPersistence.fetchByT_T(
+						SyncConstants.TYPE_FILE, typePK);
+
+				approvedSyncDLObject.setModifiedTime(modifiedTime);
+				approvedSyncDLObject.setLockExpirationDate(lockExpirationDate);
+				approvedSyncDLObject.setLockUserId(lockUserId);
+				approvedSyncDLObject.setLockUserName(lockUserName);
+
+				syncDLObjectPersistence.update(approvedSyncDLObject);
+			}
 		}
 		else if (syncDLObject.getModifiedTime() >= modifiedTime) {
 			return null;
