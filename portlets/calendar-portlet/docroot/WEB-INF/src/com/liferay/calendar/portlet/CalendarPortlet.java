7dc14eb58b19034c7d08de944d4cfebb2ae85969
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2014-09-16 10:37:50 -0300

    LPS-50016 instanceIndex will be used for more than editing instances

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/portlet/CalendarPortlet.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/portlet/CalendarPortlet.java
@@ -298,6 +298,8 @@ public class CalendarPortlet extends MVCPortlet {
 
 		long calendarBookingId = ParamUtil.getLong(
 			actionRequest, "calendarBookingId");
+		int instanceIndex = ParamUtil.getInteger(
+			actionRequest, "instanceIndex");
 
 		long calendarId = ParamUtil.getLong(actionRequest, "calendarId");
 		long[] childCalendarIds = ParamUtil.getLongValues(
@@ -341,8 +343,6 @@ public class CalendarPortlet extends MVCPortlet {
 					CalendarBookingLocalServiceUtil.getCalendarBooking(
 						calendarBookingId);
 
-				int instanceIndex = ParamUtil.getInteger(
-					actionRequest, "instanceIndex");
 				boolean allFollowing = ParamUtil.getBoolean(
 					actionRequest, "allFollowing");
 
