e7e762ccde936e77a5f2a117640d12c983ce77f0
sherryre <sherry.yang@liferay.com>
2014-09-24 12:40:33 -0700

    SOS-2465 add methods in localServiceImpl

--- portlets/share-service-portlet/docroot/WEB-INF/src/com/liferay/share/service/impl/EntitySocialFeedLocalServiceImpl.java
+++ portlets/share-service-portlet/docroot/WEB-INF/src/com/liferay/share/service/impl/EntitySocialFeedLocalServiceImpl.java
@@ -16,28 +16,64 @@ package com.liferay.share.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.share.model.EntitySocialFeed;
 import com.liferay.share.service.base.EntitySocialFeedLocalServiceBaseImpl;
+import com.liferay.share.service.persistence.EntitySocialFeedPK;
+
+import java.util.List;
+import java.util.Map;
 
 /**
- * The implementation of the entity social feed local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.share.service.EntitySocialFeedLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see com.liferay.share.service.base.EntitySocialFeedLocalServiceBaseImpl
- * @see com.liferay.share.service.EntitySocialFeedLocalServiceUtil
+ * @author Sherry Yang
  */
 @ProviderType
 public class EntitySocialFeedLocalServiceImpl
 	extends EntitySocialFeedLocalServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.share.service.EntitySocialFeedLocalServiceUtil} to access the entity social feed local service.
-	 */
+
+	public void addEntitySocialFeed(
+		long classNameId, long classPK, long feedClassNameId,
+		long feedClassPK) {
+
+		EntitySocialFeedPK pk = new EntitySocialFeedPK(
+			classNameId, classPK, feedClassNameId, feedClassPK);
+
+		EntitySocialFeed EntitySocialFeed =
+			entitySocialFeedPersistence.fetchByPrimaryKey(pk);
+
+		if (EntitySocialFeed == null) {
+			EntitySocialFeed = entitySocialFeedPersistence.create(pk);
+
+			entitySocialFeedPersistence.update(EntitySocialFeed);
+		}
+	}
+
+	public void addEntitySocialFeeds(
+		long classNameId, long classPK,
+		Map<Long, List<Long>> feedClassNameClassPKs) {
+
+			for (Long feedClassNameId : feedClassNameClassPKs.keySet()) {
+				List<Long> feedClassPKs = feedClassNameClassPKs.get(
+					feedClassNameId);
+
+				if ((feedClassPKs == null) || feedClassPKs.isEmpty()) {
+					continue;
+				}
+
+				for (Long feedClassPK : feedClassPKs) {
+					addEntitySocialFeed(
+						classNameId, classPK, feedClassNameId, feedClassPK);
+				}
+		}
+	}
+
+	public void deleteEntitySocialFeeds(long classNameId, long classPK) {
+		entitySocialFeedPersistence.removeByC_C(classNameId, classPK);
+	}
+
+	public List<EntitySocialFeed> getEntitySocialFeeds(
+		long classNameId, long classPK) {
+
+		return entitySocialFeedPersistence.findByC_C(classNameId, classPK);
+	}
+
 }
