34c79d3f0cfc83a1fc3616b512359306e815eadb
Brian Chan <brian.chan@liferay.com>
2014-09-30 13:13:50 -0700

    LPS-49937 SF

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/KBArticleLocalServiceImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/KBArticleLocalServiceImpl.java
@@ -1831,7 +1831,6 @@ public class KBArticleLocalServiceImpl extends KBArticleLocalServiceBaseImpl {
 
 		long kbArticleClassNameId = classNameLocalService.getClassNameId(
 			KBArticleConstants.getClassName());
-
 		long kbFolderClassNameId = classNameLocalService.getClassNameId(
 			KBFolderConstants.getClassName());
 
@@ -1840,7 +1839,8 @@ public class KBArticleLocalServiceImpl extends KBArticleLocalServiceBaseImpl {
 
 			throw new KBArticleParentException(
 				String.format(
-					"Invalid parent with classNameId %s and primary key %s",
+					"Invalid parent with resource class name ID %s and " +
+						"resource primary key %s",
 					resourceClassNameId, resourcePrimKey));
 		}
 	}
