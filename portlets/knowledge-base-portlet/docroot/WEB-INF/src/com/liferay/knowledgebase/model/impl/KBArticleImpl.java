bafccfb442ff3a191452cb736675dbff709b391f
adolfo.perez <adolfo.perez@liferay.com>
2014-10-01 12:04:28 +0200

    LPS-49937 get articles from current folder
    When showing the navigation, show the articles in the current folder.

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/model/impl/KBArticleImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/model/impl/KBArticleImpl.java
@@ -104,6 +104,24 @@ public class KBArticleImpl extends KBArticleBaseImpl {
 	}
 
 	@Override
+	public long getKBFolderId() throws PortalException {
+		if (getParentResourcePrimKey() ==
+				KBFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+
+			return getParentResourcePrimKey();
+		}
+
+		KBArticle kbArticle = this;
+
+		while (kbArticle.getParentResourceClassNameId() == getClassNameId()) {
+			kbArticle = KBArticleServiceUtil.getLatestKBArticle(
+				kbArticle.getParentResourcePrimKey(),
+				WorkflowConstants.STATUS_ANY);
+		}
+
+		return kbArticle.getParentResourcePrimKey();
+	}
+
 	public KBArticle getParentKBArticle() throws PortalException {
 		long parentResourcePrimKey = getParentResourcePrimKey();
 
