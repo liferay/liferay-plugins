643d293bafddbc1d454e07fb9a6d9c268d50fddd
Jonathan Lee <Jonathan.Lee@liferay.com>
2014-10-02 09:38:30 -0700

    LRDCOM-11352 Remove duplicate sql

--- portlets/notifications-portlet/docroot/WEB-INF/sql/tables.sql
+++ portlets/notifications-portlet/docroot/WEB-INF/sql/tables.sql
@@ -1,14 +1,3 @@
-create table Notification_UserNotificationEvent (
-	notificationEventId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	userNotificationEventId LONG,
-	timestamp LONG,
-	delivered BOOLEAN,
-	actionRequired BOOLEAN,
-	archived BOOLEAN
-);
-
 create table Notifications_UserNotificationEvent (
 	notificationEventId LONG not null primary key,
 	companyId LONG,
