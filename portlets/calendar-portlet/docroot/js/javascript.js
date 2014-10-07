b90d3ad98c2ef4ac5b5bf69b5edd1526a71796a2
Brian Chan <brian.chan@liferay.com>
2014-10-07 10:55:08 -0700

    LPS-50508 fixed typo on editing invited calendar event

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -1693,7 +1693,7 @@ AUI.add(
 							var content = [
 								'<p class="calendar-portlet-confirmation-text">',
 								Lang.sub(
-									Liferay.Language.get('you-are-about-to-make-changes-that-will-only-effect-your-calendar-x'),
+									Liferay.Language.get('you-are-about-to-make-changes-that-will-only-affect-your-calendar-x'),
 									[LString.escapeHTML(calendar.get('name'))]
 								),
 								'</p>'
