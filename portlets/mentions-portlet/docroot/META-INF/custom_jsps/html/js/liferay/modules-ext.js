0ea7e9d0f2c166051fce96cfee5e88c5c592f67e
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-10-09 10:03:08 +0200

    LPS-50711 Define the previous modules so the portal can use them

---
+++ portlets/mentions-portlet/docroot/META-INF/custom_jsps/html/js/liferay/modules-ext.js
@@ -0,0 +1,75 @@
+var A = AUI();
+
+A.applyConfig(
+	{
+		modules: {
+			'liferay-autocomplete-input': {
+				path: 'autocomplete_input.js',
+				requires: [
+					'aui-base',
+					'autocomplete',
+					'autocomplete-filters',
+					'autocomplete-highlighters'
+				]
+			},
+			'liferay-autocomplete-input-caretindex': {
+				condition: {
+					name: 'liferay-autocomplete-input-caretindex',
+					test: function() {
+						return SUPPORTS_INPUT_SELECTION;
+					},
+					trigger: 'liferay-autocomplete-textarea'
+				},
+				path: 'autocomplete_input_caretindex.js',
+				requires: [
+					'liferay-autocomplete-textarea'
+				]
+			},
+			'liferay-autocomplete-input-caretindex-sel': {
+				condition: {
+					name: 'liferay-autocomplete-input-caretindex-sel',
+					test: function() {
+						return !SUPPORTS_INPUT_SELECTION;
+					},
+					trigger: 'liferay-autocomplete-textarea'
+				},
+				path: 'autocomplete_input_caretindex_sel.js',
+				requires: [
+					'liferay-autocomplete-textarea'
+				]
+			},
+			'liferay-autocomplete-input-caretoffset': {
+				condition: {
+					name: 'liferay-autocomplete-input-caretoffset',
+					test: function(A) {
+						return !(A.UA.ie && A.UA.ie < 9);
+					},
+					trigger: 'liferay-autocomplete-textarea'
+				},
+				path: 'autocomplete_input_caretoffset.js',
+				requires: [
+					'liferay-autocomplete-textarea'
+				]
+			},
+			'liferay-autocomplete-input-caretoffset-sel': {
+				condition: {
+					name: 'liferay-autocomplete-input-caretoffset-sel',
+					test: function(A) {
+						return (A.UA.ie && A.UA.ie < 9);
+					},
+					trigger: 'liferay-autocomplete-textarea'
+				},
+				path: 'autocomplete_input_caretoffset_sel.js',
+				requires: [
+					'liferay-autocomplete-textarea'
+				]
+			},
+			'liferay-autocomplete-textarea': {
+				path: 'autocomplete_textarea.js',
+				requires: [
+					'liferay-autocomplete-input'
+				]
+			}
+		}
+	}
+);
