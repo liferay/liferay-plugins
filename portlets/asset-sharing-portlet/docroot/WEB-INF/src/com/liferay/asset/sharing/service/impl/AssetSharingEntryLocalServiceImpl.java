8dc4449e511cdb81d6a8479d033ea3b886058619
Brian Chan <brian.chan@liferay.com>
2014-10-10 15:36:41 -0700

    SOS-2465 SF

--- portlets/asset-sharing-portlet/docroot/WEB-INF/src/com/liferay/asset/sharing/service/impl/AssetSharingEntryLocalServiceImpl.java
+++ portlets/asset-sharing-portlet/docroot/WEB-INF/src/com/liferay/asset/sharing/service/impl/AssetSharingEntryLocalServiceImpl.java
@@ -17,24 +17,8 @@ package com.liferay.asset.sharing.service.impl;
 import com.liferay.asset.sharing.service.base.AssetSharingEntryLocalServiceBaseImpl;
 
 /**
- * The implementation of the asset sharing entry local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.asset.sharing.service.AssetSharingEntryLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see com.liferay.asset.sharing.service.base.AssetSharingEntryLocalServiceBaseImpl
- * @see com.liferay.asset.sharing.service.AssetSharingEntryLocalServiceUtil
  */
 public class AssetSharingEntryLocalServiceImpl
 	extends AssetSharingEntryLocalServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this interface directly. Always use {@link com.liferay.asset.sharing.service.AssetSharingEntryLocalServiceUtil} to access the asset sharing entry local service.
-	 */
 }
--- portlets/asset-sharing-portlet/docroot/WEB-INF/src/com/liferay/asset/sharing/service/impl/AssetSharingEntryServiceImpl.java
+++ portlets/asset-sharing-portlet/docroot/WEB-INF/src/com/liferay/asset/sharing/service/impl/AssetSharingEntryServiceImpl.java
@@ -17,24 +17,8 @@ package com.liferay.asset.sharing.service.impl;
 import com.liferay.asset.sharing.service.base.AssetSharingEntryServiceBaseImpl;
 
 /**
- * The implementation of the asset sharing entry remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.asset.sharing.service.AssetSharingEntryService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see com.liferay.asset.sharing.service.base.AssetSharingEntryServiceBaseImpl
- * @see com.liferay.asset.sharing.service.AssetSharingEntryServiceUtil
  */
 public class AssetSharingEntryServiceImpl
 	extends AssetSharingEntryServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this interface directly. Always use {@link com.liferay.asset.sharing.service.AssetSharingEntryServiceUtil} to access the asset sharing entry remote service.
-	 */
 }
