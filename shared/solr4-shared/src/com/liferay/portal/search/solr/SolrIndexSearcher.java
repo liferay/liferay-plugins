1144f71da13708961ec3fc40dd6685822f7ef6c2
Michael Han <michael.han@liferay.com>
2014-10-14 15:48:41 -0500

    LPS-50857 always fetch UID

--- shared/solr4-shared/src/com/liferay/portal/search/solr/SolrIndexSearcher.java
+++ shared/solr4-shared/src/com/liferay/portal/search/solr/SolrIndexSearcher.java
@@ -39,6 +39,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -48,6 +49,7 @@ import com.liferay.portal.search.solr.facet.SolrFacetFieldCollector;
 import com.liferay.portal.search.solr.facet.SolrFacetQueryCollector;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -256,9 +258,16 @@ public class SolrIndexSearcher extends BaseIndexSearcher {
 			return;
 		}
 
-		String[] selectedFieldNames = queryConfig.getSelectedFieldNames();
+		Set<String> selectedFieldNamesSet = SetUtil.fromArray(
+			queryConfig.getSelectedFieldNames());
 
-		solrQuery.setFields(selectedFieldNames);
+		if (!selectedFieldNamesSet.contains(Field.UID)) {
+			selectedFieldNamesSet.add(Field.UID);
+		}
+
+		solrQuery.setFields(
+			selectedFieldNamesSet.toArray(
+				new String[selectedFieldNamesSet.size()]));
 	}
 
 	protected void addSnippets(
