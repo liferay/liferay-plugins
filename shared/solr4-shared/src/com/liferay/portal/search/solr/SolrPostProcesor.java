8a7b31db022ad9d13e8a7be9707e8be884b8937a
Brian Chan <brian.chan@liferay.com>
2014-10-22 19:15:03 -0700

    LPS-51031 make it private

--- shared/solr4-shared/src/com/liferay/portal/search/solr/SolrPostProcesor.java
+++ shared/solr4-shared/src/com/liferay/portal/search/solr/SolrPostProcesor.java
@@ -25,9 +25,10 @@ import java.util.regex.Pattern;
 public class SolrPostProcesor {
 
 	public SolrPostProcesor(String query, String keywords) {
-		this.query = query;
-		this.keywords = keywords;
-		sb = new StringBuilder(query.length());
+		_query = query;
+		_keywords = keywords;
+
+		_sb = new StringBuilder(query.length());
 	}
 
 	public String postProcess() {
@@ -37,35 +38,35 @@ public class SolrPostProcesor {
 
 		appendRemainder();
 
-		return sb.toString();
+		return _sb.toString();
 	}
 
 	protected void appendPhrase() {
-		String before = query.substring(index, firstQuoteIndex);
+		String before = _query.substring(_index, firstQuoteIndex);
 
-		sb.append(before);
+		_sb.append(before);
 
-		index = secondQuoteIndex + 1;
+		_index = _secondQuoteIndex + 1;
 
-		String phrase = query.substring(firstQuoteIndex, index);
+		String phrase = _query.substring(firstQuoteIndex, _index);
 
-		if (questionMark) {
+		if (_questionMark) {
 			String regex = buildRegex(phrase);
 
 			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(keywords);
+			Matcher matcher = pattern.matcher(_keywords);
 
 			if (matcher.find()) {
-				sb.append(matcher.group());
+				_sb.append(matcher.group());
 				return;
 			}
 		}
 
-		sb.append(phrase);
+		_sb.append(phrase);
 	}
 
 	protected void appendRemainder() {
-		sb.append(query.substring(index));
+		_sb.append(_query.substring(_index));
 	}
 
 	protected String buildRegex(String phrase) {
@@ -94,37 +95,38 @@ public class SolrPostProcesor {
 	}
 
 	protected boolean findPhrase() {
-		firstQuoteIndex = query.indexOf(StringPool.QUOTE, index);
+		firstQuoteIndex = _query.indexOf(StringPool.QUOTE, _index);
 
 		if (firstQuoteIndex == -1) {
 			return false;
 		}
 
-		secondQuoteIndex = query.indexOf(StringPool.QUOTE, firstQuoteIndex + 1);
+		_secondQuoteIndex = _query.indexOf(
+			StringPool.QUOTE, firstQuoteIndex + 1);
 
-		if (secondQuoteIndex == -1) {
+		if (_secondQuoteIndex == -1) {
 			return false;
 		}
 
-		int questionMarkIndex = query.indexOf(
+		int questionMarkIndex = _query.indexOf(
 			StringPool.QUESTION, firstQuoteIndex);
 
 		if (questionMarkIndex == -1) {
-			questionMark = false;
+			_questionMark = false;
 		}
 		else {
-			questionMark = true;
+			_questionMark = true;
 		}
 
 		return true;
 	}
 
-	protected int firstQuoteIndex;
-	protected int index;
-	protected String keywords;
-	protected String query;
-	protected boolean questionMark;
-	protected StringBuilder sb;
-	protected int secondQuoteIndex;
+	private int firstQuoteIndex;
+	private int _index;
+	private String _keywords;
+	private String _query;
+	private boolean _questionMark;
+	private StringBuilder _sb;
+	private int _secondQuoteIndex;
 
 }
