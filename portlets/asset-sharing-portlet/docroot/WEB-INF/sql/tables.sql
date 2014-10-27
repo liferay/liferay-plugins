04c5829aa1fc1662e8fdcc95f3b647091df289e1
Brian Chan <brian.chan@liferay.com>
2014-10-27 11:33:20 -0700

    Frontport later

--- portlets/asset-sharing-portlet/docroot/WEB-INF/sql/tables.sql
+++
@@ -1,7 +0,0 @@
-create table AssetSharing_AssetSharingEntry (
-	classNameId LONG not null,
-	classPK LONG not null,
-	sharedToClassNameId LONG not null,
-	sharedToClassPK LONG not null,
-	primary key (classNameId, classPK, sharedToClassNameId, sharedToClassPK)
-);
