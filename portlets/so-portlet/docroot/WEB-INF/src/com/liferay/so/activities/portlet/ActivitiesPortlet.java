214a57c60684f9dff560ea7a26a3875586c0e0d6
matthewkong <matthew.kong@liferay.com>
2014-10-28 11:37:26 -0700

    SOS-2501 Apply to so-portlet

--- portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/activities/portlet/ActivitiesPortlet.java
+++ portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/activities/portlet/ActivitiesPortlet.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
@@ -49,11 +50,12 @@ import com.liferay.portlet.messageboards.util.comparator.MessageCreateDateCompar
 import com.liferay.portlet.social.model.SocialActivitySet;
 import com.liferay.portlet.social.service.SocialActivitySetLocalServiceUtil;
 import com.liferay.so.activities.util.ActivitiesUtil;
-import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import java.io.IOException;
+
 import java.text.DateFormat;
 import java.text.Format;
+
 import java.util.Date;
 import java.util.List;
 
@@ -192,13 +194,12 @@ public class ActivitiesPortlet extends MVCPortlet {
 			MicroblogsEntryLocalServiceUtil.getMicroblogsEntry(
 				microblogsEntryId);
 
-		ServiceContext serviceContext =
-			ServiceContextFactory.getInstance(
-				MicroblogsEntry.class.getName(), actionRequest);
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			MicroblogsEntry.class.getName(), actionRequest);
 
 		MicroblogsEntryServiceUtil.addMicroblogsEntry(
 			themeDisplay.getUserId(), microblogsEntry.getContent(),
-			MicroblogsEntryConstants.TYPE_REPOST, microblogsEntry.getUserId(),
+			MicroblogsEntryConstants.TYPE_REPOST,
 			microblogsEntry.getMicroblogsEntryId(),
 			microblogsEntry.getSocialRelationType(), serviceContext);
 	}
@@ -356,8 +357,7 @@ public class ActivitiesPortlet extends MVCPortlet {
 					microblogsEntry =
 						MicroblogsEntryServiceUtil.addMicroblogsEntry(
 							themeDisplay.getUserId(), body,
-							MicroblogsEntryConstants.TYPE_REPLY,
-							currentMicroblogsEntry.getUserId(), classPK,
+							MicroblogsEntryConstants.TYPE_REPLY, classPK,
 							currentMicroblogsEntry.getSocialRelationType(),
 							serviceContext);
 				}
