80a0fe05fcf67d98050782326b581a704f95db52
johnnyduong <johnny.duong@liferay.com>
2014-10-30 09:30:05 -0700

    LPS-51277 Use response.getWriter() to write the response

--- shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/BaseAlloyControllerImpl.java
+++ shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/BaseAlloyControllerImpl.java
@@ -129,21 +129,6 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 		registerAlloyController();
 	}
 
-	public void afterPropertiesSet(
-		PortletRequest portletRequest, PortletResponse portletResponse) {
-
-		initClass();
-		initServletVariables(portletRequest, portletResponse);
-		initPortletVariables();
-		initThemeDisplayVariables();
-		initMethods();
-		initPaths();
-		initIndexer();
-		initMessageListeners();
-
-		registerAlloyController();
-	}
-
 	@Override
 	public void execute() throws Exception {
 		Method method = getMethod(actionPath);
@@ -810,13 +795,6 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 		response = (HttpServletResponse)pageContext.getResponse();
 	}
 
-	protected void initServletVariables(
-		PortletRequest portletRequest, PortletResponse portletResponse) {
-
-		request = PortalUtil.getHttpServletRequest(portletRequest);
-		response = PortalUtil.getHttpServletResponse(portletResponse);
-	}
-
 	protected void initThemeDisplayVariables() {
 		themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
@@ -1119,9 +1097,7 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 		HttpServletResponse response = PortalUtil.getHttpServletResponse(
 			portletResponse);
 
-		response.setContentType(ContentTypes.APPLICATION_JSON);
-
-		ServletResponseUtil.write(response, json.toString());
+		response.getWriter().write(json.toString());
 
 		response.flushBuffer();
 	}
