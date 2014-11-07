d5661a31fb98b62b06b7a9f87480114dac267661
Dale Shan <dale.shan@liferay.com>
2014-11-07 15:59:43 +0800

    LPS-51405

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/notification/NotificationTemplateContextFactory.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/notification/NotificationTemplateContextFactory.java
@@ -26,9 +26,11 @@ import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
 
@@ -149,10 +151,11 @@ public class NotificationTemplateContextFactory {
 			User user, long calendarBookingId)
 		throws PortalException {
 
-		Group group = user.getGroup();
+		Group group = GroupLocalServiceUtil.getGroup(
+			user.getCompanyId(), GroupConstants.GUEST);
 
-		Layout layout = LayoutLocalServiceUtil.getLayout(
-			group.getDefaultPrivatePlid());
+		Layout layout = LayoutLocalServiceUtil.fetchLayout(
+			group.getDefaultPublicPlid());
 
 		String portalURL = _getPortalURL(
 			group.getCompanyId(), group.getGroupId());
