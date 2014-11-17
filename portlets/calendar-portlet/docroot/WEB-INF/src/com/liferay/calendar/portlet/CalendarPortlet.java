3fd06232ed3051239d32c2925eae31a1c85a4186
Hugo Huijser <hugo.huijser@liferay.com>
2014-11-17 15:33:13 +0800

    LPS-51402 Source formatting

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/portlet/CalendarPortlet.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/portlet/CalendarPortlet.java
@@ -101,6 +101,7 @@ import java.io.File;
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -772,12 +773,9 @@ public class CalendarPortlet extends MVCPortlet {
 					new PositionalWeekday(weekday, position));
 
 				if (frequency == Frequency.YEARLY) {
-					int month = ParamUtil.getInteger(
-						actionRequest, "startTimeMonth");
+					List<Integer> months = Arrays.asList(
+						ParamUtil.getInteger(actionRequest, "startTimeMonth"));
 
-					List<Integer> months = new ArrayList<Integer>();
-
-					months.add(month);
 					recurrence.setMonths(months);
 				}
 			}
