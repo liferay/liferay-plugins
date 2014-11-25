7bb514ec6418fb4608fd41c595d4a25db129f0be
CalvinKeum <Calvin.Keum@liferay.com>
2014-11-25 09:27:17 -0800

    LRDCOM-12837 Adding service methods for AssetEntrySet

--- portlets/asset-entry-set-portlet/docroot/WEB-INF/src/com/liferay/asset/entry/set/service/impl/AssetEntrySetLocalServiceImpl.java
+++ portlets/asset-entry-set-portlet/docroot/WEB-INF/src/com/liferay/asset/entry/set/service/impl/AssetEntrySetLocalServiceImpl.java
@@ -14,11 +14,209 @@
 
 package com.liferay.asset.entry.set.service.impl;
 
+import com.liferay.asset.entry.set.model.AssetEntrySet;
 import com.liferay.asset.entry.set.service.base.AssetEntrySetLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+
+import java.util.Date;
+import java.util.List;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Calvin Keum
  */
 public class AssetEntrySetLocalServiceImpl
 	extends AssetEntrySetLocalServiceBaseImpl {
+
+	public AssetEntrySet addAssetEntrySet(
+			long userId, long parentAssetEntrySetId, long creatorClassNameId,
+			long creatorClassPK, String content, int type,
+			int socialRelationType, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		User user = userPersistence.findByPrimaryKey(userId);
+
+		Date now = new Date();
+
+		long assetEntrySetId = counterLocalService.increment();
+
+		AssetEntrySet assetEntrySet = assetEntrySetPersistence.create(
+			assetEntrySetId);
+
+		assetEntrySet.setCompanyId(user.getCompanyId());
+		assetEntrySet.setUserId(user.getUserId());
+		assetEntrySet.setUserName(user.getFullName());
+		assetEntrySet.setCreateTime(now.getTime());
+		assetEntrySet.setModifiedTime(now.getTime());
+		assetEntrySet.setParentAssetEntrySetId(parentAssetEntrySetId);
+		assetEntrySet.setCreatorClassNameId(creatorClassNameId);
+		assetEntrySet.setCreatorClassPK(creatorClassPK);
+		assetEntrySet.setContent(content);
+
+		assetEntrySetPersistence.update(assetEntrySet);
+
+		updateAsset(
+			assetEntrySet, serviceContext.getAssetCategoryIds(),
+			serviceContext.getAssetTagNames());
+
+		return assetEntrySet;
+	}
+
+	@Override
+	public AssetEntrySet deleteAssetEntrySet(AssetEntrySet assetEntrySet)
+		throws PortalException, SystemException {
+
+		assetEntrySetPersistence.remove(assetEntrySet);
+
+		AssetEntryLocalServiceUtil.deleteEntry(
+			AssetEntrySet.class.getName(), assetEntrySet.getAssetEntrySetId());
+
+		return assetEntrySet;
+	}
+
+	@Override
+	public AssetEntrySet deleteAssetEntrySet(long assetEntrySetId)
+		throws PortalException, SystemException {
+
+		AssetEntrySet assetEntrySet = assetEntrySetPersistence.findByPrimaryKey(
+			assetEntrySetId);
+
+		return deleteAssetEntrySet(assetEntrySet);
+	}
+
+	@Override
+	public AssetEntrySet getAssetEntrySet(long assetEntrySetId)
+		throws PortalException, SystemException {
+
+		return assetEntrySetPersistence.findByPrimaryKey(assetEntrySetId);
+	}
+
+	public AssetEntrySet getAssetEntrySet(
+			long parentAssetEntrySetId, long creatorClassNameId,
+			long creatorClassPK, int start, int end)
+		throws PortalException, SystemException {
+
+		return assetEntrySetPersistence.findByPAESI_CCNI_CCPK(
+			parentAssetEntrySetId, creatorClassNameId, creatorClassPK);
+	}
+
+	public List<AssetEntrySet> getAssetEntrySets(
+			long parentAssetEntrySetId, long creatorClassNameId, int type,
+			int start, int end, OrderByComparator obc)
+		throws SystemException {
+
+		return assetEntrySetPersistence.findByPAESI_CCNI(
+			parentAssetEntrySetId, creatorClassNameId, start, end, obc);
+	}
+
+	public List<AssetEntrySet> getAssetEntrySets(
+			long creatorClassNameId, long creatorClassPK, String assetTagName,
+			boolean andOperator, int start, int end)
+		throws SystemException {
+
+		return assetEntrySetFinder.findByCCNI_CCPK_ATN(
+			creatorClassNameId, creatorClassPK, assetTagName, andOperator,
+			start, end);
+	}
+
+	public List<AssetEntrySet> getAssetEntrySets(
+			long creatorClassNameId, String assetTagName, int start, int end)
+		throws SystemException {
+
+		return assetEntrySetFinder.findByCCNI_ATN(
+			creatorClassNameId, assetTagName, start, end);
+	}
+
+	public int getAssetEntrySetsCount(
+			long parentAssetEntrySetId, long creatorClassNameId)
+		throws SystemException {
+
+		return assetEntrySetPersistence.countByPAESI_CCNI(
+			parentAssetEntrySetId, creatorClassNameId);
+	}
+
+	public int getAssetEntrySetsCount(
+			long creatorClassNameId, long creatorClassPK,
+			long parentAssetEntrySetId)
+		throws SystemException {
+
+		return assetEntrySetPersistence.countByPAESI_CCNI_CCPK(
+			parentAssetEntrySetId, creatorClassNameId, creatorClassPK);
+	}
+
+	public int getAssetEntrySetsCount(
+			long creatorClassNameId, long creatorClassPK, String assetTagName,
+			boolean andOperator)
+		throws SystemException {
+
+		return assetEntrySetFinder.countByCCNI_CCPK_ATN(
+			creatorClassNameId, creatorClassPK, assetTagName, andOperator);
+	}
+
+	public int getAssetEntrySetsCount(
+			long creatorClassNameId, String assetTagName)
+		throws SystemException {
+
+		return assetEntrySetFinder.countByCCNI_ATN(
+			creatorClassNameId, assetTagName);
+	}
+
+	public List<AssetEntrySet> getParentAssetEntrySetAssetEntrySets(
+			long parentAssetEntrySetId, int start, int end,
+			OrderByComparator orderByComparator)
+		throws SystemException {
+
+		return assetEntrySetPersistence.findByParentAssetEntrySetId(
+			parentAssetEntrySetId, start, end, orderByComparator);
+	}
+
+	public int getParentAssetEntrySetAssetEntrySetsCount(
+			long parentAssetEntrySetId)
+		throws SystemException {
+
+		return assetEntrySetPersistence.countByParentAssetEntrySetId(
+			parentAssetEntrySetId);
+	}
+
+	public void updateAsset(
+			AssetEntrySet assetEntrySet, long[] assetCategoryIds,
+			String[] assetTagNames)
+		throws PortalException, SystemException {
+
+		Group group = GroupLocalServiceUtil.getCompanyGroup(
+			assetEntrySet.getCompanyId());
+
+		AssetEntryLocalServiceUtil.updateEntry(
+			assetEntrySet.getUserId(), group.getGroupId(),
+			AssetEntrySet.class.getName(), assetEntrySet.getAssetEntrySetId(),
+			assetCategoryIds, assetTagNames);
+	}
+
+	public AssetEntrySet updateAssetEntrySet(
+			long assetEntrySetId, String content, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		AssetEntrySet assetEntrySet = assetEntrySetPersistence.findByPrimaryKey(
+			assetEntrySetId);
+
+		Date now = new Date();
+
+		assetEntrySet.setModifiedTime(now.getTime());
+		assetEntrySet.setContent(content);
+
+		assetEntrySetPersistence.update(assetEntrySet);
+
+		updateAsset(
+			assetEntrySet, serviceContext.getAssetCategoryIds(),
+			serviceContext.getAssetTagNames());
+
+		return assetEntrySet;
+	}
+
 }
