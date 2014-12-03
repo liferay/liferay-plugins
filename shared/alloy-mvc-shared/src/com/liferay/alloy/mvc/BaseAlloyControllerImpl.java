98e9fa9b079201a6c27b7e9198bedbe20c270ab9
Brian Chan <brian.chan@liferay.com>
2014-12-02 16:30:19 -0800

    LPS-51719 SF

--- shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/BaseAlloyControllerImpl.java
+++ shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/BaseAlloyControllerImpl.java
@@ -316,7 +316,7 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 			else {
 				jsonObject.put(
 					"data",
-						JSONFactoryUtil.createJSONObject(String.valueOf(data)));
+					JSONFactoryUtil.createJSONObject(String.valueOf(data)));
 			}
 
 			jsonObject.put("message", message);
