53044ef19eb3d16b90f7f3372c5af1c6686e7c3a
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-12-08 10:59:17 -0800

    LPS-51509 SF

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarResourceUtil.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarResourceUtil.java
@@ -76,14 +76,21 @@ public class CalendarResourceUtil {
 			return null;
 		}
 
-		long classNameId = PortalUtil.getClassNameId(Group.class);
-
 		CalendarResource calendarResource =
 			CalendarResourceLocalServiceUtil.fetchCalendarResource(
-				classNameId, groupId);
+				PortalUtil.getClassNameId(Group.class), groupId);
 
 		if (calendarResource != null) {
 			return calendarResource;
+		}
+
+		long userId = group.getCreatorUserId();
+
+		User user = UserLocalServiceUtil.fetchUserById(userId);
+
+		if (user == null) {
+			userId = UserLocalServiceUtil.getDefaultUserId(
+				group.getCompanyId());
 		}
 
 		Map<Locale, String> nameMap = new HashMap<Locale, String>();
@@ -92,17 +99,9 @@ public class CalendarResourceUtil {
 
 		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
 
-		long userId = group.getCreatorUserId();
-		User user = UserLocalServiceUtil.fetchUserById(userId);
-
-		if (user == null) {
-			userId = UserLocalServiceUtil.getDefaultUserId(
-				group.getCompanyId());
-		}
-
 		return CalendarResourceLocalServiceUtil.addCalendarResource(
-			userId, groupId, PortalUtil.getClassNameId(Group.class),
-			groupId, null, null, nameMap, descriptionMap, true, serviceContext);
+			userId, groupId, PortalUtil.getClassNameId(Group.class), groupId,
+			null, null, nameMap, descriptionMap, true, serviceContext);
 	}
 
 	public static CalendarResource getGroupCalendarResource(
