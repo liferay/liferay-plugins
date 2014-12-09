4a10104189a33f6740efd9a8fab455d58cf311a1
adolfo.perez <adolfo.perez@liferay.com>
2014-12-09 15:10:35 +0100

    LPS-52089 show navigation when folders available

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/KBNavigationDisplayContext.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/KBNavigationDisplayContext.java
@@ -133,9 +133,23 @@ public class KBNavigationDisplayContext {
 	}
 
 	public boolean isShowNavigation() throws PortalException {
-		boolean showNavigation = true;
-
 		long scopeGroupId = PortalUtil.getScopeGroupId(_portletRequest);
+
+		long kbFolderClassNameId = PortalUtil.getClassNameId(
+			KBFolderConstants.getClassName());
+
+		if (getResourceClassNameId() == kbFolderClassNameId) {
+			List<KBFolder> kbFolders = KnowledgeBaseUtil.
+				getAlternateRootKBFolders(
+					PortalUtil.getScopeGroupId(_portletRequest),
+					getResourcePrimKey());
+
+			if (kbFolders.size() > 1) {
+				return true;
+			}
+		}
+
+		boolean showNavigation = true;
 
 		long rootResourcePrimKey = getRootResourcePrimKey();
 
