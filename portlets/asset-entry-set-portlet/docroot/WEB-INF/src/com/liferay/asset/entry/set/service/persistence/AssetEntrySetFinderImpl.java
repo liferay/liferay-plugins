f6f1c938f184f709d14d30fe373685418f12b8f1
sherryre <sherry.yang@liferay.com>
2014-12-09 14:54:34 -0800

    LRDCOM-12839 Copy the code AssetSharingFinderImpl over

--- portlets/asset-entry-set-portlet/docroot/WEB-INF/src/com/liferay/asset/entry/set/service/persistence/AssetEntrySetFinderImpl.java
+++ portlets/asset-entry-set-portlet/docroot/WEB-INF/src/com/liferay/asset/entry/set/service/persistence/AssetEntrySetFinderImpl.java
@@ -22,6 +22,8 @@ import com.liferay.portal.kernel.dao.orm.SQLQuery;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.dao.orm.Type;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
 import com.liferay.util.dao.orm.CustomSQLUtil;
 
@@ -41,11 +43,17 @@ public class AssetEntrySetFinderImpl
 	public static final String COUNT_BY_CCNI_CCPK_ATN =
 		AssetEntrySetFinder.class.getName() + ".countByCCNI_CCPK_ATN";
 
+	public static final String COUNT_BY_USER_ID =
+		AssetSharingEntryFinder.class.getName() + ".countByUserId";
+
 	public static final String FIND_BY_CCNI_ATN =
 		AssetEntrySetFinder.class.getName() + ".findByCCNI_ATN";
 
 	public static final String FIND_BY_CCNI_CCPK_ATN =
 		AssetEntrySetFinder.class.getName() + ".findByCCNI_CCPK_ATN";
+
+	public static final String FIND_BY_USER_ID =
+		AssetSharingEntryFinder.class.getName() + ".findByUserId";
 
 	public int countByCCNI_ATN(long creatorClassNameId, String assetTagName)
 		throws SystemException {
@@ -109,6 +117,54 @@ public class AssetEntrySetFinderImpl
 			qPos.add(creatorClassNameId);
 			qPos.add(creatorClassPK);
 			qPos.add(assetTagName);
+
+			Iterator<Long> itr = q.iterate();
+
+			if (itr.hasNext()) {
+				Long count = itr.next();
+
+				if (count != null) {
+					return count.intValue();
+				}
+			}
+
+			return 0;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public int countByUserId(
+			long userId, long[] classNameIds, 
+			Map<Long, long[]> sharedToClassPKsMap)
+		throws SystemException {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(COUNT_BY_USER_ID);
+
+			sql = StringUtil.replace(
+				sql, "[$CLASS_NAME_IDS]", getClassNameIds(classNameIds));
+			sql = StringUtil.replace(
+				sql, "[$SHARED_TO_CLASS_PKS_MAP]",
+				getSharedToClassPKsMap(sharedToClassPKsMap));
+
+			SQLQuery q = session.createSQLQuery(sql);
+
+			q.addScalar(COUNT_COLUMN_NAME, Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			setClassNameIds(qPos, userId, classNameIds);
+			setSharedToClassPKsMap(qPos, userId, sharedToClassPKsMap);
 
 			Iterator<Long> itr = q.iterate();
 
@@ -195,5 +251,260 @@ public class AssetEntrySetFinderImpl
 			closeSession(session);
 		}
 	}
+
+	@Override
+	public List<Object[]> findByUserId(
+			long userId, long[] classNameIds,
+			Map<Long, long[]> sharedToClassPKsMap, int start, int end)
+		throws SystemException {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(FIND_BY_USER_ID);
+
+			sql = StringUtil.replace(
+				sql, "[$CLASS_NAME_IDS]", getClassNameIds(classNameIds));
+			sql = StringUtil.replace(
+				sql, "[$SHARED_TO_CLASS_PKS_MAP]",
+				getSharedToClassPKsMap(sharedToClassPKsMap));
+
+			SQLQuery q = session.createSQLQuery(sql);
+
+			q.addScalar("classNameId", Type.LONG);
+			q.addScalar("classPK", Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			setClassNameIds(qPos, userId, classNameIds);
+			setSharedToClassPKsMap(qPos, userId, sharedToClassPKsMap);
+
+			return (List<Object[]>)QueryUtil.list(q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	protected String getClassNameIds(long[] classNameIds) {
+		if ((classNameIds == null) || (classNameIds.length == 0)) {
+			return StringPool.SPACE;
+		}
+
+		StringBundler sb = new StringBundler();
+
+		sb.append(StringPool.OPEN_PARENTHESIS);
+
+		for (int i = 0; i < classNameIds.length; i++) {
+			sb.append("(AssetSharing_AssetSharingEntry.classNameId = ?)");
+
+			if ((i + 1) < classNameIds.length) {
+				sb.append(" OR ");
+			}
+		}
+
+		sb.append(") AND");
+
+		return sb.toString();
+	}
+
+	protected String getSharedToClassPKs(long[] sharedToClassPKs) {
+		StringBundler sb = new StringBundler();
+
+		sb.append(_SHARED_TO_CLASS_NAME_ID_SQL);
+		sb.append(" AND (");
+
+		for (int i = 0; i < sharedToClassPKs.length; i++) {
+			sb.append(_SHARED_TO_CLASS_PK_SQL);
+
+			if ((i + 1) < sharedToClassPKs.length) {
+				sb.append(" OR ");
+			}
+			else {
+				sb.append(StringPool.CLOSE_PARENTHESIS);
+			}
+		}
+
+		return sb.toString();
+	}
+
+	protected String getSharedToClassPKsMap(
+		Map<Long, long[]> sharedToClassPKsMap) {
+
+		StringBundler sb = new StringBundler();
+
+		sb.append(StringPool.OPEN_PARENTHESIS);
+
+		if (sharedToClassPKsMap != null) {
+			for (Map.Entry<Long, long[]> entry :
+					sharedToClassPKsMap.entrySet()) {
+
+				Long sharedToClassNameId = entry.getKey();
+				long[] sharedToClassPKs = entry.getValue();
+
+				if ((sharedToClassPKs == null) ||
+					(sharedToClassPKs.length == 0)) {
+
+					continue;
+				}
+
+				sb.append(StringPool.OPEN_PARENTHESIS);
+
+				if (sharedToClassNameId == _SOCIAL_RELATION_CLASS_NAME_ID) {
+					sb.append(getSocialRelationClassPKs(sharedToClassPKs));
+				}
+				else {
+					sb.append(getSharedToClassPKs(sharedToClassPKs));
+				}
+
+				sb.append(") OR ");
+			}
+		}
+
+		sb.append("(SocialActivitySet.userId = ?))");
+
+		return sb.toString();
+	}
+
+	protected String getSocialRelationClassPKs(long[] sharedToClassPKs) {
+		StringBundler sb = null;
+
+		Arrays.sort(sharedToClassPKs);
+
+		if (sharedToClassPKs.length == 1) {
+			if (sharedToClassPKs[0] ==
+					AssetSharingEntryConstants.TYPE_EVERYONE) {
+
+				sb = new StringBundler(3);
+
+				sb.append(_SHARED_TO_CLASS_NAME_ID_SQL);
+				sb.append(" AND ");
+				sb.append(_SHARED_TO_CLASS_PK_SQL);
+			}
+			else {
+				sb = new StringBundler(6);
+
+				sb.append(_SHARED_TO_CLASS_NAME_ID_SQL);
+				sb.append(" AND (");
+				sb.append(_SHARED_TO_CLASS_PK_SQL);
+				sb.append(" AND ");
+				sb.append(_USER_ID_1_SQL);
+				sb.append(StringPool.CLOSE_PARENTHESIS);
+			}
+		}
+		else if (sharedToClassPKs.length == 2) {
+			if (sharedToClassPKs[0] ==
+					AssetSharingEntryConstants.TYPE_EVERYONE) {
+
+				sb = new StringBundler(8);
+
+				sb.append(_SHARED_TO_CLASS_NAME_ID_SQL);
+				sb.append(" AND (");
+				sb.append(_SHARED_TO_CLASS_PK_SQL);
+				sb.append(" OR (");
+				sb.append(_SHARED_TO_CLASS_PK_SQL);
+				sb.append(" AND ");
+				sb.append(_USER_ID_1_SQL);
+				sb.append("))");
+			}
+			else {
+				sb = new StringBundler(8);
+
+				sb.append(_SHARED_TO_CLASS_NAME_ID_SQL);
+				sb.append(" AND ((");
+				sb.append(_SHARED_TO_CLASS_PK_SQL);
+				sb.append(" OR ");
+				sb.append(_SHARED_TO_CLASS_PK_SQL);
+				sb.append(") AND ");
+				sb.append(_USER_ID_1_SQL);
+				sb.append(StringPool.CLOSE_PARENTHESIS);
+			}
+		}
+		else {
+			sb = new StringBundler(10);
+
+			sb.append(_SHARED_TO_CLASS_NAME_ID_SQL);
+			sb.append(" AND (");
+			sb.append(_SHARED_TO_CLASS_PK_SQL);
+			sb.append(" OR ((");
+			sb.append(_SHARED_TO_CLASS_PK_SQL);
+			sb.append(" OR ");
+			sb.append(_SHARED_TO_CLASS_PK_SQL);
+			sb.append(") AND ");
+			sb.append(_USER_ID_1_SQL);
+			sb.append("))");
+		}
+
+		return sb.toString();
+	}
+
+	protected void setClassNameIds(
+		QueryPos qPos, long userId, long[] classNameIds) {
+
+		if (classNameIds != null) {
+			for (long classNameId : classNameIds) {
+				qPos.add(classNameId);
+			}
+		}
+	}
+
+	protected void setSharedToClassPKsMap(
+		QueryPos qPos, long userId, Map<Long, long[]> sharedToClassPKsMap) {
+
+		if (sharedToClassPKsMap != null) {
+			for (Long sharedToClassNameId : sharedToClassPKsMap.keySet()) {
+				qPos.add(sharedToClassNameId);
+
+				long[] sharedToClassPKs = sharedToClassPKsMap.get(
+					sharedToClassNameId);
+
+				if (sharedToClassNameId == _SOCIAL_RELATION_CLASS_NAME_ID) {
+					Arrays.sort(sharedToClassPKs);
+
+					if (sharedToClassPKs.length == 1) {
+						if (sharedToClassPKs[0] ==
+								AssetSharingEntryConstants.TYPE_EVERYONE) {
+
+							qPos.add(sharedToClassPKs[0]);
+						}
+						else {
+							qPos.add(sharedToClassPKs[0]);
+							qPos.add(userId);
+						}
+					}
+					else {
+						for (long sharedToClasPK : sharedToClassPKs) {
+							qPos.add(sharedToClasPK);
+						}
+
+						qPos.add(userId);
+					}
+				}
+				else {
+					for (long sharedToClassPK : sharedToClassPKs) {
+						qPos.add(sharedToClassPK);
+					}
+				}
+			}
+		}
+
+		qPos.add(userId);
+	}
+
+	private static final String _SHARED_TO_CLASS_NAME_ID_SQL =
+		"(AssetSharing_AssetSharingEntry.sharedToClassNameId = ?)";
+
+	private static final String _SHARED_TO_CLASS_PK_SQL =
+		"(AssetSharing_AssetSharingEntry.sharedToClassPK = ?)";
+
+	private static final long _SOCIAL_RELATION_CLASS_NAME_ID =
+		ClassNameLocalServiceUtil.fetchClassNameId(SocialRelation.class);
+
+	private static final String _USER_ID_1_SQL = "(SocialRelation.userId1 = ?)";
 
 }
