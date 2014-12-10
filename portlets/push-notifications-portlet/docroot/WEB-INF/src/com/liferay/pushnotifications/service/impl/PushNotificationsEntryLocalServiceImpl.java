e34477f64551dd0ca5f078164075f1dbb0ccfc5c
Bruno Farache <bruno.farache@liferay.com>
2014-12-10 13:28:58 -0800

    LPS-52022 We must set userJSONObject to the created pushNotifcationsEntry

--- portlets/push-notifications-portlet/docroot/WEB-INF/src/com/liferay/pushnotifications/service/impl/PushNotificationsEntryLocalServiceImpl.java
+++ portlets/push-notifications-portlet/docroot/WEB-INF/src/com/liferay/pushnotifications/service/impl/PushNotificationsEntryLocalServiceImpl.java
@@ -86,7 +86,7 @@ public class PushNotificationsEntryLocalServiceImpl
 				pushNotificationsEntries) {
 
 			JSONObject userJSONObject = createUserJSONObject(
-				pushNotificationsEntry.getUserId());
+				pushNotificationsEntry);
 
 			pushNotificationsEntry.setUser(userJSONObject);
 		}
@@ -123,7 +123,8 @@ public class PushNotificationsEntryLocalServiceImpl
 			PushNotificationsConstants.KEY_PUSH_NOTIFICATIONS_ENTRY_ID,
 			pushNotificationsEntry.getPushNotificationsEntryId());
 
-		JSONObject userJSONObject = createUserJSONObject(fromUserId);
+		JSONObject userJSONObject = createUserJSONObject(
+			pushNotificationsEntry);
 
 		jsonObject.put(PushNotificationsConstants.KEY_USER, userJSONObject);
 
@@ -165,10 +166,13 @@ public class PushNotificationsEntryLocalServiceImpl
 		return pushNotificationsEntry;
 	}
 
-	protected JSONObject createUserJSONObject(long userId)
+	protected JSONObject createUserJSONObject(
+			PushNotificationsEntry pushNotificationsEntry)
 		throws PortalException {
 
 		JSONObject userJSONObject = JSONFactoryUtil.createJSONObject();
+
+		long userId = pushNotificationsEntry.getUserId();
 
 		User user = userLocalService.getUser(userId);
 
@@ -178,6 +182,8 @@ public class PushNotificationsEntryLocalServiceImpl
 			PushNotificationsConstants.KEY_PORTRAIT_ID, user.getPortraitId());
 		userJSONObject.put(PushNotificationsConstants.KEY_USER_ID, userId);
 		userJSONObject.put(PushNotificationsConstants.KEY_UUID, user.getUuid());
+
+		pushNotificationsEntry.setUser(userJSONObject);
 
 		return userJSONObject;
 	}
