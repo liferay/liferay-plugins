b316956b0e7174dbbd104fcf28678c8b25c0afc3
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2015-01-05 16:53:24 -0300

    LPS-52439 Using correct service

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/lar/CalendarPortletDataHandler.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/lar/CalendarPortletDataHandler.java
@@ -158,7 +158,7 @@ public class CalendarPortletDataHandler extends BasePortletDataHandler {
 				NAMESPACE, "calendar-bookings")) {
 
 			ActionableDynamicQuery calendarBookingActionableDynamicQuery =
-				CalendarLocalServiceUtil.getExportActionableDynamicQuery(
+				CalendarBookingLocalServiceUtil.getExportActionableDynamicQuery(
 					portletDataContext);
 
 			calendarBookingActionableDynamicQuery.performActions();
