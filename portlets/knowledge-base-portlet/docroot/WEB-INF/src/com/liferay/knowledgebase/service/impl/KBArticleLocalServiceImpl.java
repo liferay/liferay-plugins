661c866a537fafd55c8af9c6ceb1814730b2f2fe
Jesse Rao <jesse.rao@liferay.com>
2015-01-15 16:51:06 -0500

    LPS-50749 SF Use classNameLocalService.getClassNameId

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/KBArticleLocalServiceImpl.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/service/impl/KBArticleLocalServiceImpl.java
@@ -887,7 +887,7 @@ public class KBArticleLocalServiceImpl extends KBArticleLocalServiceBaseImpl {
 		updatePermissionFields(
 			resourcePrimKey, parentResourceClassNameId, parentResourcePrimKey);
 
-		long kbFolderClassNameId = PortalUtil.getClassNameId(
+		long kbFolderClassNameId = classNameLocalService.getClassNameId(
 			KBFolderConstants.getClassName());
 
 		long kbFolderId = KBFolderConstants.DEFAULT_PARENT_FOLDER_ID;
@@ -1650,7 +1650,7 @@ public class KBArticleLocalServiceImpl extends KBArticleLocalServiceBaseImpl {
 			return resourcePrimKey;
 		}
 
-		long classNameId = PortalUtil.getClassNameId(
+		long classNameId = classNameLocalService.getClassNameId(
 			KBArticleConstants.getClassName());
 
 		if (parentResourceClassNameId == classNameId) {
@@ -1919,9 +1919,9 @@ public class KBArticleLocalServiceImpl extends KBArticleLocalServiceBaseImpl {
 			long resourceClassNameId, long resourcePrimKey)
 		throws PortalException {
 
-		long kbArticleClassNameId = PortalUtil.getClassNameId(
+		long kbArticleClassNameId = classNameLocalService.getClassNameId(
 			KBArticleConstants.getClassName());
-		long kbFolderClassNameId = PortalUtil.getClassNameId(
+		long kbFolderClassNameId = classNameLocalService.getClassNameId(
 			KBFolderConstants.getClassName());
 
 		if ((resourceClassNameId != kbArticleClassNameId) &&
