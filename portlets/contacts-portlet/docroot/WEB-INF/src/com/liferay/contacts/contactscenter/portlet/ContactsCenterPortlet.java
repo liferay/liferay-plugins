545a03b688e86d13395d57f35bfa100b2fa06d5c
Jose A. Jimenez <jose.jimenez@liferay.com>
2015-01-19 16:54:07 +0100

    SOS-2441 read asset categories properly from the request

--- portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/contactscenter/portlet/ContactsCenterPortlet.java
+++ portlets/contacts-portlet/docroot/WEB-INF/src/com/liferay/contacts/contactscenter/portlet/ContactsCenterPortlet.java
@@ -77,6 +77,7 @@ import com.liferay.portal.model.UserNotificationDeliveryConstants;
 import com.liferay.portal.model.Website;
 import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.service.UserNotificationEventLocalServiceUtil;
 import com.liferay.portal.service.UserServiceUtil;
@@ -1089,10 +1090,11 @@ public class ContactsCenterPortlet extends MVCPortlet {
 
 		User user = themeDisplay.getUser();
 
-		long[] assetCategoryIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "assetCategoryNames"), 0L);
-		String[] assetTagNames = StringUtil.split(
-			ParamUtil.getString(actionRequest, "assetTagNames"));
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				User.class.getName(), actionRequest);
+
+		long[] assetCategoryIds = serviceContext.getAssetCategoryIds();
+		String[] assetTagNames = serviceContext.getAssetTagNames();
 
 		UserLocalServiceUtil.updateAsset(
 			user.getUserId(), user, assetCategoryIds, assetTagNames);
