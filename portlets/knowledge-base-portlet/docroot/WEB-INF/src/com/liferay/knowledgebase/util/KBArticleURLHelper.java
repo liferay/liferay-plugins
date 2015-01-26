8157373bf4ec5c4f58d5b015cadefbff73083c60
adolfo.perez <adolfo.perez@liferay.com>
2015-01-26 14:57:53 +0100

    LPS-50698 add helper class to manage KBArticle URLs
    Add methods that create URLs to view a KBArticle:
      - createViewURL, default URL to access the read-only view of an
        article
      - createViewWithRedirectURL, when it is necessary to show a "back"
        link to another view.
      - createViewWithCommentsURL, when the article is shown along a set
        of comments.

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/util/KBArticleURLHelper.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.util;
+
+import com.liferay.knowledgebase.model.KBArticle;
+import com.liferay.knowledgebase.model.KBFolder;
+import com.liferay.knowledgebase.model.KBFolderConstants;
+import com.liferay.knowledgebase.service.KBFolderServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class KBArticleURLHelper {
+
+	public KBArticleURLHelper(
+		RenderResponse renderResponse, String templatePath) {
+
+		_renderResponse = renderResponse;
+		_templatePath = templatePath;
+	}
+
+	public PortletURL createViewURL(KBArticle kbArticle)
+		throws PortalException, SystemException {
+
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		if (Validator.isNull(kbArticle.getUrlTitle())) {
+			portletURL.setParameter(
+				"mvcPath", _templatePath + "view_article.jsp");
+			portletURL.setParameter(
+				"resourceClassNameId",
+				String.valueOf(kbArticle.getClassNameId()));
+			portletURL.setParameter(
+				"resourcePrimKey",
+				String.valueOf(kbArticle.getResourcePrimKey()));
+		}
+		else {
+			portletURL.setParameter("urlTitle", kbArticle.getUrlTitle());
+
+			if (kbArticle.getKbFolderId() !=
+					KBFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+
+				KBFolder kbFolder = KBFolderServiceUtil.getKBFolder(
+					kbArticle.getKbFolderId());
+
+				portletURL.setParameter(
+					"kbFolderUrlTitle", kbFolder.getUrlTitle());
+			}
+		}
+
+		return portletURL;
+	}
+
+	public PortletURL createViewWithCommentsURL(KBArticle kbArticle)
+		throws PortalException, SystemException {
+
+		PortletURL portletURL = createViewURL(kbArticle);
+
+		portletURL.setParameter("expanded", Boolean.TRUE.toString());
+
+		return portletURL;
+	}
+
+	public PortletURL createViewWithRedirectURL(
+			KBArticle kbArticle, String redirect)
+		throws PortalException, SystemException {
+
+		PortletURL portletURL = createViewURL(kbArticle);
+
+		if (Validator.isNotNull(redirect)) {
+			portletURL.setParameter("redirect", redirect);
+		}
+
+		return portletURL;
+	}
+
+	private final RenderResponse _renderResponse;
+	private final String _templatePath;
+
+}
