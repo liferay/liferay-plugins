20054c4dec50e5ca5e3952e4afdf958edf3de74c
adolfo.perez <adolfo.perez@liferay.com>
2015-01-27 17:59:02 +0100

    LPS-52948 add default selectors

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/selector/KBFolderKBArticleSelector.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase.display.selector;
+
+import com.liferay.knowledgebase.model.KBArticle;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class KBFolderKBArticleSelector implements KBArticleSelector {
+
+	@Override
+	public KBArticle findByResourcePrimKey(
+			long groupId, long ancestorResourcePrimKey, long resourcePrimKey)
+		throws PortalException, SystemException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public KBArticle findByUrlTitle(
+			long groupId, long ancestorResourcePrimKey, String kbFolderUrlTitle,
+			String urlTitle)
+		throws PortalException, SystemException {
+
+		throw new UnsupportedOperationException();
+	}
+
+}
