f67db0b8759fc7f31c71f67a04fbf98c370bfe27
adolfo.perez <adolfo.perez@liferay.com>
2015-01-27 12:00:25 +0100

    LPS-52730 use fetch instead of get to avoid NPE

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/portlet/DisplayPortlet.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/portlet/DisplayPortlet.java
@@ -463,7 +463,7 @@ public class DisplayPortlet extends BaseKBPortlet {
 		throws PortalException, SystemException {
 
 		KBArticle oldKBArticle =
-			KBArticleLocalServiceUtil.getKBArticleByUrlTitle(
+			KBArticleLocalServiceUtil.fetchKBArticleByUrlTitle(
 				groupId, oldKBFolderURLTitle, urlTitle);
 
 		KBArticle kbArticle = null;
