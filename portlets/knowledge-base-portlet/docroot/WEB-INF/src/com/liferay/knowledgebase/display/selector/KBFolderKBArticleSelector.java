58cc580472c47bcf071b4a8248ee031560c7fafb
adolfo.perez <adolfo.perez@liferay.com>
2015-01-28 12:00:09 +0100

    LPS-52948 basic selection strategy for KBFolder
    When selecting a KBArticle to show in a Display portlet configured with
    a KBFolder, the minimum viable strategy is:
    
      1. Fetch the configured KBFolder.
    
      2. If it doesn't exist, it means the folder was deleted. By returning
      null, the portlet will be shown in an unconfigured state.
    
      3. Fetch the requested article. If it doesn't exist, or it isn't a
      descendant of the folder, try to find an article with the same
      friendly URL, or return the first child in the folder (if
      none, return null).
    
    Note that in step 3, when no child exists with the given
    kbFolderUrlTitle, we try to show the first child article inside a
    subfolder, and if that fails, an article inside the ancestor. This is
    needed to avoid showing the portlet in an unconfigured state if someone
    uses the wrong friendly URL.

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/selector/KBFolderKBArticleSelector.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/selector/KBFolderKBArticleSelector.java
@@ -15,8 +15,15 @@
 package com.liferay.knowledgebase.display.selector;
 
 import com.liferay.knowledgebase.model.KBArticle;
+import com.liferay.knowledgebase.model.KBFolder;
+import com.liferay.knowledgebase.service.KBArticleLocalServiceUtil;
+import com.liferay.knowledgebase.service.KBFolderLocalServiceUtil;
+import com.liferay.knowledgebase.util.comparator.KBArticlePriorityComparator;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.List;
 
 /**
  * @author Adolfo Pérez
@@ -28,7 +35,21 @@ public class KBFolderKBArticleSelector implements KBArticleSelector {
 			long groupId, long ancestorResourcePrimKey, long resourcePrimKey)
 		throws PortalException, SystemException {
 
-		throw new UnsupportedOperationException();
+		KBFolder ancestorKBFolder = KBFolderLocalServiceUtil.fetchKBFolder(
+			ancestorResourcePrimKey);
+
+		if (ancestorKBFolder == null) {
+			return null;
+		}
+
+		KBArticle kbArticle = KBArticleLocalServiceUtil.fetchLatestKBArticle(
+			resourcePrimKey, WorkflowConstants.STATUS_APPROVED);
+
+		if ((kbArticle == null) || !isDescendant(kbArticle, ancestorKBFolder)) {
+			return findClosestMatchingKBArticle(groupId, ancestorKBFolder);
+		}
+
+		return kbArticle;
 	}
 
 	@Override
@@ -37,7 +58,120 @@ public class KBFolderKBArticleSelector implements KBArticleSelector {
 			String urlTitle)
 		throws PortalException, SystemException {
 
-		throw new UnsupportedOperationException();
+		KBFolder ancestorKBFolder = KBFolderLocalServiceUtil.fetchKBFolder(
+			ancestorResourcePrimKey);
+
+		if (ancestorKBFolder == null) {
+			return null;
+		}
+
+		KBArticle kbArticle =
+			KBArticleLocalServiceUtil.fetchKBArticleByUrlTitle(
+				groupId, kbFolderUrlTitle, urlTitle);
+
+		if ((kbArticle == null) || !isDescendant(kbArticle, ancestorKBFolder)) {
+			return findClosestMatchingKBArticle(
+				groupId, ancestorKBFolder, kbFolderUrlTitle, urlTitle);
+		}
+
+		return kbArticle;
+	}
+
+	protected KBArticle findClosestMatchingKBArticle(
+			long groupId, KBFolder ancestorKBFolder)
+		throws PortalException, SystemException {
+
+		List<KBFolder> kbFolders = KBFolderLocalServiceUtil.getKBFolders(
+			groupId, ancestorKBFolder.getKbFolderId(), 0, 1);
+
+		KBFolder kbFolder = null;
+
+		if (!kbFolders.isEmpty()) {
+			kbFolder = kbFolders.get(0);
+		}
+		else {
+			kbFolder = ancestorKBFolder;
+		}
+
+		List<KBArticle> kbArticles =
+			KBArticleLocalServiceUtil.getKBArticles(
+				groupId, kbFolder.getKbFolderId(),
+				WorkflowConstants.STATUS_APPROVED, 0, 1,
+				new KBArticlePriorityComparator());
+
+		if (kbArticles.isEmpty()) {
+			return null;
+		}
+
+		return kbArticles.get(0);
+	}
+
+	protected KBArticle findClosestMatchingKBArticle(
+			long groupId, KBFolder ancestorKBFolder, String kbFolderUrlTitle,
+			String urlTitle)
+		throws PortalException, SystemException {
+
+		KBFolder kbFolder = getCandidateKBFolder(
+			groupId, ancestorKBFolder, kbFolderUrlTitle);
+
+		KBArticle kbArticle =
+			KBArticleLocalServiceUtil.fetchKBArticleByUrlTitle(
+				groupId, kbFolder.getKbFolderId(), urlTitle);
+
+		if (kbArticle != null) {
+			return kbArticle;
+		}
+
+		List<KBArticle> kbArticles =
+			KBArticleLocalServiceUtil.getKBArticles(
+				groupId, kbFolder.getKbFolderId(),
+				WorkflowConstants.STATUS_APPROVED, 0, 1,
+				new KBArticlePriorityComparator());
+
+		if (kbArticles.isEmpty()) {
+			return null;
+		}
+
+		return kbArticles.get(0);
+	}
+
+	protected KBFolder getCandidateKBFolder(
+			long groupId, KBFolder ancestorKBFolder, String kbFolderUrlTitle)
+		throws PortalException, SystemException {
+
+		KBFolder kbFolder =
+			KBFolderLocalServiceUtil.fetchKBFolderByUrlTitle(
+				groupId, ancestorKBFolder.getKbFolderId(), kbFolderUrlTitle);
+
+		if (kbFolder != null) {
+			return kbFolder;
+		}
+
+		List<KBFolder> kbFolders = KBFolderLocalServiceUtil.getKBFolders(
+			groupId, ancestorKBFolder.getKbFolderId(), 0, 1);
+
+		if (!kbFolders.isEmpty()) {
+			return kbFolders.get(0);
+		}
+
+		return ancestorKBFolder;
+	}
+
+	protected boolean isDescendant(
+			KBArticle kbArticle, KBFolder ancestorKBFolder)
+		throws PortalException, SystemException {
+
+		KBFolder parentKBFolder = KBFolderLocalServiceUtil.fetchKBFolder(
+			kbArticle.getKbFolderId());
+
+		while ((parentKBFolder != null) &&
+			   !parentKBFolder.equals(ancestorKBFolder)) {
+
+			parentKBFolder = KBFolderLocalServiceUtil.fetchKBFolder(
+				parentKBFolder.getParentKBFolderId());
+		}
+
+		return parentKBFolder != null;
 	}
 
 }
