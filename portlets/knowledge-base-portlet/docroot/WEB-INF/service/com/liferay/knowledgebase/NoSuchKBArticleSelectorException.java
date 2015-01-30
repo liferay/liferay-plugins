bcb0a857cf9a5dfb577ced8305a70aefcf578b78
adolfo.perez <adolfo.perez@liferay.com>
2015-01-30 10:27:04 +0100

    LPS-52948 add missing exception

---
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/service/com/liferay/knowledgebase/NoSuchKBArticleSelectorException.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledgebase;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.NoSuchModelException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@ProviderType
+public class NoSuchKBArticleSelectorException extends NoSuchModelException {
+
+	public NoSuchKBArticleSelectorException() {
+	}
+
+	public NoSuchKBArticleSelectorException(String msg) {
+		super(msg);
+	}
+
+	public NoSuchKBArticleSelectorException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public NoSuchKBArticleSelectorException(Throwable cause) {
+		super(cause);
+	}
+
+}
