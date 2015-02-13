deddb98d9369b4da4398876948b35825707391b5
Brian Chan <brian.chan@liferay.com>
2015-02-13 09:55:38 -0800

    LPS-52781 Consistency

--- tools/templates/portlet_spring_mvc_tmpl/docroot/WEB-INF/jsp/view.jsp
+++ tools/templates/portlet_spring_mvc_tmpl/docroot/WEB-INF/jsp/view.jsp
@@ -18,6 +18,6 @@
 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-This is the <b>@portlet.display.name@</b> portlet.
-<br/>
+This is the <b>@portlet.display.name@</b> portlet.<br />
+
 <c:out escapeXml="true" value="${releaseInfo}" />.
--- tools/templates/portlet_spring_mvc_tmpl/docroot/WEB-INF/src/PortletViewController.java
+++ tools/templates/portlet_spring_mvc_tmpl/docroot/WEB-INF/src/PortletViewController.java
@@ -20,9 +20,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
-/**
- * This class is base controller for VIEW mode.
- */
 @Controller
 @RequestMapping("VIEW")
 public class @portlet.java.class.name@PortletViewController {
