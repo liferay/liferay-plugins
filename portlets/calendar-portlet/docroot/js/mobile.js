1445876af6d3297e838ae56b9e98bc6c70ce764a
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-02-18 17:40:55 -0800

    LPS-53766 Convert view buttons into a dropdown list on mobile view

--- portlets/calendar-portlet/docroot/js/mobile.js
+++ portlets/calendar-portlet/docroot/js/mobile.js
@@ -1,21 +1,91 @@
 AUI.add(
 	'scheduler-mobile',
 	function(A) {
+		var CSS_SCHEDULER_VIEW = A.getClassName('scheduler-base', 'view'),
+			CSS_SCHEDULER_VIEW_ = A.getClassName('scheduler-base', 'view', ''),
+			
+			TPL_SCHEDULER_VIEW_BUTTON = '<button type="button" class="' + [CSS_SCHEDULER_VIEW, CSS_SCHEDULER_VIEW_].join(' ') +
+				'{name}" data-view-name="{name}">{label}</button>',
+			TPL_SCHEDULER_VIEW_LIST = '<option class="' + [CSS_SCHEDULER_VIEW, CSS_SCHEDULER_VIEW_].join(' ') +
+				'{name}" data-view-name="{name}">{label}</option>',
+			TPL_SCHEDULER_VIEWS_SELECT = '<select class="form-control visible-phone"></select>';
+		
 		var SchedulerMobile = A.Component.create(
 			{
-				ATTRS: {},
+				ATTRS: {
+					viewsSelectNode: {
+						valueFn: function() {
+							return A.Node.create(TPL_SCHEDULER_VIEWS_SELECT);
+						}
+					},
+				},
 
 				EXTENDS: A.Scheduler,
 				
 				NAME: 'scheduler-mobile',
 
 				prototype: {
-					syncStdContent: function() {
+					initializer: function() {
 						var instance = this;
+
+						instance.viewsSelectNode = instance.get('viewsSelectNode');
+					},
+
+					renderDropdownList: function() {
+						var instance = this;
+
+						instance.viewsSelectNode.on('change', A.bind(instance._onSelectionChange, instance));
+					},
+
+					syncStdContent: function() {
+						var instance = this,
+							views = instance.get('views');
 
 						SchedulerMobile.superclass.syncStdContent.apply(this, arguments);
 
 						instance.iconPrevNode.insert(instance.todayNode, 'after');
+
+						A.Array.each(views, function(view) {
+							instance.viewsSelectNode.append(instance._createViewTriggerNode(view, TPL_SCHEDULER_VIEW_LIST));
+						});
+
+						instance.viewsNode.all('button').addClass('hidden-phone');
+						instance.viewsNode.append(instance.viewsSelectNode);
+					},
+
+					_afterRender: function(event) {
+						var instance = this;
+
+						SchedulerMobile.superclass._afterRender.apply(this, arguments);
+
+						instance.renderDropdownList();
+					},
+
+					_createViewTriggerNode: function(view, tpl) {
+						var instance = this,
+							name = view.get('name'),
+							tpl = tpl ? tpl : TPL_SCHEDULER_VIEW_BUTTON;
+
+						view.set(
+							'triggerNode',
+							A.Node.create(
+								A.Lang.sub(tpl, {
+									name: name,
+									label: (instance.getString(name) || name)
+								})
+							)
+						);
+
+						return view.get('triggerNode');
+					},
+
+					_onSelectionChange: function(event) {
+						var instance = this,
+							target = event.target,
+							index = target.get('selectedIndex'),
+							viewName = target.get('options').item(index).attr('data-view-name');
+
+						instance.set('activeView', instance.getViewByName(viewName));
 					}
 				}
 			}
