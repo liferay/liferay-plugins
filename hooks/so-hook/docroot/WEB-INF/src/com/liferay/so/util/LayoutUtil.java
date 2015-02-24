14473b468905292ce3c637ad87004841a8af89ab
Brian Chan <brian.chan@liferay.com>
2015-02-24 07:38:27 -0800

    LPS-53579 Sort

--- hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/util/LayoutUtil.java
+++ hooks/so-hook/docroot/WEB-INF/src/com/liferay/so/util/LayoutUtil.java
@@ -149,6 +149,9 @@ public class LayoutUtil {
 				configureProfile(layout, portletId);
 				removePortletBorder(layout, portletId);
 			}
+			else if (portletId.startsWith(BreadcrumbPortletKeys.BREADCRUMB)) {
+				removePortletBorder(layout, portletId);
+			}
 			else if (portletId.startsWith(PortletKeys.ASSET_PUBLISHER)) {
 				configureAssetPublisher(layout);
 				updatePortletTitle(layout, portletId, "related-content");
@@ -156,9 +159,6 @@ public class LayoutUtil {
 			else if (portletId.startsWith(PortletKeys.BLOGS_AGGREGATOR)) {
 				configureBlogsAggregator(layout);
 				updatePortletTitle(layout, portletId, "recent-blogs");
-			}
-			else if (portletId.startsWith(BreadcrumbPortletKeys.BREADCRUMB)) {
-				removePortletBorder(layout, portletId);
 			}
 			else if (portletId.startsWith(PortletKeys.MESSAGE_BOARDS)) {
 				configureMessageBoards(layout);
