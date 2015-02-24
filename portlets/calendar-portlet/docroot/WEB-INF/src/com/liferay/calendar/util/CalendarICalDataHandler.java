b9e65e0b1aa6160d03376fbd4787b2c5fb4fb538
Brian Chan <brian.chan@liferay.com>
2015-02-24 07:42:18 -0800

    SF

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarICalDataHandler.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarICalDataHandler.java
@@ -32,7 +32,6 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Time;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.ModelHintsUtil;
