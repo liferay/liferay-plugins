d8e04ca12b251db8635044f8dd146abad7d9cee9
adolfo.perez <adolfo.perez@liferay.com>
2015-02-26 10:43:47 +0100

    LPS-53807 When editing an article, return requested article
    When an "Edit" action is requested, the article `resourcePrimKey` is
    already provided. No need to guess the article using the article
    selectors.

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/portlet/DisplayPortlet.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/portlet/DisplayPortlet.java
@@ -254,6 +254,16 @@ public class DisplayPortlet extends BaseKBPortlet {
 	protected KBArticle getKBArticle(RenderRequest renderRequest)
 		throws PortalException, SystemException {
 
+		String mvcPath = ParamUtil.getString(renderRequest, "mvcPath");
+
+		if (mvcPath.endsWith("/edit_article.jsp")) {
+			long resourcePrimKey = ParamUtil.getLong(
+				renderRequest, "resourcePrimKey");
+
+			return KBArticleLocalServiceUtil.getLatestKBArticle(
+				resourcePrimKey, WorkflowConstants.STATUS_ANY);
+		}
+
 		PortletPreferences portletPreferences = renderRequest.getPreferences();
 
 		long kbFolderClassNameId = ClassNameLocalServiceUtil.getClassNameId(
