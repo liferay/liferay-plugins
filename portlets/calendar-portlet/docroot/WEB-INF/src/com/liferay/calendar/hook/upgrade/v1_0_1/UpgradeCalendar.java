8be3e9ec532aaf801cf7503418b1149c08fca241
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-03-03 12:19:08 -0300

    LPS-53704 SF

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/hook/upgrade/v1_0_1/UpgradeCalendar.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/hook/upgrade/v1_0_1/UpgradeCalendar.java
@@ -14,18 +14,17 @@
 
 package com.liferay.calendar.hook.upgrade.v1_0_1;
 
+import com.liferay.compat.portal.util.PortalUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  * @author Adam Brandizzi
@@ -35,45 +34,38 @@ public class UpgradeCalendar extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 
 		try {
 			con = DataAccess.getUpgradeOptimizedConnection();
 
-			PreparedStatement ps = con.prepareStatement(
-				"select C.calendarId, CR.classNameId, U.timeZoneId from " +
-					"Calendar C inner join CalendarResource CR on "+
-					"C.calendarResourceId = CR.calendarResourceId inner join " +
-					"User_ U on CR.userId = U.userId");
+			StringBundler sb = new StringBundler(6);
 
-			ResultSet rs = ps.executeQuery();
+			sb.append("select Calendar.calendarId, CalendarResource.");
+			sb.append("classNameId, User_.timeZoneId from Calendar ");
+			sb.append("inner join CalendarResource on Calendar.");
+			sb.append("calendarResourceId = CalendarResource.");
+			sb.append("calendarResourceId inner join User_ on ");
+			sb.append("CalendarResource.userId = User_.userId");
 
-			long userClassNameId = getClassNameId(con, User.class.getName());
-			String portalTimeZoneId = PropsUtil.get(
-				PropsKeys.COMPANY_DEFAULT_TIME_ZONE);
-			PreparedStatement updatePs = con.prepareStatement(
-				"update Calendar set timeZoneId = ? where calendarId = ?");
+			ps = con.prepareStatement(sb.toString());
+
+			rs = ps.executeQuery();
+
+			long userClassNameId = PortalUtil.getClassNameId(User.class);
 
 			while (rs.next()) {
 				long calendarId = rs.getLong(1);
 				long classNameId = rs.getLong(2);
-				String timeZoneId = portalTimeZoneId;
+
+				String timeZoneId = _PORTAL_TIME_ZONE_ID;
 
 				if (classNameId == userClassNameId) {
 					timeZoneId = rs.getString(3);
 				}
 
-				updatePs.setString(1, timeZoneId);
-				updatePs.setLong(2, calendarId);
-
-				int count = updatePs.executeUpdate();
-
-				if (count <= 0) {
-					if (_log.isWarnEnabled()) {
-						_log.warn(
-							"Calendar " + calendarId +
-							" did not get its time zone set");
-					}
-				}
+				updateCalendarTimeZoneId(calendarId, timeZoneId);
 			}
 		}
 		finally {
@@ -81,23 +73,15 @@ public class UpgradeCalendar extends UpgradeProcess {
 		}
 	}
 
-	protected long getClassNameId(Connection con, String className)
-		throws SQLException {
+	protected void updateCalendarTimeZoneId(long calendarId, String timeZoneId)
+		throws Exception {
 
-		PreparedStatement ps = con.prepareStatement(
-			"select classNameId from ClassName_ where value = ?");
-
-		ps.setString(1, className);
-
-		ResultSet rs = ps.executeQuery();
-
-		if (rs.next()) {
-			return rs.getLong("classNameId");
-		}
-
-		return 0;
+		runSQL(
+			"update Calendar set timeZoneId = '" + timeZoneId +
+				"' where calendarId = " + calendarId);
 	}
 
-	private static Log _log = LogFactoryUtil.getLog(UpgradeCalendar.class);
+	private static final String _PORTAL_TIME_ZONE_ID = PropsUtil.get(
+		PropsKeys.COMPANY_DEFAULT_TIME_ZONE);
 
 }
