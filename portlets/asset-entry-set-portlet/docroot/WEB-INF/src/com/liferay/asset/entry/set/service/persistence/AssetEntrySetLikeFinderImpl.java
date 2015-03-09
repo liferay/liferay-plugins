08b4a8a93b3ba1c2ce421245bf1cf49503bba2c7
sherryre <sherry.yang@liferay.com>
2015-03-09 12:41:25 -0700

    CONNECT-531 Add AssetEntrySetLikeFinderImpl

---
+++ portlets/asset-entry-set-portlet/docroot/WEB-INF/src/com/liferay/asset/entry/set/service/persistence/AssetEntrySetLikeFinderImpl.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.entry.set.service.persistence;
+
+import com.liferay.asset.entry.set.model.AssetEntrySetLike;
+import com.liferay.asset.entry.set.model.impl.AssetEntrySetLikeImpl;
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.util.dao.orm.CustomSQLUtil;
+
+import java.util.List;
+
+/**
+ * @author Sherry Yang
+ */
+public class AssetEntrySetLikeFinderImpl
+	extends BasePersistenceImpl<AssetEntrySetLike>
+	implements AssetEntrySetLikeFinder {
+
+	public static final String FIND_BY_NO_C_C =
+		AssetEntrySetLikeFinder.class.getName() + ".findByNoC_C";
+
+	public List<AssetEntrySetLike> findByNoC_C(
+			long classNameId, long classPK, int start, int end)
+		throws SystemException {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(FIND_BY_NO_C_C);
+
+			SQLQuery q = session.createSQLQuery(sql);
+
+			q.addEntity("AssetEntrySetLike", AssetEntrySetLikeImpl.class);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(classNameId);
+			qPos.add(classPK);
+
+			return (List<AssetEntrySetLike>)QueryUtil.list(
+				q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+}
