8df0b78ef8df83b62f969f0a7d3fcf473ffa1a40
Brian Chan <brian.chan@liferay.com>
2015-04-09 10:51:05 -0700

    Remove unused bbb

--- portlets/bbb-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,7 +0,0 @@
-create index IX_D41A5517 on BBBMeeting (bbbServerId, status);
-create index IX_4E58622B on BBBMeeting (groupId);
-create index IX_85871D45 on BBBMeeting (status);
-
-create unique index IX_9F11BB2B on BBBParticipant (bbbMeetingId, emailAddress);
-
-create index IX_BE006FE on BBBServer (active_);
