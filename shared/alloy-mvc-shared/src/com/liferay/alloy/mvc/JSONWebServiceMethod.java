409eacec3e2e144dbb45029230dcbfc662dbe8d9
Ethan Bustad <ethan.bustad@liferay.com>
2015-04-15 17:41:33 -0700

    LPS-54861 Create annotation to mark controller methods for JSONWS registration

---
+++ shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/JSONWebServiceMethod.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.alloy.mvc;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.portlet.PortletRequest;
+
+/**
+ * @author Ethan Bustad
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface JSONWebServiceMethod {
+
+	public String lifecycle() default PortletRequest.RENDER_PHASE;
+
+	public JSONWebServiceParameter[] parameters() default {};
+
+}
