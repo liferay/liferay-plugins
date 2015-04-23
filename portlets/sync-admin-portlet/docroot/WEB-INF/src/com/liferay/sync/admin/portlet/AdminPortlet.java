8145731eeb9973aee5b9dd8c4a53a23947196b9f
Brian Chan <brian.chan@liferay.com>
2015-04-23 14:04:00 -0700

    SYNC-1305 SF

--- portlets/sync-admin-portlet/docroot/WEB-INF/src/com/liferay/sync/admin/portlet/AdminPortlet.java
+++ portlets/sync-admin-portlet/docroot/WEB-INF/src/com/liferay/sync/admin/portlet/AdminPortlet.java
@@ -136,11 +136,9 @@ public class AdminPortlet extends MVCPortlet {
 			portletPreferences.setValue(
 				PortletPropsKeys.SYNC_OAUTH_APPLICATION_ID,
 				String.valueOf(oAuthApplication.getOAuthApplicationId()));
-
 			portletPreferences.setValue(
 				PortletPropsKeys.SYNC_OAUTH_CONSUMER_KEY,
 				oAuthApplication.getConsumerKey());
-
 			portletPreferences.setValue(
 				PortletPropsKeys.SYNC_OAUTH_CONSUMER_SECRET,
 				oAuthApplication.getConsumerSecret());
