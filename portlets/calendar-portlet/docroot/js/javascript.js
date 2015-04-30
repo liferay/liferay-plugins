f4c99562655ec62d8a5b1209ae6a908eb124f419
Brian Chan <brian.chan@liferay.com>
2015-04-30 11:29:39 -0700

    LPS-54981 SF

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -1024,14 +1024,6 @@ AUI.add(
 						instance.on('statusChange', instance._onStatusChange);
 					},
 
-					syncUI: function() {
-						var instance = this;
-
-						Liferay.SchedulerEvent.superclass.syncUI.apply(instance, arguments);
-
-						instance._uiSetStatus(instance.get('status'));
-					},
-
 					isMasterBooking: function() {
 						var instance = this;
 
@@ -1066,6 +1058,14 @@ AUI.add(
 								);
 							}
 						}
+					},
+
+					syncUI: function() {
+						var instance = this;
+
+						Liferay.SchedulerEvent.superclass.syncUI.apply(instance, arguments);
+
+						instance._uiSetStatus(instance.get('status'));
 					},
 
 					_onLoadingChange: function(event) {
