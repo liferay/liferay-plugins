a30493fc9c63806fbb0e1da57afb05e34c9f01af
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-05-06 13:09:56 +0200

    LPS-55392 Calendar: Incorrect display of overnight events in Week and Day view, when timezone is set in portal-ext

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -1152,7 +1152,7 @@ AUI.add(
 						date = DateMath.add(date, DateMath.WEEK, 1);
 					}
 
-					return CalendarUtil.toUTC(date);
+					return date;
 				},
 
 				getLoadStartDate: function(activeView) {
@@ -1167,7 +1167,7 @@ AUI.add(
 						date = DateMath.subtract(date, DateMath.WEEK, 1);
 					}
 
-					return CalendarUtil.toUTC(date);
+					return date;
 				},
 
 				_doRead: function(options, callback) {
