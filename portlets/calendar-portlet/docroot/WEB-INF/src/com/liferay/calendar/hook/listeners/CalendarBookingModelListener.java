dd5385010d4578fe50e19a96d1e9ea88c620e2cf
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2015-05-07 17:46:33 -0300

    LPS-55203 Deleting CalendarBooking should also delete CalEvent (using listener)

---
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/hook/listeners/CalendarBookingModelListener.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.hook.listeners;
+
+import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.portal.ModelListenerException;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.BaseModelListener;
+import com.liferay.portlet.calendar.model.CalEvent;
+import com.liferay.portlet.calendar.service.CalEventLocalServiceUtil;
+
+/**
+ * @author Adam Brandizzi
+ */
+public class CalendarBookingModelListener
+	extends BaseModelListener<CalendarBooking> {
+
+	@Override
+	public void onAfterRemove(CalendarBooking calendarBooking)
+		throws ModelListenerException {
+
+		CalEvent calEvent =
+			CalEventLocalServiceUtil.fetchCalEventByUuidAndGroupId(
+				calendarBooking.getUuid(), calendarBooking.getGroupId());
+
+		if (Validator.isNotNull(calEvent)) {
+			CalEventLocalServiceUtil.deleteCalEvent(calEvent);
+		}
+	}
+
+}
