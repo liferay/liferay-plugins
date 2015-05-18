eaed5a6f270278cbebda873d9a24dc2f028b7c72
André de Oliveira <andre.oliveira@liferay.com>
2015-05-18 17:17:43 -0300

    LPS-55772 Behavior common to both Auth modes goes into superclass
    https://issues.liferay.com/browse/LPS-55772

--- shared/solr4-shared/src/com/liferay/portal/search/solr/http/CertAuthPoolingHttpClientFactory.java
+++ shared/solr4-shared/src/com/liferay/portal/search/solr/http/CertAuthPoolingHttpClientFactory.java
@@ -16,53 +16,18 @@ package com.liferay.portal.search.solr.http;
 
 import com.liferay.portal.kernel.util.Http;
 
-import java.util.List;
-
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.client.HttpClient;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 
 /**
  * @author László Csontos
  * @author André de Oliveira
  */
-public class CertAuthPoolingHttpClientFactory implements HttpClientFactory {
-
-	@Override
-	public HttpClient createInstance() throws Exception {
-		SSLSocketFactory sslSocketFactory = _sslSocketFactoryBuilder.build();
-
-		_basePoolingHttpClientFactory = new BasePoolingHttpClientFactory(
-			createPoolingClientConnectionManager(sslSocketFactory));
-
-		_basePoolingHttpClientFactory.setDefaultMaxConnectionsPerRoute(
-			_defaultMaxConnectionsPerRoute);
-		_basePoolingHttpClientFactory.setHttpRequestInterceptors(
-			_httpRequestInterceptors);
-		_basePoolingHttpClientFactory.setMaxTotalConnections(
-			_maxTotalConnections);
-
-		return _basePoolingHttpClientFactory.createInstance();
-	}
-
-	public void setDefaultMaxConnectionsPerRoute(
-		Integer defaultMaxConnectionsPerRoute) {
-
-		_defaultMaxConnectionsPerRoute = defaultMaxConnectionsPerRoute;
-	}
-
-	public void setHttpRequestInterceptors(
-		List<HttpRequestInterceptor> httpRequestInterceptors) {
-
-		_httpRequestInterceptors = httpRequestInterceptors;
-	}
-
-	public void setMaxTotalConnections(Integer maxTotalConnections) {
-		_maxTotalConnections = maxTotalConnections;
-	}
+public class CertAuthPoolingHttpClientFactory
+	extends BasePoolingHttpClientFactory {
 
 	public void setSslSocketFactoryBuilder(
 		SSLSocketFactoryBuilder sslSocketFactoryBuilder) {
@@ -71,13 +36,14 @@ public class CertAuthPoolingHttpClientFactory implements HttpClientFactory {
 	}
 
 	@Override
-	public void shutdown() {
-		_basePoolingHttpClientFactory.shutdown();
+	protected void configure(DefaultHttpClient defaultHttpClient) {
 	}
 
+	@Override
 	protected PoolingClientConnectionManager
-		createPoolingClientConnectionManager(
-			SSLSocketFactory sslSocketFactory) {
+		createPoolingClientConnectionManager() throws Exception {
+
+		SSLSocketFactory sslSocketFactory = _sslSocketFactoryBuilder.build();
 
 		SchemeRegistry schemeRegistry = createSchemeRegistry(sslSocketFactory);
 
@@ -97,10 +63,6 @@ public class CertAuthPoolingHttpClientFactory implements HttpClientFactory {
 		return schemeRegistry;
 	}
 
-	private BasePoolingHttpClientFactory _basePoolingHttpClientFactory;
-	private Integer _defaultMaxConnectionsPerRoute;
-	private List<HttpRequestInterceptor> _httpRequestInterceptors;
-	private Integer _maxTotalConnections;
 	private SSLSocketFactoryBuilder _sslSocketFactoryBuilder;
 
 }
