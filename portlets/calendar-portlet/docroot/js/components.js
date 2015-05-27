fd0d5146d525f45200dc62f861eb9f90dcb01dc6
westonhancock <weston.hancock@liferay.com>
2015-05-27 10:33:45 -0700

    LPS-55661 - General source formatting

--- portlets/calendar-portlet/docroot/js/components.js
+++ portlets/calendar-portlet/docroot/js/components.js
@@ -53,7 +53,7 @@
 						}
 					);
 
-					return (found && found.fn);
+					return found && found.fn;
 				}
 			);
 
@@ -242,7 +242,7 @@
 									function(item, index) {
 										var id = item.attr('data-id');
 
-										item.toggleClass(CSS_SIMPLE_MENU_ITEM_HIDDEN, (AArray.indexOf(val, id) > -1));
+										item.toggleClass(CSS_SIMPLE_MENU_ITEM_HIDDEN, AArray.indexOf(val, id) > -1);
 									}
 								);
 							}
@@ -515,7 +515,7 @@
 								simpleMenu.setAttrs(
 									{
 										alignNode: target,
-										visible: ((simpleMenu.get('align.node') !== target) || !simpleMenu.get('visible'))
+										visible: simpleMenu.get('align.node') !== target || !simpleMenu.get('visible')
 									}
 								);
 							}
@@ -856,6 +856,7 @@
 							var instance = this;
 
 							var target = event.target;
+
 							var checked = target.get('checked');
 							var elements = target.ancestor().siblings('input[type=text],select');
 
@@ -936,7 +937,7 @@
 					var hours = date.getHours();
 					var minutes = date.getMinutes();
 
-					var amPm = (hours < 12) ? 0 : 1;
+					var amPm = hours < 12 ? 0 : 1;
 
 					if (amPm === 1) {
 						hours -= 12;
@@ -969,12 +970,12 @@
 
 							var value = toInt(currentTarget.val());
 
-							if ((selectedSetter === 3) && (date.getHours() > 12)) {
+							if (selectedSetter === 3 && date.getHours() > 12) {
 								value += 12;
 							}
 
 							if (selectedSetter === 5) {
-								value = date.getHours() + ((value === 1) ? 12 : -12);
+								value = date.getHours() + (value === 1 ? 12 : -12);
 							}
 
 							setters[selectedSetter].call(date, value);
@@ -1069,14 +1070,14 @@
 						position = instance.POSITION_LABELS[recurrence.positionalWeekday.position];
 						weekDay = instance.WEEKDAY_LABELS[recurrence.positionalWeekday.weekday];
 					}
-					else if ((recurrence.frequency == instance.FREQUENCY.WEEKLY) && (recurrence.weekdays.length > 0)) {
+					else if (recurrence.frequency == instance.FREQUENCY.WEEKLY && recurrence.weekdays.length > 0) {
 						template.push(STR_SPACE, TPL_SPAN, Liferay.Language.get('on'), TPL_SPAN_CLOSE, ' {weekDays}');
 					}
 
-					if (recurrence.count && (recurrence.endValue === 'after')) {
+					if (recurrence.count && recurrence.endValue === 'after') {
 						template.push(', {count} ', Liferay.Language.get('times'));
 					}
-					else if (recurrence.untilDate && (recurrence.endValue === 'on')) {
+					else if (recurrence.untilDate && recurrence.endValue === 'on') {
 						var untilDate = recurrence.untilDate;
 
 						template.push(
--- portlets/opensocial-portlet/docroot/editor/js/main.js
+++ portlets/opensocial-portlet/docroot/editor/js/main.js
@@ -762,6 +762,7 @@ AUI.add(
 
 						if (tab && tab.get(IS_DIRTY) && !event.noConfirm) {
 							var tabFileName = tab.get('fileName');
+
 							var message = Liferay.Language.get('has-not-been-saved-are-you-sure-you-want-to-close-the-tab', tabFileName);
 
 							instance._showConfirmationDialog(message, instance._closeFileEntry, entryId);
