1e4022ec557c2f6d9397c8c527ac9c78a3412984
Shinn Lok <shinn.lok@liferay.com>
2015-06-10 10:19:21 -0700

    SYNC-1373 Throw exception if service is missing

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectServiceImpl.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectServiceImpl.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.jsonwebservice.NoSuchJSONWebServiceException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.plugin.PluginPackage;
@@ -1351,8 +1352,10 @@ public class SyncDLObjectServiceImpl extends SyncDLObjectServiceBaseImpl {
 
 			return updateFolder(folderId, name, description, serviceContext);
 		}
-
-		return null;
+		else {
+			throw new NoSuchJSONWebServiceException(
+				"No JSON web service action with path " + urlPath);
+		}
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(
