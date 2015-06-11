fe075cd90a8446afa4a33c674b823b4e2270489e
Brian Chan <brian.chan@liferay.com>
2015-06-11 08:45:17 -0700

    LPS-56190 SF

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/util/AdminIndexer.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/util/AdminIndexer.java
@@ -259,8 +259,9 @@ public class AdminIndexer extends BaseIndexer {
 					catch (PortalException e) {
 						if (_log.isWarnEnabled()) {
 							_log.warn(
-								"Unable to index article: " +
-									kbArticle.getKbArticleId(), e);
+								"Unable to index knowledge base article " +
+									kbArticle.getKbArticleId(),
+								e);
 						}
 					}
 				}
