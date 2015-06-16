9b1ee7daa43794b95d43011a5d7c2382c69249fb
Michael Han <michael.han@liferay.com>
2015-06-16 10:52:33 -0500

    LPS-50720 add solr configuration

---
+++ shared/solr4-shared/src/com/liferay/portal/search/solr/configuration/SolrConfiguration.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Michael C. Han
+ */
+@Meta.OCD(id = "com.liferay.portal.search.solr.configuration.SolrConfiguration")
+public interface SolrConfiguration {
+
+	@Meta.AD(
+		deflt = "BASIC", optionLabels = {"Basic", "Cert"},
+		optionValues = {"BASIC", "CERT"},
+		required = false
+	)
+	public String authenticationMode();
+
+	@Meta.AD(deflt = "solr", required = false)
+	public String basicAuthPassword();
+
+	@Meta.AD(deflt = "solr", required = false)
+	public String basicAuthUserName();
+
+	@Meta.AD(deflt = "20", required = false)
+	public int defaultMaxConnectionsPerRoute();
+
+	@Meta.AD(deflt = "secret", required = false)
+	public String keyStorePassword();
+
+	@Meta.AD(deflt = "classpath:/keystore.jks", required = false)
+	public String keyStorePath();
+
+	@Meta.AD(deflt = "JKS", required = false)
+	public String keyStoreType();
+
+	@Meta.AD(
+		deflt = "true",
+		description = "Set to true to only log exceptions from Solr and not rethrow them",
+		required = false
+	)
+	public boolean logExceptionsOnly();
+
+	@Meta.AD(deflt = "20", required = false)
+	public int maxTotalConnections();
+
+	@Meta.AD(deflt = "secret", required = false)
+	public String trustStorePassword();
+
+	@Meta.AD(deflt = "classpath:/truststore.jks", required = false)
+	public String trustStorePath();
+
+	@Meta.AD(deflt = "JKS", required = false)
+	public String trustStoreType();
+
+	@Meta.AD(deflt = "http://localhost:8080/solr", required = false)
+	public String url();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean verifyServerCertificate();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean verifyServerName();
+
+}
