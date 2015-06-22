aee1fa810f544a0d3f812cea0eeac06225c0e9c0
Shinn Lok <shinn.lok@liferay.com>
2015-06-22 12:04:32 -0700

    SYNC-1385 Reject tokens that were issued before a password change

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/hook/security/auth/verifier/SyncAuthVerifier.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/hook/security/auth/verifier/SyncAuthVerifier.java
@@ -28,12 +28,15 @@ import com.liferay.portal.kernel.util.PwdGenerator;
 import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.AccessControlContext;
 import com.liferay.portal.security.auth.AuthException;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.sync.util.PortletPropsValues;
 
 import java.lang.reflect.Method;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
@@ -78,7 +81,21 @@ public class SyncAuthVerifier extends BaseAutoLogin implements AuthVerifier {
 				return null;
 			}
 
-			return userIdJsonPrimitive.getAsString();
+			long userId = userIdJsonPrimitive.getAsLong();
+
+			User user = UserLocalServiceUtil.fetchUser(userId);
+
+			Date passwordModifiedDate = user.getPasswordModifiedDate();
+
+			if (passwordModifiedDate != null) {
+				Instant instant = jsonToken.getIssuedAt();
+
+				if (instant.isBefore(passwordModifiedDate.getTime())) {
+					return null;
+				}
+			}
+
+			return String.valueOf(userId);
 		}
 		catch (Exception e) {
 			return null;
