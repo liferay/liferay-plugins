6de16fe71ffcb9922c4564584e6533e5d00327c8
Máté Thurzó <mate.thurzo@liferay.com>
2015-08-01 18:19:09 +0200

    LPS-57585 WSRP - Upgrade process

---
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/hook/upgrade/v1_1_0/UpgradeLastPublishDate.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wsrp.hook.upgrade.v1_1_0;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradeLastPublishDate;
+
+/**
+ * @author Mate Thurzo
+ */
+public class UpgradeLastPublishDate extends BaseUpgradeLastPublishDate {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("alter table WSRPConsumerPortlet add lastPublishDate DATE null");
+
+		updateLastPublishDates("1_WAR_wsrpportlet", "WSRPConsumerPortlet");
+
+		runSQL("alter table WSRPConsumer add lastPublishDate DATE null");
+
+		updateLastPublishDates("1_WAR_wsrpportlet", "WSRPConsumer");
+
+		runSQL("alter table WSRPProducer add lastPublishDate DATE null");
+
+		updateLastPublishDates("1_WAR_wsrpportlet", "WSRPProducer");
+	}
+
+}
