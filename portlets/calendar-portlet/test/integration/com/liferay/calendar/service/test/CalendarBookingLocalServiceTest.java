be9d4432dcf4f996876f3c4ee8d34afb9633bcc9
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2015-08-06 14:24:18 -0300

    LPS-56337 Creating test (and ignoring only tests known to be unsatisfied)

---
+++ portlets/calendar-portlet/test/integration/com/liferay/calendar/service/test/CalendarBookingLocalServiceTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.service.test;
+
+import com.liferay.calendar.CalendarBookingRecurrenceException;
+import com.liferay.calendar.model.Calendar;
+import com.liferay.calendar.model.CalendarBookingConstants;
+import com.liferay.calendar.model.CalendarResource;
+import com.liferay.calendar.recurrence.Frequency;
+import com.liferay.calendar.recurrence.PositionalWeekday;
+import com.liferay.calendar.recurrence.Recurrence;
+import com.liferay.calendar.recurrence.RecurrenceSerializer;
+import com.liferay.calendar.service.CalendarBookingLocalServiceUtil;
+import com.liferay.calendar.util.CalendarResourceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+import java.util.Collections;
+
+import org.jboss.arquillian.junit.Arquillian;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Adam Brandizzi
+ */
+@RunWith(Arquillian.class)
+public class CalendarBookingLocalServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_user = UserTestUtil.addUser();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		UserLocalServiceUtil.deleteUser(_user);
+	}
+
+	@Test(expected = CalendarBookingRecurrenceException.class)
+	public void testStartDateBeforeUntilDateThrowsRecurrenceException()
+		throws PortalException {
+
+		ServiceContext serviceContext = createServiceContext();
+
+		CalendarResource calendarResource =
+			CalendarResourceUtil.getUserCalendarResource(
+				_user.getUserId(), serviceContext);
+
+		Calendar calendar = calendarResource.getDefaultCalendar();
+
+		long startTime = DateUtil.newTime();
+
+		java.util.Calendar untilJCalendar = CalendarFactoryUtil.getCalendar(
+			startTime);
+
+		untilJCalendar.add(java.util.Calendar.DAY_OF_MONTH, -2);
+
+		Recurrence recurrence = new Recurrence();
+
+		recurrence.setFrequency(Frequency.DAILY);
+		recurrence.setUntilJCalendar(untilJCalendar);
+		recurrence.setPositionalWeekdays(
+			Collections.<PositionalWeekday>emptyList());
+
+		CalendarBookingLocalServiceUtil.addCalendarBooking(
+			_user.getUserId(), calendar.getCalendarId(), new long[0],
+			CalendarBookingConstants.PARENT_CALENDAR_BOOKING_ID_DEFAULT,
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomString(), startTime,
+			startTime + (Time.HOUR * 10), false,
+			RecurrenceSerializer.serialize(recurrence), 0, null, 0, null,
+			serviceContext);
+	}
+
+	protected ServiceContext createServiceContext() {
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setCompanyId(_user.getCompanyId());
+
+		return serviceContext;
+	}
+
+	private User _user;
+
+}
