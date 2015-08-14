aeb93e7fd34fe7bcb658491b7c0657c2f8a806a2
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2015-08-14 15:37:13 -0300

    LPS-56821 Test for JCalendarUtil.mergeDateTime()

--- portlets/calendar-portlet/test/unit/com/liferay/calendar/util/JCalendarUtilTest.java
+++ portlets/calendar-portlet/test/unit/com/liferay/calendar/util/JCalendarUtilTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.calendar.util;
 
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.TimeZoneUtil;
 import com.liferay.portal.util.CalendarFactoryImpl;
@@ -129,7 +130,53 @@ public class JCalendarUtilTest {
 			Calendar.FRIDAY, jCalendar.get(Calendar.DAY_OF_WEEK));
 	}
 
+	@Test
+	public void testMergeDateTime() {
+		Calendar dateJCalendar = CalendarFactoryUtil.getCalendar(
+			RandomTestUtil.randomInt(2000, 2100),
+			RandomTestUtil.randomInt(0, 12), RandomTestUtil.randomInt(1, 29),
+			RandomTestUtil.randomInt(0, 24), RandomTestUtil.randomInt(0, 60),
+			RandomTestUtil.randomInt(0, 60), RandomTestUtil.randomInt(0, 60),
+			_losAngelesTimeZone);
+
+		Calendar timeJCalendar = CalendarFactoryUtil.getCalendar(
+			RandomTestUtil.randomInt(2000, 2100),
+			RandomTestUtil.randomInt(0, 12), RandomTestUtil.randomInt(1, 29),
+			RandomTestUtil.randomInt(0, 24), RandomTestUtil.randomInt(0, 60),
+			RandomTestUtil.randomInt(0, 60), RandomTestUtil.randomInt(0, 60),
+			_madridTimeZone);
+
+		Calendar jCalendar = JCalendarUtil.mergeDateTime(
+			dateJCalendar, timeJCalendar, _calcuttaTimeZone);
+
+		Assert.assertEquals(
+			dateJCalendar.get(Calendar.YEAR), jCalendar.get(Calendar.YEAR));
+		Assert.assertEquals(
+			dateJCalendar.get(Calendar.MONTH), jCalendar.get(Calendar.MONTH));
+		Assert.assertEquals(
+			dateJCalendar.get(Calendar.DAY_OF_MONTH),
+			jCalendar.get(Calendar.DAY_OF_MONTH));
+
+		Assert.assertEquals(
+			timeJCalendar.get(Calendar.HOUR), jCalendar.get(Calendar.HOUR));
+		Assert.assertEquals(
+			timeJCalendar.get(Calendar.MINUTE), jCalendar.get(Calendar.MINUTE));
+		Assert.assertEquals(
+			timeJCalendar.get(Calendar.SECOND), jCalendar.get(Calendar.SECOND));
+		Assert.assertEquals(
+			timeJCalendar.get(Calendar.MILLISECOND),
+			jCalendar.get(Calendar.MILLISECOND));
+		Assert.assertEquals(
+			timeJCalendar.get(Calendar.AM_PM), jCalendar.get(Calendar.AM_PM));
+
+		Assert.assertEquals(_calcuttaTimeZone, jCalendar.getTimeZone());
+	}
+
+	private static final TimeZone _calcuttaTimeZone = TimeZone.getTimeZone(
+		"Asia/Calcutta");
 	private static TimeZone _losAngelesTimeZone = TimeZone.getTimeZone(
 		"America/Los_Angeles");
+	private static final TimeZone _madridTimeZone = TimeZone.getTimeZone(
+		"Europe/Madrid");
 
 }
