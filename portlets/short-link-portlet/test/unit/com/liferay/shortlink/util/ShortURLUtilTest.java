37bfb770263fb3cf910822354490abc12cbcb374
Miroslav Ligas <miroslav.ligas@liferay.com>
2015-08-26 16:31:24 +0100

    LRIS-16694 moved to right package

--- portlets/short-link-portlet/test/unit/com/liferay/shortlink/util/ShortURLUtilTest.java
+++ portlets/short-link-portlet/test/unit/com/liferay/shortlink/util/ShortURLUtilTest.java
@@ -14,12 +14,12 @@
 
 package com.liferay.shortlink.util;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Miroslav Ligas
