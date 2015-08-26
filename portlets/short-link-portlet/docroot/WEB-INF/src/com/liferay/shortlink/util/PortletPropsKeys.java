ae5f3daffa61ed485791af0500cbfb204724a0b4
Brian Chan <brian.chan@liferay.com>
2015-08-26 10:33:44 -0700

    LRIS-16694 Not useless. Makes it known it's static and final and can be used by anyone. We do this for all constants.

--- portlets/short-link-portlet/docroot/WEB-INF/src/com/liferay/shortlink/util/PortletPropsKeys.java
+++ portlets/short-link-portlet/docroot/WEB-INF/src/com/liferay/shortlink/util/PortletPropsKeys.java
@@ -19,12 +19,14 @@ package com.liferay.shortlink.util;
  */
 public interface PortletPropsKeys {
 
-	String SHORT_URL_CLEAN_UP_THRESHOLD = "short.url.clean.up.threshold";
+	public static final String SHORT_URL_CLEAN_UP_THRESHOLD =
+		"short.url.clean.up.threshold";
 
-	String SHORT_URL_HOSTNAME = "short.url.hostname";
+	public static final String SHORT_URL_HOSTNAME = "short.url.hostname";
 
-	String SHORT_URL_LIFETIME = "short.url.lifetime";
+	public static final String SHORT_URL_LIFETIME = "short.url.lifetime";
 
-	String SHORT_URL_NOT_FOUND_REDIRECT = "short.url.not.found.redirect";
+	public static final String SHORT_URL_NOT_FOUND_REDIRECT =
+		"short.url.not.found.redirect";
 
 }
