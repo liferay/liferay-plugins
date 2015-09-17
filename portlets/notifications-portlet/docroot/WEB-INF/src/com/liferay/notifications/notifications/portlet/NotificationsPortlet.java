8463498713ba0c3952ab837cb450703f6e67b64f
Jonathan McCann <jonathan.mccann@liferay.com>
2015-09-17 12:24:30 -0700

    LPS-58384 Source Formatting

--- portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/notifications/portlet/NotificationsPortlet.java
+++ portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/notifications/portlet/NotificationsPortlet.java
@@ -728,7 +728,7 @@ public class NotificationsPortlet extends MVCPortlet {
 			userNotificationEvent);
 	}
 
-	private static final String _ACTION_DIV_DEFAULT = 
+	private static final String _ACTION_DIV_DEFAULT =
 		"<div class=\"clearfix\">";
 
 	private static final String _BODY_TEMPLATE_DEFAULT =
