1bda350e24dc8fc096bd9c87af682d696364744c
Shinn Lok <shinn.lok@liferay.com>
2015-09-18 12:01:54 -0700

    SYNC-1435 There is scope validation after LPS-47464. Since we check the SCOPE_INDIVIDUAL scope manually, replicate the logic by reversing and calling directly.

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectServiceImpl.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/service/impl/SyncDLObjectServiceImpl.java
@@ -47,13 +47,11 @@ import com.liferay.portal.kernel.zip.ZipReaderFactoryUtil;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.model.Organization;
-import com.liferay.portal.model.Resource;
 import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.service.ResourceLocalServiceUtil;
 import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.util.comparator.GroupNameComparator;
@@ -1189,33 +1187,32 @@ public class SyncDLObjectServiceImpl extends SyncDLObjectServiceBaseImpl {
 
 		PermissionChecker permissionChecker = getPermissionChecker();
 
-		List<Resource> resources = new ArrayList<>(3);
-
-		Resource groupResource = ResourceLocalServiceUtil.getResource(
-			permissionChecker.getCompanyId(), name,
-			ResourceConstants.SCOPE_GROUP, String.valueOf(groupId));
-
-		resources.add(groupResource);
-
-		Resource groupTemplateResource = ResourceLocalServiceUtil.getResource(
-			permissionChecker.getCompanyId(), name,
-			ResourceConstants.SCOPE_GROUP_TEMPLATE,
-			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID));
-
-		resources.add(groupTemplateResource);
-
-		Resource companyResource = ResourceLocalServiceUtil.getResource(
-			permissionChecker.getCompanyId(), name,
-			ResourceConstants.SCOPE_COMPANY,
-			String.valueOf(permissionChecker.getCompanyId()));
-
-		resources.add(companyResource);
-
 		long[] roleIds = permissionChecker.getRoleIds(
 			permissionChecker.getUserId(), groupId);
 
-		return ResourcePermissionLocalServiceUtil.hasResourcePermission(
-			resources, roleIds, ActionKeys.VIEW);
+		if (roleIds.length == 0) {
+			return false;
+		}
+
+		if (ResourcePermissionLocalServiceUtil.hasResourcePermission(
+				permissionChecker.getCompanyId(), name,
+				ResourceConstants.SCOPE_COMPANY,
+				String.valueOf(permissionChecker.getCompanyId()), roleIds,
+				ActionKeys.VIEW) ||
+			ResourcePermissionLocalServiceUtil.hasResourcePermission(
+				permissionChecker.getCompanyId(), name,
+				ResourceConstants.SCOPE_GROUP_TEMPLATE,
+				String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID), roleIds,
+				ActionKeys.VIEW) ||
+			ResourcePermissionLocalServiceUtil.hasResourcePermission(
+				permissionChecker.getCompanyId(), name,
+				ResourceConstants.SCOPE_GROUP, String.valueOf(groupId), roleIds,
+				ActionKeys.VIEW)) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	protected boolean hasPermission(
