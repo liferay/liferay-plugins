373e616fae8117ca986acb123720f367a6e1a1ae
Juan G <juan.gonzalez@liferay.com>
2015-10-08 14:08:50 +0200

    LPS-56769 Add test portlets

---
+++ portlets/test-runtime-action-portlet/docroot/WEB-INF/src/com/liferay/testruntimeaction/portlet/TestPortlet.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.testruntimeaction.portlet;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+/**
+ * @author Juan Gonzalez
+ */
+public class TestPortlet extends MVCPortlet {
+
+	public void customAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws IOException, PortletException {
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Action executed");
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(TestPortlet.class);
+
+}
