3e3a1f10a49ec479e6c30ff8ac3a091af263d39c
Douglas Wong <douglas.wong@liferay.com>
2015-12-21 17:36:51 -0800

    LPS-61528 unable to select supported versions in marketplace portlet

--- portlets/marketplace-portlet/docroot/WEB-INF/src/com/liferay/marketplace/store/portlet/StorePortlet.java
+++ portlets/marketplace-portlet/docroot/WEB-INF/src/com/liferay/marketplace/store/portlet/StorePortlet.java
@@ -296,9 +296,13 @@ public class StorePortlet extends RemoteMVCPortlet {
 		parameterMap.put(
 			"clientBuild",
 			new String[] {String.valueOf(MarketplaceConstants.CLIENT_BUILD)});
-		parameterMap.put(
-			"compatibility",
-			new String[] {String.valueOf(ReleaseInfo.getBuildNumber())});
+
+		if (!parameterMap.containsKey("compatibility")) {
+			parameterMap.put(
+				"compatibility",
+				new String[] {String.valueOf(ReleaseInfo.getBuildNumber())});
+		}
+
 		parameterMap.put(
 			"supportsHotDeploy",
 			new String[] {
