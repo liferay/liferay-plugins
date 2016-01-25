730f732a12752de114180eef215122a4c2d56940
Shinn Lok <shinn.lok@liferay.com>
2016-01-25 11:16:11 -0800

    LPS-62168 Update keys

--- portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/consumer/portlet/ConsumerPortlet.java
+++ portlets/wsrp-portlet/docroot/WEB-INF/src/com/liferay/wsrp/consumer/portlet/ConsumerPortlet.java
@@ -587,7 +587,7 @@ public class ConsumerPortlet extends GenericPortlet {
 
 		Online online = new Online();
 
-		String email = getOnlineEmail(user, "E-mail");
+		String email = getOnlineEmail(user, "email");
 
 		if (email != null) {
 			online.setEmail(email);
@@ -875,7 +875,7 @@ public class ConsumerPortlet extends GenericPortlet {
 		Telecom telecom = new Telecom();
 
 		TelephoneNum faxTelephoneNum = getTelephoneNum(
-			user, listTypeName + " Fax");
+			user, listTypeName + "-fax");
 
 		if (faxTelephoneNum != null) {
 			telecom.setFax(faxTelephoneNum);
@@ -923,7 +923,7 @@ public class ConsumerPortlet extends GenericPortlet {
 
 		userProfile.setBdate(bdate);
 
-		Contact businessInfoContact = getContact(user, "Business");
+		Contact businessInfoContact = getContact(user, "business");
 
 		userProfile.setBusinessInfo(businessInfoContact);
 
@@ -931,7 +931,7 @@ public class ConsumerPortlet extends GenericPortlet {
 
 		userProfile.setGender(gender);
 
-		Contact homeInfoContact = getContact(user, "Personal");
+		Contact homeInfoContact = getContact(user, "personal");
 
 		userProfile.setHomeInfo(homeInfoContact);
 
