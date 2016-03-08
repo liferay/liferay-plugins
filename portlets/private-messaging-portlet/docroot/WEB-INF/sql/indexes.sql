2dbcd63307f557cd880c2a241abd29ef1fd7c959
peterfellwock <peter.fellwock@liferay.com>
2016-03-08 11:10:31 -0800

    LPS-63528 Move Private Messaging portlet to core

--- portlets/private-messaging-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,4 +0,0 @@
-create index IX_A821854B on PM_UserThread (mbThreadId);
-create index IX_434EE852 on PM_UserThread (userId, deleted);
-create index IX_466F2985 on PM_UserThread (userId, mbThreadId);
-create index IX_A16EF3C7 on PM_UserThread (userId, read_, deleted);
