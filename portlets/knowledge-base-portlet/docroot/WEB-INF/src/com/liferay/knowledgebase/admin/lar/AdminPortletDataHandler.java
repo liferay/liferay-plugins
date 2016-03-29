03166b89201e04a8cdcbcd2446092f033e2163c3
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2016-03-29 16:10:01 +0200

    LPS-64447 ant format-source

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/lar/AdminPortletDataHandler.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/lar/AdminPortletDataHandler.java
@@ -206,6 +206,7 @@ public class AdminPortletDataHandler extends BasePortletDataHandler {
 					OrderFactoryUtil.addOrderByComparator(
 						dynamicQuery, new KBArticleVersionComparator(true));
 				}
+
 			});
 
 		return exportActionableDynamicQuery;
