458a83dafaa449b1f72af51d06f7aa5b4d7cc9de
hudakl <laszlo.hudak@liferay.com>
2016-04-06 10:53:43 +0200

    LPS-64903 Make sure the notification is sent, only after the database modifications are finished

--- portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/util/NotificationsUtil.java
+++ portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/util/NotificationsUtil.java
@@ -20,14 +20,12 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.messaging.DestinationNames;
-import com.liferay.portal.kernel.messaging.MessageBusUtil;
 import com.liferay.portal.kernel.notifications.NotificationEvent;
 import com.liferay.portal.kernel.notifications.NotificationEventFactoryUtil;
 import com.liferay.portal.kernel.notifications.UserNotificationManagerUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
-import com.liferay.portal.kernel.process.ProcessCallable;
 import com.liferay.portal.kernel.process.ProcessException;
+import com.liferay.portal.kernel.transaction.TransactionCommitCallbackRegistryUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -49,6 +47,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 /**
  * @author Lin Cui
@@ -205,15 +204,14 @@ public class NotificationsUtil {
 		notificationEventJSONObject.put("notificationType", notificationType);
 		notificationEventJSONObject.put("userId", userId);
 
-		MessageBusUtil.sendMessage(
-			DestinationNames.ASYNC_SERVICE,
+		TransactionCommitCallbackRegistryUtil.registerCallback(
 			new NotificationProcessCallable(
 				companyId, portletKey, notificationEventJSONObject,
 				subscribersOVPs));
 	}
 
 	private static class NotificationProcessCallable
-		implements ProcessCallable<Serializable> {
+		implements Callable<Serializable> {
 
 		public NotificationProcessCallable(
 			long companyId, String portletKey,
