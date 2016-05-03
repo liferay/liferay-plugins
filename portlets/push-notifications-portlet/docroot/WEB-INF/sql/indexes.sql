caf7d4d31411842276a8d0918dd0504e80382d45
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-05-03 10:13:35 -0700

    LPS-65093 push-notifications-portlet is in liferay-portal now

--- portlets/push-notifications-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,3 +0,0 @@
-create index IX_7758D8B1 on PushNotificationsDevice (platform[$COLUMN_LENGTH:75$]);
-create unique index IX_2F3EDC9F on PushNotificationsDevice (token[$COLUMN_LENGTH:4000$]);
-create index IX_2FBF066B on PushNotificationsDevice (userId, platform[$COLUMN_LENGTH:75$]);
--- portlets/push-notifications-portlet/docroot/WEB-INF/sql/tables.sql
+++
@@ -1,7 +0,0 @@
-create table PushNotificationsDevice (
-	pushNotificationsDeviceId LONG not null primary key,
-	userId LONG,
-	createDate DATE null,
-	platform VARCHAR(75) null,
-	token STRING null
-);
