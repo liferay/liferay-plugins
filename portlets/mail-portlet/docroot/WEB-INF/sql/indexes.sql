2a4bca6f3eb0e61b57dd5a68187475e43439dd84
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-05-04 14:48:01 -0700

    LPS-65517 mail-portlet is in liferay-portal now

--- portlets/mail-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,8 +0,0 @@
-create index IX_6B92F85F on Mail_Account (userId, address[$COLUMN_LENGTH:75$]);
-
-create index IX_F661D061 on Mail_Attachment (messageId);
-
-create index IX_310E554A on Mail_Folder (accountId, fullName[$COLUMN_LENGTH:75$]);
-
-create index IX_163EBD83 on Mail_Message (companyId);
-create index IX_200D262A on Mail_Message (folderId, remoteMessageId);
