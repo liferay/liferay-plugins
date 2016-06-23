5c768d9f645ba24fbfca18729d5fec140fc6142b
Inacio Nery <inacio.nery@liferay.com>
2016-06-23 15:05:35 -0300

    LPS-55278 Add UpgradePortletPreferences version 1_0_3 to changing the portlet preferences variable for timeFormat

---
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/hook/upgrade/v1_0_3/UpgradePortletPreferences.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.hook.upgrade.v1_0_3;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradePortletPreferences;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+
+/**
+ * @author Inácio Nery
+ */
+public class UpgradePortletPreferences extends BaseUpgradePortletPreferences {
+
+	@Override
+	protected String getUpdatePortletPreferencesWhereClause() {
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("(preferences like '%isoTimeFormat%");
+		sb.append(Boolean.TRUE.toString());
+		sb.append("%') or (preferences like '%isoTimeFormat%");
+		sb.append(Boolean.FALSE.toString());
+		sb.append("%')");
+
+		return sb.toString();
+	}
+
+	@Override
+	protected String upgradePreferences(
+			long companyId, long ownerId, int ownerType, long plid,
+			String portletId, String xml)
+		throws Exception {
+
+		PortletPreferences portletPreferences =
+			PortletPreferencesFactoryUtil.fromXML(
+				companyId, ownerId, ownerType, plid, portletId, xml);
+
+		String value = portletPreferences.getValue(
+			"isoTimeFormat", Boolean.FALSE.toString());
+
+		if (value.equals(Boolean.TRUE.toString())) {
+			portletPreferences.setValue("timeFormat", "24-hour");
+		}
+		else {
+			portletPreferences.setValue("timeFormat", "am-pm");
+		}
+
+		portletPreferences.reset("isoTimeFormat");
+
+		return PortletPreferencesFactoryUtil.toXML(portletPreferences);
+	}
+
+}
