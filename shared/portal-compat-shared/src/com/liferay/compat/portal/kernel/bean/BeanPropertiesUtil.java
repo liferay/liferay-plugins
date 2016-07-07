a2601647666f9bd5833a186fc79490fd1c464f8e
Wesley Gong <wesley.gong@liferay.com>
2016-07-07 15:25:21 -0700

    LPS-67028 Blind Copy

--- shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/bean/BeanPropertiesUtil.java
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/bean/BeanPropertiesUtil.java
@@ -14,15 +14,65 @@
 
 package com.liferay.compat.portal.kernel.bean;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.User;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 
 import java.lang.reflect.Method;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import jodd.bean.BeanUtil;
+
 /**
  * @author Danny Situ
+ * @author Wesley Gong
  */
 public class BeanPropertiesUtil
 	extends com.liferay.portal.kernel.bean.BeanPropertiesUtil {
+
+	public static void setProperties(Object bean, HttpServletRequest request) {
+		Enumeration<String> enu = request.getParameterNames();
+
+		while (enu.hasMoreElements()) {
+			String name = enu.nextElement();
+
+			String value = request.getParameter(name);
+
+			BeanUtil.setPropertyForcedSilent(bean, name, value);
+
+			if (name.endsWith("Month")) {
+				String dateParam = name.substring(0, name.lastIndexOf("Month"));
+
+				if (request.getParameter(dateParam) != null) {
+					continue;
+				}
+
+				Class<?> propertyTypeClass = BeanUtil.getPropertyType(
+					bean, dateParam);
+
+				if ((propertyTypeClass == null) ||
+					!propertyTypeClass.equals(Date.class)) {
+
+					continue;
+				}
+
+				Date date = getDate(dateParam, request);
+
+				if (date != null) {
+					BeanUtil.setPropertyForcedSilent(bean, dateParam, date);
+				}
+			}
+		}
+	}
 
 	public static void setPropertySilent(
 		Object bean, String param, Object value) {
@@ -40,6 +90,37 @@ public class BeanPropertiesUtil
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	protected static Date getDate(String param, HttpServletRequest request) {
+		int month = ParamUtil.getInteger(request, param + "Month");
+		int day = ParamUtil.getInteger(request, param + "Day");
+		int year = ParamUtil.getInteger(request, param + "Year");
+		int hour = ParamUtil.getInteger(request, param + "Hour", -1);
+		int minute = ParamUtil.getInteger(request, param + "Minute");
+
+		int amPm = ParamUtil.getInteger(request, param + "AmPm");
+
+		if (amPm == Calendar.PM) {
+			hour += 12;
+		}
+
+		if (hour == -1) {
+			return PortalUtil.getDate(month, day, year);
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		User user = themeDisplay.getUser();
+
+		try {
+			return PortalUtil.getDate(
+				month, day, year, hour, minute, user.getTimeZone(), null);
+		}
+		catch (PortalException pe) {
+			return null;
 		}
 	}
 
