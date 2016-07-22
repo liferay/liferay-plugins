e3c70ce1761f5c04b4ba64e88d41266990edc164
Wesley Gong <wesley.gong@liferay.com>
2016-07-22 14:51:07 -0700

    LRAMF-345 Ran ant format-source

--- shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/BaseAlloyControllerImpl.java
+++ shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/BaseAlloyControllerImpl.java
@@ -15,8 +15,8 @@
 package com.liferay.alloy.mvc;
 
 import com.liferay.alloy.mvc.jsonwebservice.AlloyMockUtil;
-import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.compat.portal.kernel.bean.BeanPropertiesUtil;
+import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.bean.ConstantsBeanFactoryUtil;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.json.JSONArray;
