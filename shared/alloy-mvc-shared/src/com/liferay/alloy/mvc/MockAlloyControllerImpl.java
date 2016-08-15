95a1f02c9002b6a092eb55924ea53e76dc09788e
William Newbury <william.newbury@liferay.com>
2016-08-15 13:34:57 -0700

    LRAMF-351 Make a new mockAlloyControllerImpl for message listeners who only need updateModel

---
+++ shared/alloy-mvc-shared/src/com/liferay/alloy/mvc/MockAlloyControllerImpl.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.alloy.mvc;
+
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author William Newbury
+ */
+public class MockAlloyControllerImpl
+	extends BaseAlloyControllerImpl implements AlloyController {
+
+	public MockAlloyControllerImpl(
+		BaseAlloyControllerImpl baseAlloyControllerImpl) {
+
+		company = baseAlloyControllerImpl.company;
+		themeDisplay = baseAlloyControllerImpl.themeDisplay;
+
+		try {
+			user = UserLocalServiceUtil.getDefaultUser(company.getCompanyId());
+		}
+		catch (Exception e) {
+			user = baseAlloyControllerImpl.user;
+		}
+	}
+
+	@Override
+	public void afterPropertiesSet() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void execute() throws Exception {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Portlet getPortlet() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public HttpServletRequest getRequest() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getResponseContent() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void indexModel(BaseModel<?> baseModel) throws Exception {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String translate(String pattern, Object... arguments) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void updateModel(BaseModel<?> baseModel, Object... properties)
+		throws Exception {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	protected void setAttachedModel(BaseModel<?> baseModel) throws Exception {
+	}
+
+}
