8f6430aa83bad0930162707f342a1a406bc8832f
Beck Liu <beck.liu@liferay.com>
2016-09-08 12:57:39 +0800

    Revert "LPS-55336 Blind copy ConstantsBeanFactoryImpl imports and methods"
    This reverts commit e3df18564a5741265d9567eb30c5a326f1eb6b5a.

--- shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/bean/ConstantsBeanFactoryUtil.java
+++ shared/portal-compat-shared/src/com/liferay/compat/portal/kernel/bean/ConstantsBeanFactoryUtil.java
@@ -14,186 +14,25 @@
 
 package com.liferay.portal.kernel.bean;
 
-import com.liferay.portal.kernel.bean.ConstantsBeanFactory;
-import com.liferay.portal.kernel.memory.EqualityWeakReference;
-import com.liferay.portal.kernel.util.ReflectionUtil;
-
-import java.lang.ref.Reference;
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.WeakReference;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
+import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
 
 /**
  * @author Shuyang Zhou
  */
 public class ConstantsBeanFactoryUtil {
 
-	@Override
-	public Object getConstantsBean(Class<?> constantsClass) {
-		Reference<?> constantsBeanReference = constantsBeans.get(
-			new EqualityWeakReference<Class<?>>(constantsClass));
-
-		Object constantsBean = null;
-
-		if (constantsBeanReference != null) {
-			constantsBean = constantsBeanReference.get();
-		}
-
-		if (constantsBean == null) {
-			constantsBean = createConstantsBean(constantsClass);
-
-			constantsBeans.put(
-				new EqualityWeakReference<Class<?>>(
-					constantsClass, constantsClassReferenceQueue),
-				new WeakReference<Object>(constantsBean));
-		}
-
-		while (true) {
-			EqualityWeakReference<Class<?>> staleConstantsClassReference =
-				(EqualityWeakReference<Class<?>>)
-					constantsClassReferenceQueue.poll();
-
-			if (staleConstantsClassReference == null) {
-				break;
-			}
-
-			constantsBeans.remove(staleConstantsClassReference);
-		}
-
-		return constantsBean;
+	public static Object getConstantsBean(Class<?> constantsClass) {
+		return _constantsBeanFactory.getConstantsBean(constantsClass);
 	}
 
-	protected static Object createConstantsBean(Class<?> constantsClass) {
-		ClassLoader classLoader = constantsClass.getClassLoader();
+	public void setConstantsBeanFactory(
+		ConstantsBeanFactory constantsBeanFactory) {
 
-		String constantsBeanClassName = constantsClass.getName() + "Bean";
+		PortalRuntimePermission.checkSetBeanProperty(getClass());
 
-		Class<?> constantsBeanClass = null;
-
-		synchronized (classLoader) {
-			try {
-				constantsBeanClass = classLoader.loadClass(
-					constantsBeanClassName);
-			}
-			catch (ClassNotFoundException cnfe) {
-			}
-
-			try {
-				if (constantsBeanClass == null) {
-					Method defineClassMethod = ReflectionUtil.getDeclaredMethod(
-						ClassLoader.class, "defineClass", String.class,
-						byte[].class, int.class, int.class);
-
-					byte[] classData = generateConstantsBeanClassData(
-						constantsClass);
-
-					constantsBeanClass = (Class<?>)defineClassMethod.invoke(
-						classLoader, constantsBeanClassName, classData, 0,
-						classData.length);
-				}
-
-				return constantsBeanClass.newInstance();
-			}
-			catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
+		_constantsBeanFactory = constantsBeanFactory;
 	}
 
-	protected static byte[] generateConstantsBeanClassData(
-		Class<?> constantsClass) {
-
-		String constantsClassBinaryName = getClassBinaryName(constantsClass);
-
-		String constantsBeanClassBinaryName = constantsClassBinaryName + "Bean";
-
-		String objectClassBinaryName = getClassBinaryName(Object.class);
-
-		ClassWriter classWriter = new ClassWriter(0);
-
-		classWriter.visit(
-			Opcodes.V1_5, Opcodes.ACC_PUBLIC | Opcodes.ACC_SUPER,
-			constantsBeanClassBinaryName, null, objectClassBinaryName, null);
-
-		MethodVisitor methodVisitor = classWriter.visitMethod(
-			Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
-
-		methodVisitor.visitCode();
-		methodVisitor.visitVarInsn(Opcodes.ALOAD, 0);
-		methodVisitor.visitMethodInsn(
-			Opcodes.INVOKESPECIAL, objectClassBinaryName, "<init>", "()V");
-		methodVisitor.visitInsn(Opcodes.RETURN);
-		methodVisitor.visitMaxs(1, 1);
-		methodVisitor.visitEnd();
-
-		Field[] fields = constantsClass.getFields();
-
-		for (Field field :fields) {
-			if (Modifier.isStatic(field.getModifiers())) {
-				Class<?> fieldClass = field.getType();
-
-				Type fieldType = Type.getType(fieldClass);
-
-				methodVisitor = classWriter.visitMethod(
-					Opcodes.ACC_PUBLIC, "get" + field.getName(),
-					"()" + fieldType.getDescriptor(), null, null);
-
-				methodVisitor.visitCode();
-				methodVisitor.visitFieldInsn(
-					Opcodes.GETSTATIC, constantsClassBinaryName,
-					field.getName(), fieldType.getDescriptor());
-
-				int returnOpcode = Opcodes.ARETURN;
-
-				if (fieldClass.isPrimitive()) {
-					if (fieldClass == Float.TYPE) {
-						returnOpcode = Opcodes.FRETURN;
-					}
-					else if (fieldClass == Double.TYPE) {
-						returnOpcode = Opcodes.DRETURN;
-					}
-					else if (fieldClass == Long.TYPE) {
-						returnOpcode = Opcodes.LRETURN;
-					}
-					else {
-						returnOpcode = Opcodes.IRETURN;
-					}
-				}
-
-				methodVisitor.visitInsn(returnOpcode);
-
-				methodVisitor.visitMaxs(fieldType.getSize(), 1);
-				methodVisitor.visitEnd();
-			}
-		}
-
-		classWriter.visitEnd();
-
-		return classWriter.toByteArray();
-	}
-
-	protected static String getClassBinaryName(Class<?> clazz) {
-		String className = clazz.getName();
-
-		return className.replace('.', '/');
-	}
-
-	protected static ConcurrentMap
-		<EqualityWeakReference<Class<?>>, Reference<?>>
-			constantsBeans =
-				new ConcurrentHashMap
-					<EqualityWeakReference<Class<?>>, Reference<?>>();
-	protected static ReferenceQueue<Class<?>> constantsClassReferenceQueue =
-		new ReferenceQueue<Class<?>>();
+	private static ConstantsBeanFactory _constantsBeanFactory;
 
 }
