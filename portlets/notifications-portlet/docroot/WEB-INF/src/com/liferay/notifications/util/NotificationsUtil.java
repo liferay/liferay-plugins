a16df8b05e851aaeb1c1bb1850598343d3c00546
Roberto Díaz <roberto.diaz@liferay.com>
2016-12-21 15:05:35 +0100

    LPS-64903 Log the error

--- portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/util/NotificationsUtil.java
+++ portlets/notifications-portlet/docroot/WEB-INF/src/com/liferay/notifications/util/NotificationsUtil.java
@@ -20,6 +20,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.notifications.NotificationEvent;
 import com.liferay.portal.kernel.notifications.NotificationEventFactoryUtil;
 import com.liferay.portal.kernel.notifications.UserNotificationManagerUtil;
@@ -306,6 +308,8 @@ public class NotificationsUtil {
 						}
 					}
 					catch (Exception e) {
+						_log.error(e, e);
+
 						continue;
 					}
 
@@ -331,6 +335,9 @@ public class NotificationsUtil {
 		}
 
 		private static final long serialVersionUID = 1L;
+
+		private static Log _log = LogFactoryUtil.getLog(
+			NotificationsUtil.class);
 
 		private long _companyId;
 		private JSONObject _notificationEventJSONObject;
