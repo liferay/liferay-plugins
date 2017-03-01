619c8704662559c4541a4d3686f4c237066cb96b
Brian Lee <brian.lee@liferay.com>
2017-02-28 16:10:40 -0800

    LRQA-30745 Print string to Liferay log manually

--- ext/sample-ext/docroot/WEB-INF/ext-impl/src/com/liferay/portal/servlet/context/ExtAddEntryWebXmlPortalContextLoaderListener.java
+++ ext/sample-ext/docroot/WEB-INF/ext-impl/src/com/liferay/portal/servlet/context/ExtAddEntryWebXmlPortalContextLoaderListener.java
@@ -14,6 +14,21 @@
 
 package com.liferay.portal.servlet.context;
 
+import com.liferay.portal.kernel.util.ServerDetector;
+import com.liferay.portal.kernel.util.SystemProperties;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -28,6 +43,41 @@ public class ExtAddEntryWebXmlPortalContextLoaderListener
 
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		System.out.println("EXT_ADD_ENTRY_WEB_XML_INSTALLED");
+
+		Date date = new Date();
+
+		String formattedDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
+
+		String logPath = null;
+
+		if (ServerDetector.isJBoss()) {
+			logPath = SystemProperties.get("jboss.home.dir") +
+				"/../logs/liferay." + formattedDate + ".log";
+		}
+		else if (ServerDetector.isTomcat()) {
+			logPath = SystemProperties.get("catalina.base") +
+				"/../logs/liferay." + formattedDate + ".log";
+		}
+		else if (ServerDetector.isWebLogic()) {
+			logPath = SystemProperties.get("env.DOMAIN_HOME") +
+				"/../logs/liferay." + formattedDate + ".log";
+		}
+		else {
+			logPath = SystemProperties.get("user.dir") +
+				"/liferay/logs/liferay." + formattedDate + ".log";
+		}
+
+		try (BufferedWriter bufferedWriter = Files.newBufferedWriter(
+				Paths.get(logPath), Charset.defaultCharset(),
+				StandardOpenOption.APPEND, StandardOpenOption.CREATE,
+				StandardOpenOption.WRITE)) {
+
+			bufferedWriter.write("EXT_ADD_ENTRY_WEB_XML_INSTALLED");
+
+			bufferedWriter.newLine();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 	}
 
 }
