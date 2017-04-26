brianchandotcom/liferay-portal

LPS-71504 Fix NPE

modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/selector/KBFolderKBArticleSelector.java
@@ -110,8 +110,12 @@ public class KBFolderKBArticleSelector implements KBArticleSelector {
 			}
 		}
 
-		KBArticle kbArticle = _kbArticleService.fetchKBArticleByUrlTitle(
-			groupId, kbFolder.getKbFolderId(), urlTitle);
+		KBArticle kbArticle = null;
+
+		if (kbFolder != null) {
+			kbArticle = _kbArticleService.fetchKBArticleByUrlTitle(
+				groupId, kbFolder.getKbFolderId(), urlTitle);
+		}
 
 		if ((kbArticle == null) || !isDescendant(kbArticle, ancestorKBFolder)) {
 			return findClosestMatchingKBArticle(

liferay/liferay-plugins

portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/display/selector/KBFolderKBArticleSelector.java
@@ -101,9 +101,12 @@ public class KBFolderKBArticleSelector implements KBArticleSelector {
 			}
 		}
 
-		KBArticle kbArticle =
-			KBArticleLocalServiceUtil.fetchKBArticleByUrlTitle(
+		KBArticle kbArticle = null;
+
+		if (kbFolder != null) {
+			kbArticle = KBArticleLocalServiceUtil.fetchKBArticleByUrlTitle(
 				groupId, kbFolder.getKbFolderId(), urlTitle);
+		}
 
 		if ((kbArticle == null) || !isDescendant(kbArticle, ancestorKBFolder)) {
 			return findClosestMatchingKBArticle(