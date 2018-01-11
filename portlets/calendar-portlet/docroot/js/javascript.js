f577e1a9db45e207296e608e74ef0003a6f64993
mbowerman <michael.bowerman@liferay.com>
2018-01-11 10:55:24 -0800

    LPS-76740 doAsUserId needs to be taken into account before firing off the resourceRequest

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -449,6 +449,7 @@ AUI.add(
 
 				var url = Liferay.PortletURL.createResourceURL();
 
+				url.setDoAsUserId(Liferay.ThemeDisplay.getDoAsUserIdEncoded());
 				url.setPortletId('1_WAR_calendarportlet');
 				url.setResourceId(params.resourceId);
 
